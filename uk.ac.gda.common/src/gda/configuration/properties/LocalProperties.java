5744e43009008e4642ef940b93fc6b56dd1065ea
Oliver Buckley <oliver.buckley@diamond.ac.uk>
2012-04-19 15:11:47 +0100

    GDA COMMON: Added constant for GUI titlebar prefix

--- uk.ac.gda.common/src/gda/configuration/properties/LocalProperties.java
+++ uk.ac.gda.common/src/gda/configuration/properties/LocalProperties.java
@@ -230,11 +230,16 @@ public class LocalProperties {
 	 */
 	public static final String GDA_SCAN_MULTITHREADED_SCANDATA_POINT_PIPElINE_POINTS_TO_COMPUTE_SIMULTANEOUSELY = "gda.scan.multithreadedScanDataPointPipeline.pointsToComputeSimultaneousely";
 
+	/**
+	 * Prefix for the title of the GDA window
+	 */
+	public static final String GDA_GUI_TITLEBAR_PREFIX = "gda.gui.titlebar.prefix";
 	
 	/**
 	 * Name of the numtracker used to derive the scan id. If not set then the scan id is not set and relies on filewriters to set it.
 	 */
 	private static final String GDA_SCAN_SETS_SCANNUMBER = "gda.scan.sets.scannumber";
+	
 	
 	public static boolean isScanSetsScanNumber(){
 		return LocalProperties.check(LocalProperties.GDA_SCAN_SETS_SCANNUMBER);
