34f21643e7a307a4278eba632b7585fb2e270c1e
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-04-25 17:01:22 +0100

    uk.ac.gda.server.exafs.epics. XMapDetectorFromMCA.
    Allow the ROI settings to be read from the device at atScanStart rather 
    than forcing the use of the vortex_parameter config file.
    
    Make use of analysers support of the EPICS mca record to speed up
    reading data.
    
    Report roi window as attribute to the data
    
    Allow use of roi names as extra names

--- uk.ac.gda.server.exafs.epics/src/gda/device/detector/xmap/XmapDetectorFromEpicsMca.java
+++ uk.ac.gda.server.exafs.epics/src/gda/device/detector/xmap/XmapDetectorFromEpicsMca.java
@@ -18,7 +18,10 @@
 
 package gda.device.detector.xmap;
 
+import gda.data.nexus.extractor.NexusExtractor;
+import gda.data.nexus.extractor.NexusGroupData;
 import gda.data.nexus.tree.INexusTree;
+import gda.data.nexus.tree.NexusTreeNode;
 import gda.data.nexus.tree.NexusTreeProvider;
 import gda.device.Analyser;
 import gda.device.Detector;
@@ -29,13 +32,18 @@ import gda.device.detector.NXDetectorData;
 import gda.device.detector.NexusDetector;
 import gda.device.detector.analyser.EpicsMCA;
 import gda.device.detector.analyser.EpicsMCAPresets;
+import gda.device.detector.analyser.EpicsMCARegionOfInterest;
+import gda.device.detector.analyser.IEpicsMCA;
 import gda.device.scannable.PositionConvertorFunctions;
 import gda.factory.FactoryException;
+import gda.scan.ScanBase;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.nexusformat.NexusFile;
@@ -70,6 +78,53 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 	private VortexParameters vortexParameters;
 
 	private int actualNumberOfconfigredRois = 0;
+
+	private boolean readConfigFromDeviceAtScanStart=false;
+
+	private boolean prefixExtraNameWithDetElement=true;
+
+	private boolean useConfigFileAtConfigure=true;
+
+	
+	
+	public boolean isUseConfigFileAtConfigure() {
+		return useConfigFileAtConfigure;
+	}
+
+	/*
+	 * useConfigFileAtConfigure - if True the config file is used to setup the device in the configure method
+	 */
+	public void setUseConfigFileAtConfigure(boolean useConfigFileAtConfigure) {
+		this.useConfigFileAtConfigure = useConfigFileAtConfigure;
+	}
+
+	public boolean isPrefixExtraNameWithDetElement() {
+		return prefixExtraNameWithDetElement;
+	}
+
+	/**
+	 * @param prefixExtraNameWithDetElement If true the extraName for each roi is set to detector name + "_" + roiName.
+	 * If false the extraName is simply the name of the roi. Default is true
+	 * 
+	 * if true
+	 */
+	public void setPrefixExtraNameWithDetElement(boolean prefixExtraNameWithDetElement) {
+		this.prefixExtraNameWithDetElement = prefixExtraNameWithDetElement;
+	}
+
+	public boolean isReadConfigFromDeviceAtScanStart() {
+		return readConfigFromDeviceAtScanStart;
+	}
+
+	/**
+	 * Readout requires vortexParameters to be defined. If readConfigFromDeviceAtScanStart is true then
+	 * this parameter is set aat atScanStart by reading from the device. This allows roi's to be defined ont he
+	 * device itself rather than having to edit the configFile. Default value is false;
+	 * @param readConfigFromDeviceAtScanStart
+	 */
+	public void setReadConfigFromDeviceAtScanStart(boolean readConfigFromDeviceAtScanStart) {
+		this.readConfigFromDeviceAtScanStart = readConfigFromDeviceAtScanStart;
+	}
 
 	/**
 	 * Returns common value if all elements in the given list are equal, otherwise throws an IllegalStateException
@@ -169,6 +224,21 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 		return useLiveTime;
 	}
 
+	private void checkConfigured() throws DeviceException{
+		if( !configured || ! super.isConfigured())
+			throw new DeviceException(getName() + " is not yet configured or configuration failed");
+	}
+	@Override
+	public boolean isConfigured() {
+		try {
+			checkConfigured();
+			return true;
+		} catch (DeviceException e) {
+			logger.error(e.getMessage(), e);
+		}
+		return false;
+	}
+
 	@Override
 	public void configure() throws FactoryException {
 		if (!configured) {			
@@ -176,11 +246,15 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 				throw new FactoryException("No anyalsers have been configured");
 			}
 			try {
-				loadConfigurationFromFile();
+				if( useConfigFileAtConfigure){
+					loadConfigurationFromFile();
+				} else {
+					vortexParameters = createVortexParameters();
+				}
+				configured = true;
 			} catch (Exception e) {
 				throw new FactoryException("Cannot load xml file "+ getConfigFileName(), e);
 			}
-			configured = true;
 		}
 	}
 	
@@ -188,33 +262,49 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 	public int getNumberOfMca() throws DeviceException {
 		return analysers.size();
 	}
-
+	
 	public void loadConfigurationFromFile() throws Exception {
 		if (getConfigFileName() == null) {
 			logger.warn(getName()  + " has no configFileName configured so the configuration cannot be loaded.");
 			return;
 		}
-		vortexParameters = (VortexParameters) XMLHelpers.createFromXML(VortexParameters.mappingURL,
-				VortexParameters.class, VortexParameters.schemaURL, getConfigFileName());
-		// Number of ROIs defined in XML file.
-		configureRegionsOfInterest(vortexParameters);
-		configureChannelLabels(vortexParameters);
+		vortexParameters = readConfigFile(getConfigFileName());
+		configureHardware(vortexParameters);
 	
 	}
 
+	public void configureHardware(VortexParameters vortexParameters) throws Exception {
+		// Number of ROIs defined in XML file.
+		configureRegionsOfInterest(vortexParameters);
+		configureChannelLabels(vortexParameters);
+		this.vortexParameters=vortexParameters;
+	
+	}
+
+	public VortexParameters readConfigFile(String fileName) throws Exception {
+		return (VortexParameters) XMLHelpers.createFromXML(VortexParameters.mappingURL,
+				VortexParameters.class, VortexParameters.schemaURL, fileName);
+	}
+	
+	
 	private void configureRegionsOfInterest(final VortexParameters vp) throws Exception {
 		try {			
-			int mcaIndex = 0;
-			for (DetectorElement e : vp.getDetectorList()) {
-				final double[][] rois = new double[e.getRegionList().size()][2];
-				int iregion = 0;
-				for (RegionOfInterest roi : e.getRegionList()) {
-					rois[iregion][0] = roi.getWindowStart();
-					rois[iregion][1] = roi.getWindowEnd();
-					++iregion;
+			List<DetectorElement> detectorList = vp.getDetectorList();
+			if( detectorList != null ){
+				if( detectorList.size() > analysers.size())
+					throw new Exception("Number of detectors in vortext parameters > number of analysers");
+				int mcaIndex = 0;
+				for (DetectorElement e : detectorList) {
+					final double[][] rois = new double[e.getRegionList().size()][2];
+					int iregion = 0;
+					for (RegionOfInterest roi : e.getRegionList()) {
+						rois[iregion][0] = roi.getWindowStart();
+						rois[iregion][1] = roi.getWindowEnd();
+						++iregion;
+					}
+					setROI(rois, mcaIndex);
+					++mcaIndex;
 				}
-				setROI(rois, mcaIndex);
-				++mcaIndex;
 			}
 		} catch (DeviceException e) {
 			logger.error(e.getMessage(), e);
@@ -262,6 +352,8 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 		for (Analyser analyser : analysers) {
 			if (analyser instanceof EpicsMCA) {
 				((EpicsMCA) analyser).eraseStartAcquisition();
+			} else if (analyser instanceof IEpicsMCA) {
+				((IEpicsMCA) analyser).eraseStartAcquisition();
 			}  else {
 				clear();
 				start();
@@ -372,8 +464,11 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 
 	@Override
 	public int[] getData(int mcaNumber) throws DeviceException {
+		Object data = analysers.get(mcaNumber).getData();
+		if( data instanceof int[])
+			return (int[])data;
 		int[] integerArray = ArrayUtils.toPrimitive(
-				PositionConvertorFunctions.toIntegerArray(analysers.get(mcaNumber).getData()));
+				PositionConvertorFunctions.toIntegerArray(data));
 		return ArrayUtils.subarray(integerArray, 0, (int) analysers.get(mcaNumber).getNumberOfChannels());
 	}
 
@@ -384,10 +479,23 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 
 	@Override
 	public void atScanStart() throws DeviceException {
-		for (Analyser analyser : analysers) {
-			analyser.atScanStart();
+		try {
+			if( readConfigFromDeviceAtScanStart){
+				vortexParameters = createVortexParameters();
+			}
+			checkConfigured();
+			for (Analyser analyser : analysers) {
+				analyser.atScanStart();
+			}
+			firstScanPointDone = false;
+		} catch (Exception e) {
+			throw new DeviceException("Error in atScanStart",e);
 		}
 	}
+	/**
+	 * Flag used to added metadata to nexus data on first point in scan only
+	*/
+	boolean firstScanPointDone=false;
 	@Override
 	public NexusTreeProvider readout() throws DeviceException {
 	// 		"Element0_realtime", "Element0_livetime", "Element0_Pb_K", "Element0_Fe_K", "Element1_realtime", "Element1_livetime", "Element1_Pb_K", "Element1_Fe_K"}, xmap.getExtraNames());
@@ -401,8 +509,7 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 		double[] summation =null;
 		for (int element = 0; element < vortexParameters.getDetectorList().size(); element++) {
 			DetectorElement thisElement = vortexParameters.getDetectorList().get(element);
-	
-			
+
 			// Real and live time
 			float[] realAndLiveTime = getRealAndLiveTime(element);
 			output.addData(detTree, thisElement.getName()+"_realtime", null, NexusFile.NX_FLOAT64, new double[]{realAndLiveTime[0]}, "s", 1);
@@ -410,17 +517,36 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 			output.addData(detTree, thisElement.getName()+"_livetime", null, NexusFile.NX_FLOAT64, new double[]{realAndLiveTime[1]}, "s", 1);
 			output.setPlottableValue(thisElement.getName()+"_livetime", (double) realAndLiveTime[1]);
 			
+			Analyser analyser = analysers.get(element);
+			IEpicsMCA epicsMCA = (analyser instanceof IEpicsMCA) ? (IEpicsMCA) analyser : null;
+			HashMap<Integer, double[][]> regionsOfInterestCountCache = null;
+
 			// REGIONS
 			for (int roiIndex = 0; roiIndex < thisElement.getRegionList().size(); roiIndex++) {
 	
 				final RegionOfInterest roi = thisElement.getRegionList().get(roiIndex);
-				double count = getROICounts(roiIndex)[element];
-				output.addData(detTree, thisElement.getName()+"_"+roi.getRoiName(), null, NexusFile.NX_FLOAT64, new double[]{count}, "counts", 1);
-				output.setPlottableValue(thisElement.getName()+"_"+roi.getRoiName(), count);
+				if( roi.getRoiStart() != -1){
+					double count;
+					if( epicsMCA != null){
+						/* This causes far less channel access calls than getROICountsUsingCache
+						*/
+						count = readNetCounts ? epicsMCA.getRoiNetCount(roiIndex):epicsMCA.getRoiCount(roiIndex);
+					} else {
+						if( regionsOfInterestCountCache == null)
+							regionsOfInterestCountCache = new HashMap<Integer, double[][]>();
+						count = getROICountsUsingCache(roiIndex, regionsOfInterestCountCache)[element];
+					}
+					INexusTree roidata = output.addData(detTree, getExtraName(thisElement, roi), null, NexusFile.NX_FLOAT64, new double[]{count}, "counts", 1);
+					if(!firstScanPointDone){
+						roidata.addChildNode(new NexusTreeNode("start",NexusExtractor.AttrClassName, roidata, new NexusGroupData(roi.getRoiStart())));
+						roidata.addChildNode(new NexusTreeNode("end",NexusExtractor.AttrClassName, roidata, new NexusGroupData(roi.getRoiEnd())));
+					}
+					output.setPlottableValue(getExtraName(thisElement, roi), count);
+				}
 	
 			}
 			//add the full spectrum
-			//output.addData(detTree, thisElement.getName()+"_fullSpectrum", new int[]{detectorData[element].length}, NexusFile.NX_INT32, detectorData[element], "counts", 1);
+			output.addData(detTree, thisElement.getName()+"_fullSpectrum", new int[]{detectorData[element].length}, NexusFile.NX_INT32, detectorData[element], "counts", 1);
 			if(sumAllElementData)
 			{
 				if(summation == null)
@@ -431,11 +557,11 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 				}
 			}
 		}
-		output.addData(detTree, "fullSpectrum", new int[]{detectorData.length, detectorData[0].length}, NexusFile.NX_INT32, detectorData, "counts", 1);
 		logger.info("xmap - total readout time: " + (System.nanoTime() - startTime)/1000000000.);
 		
 		if(summation != null)
 			output.addData(detTree, "allElementSum", new int[]{summation.length}, NexusFile.NX_INT32, summation, "counts",1);
+		firstScanPointDone=true;
 		return output;
 	}
 
@@ -495,25 +621,48 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 		return actualNumberOfconfigredRois;
 	}
 
-	@Override
-	public double[] getROICounts(int iRoi) throws DeviceException {
+	/* 
+	 * performance optimized version 
+	 */
+	double[] getROICountsUsingCache(int iRoi, Map<Integer,double[][]> regionsOfInterestCountCache) throws DeviceException {
 		double[] countsFromEachMca = new double[getNumberOfMca()];
 		for (int mcaIndex = 0; mcaIndex < countsFromEachMca.length; mcaIndex++) {
-			double[][] regionsOfInterestCount = analysers.get(mcaIndex).getRegionsOfInterestCount();
-			countsFromEachMca[mcaIndex] = regionsOfInterestCount[iRoi][(readNetCounts) ? 1 : 0];
+			double[][] ds=null;
+			if( regionsOfInterestCountCache != null && regionsOfInterestCountCache.containsKey(mcaIndex)){
+				ds = regionsOfInterestCountCache.get(mcaIndex);
+			} else {
+				ds = analysers.get(mcaIndex).getRegionsOfInterestCount();
+				if( regionsOfInterestCountCache != null){
+					regionsOfInterestCountCache.put(mcaIndex, ds);
+				}
+			}
+			if( ds != null)
+				countsFromEachMca[mcaIndex] = ds[iRoi][(readNetCounts) ? 1 : 0];
 		}
 		return countsFromEachMca;
 	}
 
+	
+	@Override
+	public double[] getROICounts(int iRoi) throws DeviceException {
+		return getROICountsUsingCache(iRoi, null);
+	}
+
+	String getExtraName(DetectorElement detEl, RegionOfInterest roi){
+		return prefixExtraNameWithDetElement ? detEl.getName()+"_"+roi.getRoiName() : roi.getRoiName();
+	}
 	@Override
 	public String[] getExtraNames() {
+		if( !isConfigured()) 
+			return new String[]{};
 		List<String> extraNames = new ArrayList<String>();
 		for (DetectorElement thisElement: vortexParameters.getDetectorList()) {
 			if (thisElement.isExcluded()) continue;
 			extraNames.add(thisElement.getName() + "_realtime");
 			extraNames.add(thisElement.getName() + "_livetime");
 			for (RegionOfInterest roi : thisElement.getRegionList()) {
-				extraNames.add(thisElement.getName()+"_"+roi.getRoiName());
+				if( roi.getRoiStart() != -1)
+					extraNames.add(getExtraName(thisElement, roi));
 			}
 		}
 		return extraNames.toArray(new String[]{});
@@ -521,6 +670,8 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 
 	@Override
 	public String[] getOutputFormat() {
+		if( !isConfigured())
+			return new String[]{};
 		String[] formats = new String[getExtraNames().length];
 		for (int i = 0; i < formats.length; i++) {
 			formats[i] = "%5.5g";
@@ -564,4 +715,44 @@ public class XmapDetectorFromEpicsMca extends DetectorBase implements XmapDetect
 		throw new RuntimeException("Not supported");
 	}
 
+	public VortexParameters createVortexParameters() throws Exception{
+		VortexParameters vortexParameters = (VortexParameters) XMLHelpers.createFromXML(VortexParameters.mappingURL,
+				VortexParameters.class, VortexParameters.schemaURL, getConfigFileName());
+		
+		//make a deep copy we can then change a little
+		VortexParameters vp = new VortexParameters(vortexParameters);
+		
+		for(int i=0; i< vp.getDetectorList().size(); i++){
+			//There is 1 detector element per analyser
+			DetectorElement de = vp.getDetectorList().get(i);
+			if(!de.isExcluded()){
+				//read regionlist from mca
+				Analyser anly = analysers.get(i);
+				int regions = anly.getNumberOfRegions();
+				//either fill in an existing region or create a new one
+				List<RegionOfInterest> regionList = de.getRegionList();
+				for( int ir=0; ir< regions; ir++){
+					if( ir >= regionList.size()){
+						regionList.add(new RegionOfInterest());
+					}
+					RegionOfInterest roi = regionList.get(ir);
+					if( anly instanceof IEpicsMCA){
+						IEpicsMCA mca =(IEpicsMCA)anly;
+						EpicsMCARegionOfInterest mcaRoi = mca.getNthRegionOfInterest(ir);
+						String regionName = mcaRoi.getRegionName();
+						roi.setRoiName(regionName.isEmpty() ? Integer.toString(ir) : regionName);
+						roi.setWindowStart((int)mcaRoi.getRegionLow());
+						roi.setWindowEnd((int)mcaRoi.getRegionHigh());
+					}
+				}
+			}
+		}
+		return vp;
+	}
+	@Override
+	public void waitWhileBusy() throws DeviceException, InterruptedException {
+		for (Analyser analyser : analysers) {
+			analyser.waitWhileBusy();
+		}
+	}	
 }
