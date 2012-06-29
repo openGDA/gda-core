0ab0935544fd3233661d7528ed1933475369a79a
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2012-06-29 09:08:34 +0100

    GDA-5052 interrupted flag now reset at the appropriate time during scan abort.
    I reverted recent change to checkForInterrupts and instead reset the
    interrupted flag just before the InterruptedException is thrown to the
    calling code.

--- uk.ac.gda.core/src/gda/scan/ScanBase.java
+++ uk.ac.gda.core/src/gda/scan/ScanBase.java
@@ -218,13 +218,6 @@ public abstract class ScanBase implements Scan {
 	 * @throws InterruptedException
 	 */
 	public static void checkForInterrupts() throws InterruptedException {
-		
-		if (InterfaceProvider.getScanStatusHolder().getScanStatus() == Jython.IDLE) {
-			paused = false;
-			interrupted = false;
-			return;
-		}
-		
 		try {
 			if (paused & !interrupted) {
 				InterfaceProvider.getScanStatusHolder().setScanStatus(Jython.PAUSED);
@@ -1061,6 +1054,10 @@ public abstract class ScanBase implements Scan {
 				message += ": " + errorMessage;
 			}
 			logger.info(message);
+			
+			// Reset flag as we have now aborted the scan. Instead throw the exception to inform
+			// the code which created and ran the scan that it has been aborted.
+			interrupted = false;
 			throw new InterruptedException(message);
 		}
 	}
