98c4c5898ea93f52a45d2c9f1f053a1050d41fa0
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-07-16 16:44:19 +0100

    Protect PlotServerAdapter NPE if guiName is null

--- uk.ac.gda.analysis/src/uk/ac/diamond/scisoft/analysis/plotserver/corba/impl/PlotserverAdapter.java
+++ uk.ac.gda.analysis/src/uk/ac/diamond/scisoft/analysis/plotserver/corba/impl/PlotserverAdapter.java
@@ -249,7 +249,7 @@ public class PlotserverAdapter extends DeviceAdapter implements PlotServer {
 				org.omg.CORBA.Any any = corbaPlotServer.getGuiNames();
 				String [] otherNames = (String[]) any.extract_Value();
 				if( otherNames != null){
-					if( guiNames != null){
+					if( guiNames != null && guiNames.length>0){
 						List<String> asList = Arrays.asList(guiNames);
 						asList.addAll(Arrays.asList(otherNames));
 						return asList.toArray(new String[0]);
