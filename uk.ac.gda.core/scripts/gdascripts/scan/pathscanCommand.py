795d0c231fa8645bdb590b9a622510c9b7baefb0
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2012-07-24 16:31:51 +0100

    pathscanCommand.py - add "pathscan" command that allow user to input explicitly scannable group and path as please see GDA-4947
    pathscanfromPathFile.py - add "pathscan" command where scan path are
    inputed from a path data file.

---
+++ uk.ac.gda.core/scripts/gdascripts/scan/pathscanCommand.py
@@ -0,0 +1,29 @@
+'''
+Usage:
+    1. run this script from jython editor, you will have an object called 'pathscan'
+    2. start your scan using
+    >>>pathscan (x,y.z) ([1,1,1],[2,2,2],[3,4,5]) edxd 5.0
+    
+Created on 24 July 2012
+
+@author: fy65
+'''
+from dummy_detector import DummyDetector
+try:
+    if dummydetector == None: #@UndefinedVariable
+        dummydetector=DummyDetector("dummydetector")
+except:
+    dummydetector=DummyDetector("dummydetector")
+        
+from gda.device.scannable.scannablegroup import ScannableGroup
+from gda.jython.commands.ScannableCommands import scan
+from gda.jython.commands.GeneralCommands import alias
+def pathscan(scannables, path, detector, exposure, *args): #@UndefinedVariable
+    ''' Scan a group of scannables following the specified path and collect data at each point from specified detector and time'''
+    sg=ScannableGroup()
+    for each in scannables:
+        sg.addGroupMember(each)
+    sg.setName("pathgroup")
+    scan([sg, path, detector, exposure]+list(args))
+
+alias("pathscan")
