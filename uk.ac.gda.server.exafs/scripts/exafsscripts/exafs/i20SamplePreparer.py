b875ed09249b56fd59af74670d8b9f067b37858d
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2012-07-31 11:36:16 +0100

    GDA-5009 new scripts to replace use of xas_scans to enable looping
    These will only be used once localStation imports them. Untested on 
    beamline as yet.

---
+++ uk.ac.gda.server.exafs/scripts/exafsscripts/exafs/i20SamplePreparer.py
@@ -0,0 +1,23 @@
+from gda.configuration.properties import LocalProperties
+from gda.exafs.scan import BeanGroup
+from gda.exafs.scan import BeanGroups
+from gda.factory import Finder
+from uk.ac.gda.beans.exafs import XasScanParameters
+from uk.ac.gda.beans.exafs import XesScanParameters
+from uk.ac.gda.beans.exafs import XanesScanParameters
+from uk.ac.gda.beans.exafs.i20 import I20SampleParameters
+
+from BeamlineParameters import JythonNameSpaceMapping
+
+from exafsscripts.exafs.configFluoDetector import configFluoDetector
+
+class I20SamplePreparer:
+    def __init__(self):
+        pass
+    
+    def prepare(self, sampleParameters):
+        # if microreactor, then add this to the list of detectors
+        if sampleParameters.getSampleEnvironment() == I20SampleParameters.SAMPLE_ENV[4] :
+            return [Finder.getInstance().find("cirrus")]
+
+        return []
