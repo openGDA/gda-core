f32c1c1c1393a6c6a8a71f8bd1789f0ed9af864e
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2012-07-31 14:18:34 +0100

    Prevent going off end of array of descriptions

--- uk.ac.gda.server.exafs/src/gda/data/scan/datawriter/XasAsciiDataWriter.java
+++ uk.ac.gda.server.exafs/src/gda/data/scan/datawriter/XasAsciiDataWriter.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright © 2009 Diamond Light Source Ltd.
+ * Copyright © 2012 Diamond Light Source Ltd.
  *
  * This file is part of GDA.
  *
@@ -26,7 +26,6 @@ import gda.exafs.scan.BeanGroup;
 import gda.scan.IScanDataPoint;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -47,9 +46,6 @@ public class XasAsciiDataWriter extends AsciiDataWriter {
 
 	protected static BeanGroup group;
 
-	/**
-	 * @param group
-	 */
 	public static void setBeanGroup(BeanGroup group) {
 		XasAsciiDataWriter.group = group;
 	}
@@ -60,11 +56,6 @@ public class XasAsciiDataWriter extends AsciiDataWriter {
 
 	private String nexusFilePath;
 
-	/**
-	 * Constructor
-	 * 
-	 * @throws InstantiationException
-	 */
 	public XasAsciiDataWriter() throws InstantiationException {
 		super();
 		determineFilename();
@@ -75,7 +66,7 @@ public class XasAsciiDataWriter extends AsciiDataWriter {
 	public void writeHeader() {
 		
 		// use configured header first
-		super.writeHeader();
+		super.writeHeader(); 
 
 		try {
 			// only do this when running from the GUI
@@ -94,27 +85,21 @@ public class XasAsciiDataWriter extends AsciiDataWriter {
 			file.write("#\n");
 			final ISampleParameters p = (ISampleParameters) getBean(group.getSample());
 			// write out sample parameters
-			if (p != null) {
-//				if (p.getName() != null)
-//					file.write("# Sample name: " + p.getName() + "\n");
-				if (p.getDescriptions() != null){
-					file.write("# Sample description: " + p.getDescriptions().get(0) + "\n");
-					file.write("# Additional comments: " + p.getDescriptions().get(1) + "\n");
+			if (p != null) {				
+				for (int i =0; i < p.getDescriptions().size(); i++){
+					String startMsg = "# ";
+					if (i==0){
+						startMsg += "Sample description: ";
+					} else {
+						startMsg += "Additional comments: ";
+					}
+					file.write(startMsg + p.getDescriptions().get(i) + "\n");
 				}
-					//for (String des : p.getDescriptions()) {
-						//file.write("# Sample description");
-						//file.write(String.format("%s\n", des));
-					//}
 			}
-
 			file.flush();
-
-		} catch (IOException e) {
-			logger.error("Exception while writing out header of ascii file: " + fileUrl);
 		} catch (Exception e) {
-			logger.error("Exception while calculating dark current for ascii file: " + fileUrl);
+			logger.error("Exception while writing out header of ascii file: " + fileUrl);
 		}
-
 	}
 
 	/**
@@ -235,30 +220,4 @@ public class XasAsciiDataWriter extends AsciiDataWriter {
 	public void setNexusFilePath(String nexusFileTemplate) {
 		this.nexusFilePath = nexusFileTemplate;
 	}
-
-	/**
-	 * 
-	 Example of Header for Ascii Output file for I20 Below is the example header. #’s distinguish the header and
-	 * footer from the data. In the case of transmission detection, the red text is to be included; in the case of
-	 * fluorescence detection, the blue text is to be included. A footer shows the ring current at the end of the scan.
-	 * # DIAMOND LIGHT SOURCE # Instrument: I20-XAS; Date: Day dd/mm/yyyy hh:mm:ss # Ring energy: 3 GeV # Initial ring
-	 * current = 230 mA # Filling mode: uniform filling # Wiggler gap selected: 11 mm # Primary slits: vertical gap=3
-	 * mm; horizontal gap=3 mm; vertical offset=1 mm; horizontal offset=1 mm # Secondary slits: vertical gap=3 mm;
-	 * horizontal gap=3 mm; vertical offset=1 mm; horizontal offset=1 mm # Optic Hutch Mirrors Coating: silicon or
-	 * rhodium # Monochromator Crystal cut: Si(111) # Experimental slits: vertical gap=3 mm; horizontal gap=3 mm;
-	 * vertical offset=1 mm; horizontal offset=1 mm # Incident angle for Harmonic Rejection Mirrors: 3.5 mrad # Harmonic
-	 * Rejection Mirrors Coating: silicon or rhodium # Transmission measurement # Range of Ion Chambers: I0=10^-8;
-	 * It=10^-8; Iref=10^-8 # Fluorescence measurement # Range of Ion Chambers: I0=10^-8; It=10^-8; Iref=10^-8 #
-	 * Detector: Ge (XSPRESS) or Si (XIA) # # Ascii output file name: 'XXXXXX.dat' # Nexus output file:
-	 * '/dls_sw/dasc/users/data/nexus/XXXXXX.nxs' # Sample name: 'Sample123' # Comment: 'This comment and the sample
-	 * name are taken from the corresponding comment and sample name in the sample parameters file.' # # Sample
-	 * parameter file: '/dls_sw/dasc/users/scripts/Sample_Parameters.xml' # Scan parameter file:
-	 * '/dls_sw/dasc/users/scripts/XAS_Parameters.xml' # Detector parameter file:
-	 * '/dls_sw/dasc/users/scripts/Detector_Parameters.xml' # Output parameter file:
-	 * '/dls_sw/dasc/users/scripts/Output_Parameters.xml' # # Dark current: # E I0 It If ln(I0/It) ln(It/Ir) FF FF/I0
-	 * Integration_time DATA … DATA … DATA … # DIAMOND LIGHT SOURCE # Instrument: I20-XAS; Date: Day dd/mm/yyyy hh:mm:ss
-	 * # Final ring current = 225 mA The 9 columns of data to be recorded are described below. When the measurement is
-	 * being made in transmission only (without a fluorescence detector), the columns FF and FF/I0 are not to be
-	 * included, in this case only 7 columns of data are recorded.
-	 **/
 }
