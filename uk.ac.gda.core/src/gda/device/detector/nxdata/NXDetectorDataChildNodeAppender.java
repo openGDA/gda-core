3db5ad6f7a9ae66f7cf30ffc6dfd882eb1b21419
Rob Walton <rob.walton@diamond.ac.uk>
2012-08-22 23:23:17 +0100

    GDA-5129 - Created NXDetectorDataChileNodeAppender suitable for adding detector metadata

---
+++ uk.ac.gda.core/src/gda/device/detector/nxdata/NXDetectorDataChildNodeAppender.java
@@ -0,0 +1,38 @@
+/*-
+ * Copyright © 2012 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.detector.nxdata;
+
+import gda.data.nexus.tree.INexusTree;
+import gda.device.detector.NXDetectorData;
+
+public class NXDetectorDataChildNodeAppender implements NXDetectorDataAppender {
+
+	private final INexusTree treeToAppend;
+	
+	public NXDetectorDataChildNodeAppender(INexusTree treeToAppend) {
+		this.treeToAppend = treeToAppend;
+		
+	}
+	@Override
+	public void appendTo(NXDetectorData data, String detectorName) {
+		INexusTree detTree = data.getDetTree(detectorName);
+		detTree.addChildNode(treeToAppend);
+	}
+
+}
