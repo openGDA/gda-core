53033dcbd4cb23f03bf865c12576f997749278ae
Ravi Somayaji <ravi.somayaji@diamond.ac.uk>
2012-08-29 15:08:50 +0100

    GDA-5177 - Parameter view included in the perspective

--- uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/PerspectiveFactory.java
+++ uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/PerspectiveFactory.java
@@ -3,6 +3,7 @@ package uk.ac.diamond.tomography.reconstruction;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import uk.ac.diamond.tomography.reconstruction.views.ParameterView;
 import uk.ac.diamond.tomography.reconstruction.views.ProjectionsView;
 
 public class PerspectiveFactory implements IPerspectiveFactory {
@@ -16,7 +17,7 @@ public class PerspectiveFactory implements IPerspectiveFactory {
 				IPageLayout.RIGHT, 0.5f, IPageLayout.ID_EDITOR_AREA);
 		layout.addView(ProjectionsView.ID, IPageLayout.BOTTOM, 0.3f,
 				IPageLayout.ID_PROJECT_EXPLORER);
-		layout.addView(IPageLayout.ID_PROP_SHEET, IPageLayout.BOTTOM, 0.5f,
+		layout.addView(ParameterView.ID, IPageLayout.BOTTOM, 0.5f,
 				ProjectionsView.ID);
 		layout.addPlaceholder(
 				"org.dawb.workbench.plotting.views.toolPageView.1D_and_2D",
