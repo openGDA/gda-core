c888543a8d48213adb096371ef65a5946129579b
Ravi Somayaji <ravi.somayaji@diamond.ac.uk>
2012-09-05 14:05:27 +0100

    GDA-5177 - Removed the ParameterEditor as this is replaced by the generated editor.

--- uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/Activator.java
+++ uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/Activator.java
@@ -34,6 +34,8 @@ public class Activator extends AbstractUIPlugin {
 	// The shared instance
 	private static Activator plugin;
 
+	public static final String TOMOGRAPHY_SETTINGS = ".tomographySettings";
+
 	/**
 	 * The constructor
 	 */
