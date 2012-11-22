db2db8f7c9cc872f1cbf54dd59ab5fc37735d7d0
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2012-11-22 10:03:02 +0000

    GDA-5367 change the sensitivity at the start of a scan

--- uk.ac.gda.server.exafs/scripts/exafsscripts/exafs/i20DetectorPreparer.py
+++ uk.ac.gda.server.exafs/scripts/exafsscripts/exafs/i20DetectorPreparer.py
@@ -1,10 +1,19 @@
+from java.lang import Exception
+
 from gda.device.detector.xspress import Xspress2DetectorConfiguration
 
 
 class I20DetectorPreparer:
-    def __init__(self, xspress2system, ExafsScriptObserver):
+    """
+     Assume the sensitivities and offsets are arrays of scannables in order: [I0,It,Iref,I1]
+    """
+    def __init__(self, xspress2system, ExafsScriptObserver,sensitivities, sensitivity_units ,offsets, offset_units):
         self.xspress2system = xspress2system
         self.ExafsScriptObserver = ExafsScriptObserver
+        self.sensitivities = sensitivities 
+        self.sensitivity_units = sensitivity_units 
+        self.offsets = offsets
+        self.offset_units = offset_units
         
     def prepare(self, detectorParameters, outputParameters, scriptFolder):
         """
@@ -21,3 +30,27 @@ class I20DetectorPreparer:
                 from gda.device.detector import VortexDetectorConfiguration
                 VortexDetectorConfiguration(self.ExafsScriptObserver,fullFileName,None,outputParameters).configure()
             
+        for ionChamberParams in detectorParameters.getTransmissionParameters().getIonChamberParameters():
+            if ionChamberParams.getChangeSensitivity():
+                ionChamberName = ionChamberParams.getName()
+                if ionChamberParams.getGain() == None or ionChamberParams.getGain() == "":
+                    continue
+                sensitivity, units = ionChamberParams.getGain().split()
+                
+                index = 0
+                if ionChamberName == "It":
+                    index = 1;
+                elif ionChamberName == "Iref":
+                    index = 2;
+                elif ionChamberName == "I1":
+                    index = 3;
+                
+                try:
+                    print "Changing sensitivity of",ionChamberName,"to",ionChamberParams.getGain()
+                    self.sensitivities[index](sensitivity)
+                    self.sensitivity_units[index](units)
+                except Exception, e:
+                    print "Exception while trying to change the sensitivity of ion chamber",ionChamberName
+                    print "Set the ion chamber sensitivity manually, uncheck the box in the Detector Parameters editor and restart the scan"
+                    print "Please report this problem to Data Acquisition"
+                    raise e
