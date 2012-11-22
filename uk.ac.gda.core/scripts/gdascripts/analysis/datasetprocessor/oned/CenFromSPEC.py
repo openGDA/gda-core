ede24448bccc0c39fb0144497452013b3bf792d5
Rob Walton <rob.walton@diamond.ac.uk>
2012-11-22 14:41:36 +0000

    BLBXVI-8: Created CEN scan data processor (from SPEC)

---
+++ uk.ac.gda.core/scripts/gdascripts/analysis/datasetprocessor/oned/CenFromSPEC.py
@@ -0,0 +1,41 @@
+from XYDataSetProcessor import XYDataSetFunction
+from gda.analysis import ScanFileHolder
+import math
+
+def interp(xlist, fractional_index):
+	lower_index = int(math.floor(fractional_index))
+	upper_index = int(math.ceil(fractional_index))
+	fraction_toward_upper = fractional_index - lower_index
+	return xlist[lower_index] + fraction_toward_upper * (xlist[upper_index] - xlist[lower_index]) 
+
+class CenFromSPEC(XYDataSetFunction):
+	"""See http://www.certif.com/spec_help/data.html%20for%20cfwhm
+	1. choose max value
+	2. create box to left and right containing all counts
+	3. feature is at the middle of this box, also return height and width
+	"""
+
+	def __init__(self, name='cen', labelList=('cen', 'height', 'width'),formatString=''):
+		XYDataSetFunction.__init__(self, name, labelList,'cen', formatString)
+	
+	def _process(self,xDataSet, yDataSet):
+		
+		xlist = list(xDataSet.getBuffer())
+		ylist = list(yDataSet.getBuffer())		
+		
+		height = max(ylist)
+		index_max_val = ylist.index(height)
+		
+		counts_left = sum(ylist[:index_max_val]) + height / 2.
+		index_left_edge = index_max_val - (counts_left / height)
+		pos_left_edge = interp(xlist, index_left_edge)
+
+		counts_right = sum(ylist[index_max_val+1:]) + height / 2.
+		index_right_edge = index_max_val + (counts_right / height)		
+		pos_right_edge = interp(xlist, index_right_edge)
+
+		cen = (pos_left_edge + pos_right_edge) /2. 
+
+		width = pos_right_edge - pos_left_edge
+
+		return cen, height, width
---
+++ uk.ac.gda.core/scripts/testjy/gdascripts_test/analysis_test/datasetprocessor_test/oned_test/TestCen.py
@@ -0,0 +1,66 @@
+from gdascripts.analysis.datasetprocessor.oned.CenFromSPEC import CenFromSPEC, interp
+
+from gda.analysis import DataSet
+
+
+import unittest
+
+
+def close(l1, l2):
+	for v1, v2 in zip(l1, l2):
+		if abs(v1-v2) > .01:
+			return False
+	return True
+
+def closer(l1, l2, tolerance=0.01):
+	for v1, v2 in zip(l1, l2):
+		if abs(v1-v2) > tolerance:
+			return False
+	return True
+
+
+class TestCOM(unittest.TestCase):
+	
+	def setUp(self):
+		self.p = CenFromSPEC() # -> cen, height, width
+		self.x = DataSet.array([10.,11.,12.,13.,14.,15.,16.,17.,18.,19.])
+		self.l = [0, 10, 20, 30, 40]
+		
+	def test_process_ones(self):
+		y = DataSet.array([0, 0, 1, 1.000000001, 1, 1, 1, 0, 0, 0])
+		cen, height, width = self.p._process(self.x, y)
+		self.assertAlmostEqual(cen, 14)
+		self.assertAlmostEqual(height, 1.000000001)
+		self.assertAlmostEqual(width, 5)
+
+	def test_process(self):
+		y = DataSet.array([0, 0, 1, 1.000000001, 1, 1, 0, 1, 0, 0])
+		cen, height, width = self.p._process(self.x, y)
+		self.assertAlmostEqual(cen, 14)
+		self.assertAlmostEqual(height, 1.000000001)
+		self.assertAlmostEqual(width, 5)
+		
+	def test_interp_0(self):
+		self.assertEqual(interp(self.l, 0), 0)
+		
+	def test_interp_2(self):
+		self.assertEqual(interp(self.l, 2), 20)
+		
+	def test_interp_4(self):
+		self.assertEqual(interp(self.l, 4), 40)
+		
+	def test_interp_2_1(self):
+		self.assertEqual(interp(self.l, 2.1), 21)
+		
+	def test_interp_2_5(self):
+		self.assertEqual(interp(self.l, 2.5), 25)
+
+
+def suite():
+	suite = unittest.TestSuite()
+	suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestCOM))
+	return suite 
+
+
+if __name__ == '__main__':
+	unittest.TextTestRunner(verbosity=2).run(suite())
