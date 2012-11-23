676916fa789a12304b1f39565086e4b2411d2206
Rob Walton <rob.walton@diamond.ac.uk>
2012-11-23 13:59:39 +0000

    Tuner.py now has configurable backlash distance.
    Default is 50% of range as it was before

--- uk.ac.gda.core/scripts/gdascripts/scan/process/tuner.py
+++ uk.ac.gda.core/scripts/gdascripts/scan/process/tuner.py
@@ -7,15 +7,18 @@ class Tuner(object):
 		self.scan = scanClass([ScanDataProcessor([datasetProcessor])])
 		self.args = args
 		self.use_backlash_correction = False
+		self.fraction_of_range_for_backlash = .5
+		
+		
 		
 	def tune(self):
 		r = self.scan(*self.args)
 		print r
 		if self.use_backlash_correction:
-			range = abs(self.args[2] - self.args[1])
+			full_range = abs(self.args[2] - self.args[1])
 			scn = self.args[0]
 			peakpos = r.__dict__[self.processorName].scn[scn.name]
-			print "Moving %s to %f to allow for backlash" % (scn.name, peakpos-range/2.)
-			scn.moveTo(peakpos-range/2.)
+			print "Moving %s to %f to allow for backlash" % (scn.name, peakpos - (full_range * self.fraction_of_range_for_backlash))
+			scn.moveTo(peakpos - (full_range * self.fraction_of_range_for_backlash))
 		r.__dict__[self.processorName].go()
 
