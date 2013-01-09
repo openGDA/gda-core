f053d63c07e1002c1f3b93d7a26b130da6c72c01
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-01-09 16:26:09 +0000

    latest BioSAXS changes pending tests on hardware (or simulation)

--- uk.ac.gda.devices.bssc/scripts/BSSC.py
+++ uk.ac.gda.devices.bssc/scripts/BSSC.py
@@ -12,11 +12,13 @@ class BSSCRun:
         self.bssc = gda.factory.Finder.getInstance().listAllLocalObjects("uk.ac.gda.devices.bssc.BioSAXSSampleChanger")[0]
         self.tfg = gda.factory.Finder.getInstance().listAllLocalObjects("gda.device.Timer")[0]
         self.detector = gda.factory.Finder.getInstance().listAllLocalObjects("uk.ac.gda.server.ncd.detectorsystem.NcdDetectorSystem")[0]
-	self.cam = gda.factory.Finder.getInstance().find("bsaxscam")
+        self.cam = gda.factory.Finder.getInstance().find("bsaxscam")
+        self.shutter = gda.factory.Finder.getInstance().find("shutter")
         self.progresscounter = 0
         self.sampleprogresscounter = 0
-        self.overheadsteps = 4
+        self.overheadsteps = 5
         self.stepspersample = 9 
+        self.samplevolume = 10
         self.totalSteps = self.overheadsteps + self.bean.getMeasurements().size() * self.stepspersample
         self.lastreportedmeasurement = None
         
@@ -74,27 +76,48 @@ class BSSCRun:
     def setTitle(self, title):
         GDAMetadataProvider.getInstance().setMetadataValue("title", title)
     
-    def loadSample(self, location):
-        self.monitorAsynchronousMethod(self.bssc.fill(location.getPlate(), location.getRowAsInt(), location.getColumn(), 10.0))
-    
-    def loadBuffer(self, location):
-        self.monitorAsynchronousMethod(self.bssc.fill(location.getPlate(), location.getRowAsInt(), location.getColumn(), 10.0))
+    def loadWell(self, location):
+        self.monitorAsynchronousMethod(self.bssc.fill(location.getPlate(), location.getRowAsInt(), location.getColumn(), self.samplevolume))
+
+    def unloadIntoWell(self, location):
+        self.monitorAsynchronousMethod(self.bssc.recouperate(location.getPlate(), location.getRowAsInt(), location.getColumn()))
     
     def expose(self, duration):
         taskid = self.bssc.push(10.0, 5.0/duration)
         gda.jython.commands.ScannableCommands.staticscan([self.detector])
         #self.monitorAsynchronousMethod(taskid)
+        
+    def openShutter(self):
+        self.shutter.asynchronousMoveTo("Open")
     
-    def run(self):
-        self.reportProgress("Initialising");
-        self.checkDevice()
-        self.reportProgress("Setting Storage Temperature")
-        self.setStorageTemperature()
-        self.reportProgress("Performing Courtesy Cell Wash")
-        self.bssc.setViscosityLevel("high")
-        self.bssc.setSampleType("yellow")
-        self.clean()
-        for titration in self.bean.getMeasurements():
+    def closeShutter(self):
+        self.shutter.asynchronousMoveTo("Closed")
+        
+    def measureBuffer(self, titration, duration):
+        if titration != None:
+            self.reportSampleProgress(titration, "Sucking in Buffer")
+            self.loadWell(titration.getBufferLocation())
+            self.reportSampleProgress(titration, "Exposing Buffer")
+            self.setTitle("Buffer for next and preceding sample measurement")
+            self.expose(duration)
+            self.reportSampleProgress(titration, "Cleaning after Buffer")
+            self.clean()
+   
+    def measureSample(self, titration, duration):
+        if titration != None:     
+            self.reportSampleProgress(titration, "Sucking in Sample")
+            self.loadWell(titration.getLocation())
+            self.reportSampleProgress(titration, "Exposing Sample")
+            self.setTitle("Sample: %s (Location %s)" % (titration.getSampleName(), titration.getLocation().toString()))
+            self.expose(duration)
+            if titration.isRecouperate():
+                self.reportSampleProgress(titration, "Recouperating Sample and Cleaning")
+                self.unloadIntoWell(titration.getLocation)
+            else:
+                self.reportSampleProgress(titration, "Cleaning after Sample")
+            self.clean()
+
+    def setUpRobotAndDetector(self, titration):
             self.reportSampleProgress(titration, "Setting Up Robot")
             self.bssc.setViscosityLevel(titration.getViscosity())
             if titration.isYellowSample():
@@ -104,20 +127,41 @@ class BSSCRun:
             self.reportSampleProgress(titration, "Checking Exposure Cell Temperature")
             self.setExposureTemperature(titration.getExposureTemperature())
             self.reportSampleProgress(titration, "Setting Up Time Frame Generator")
-            duration = self.setupTfg(titration.getFrames(), titration.getTimePerFrame())
-            self.reportSampleProgress(titration, "Sucking in Sample")
-            self.loadSample(titration.getLocation())
-            self.reportSampleProgress(titration, "Exposing Sample")
-            self.setTitle("Sample: %s (Location %s)" % (titration.getSampleName(), titration.getLocation().toString()))
-            self.expose(duration)
-            self.reportSampleProgress(titration, "Cleaning after Sample")
-            self.clean()
-            #self.reportSampleProgress(titration, "Sucking in Buffer")
-            #self.loadBuffer(titration.getLocation())
-            #self.reportSampleProgress(titration, "Exposing Buffer")
-            #self.setTitle("Buffer: %s (for Sample %s at Location %s)" % (titration.getBufferName(), titration.getSampleName(), titration.getLocation().toString()))
-            #self.expose(duration)
-            #self.reportSampleProgress(titration, "Cleaning after Buffer")
-            #self.clean()
-        self.reportProgress("Finalising")
+            return self.setupTfg(titration.getFrames(), titration.getTimePerFrame())
+        
+    def tritrationsCanUseSameBufferMeasurement(self, t1, t2):
+        if not t1.getBufferLocation().equals(t2.getBufferLocation()):
+            return false
+        if abs(t1.getExposureTemperature() - t2.getExposureTemperature()) > 0.1:
+            return false
+        if t1.getFrames() != t2.getFrames():
+            return false
+        if abs(t1.getTimePerFrame() - t2.getTimePerFrame()) > 0.001:
+            return false
+        return true
+        
+    def run(self):
+        self.reportProgress("Initialising");
+        self.checkDevice()
+        self.reportProgress("Setting Storage Temperature")
+        self.setStorageTemperature()
+        self.reportProgress("Performing Courtesy Cell Wash")
+        self.bssc.setViscosityLevel("high")
+        self.bssc.setSampleType("yellow")
+        self.clean()
+        self.reportProgress("Opening Shutter")
+        self.openShutter()
+        lastTitration=None
+        duration=None
+        for titration in self.bean.getMeasurements():
+            if lastTitration != None and not self.tritrationsCanUseSameBufferMeasurement(lastTitration, titration):
+                # everything would be set up from previous sample then
+                self.measureBuffer(lastTitration, duration)
+            duration = self.setUpRobotAndDetector(titration)
+            self.measureSample(titration, duration)
+            lastTitration = titration
+        self.measureBuffer(lastTitration, duration)
+        self.reportProgress("Closing shutter")
+        self.closeShutter()
         time.sleep(2)
+        
