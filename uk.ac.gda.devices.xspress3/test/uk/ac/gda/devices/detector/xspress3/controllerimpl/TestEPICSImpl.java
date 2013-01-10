95b28a5b7d3a58325fa091e15b17eafbb9163d84
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2013-01-10 12:18:17 +0000

    write test harness to test code with hardware

--- uk.ac.gda.devices.xspress3/test/uk/ac/gda/devices/detector/xspress3/controllerimpl/TestEPICSImpl.java
+++ uk.ac.gda.devices.xspress3/test/uk/ac/gda/devices/detector/xspress3/controllerimpl/TestEPICSImpl.java
@@ -1,7 +1,10 @@
 package uk.ac.gda.devices.detector.xspress3.controllerimpl;
 
-import org.junit.Before;
-import org.junit.Test;
+import gda.configuration.properties.LocalProperties;
+import gda.device.DeviceException;
+import gda.factory.FactoryException;
+import uk.ac.gda.devices.detector.xspress3.ROI;
+import uk.ac.gda.devices.detector.xspress3.Xspress3Detector;
 
 /**
  * Has a dependency on an Epics simulation being available, so this should not
@@ -12,18 +15,49 @@ import org.junit.Test;
  */
 public class TestEPICSImpl {
 	
-	@Before
-	public void setup(){
-		// connect to Epics
-	}
-	
-	@Test
-	public void TestCanConnect() {
-		
-	}
-	
-	@Test
-	public void TestCanSetNumImagesToCollect(){
+	private static EpicsController x3c;
+	private static Xspress3Detector x3d;
+
+	public static void main(String[] args){
+		try {
+			// set properties file
+			// -Dgov.aps.jca.JCALibrary.properties=${project_loc:i20-config}/jca/dummy/JCALibrary.properties
+			LocalProperties.set("gov.aps.jca.JCALibrary.properties",TestEPICSImpl.class.getResource("JCALibrary.properties").getFile());
+			
+			// create Controller
+			x3c = new EpicsController();
+			x3c.configure();
+			
+			// create detector
+			x3d = new Xspress3Detector(x3c);
+			x3d.configure();
+			
+			// setup and run a series of frames
+			x3d.setFilePath("/scratch/");
+			x3d.setFilePrefix("TestEPICSImpl");
+			x3d.setFileNumber(1);
+			x3d.setNumberOfFramesToCollect(20);
+			ROI[] rois = x3d.getRegionsOfInterest();
+			rois[0].setStart(100);
+			rois[1].setEnd(200);
+			
+			x3d.atScanLineStart();
+			x3d.collectData();
+			x3d.waitWhileBusy();
+			x3d.atScanLineEnd();
+			
+			// readout various data
+			
+		} catch (FactoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DeviceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }
