7c96e3bde939649482b29e38ff2d0512b4d6bf13
Ravi Somayaji <ravi.somayaji@diamond.ac.uk>
2013-01-14 16:39:28 +0000

    Revert "JEEP-123 - Tilt preset cam1_roll and ss1_rx values so that matlab tilt procedures evaluate correct values."
    This reverts commit fe01c42140821624e3d22627e7f00b94e2da3eb8.

--- uk.ac.gda.client.tomo/src/uk/ac/gda/client/tomo/alignment/view/handlers/impl/TiltController.java
+++ uk.ac.gda.client.tomo/src/uk/ac/gda/client/tomo/alignment/view/handlers/impl/TiltController.java
@@ -82,18 +82,6 @@ public class TiltController implements ITiltController {
 
 	private boolean test = false;
 
-	private double preTiltSs1RxVal = 0.2;
-
-	private double preTiltCam1RollVal = 0.5;
-
-	public void setPreTiltCam1RollVal(double preTiltCam1RollVal) {
-		this.preTiltCam1RollVal = preTiltCam1RollVal;
-	}
-
-	public void setPreTiltSs1RxVal(double preTiltSs1RxVal) {
-		this.preTiltSs1RxVal = preTiltSs1RxVal;
-	}
-
 	public String getResult() {
 		return result;
 	}
@@ -142,12 +130,6 @@ public class TiltController implements ITiltController {
 		String firstScanFolder = null;
 		String secondScanFolder = null;
 		try {
-			// Move the cam1_roll and the ss1_rx to preset values so that the matlab tilt procedure evaluates good
-			// values.
-			cameraModuleMotorHandler.moveCam1Roll(progress, preTiltCam1RollVal);
-			sampleStageMotorHandler.moveSs1RxBy(progress, preTiltSs1RxVal);
-			//
-
 			cameraHandler.setUpForTilt(minY, maxY, minX, maxX);
 			logger.debug("Set the camera minY at:" + minY + " and maxY at:" + maxY);
 			double txOffset = getTxOffset(module);
@@ -174,15 +156,13 @@ public class TiltController implements ITiltController {
 							logger.debug("motorsto move:{}", motorsToMove);
 							if (!progress.isCanceled()) {
 								if (motorsToMove != null) {
-									logger.debug("Current cam1_roll is :{}",
-											cameraModuleMotorHandler.getCam1RollPosition());
+									logger.debug("Current cam1_roll is :{}", cameraModuleMotorHandler.getCam1RollPosition());
 									logger.debug("Current rx is :{}", sampleStageMotorHandler.getSs1RxPosition());
 									double rz = motorsToMove[0];// roll
 									double rx = motorsToMove[1];// pitch
 
 									// sampleStageMotorHandler.moveSs1RzBy(progress, -rz);
-									cameraModuleMotorHandler.moveCam1Roll(progress,
-											cameraModuleMotorHandler.getCam1RollPosition() - rz);
+									cameraModuleMotorHandler.moveCam1Roll(progress, cameraModuleMotorHandler.getCam1RollPosition() - rz);
 									sampleStageMotorHandler.moveSs1RxBy(progress, -rx);
 
 									logger.debug("After move cam1 roll is :{}",
@@ -505,8 +485,7 @@ public class TiltController implements ITiltController {
 				// return null;
 				// }
 				try {
-					motorsToMove[count] = Double.parseDouble(token);
-					count = count + 1;
+					motorsToMove[count++] = Double.parseDouble(token);
 					// Only interested in the first two decimals
 					if (count == 2) {
 						break;
