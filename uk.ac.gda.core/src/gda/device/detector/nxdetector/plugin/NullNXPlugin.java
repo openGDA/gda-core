d4fe8ae997257fb70e22c2cf03a12cd12d4284ec
Rob Walton <rob.walton@diamond.ac.uk>
2013-02-14 12:00:06 +0000

    BLVII-57 - Created a NullNXPlugin which does nothing.

---
+++ uk.ac.gda.core/src/gda/device/detector/nxdetector/plugin/NullNXPlugin.java
@@ -0,0 +1,90 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.detector.nxdetector.plugin;
+
+import gda.device.DeviceException;
+import gda.device.detector.nxdata.NXDetectorDataAppender;
+import gda.device.detector.nxdata.NXDetectorDataNullAppender;
+import gda.device.detector.nxdetector.NXPlugin;
+import gda.scan.ScanInformation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+/**
+ * A null NXPlugin implementation. It can be used but will do nothing. Useful as a base class, but
+ * best used only if not overriding the bulk of these methods anyway.
+ */
+public class NullNXPlugin implements NXPlugin {
+
+	@Override
+	public String getName() {
+		return "null_nx_plugin";
+	}
+
+	@Override
+	public boolean willRequireCallbacks() {
+		return false;
+	}
+
+	@Override
+	public void prepareForCollection(int numberImagesPerCollection, ScanInformation scanInfo) throws Exception {
+	}
+
+	@Override
+	public void prepareForLine() throws Exception {
+	}
+
+	@Override
+	public void completeLine() throws Exception {
+	}
+
+	@Override
+	public void completeCollection() throws Exception {
+	}
+
+	@Override
+	public void atCommandFailure() throws Exception {
+	}
+
+	@Override
+	public void stop() throws Exception {
+	}
+
+	@Override
+	public List<String> getInputStreamNames() {
+		return Arrays.asList();
+	}
+
+	@Override
+	public List<String> getInputStreamFormats() {
+		return Arrays.asList();
+	}
+
+	@Override
+	public List<NXDetectorDataAppender> read(int maxToRead) throws NoSuchElementException, InterruptedException,
+			DeviceException {
+		List<NXDetectorDataAppender> appenders = new ArrayList<NXDetectorDataAppender>();
+		appenders.add(new NXDetectorDataNullAppender());
+		return appenders;
+	}
+
+}
