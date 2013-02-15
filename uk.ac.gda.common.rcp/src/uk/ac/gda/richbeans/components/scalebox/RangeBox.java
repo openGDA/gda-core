7cc44256dd120ee641bf5a2427b0b286fcc5bc5e
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2013-02-15 11:32:14 +0000

    BLXX-62 add constructor to override standard icon and tooltip

--- uk.ac.gda.common.rcp/src/uk/ac/gda/richbeans/components/scalebox/RangeBox.java
+++ uk.ac.gda.common.rcp/src/uk/ac/gda/richbeans/components/scalebox/RangeBox.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright © 2009 Diamond Light Source Ltd.
+ * Copyright © 2013 Diamond Light Source Ltd.
  *
  * This file is part of GDA.
  *
@@ -41,47 +41,58 @@ import uk.ac.gda.richbeans.event.ValueEvent;
 
 import com.swtdesigner.SWTResourceManager;
 
-
 /**
- * This class is a bounded, united object which can be used for a general
- * value input in gda SWT UI.
+ * This class is a bounded, united object which can be used for a general value input in gda SWT UI. In addition to the
+ * standard single value the box can save value as a list of values or an iteration.
  * 
- * In addition to the standard single value the box can save value as a list of values
- * or an iteration.
- * 
- * @author fcp94556
- *
  */
 public class RangeBox extends NumberBox implements IRangeWidget {
-		
-    private String  dialogTitle;
-    private boolean rangeOnly=false;
-    private boolean error=false;
-    private RangeDialog dialog=null;
+
+	private String dialogTitle;
+	private boolean rangeOnly = false;
+	private boolean error = false;
+	private RangeDialog dialog = null;
+
 	/**
-	 * Create the composite RangeBox for use in GDA UI.
+	 * Constructor to overrule the default icon and tooltip for the button.
 	 * 
-	 * After creating the composite set the scale properties (max, min etc.).
-	 * These are used at run time by a lazy initiated form.
-	 *  
+	 * @param parent
+	 * @param style
+	 * @param buttonText
+	 * @param buttonToolTip
+	 */
+	public RangeBox(final Composite parent, final int style, String buttonText, String buttonToolTip) {
+		this(parent, style);
+		this.button.setImage(null);
+		this.button.setToolTipText(buttonToolTip);
+		this.button.setText(buttonText);
+		final GridData bLayout = (GridData) button.getLayoutData();
+		bLayout.widthHint = SWT.DEFAULT;
+	}
+
+	/**
+	 * Create the composite RangeBox for use in GDA UI. After creating the composite set the scale properties (max, min
+	 * etc.). These are used at run time by a lazy initiated form.
+	 * 
 	 * @param parent
 	 * @param style
 	 */
 	public RangeBox(final Composite parent, final int style) {
-		
+
 		super(parent, style);
-		
-		this.buttonSelection = new SelectionAdapter()  {
+
+		this.buttonSelection = new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				dialog = new RangeDialog(getShell(), isIntegerBox());
 				dialog.setRangeOnly(isRangeOnly());
 				dialog.create();
-				if (getDialogTitle()!=null) dialog.getShell().setText(getDialogTitle());
+				if (getDialogTitle() != null)
+					dialog.getShell().setText(getDialogTitle());
 				dialog.setUnit(getUnit());
 				dialog.setValue(getValue());
 				dialog.setBounds(RangeBox.this);
-				if (dialog.open()==Window.OK) {
+				if (dialog.open() == Window.OK) {
 					checkValue(dialog.getValue());
 				}
 				dialog = null;
@@ -92,123 +103,137 @@ public class RangeBox extends NumberBox implements IRangeWidget {
 		this.button.setToolTipText("Open range form which allows this value to be defined as a range.");
 		this.button.setImage(SWTResourceManager.getImage(RangeBox.class, "/icons/arrow_divide.png"));
 		this.button.setText("");
-		final GridData bLayout = (GridData)button.getLayoutData();
-		
-		// Platform dependant sizes but they work
-		// on linux RHEL5 ok.
+		final GridData bLayout = (GridData) button.getLayoutData();
+
+		// Platform dependant sizes but they work on linux RHEL5 ok.
 		bLayout.widthHint = 29;
 
 	}
-	
+
 	@Override
 	public void closeDialog() {
-		if (dialog!=null) dialog.close();
+		if (dialog != null)
+			dialog.close();
 	}
-	
+
 	@Override
 	protected void createTextListeners(final StyledText text) {
 		this.focusListener = new FocusAdapter() {
 			@Override
 			public void focusLost(FocusEvent e) {
-				if (!isOn()) return;
+				if (!isOn())
+					return;
 				try {
 					on = false;
-				    checkValue(text.getText());
+					checkValue(text.getText());
 				} finally {
 					on = true;
 				}
 			}
-	    };
-	    text.addFocusListener(focusListener);
-	    
-	    this.modifyListener = new ModifyListener() {
+		};
+		text.addFocusListener(focusListener);
+
+		this.modifyListener = new ModifyListener() {
 			@Override
 			public void modifyText(final ModifyEvent e) {
- 			    checkValue(text.getText());
- 			    final ValueEvent evt = new ValueEvent(RangeBox.this,getFieldName());
- 			    evt.setValue(getValue());
- 			    eventDelegate.notifyValueListeners(evt);
-			} 	
-	    };
-	    text.addModifyListener(modifyListener);
-	    
+				checkValue(text.getText());
+				final ValueEvent evt = new ValueEvent(RangeBox.this, getFieldName());
+				evt.setValue(getValue());
+				eventDelegate.notifyValueListeners(evt);
+			}
+		};
+		text.addModifyListener(modifyListener);
+
 	}
 
 	@Override
 	protected void checkValue(String txt) {
-		
-		if (txt==null)              return;
-		if ("".equals(txt.trim()))  return;
-		if ("-".equals(txt.trim())) return;
+
+		if (txt == null)
+			return;
+		if ("".equals(txt.trim()))
+			return;
+		if ("-".equals(txt.trim()))
+			return;
 
 		// If a simple double value then super can deal with it.
 		try {
 			final Pattern regExp = getRegExpression();
-			if (regExp.matcher(txt).matches() || regExp.matcher(txt+" "+unit).matches() ) {
+			if (regExp.matcher(txt).matches() || regExp.matcher(txt + " " + unit).matches()) {
 				super.checkValue(txt);
-                return;				
+				return;
 			}
-			
+
 		} catch (Throwable ignored) {
 			// We parse the value as if it was multiple
 		}
 		final String value = DOEUtils.removeUnit(txt, unit);
-		if (unit!=null) {
-			txt = value.trim()+" "+unit;
+		if (unit != null) {
+			txt = value.trim() + " " + unit;
 		}
 		if (!txt.equals(text.getText())) {
 			final int pos = text.getCaretOffset();
 			text.setText(txt);
 			text.setCaretOffset(pos);
 		}
-		
+
 		// We have stripped unit from value so do not need to match it.
-		boolean isList  = DOEUtils.isList(value, getDecimalPlaces(), null);
-		if (isRangeOnly()) isList = false;
+		boolean isList = DOEUtils.isList(value, getDecimalPlaces(), null);
+		if (isRangeOnly())
+			isList = false;
 		final boolean isRange = DOEUtils.isRange(value, getDecimalPlaces(), null);
-		if (!isList&&!isRange) {
-			error=true;
-			if (this.red==null) red = getDisplay().getSystemColor(SWT.COLOR_RED);
-			if (!red.isDisposed()) text.setForeground(red);
+		if (!isList && !isRange) {
+			error = true;
+			if (this.red == null)
+				red = getDisplay().getSystemColor(SWT.COLOR_RED);
+			if (!red.isDisposed())
+				text.setForeground(red);
 
 			if (isRangeOnly()) {
 				setTooltipOveride("A range should be entered in the form: <start>; <stop>; <increment>.\n");
-				
+
 			} else {
-				setTooltipOveride("A range should be entered in the form: <start>; <stop>; <increment>.\n\n"+
-				                  "A list should be a comma separerated list of numbers (two or more).");
+				setTooltipOveride("A range should be entered in the form: <start>; <stop>; <increment>.\n\n"
+						+ "A list should be a comma separerated list of numbers (two or more).");
 			}
-			
+
 		} else {
-			error=false;
-			
+			error = false;
+
 			// Check bounds values
 			if (!checkBounds(value)) {
 				return;
 			}
-			
+
 			setTooltipOveride(null);
 			if (isEditable()) {
-				if (black==null) black = getDisplay().getSystemColor(SWT.COLOR_BLACK);
-				if (!black.isDisposed()) text.setForeground(black);
+				if (black == null)
+					black = getDisplay().getSystemColor(SWT.COLOR_BLACK);
+				if (!black.isDisposed())
+					text.setForeground(black);
 			} else {
-				if (grey==null) grey = getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY);
-				if (!grey.isDisposed()) text.setForeground(grey);
+				if (grey == null)
+					grey = getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY);
+				if (!grey.isDisposed())
+					text.setForeground(grey);
 			}
 		}
 	}
-	
+
 	private boolean checkBounds(final String value) {
-		
-		error=false;
+
+		error = false;
 		final List<? extends Number> dbls = DOEUtils.expand(value);
 		for (Number val : dbls) {
 			if (!isValidBounds(val.doubleValue())) {
-				error=true;
-				if (this.red==null) red = getDisplay().getSystemColor(SWT.COLOR_RED);
-				if (!red.isDisposed()) text.setForeground(red);
+				error = true;
+				if (this.red == null)
+					red = getDisplay().getSystemColor(SWT.COLOR_RED);
+				if (!red.isDisposed())
+					text.setForeground(red);
 
-				setTooltipOveride("The range '"+value+"' would give the value '"+val+"' which is out of bounds.");
+				setTooltipOveride("The range '" + value + "' would give the value '" + val
+						+ "' which is out of bounds.");
 				return false;
 			}
 		}
@@ -217,31 +242,33 @@ public class RangeBox extends NumberBox implements IRangeWidget {
 
 	@Override
 	public void setValue(final Object value) {
-		if (value==null) {
+		if (value == null) {
 			this.text.setText("");
 			return;
 		}
 		checkValue(value.toString());
 	}
-	
+
 	@Override
 	public String getValue() {
-		
+
 		final String txt = text.getText();
-		if (txt==null)             return null;
-		if ("".equals(txt.trim())) return null;
-		
+		if (txt == null)
+			return null;
+		if ("".equals(txt.trim()))
+			return null;
+
 		return DOEUtils.removeUnit(txt, unit);
 	}
-	
+
 	/**
 	 * Expressions are not currently evaluated by RangeBoxes
 	 */
 	@Override
 	public void setExpressionManager(IExpressionManager man) {
-        this.expressionManager = null;
+		this.expressionManager = null;
 	}
-	
+
 	@Override
 	public boolean isExpressionAllowed() {
 		return false;
@@ -272,5 +299,3 @@ public class RangeBox extends NumberBox implements IRangeWidget {
 	}
 
 }
-
-	
