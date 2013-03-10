f13cd44dd45bf5cff3101be6fc6ee207a7798187
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2013-03-10 18:13:20 +0000

    add stop command and handler stubs

--- org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/regioneditor/RegionView.java
+++ org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/regioneditor/RegionView.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.command.SetCommand;
