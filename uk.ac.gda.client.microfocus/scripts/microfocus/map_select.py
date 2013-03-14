c92e262d20a3329b9fcdef79265f73e658ca332d
Chris Coles <chris.coles@diamond.ac.uk>
2013-03-14 19:23:31 +0000

    I18: Changes made on beamline since 8.26 release. Added edges M1-M5. Changes to map scan scripts

--- uk.ac.gda.client.microfocus/scripts/microfocus/map_select.py
+++ uk.ac.gda.client.microfocus/scripts/microfocus/map_select.py
@@ -3,10 +3,12 @@ from gda.data import PathConstructor
 
 class MapSelect():
     
-    def __init__(self, non_raster, raster):
+    def __init__(self, non_raster, raster, raster_return_write):
         self.non_raster=non_raster
         self.raster=raster
+        self.raster_return_write = raster_return_write
         self.scanBean=None
+        self.raster_mode = raster
         
     def __call__(self, sampleFileName, scanFileName, detectorFileName, outputFileName, folderName=None, scanNumber= -1, validation=True):
         
@@ -15,7 +17,7 @@ class MapSelect():
      
         self.scanBean = BeansFactory.getBeanObject(xmlFolderName, scanFileName)
         if(self.scanBean.isRaster()):
-            self.raster(sampleFileName, scanFileName, detectorFileName, outputFileName, folderName, scanNumber, validation)
+            self.raster_mode(sampleFileName, scanFileName, detectorFileName, outputFileName, folderName, scanNumber, validation)
             return
         
         self.non_raster(sampleFileName, scanFileName, detectorFileName, outputFileName, folderName, scanNumber, validation)
@@ -23,6 +25,12 @@ class MapSelect():
     def getMFD(self):
         if self.scanBean!=None:
             if(self.scanBean.isRaster()):
-                return self.raster.getMFD()
+                return self.raster_mode.getMFD()
             else:
                 return self.non_raster.getMFD()
+            
+    def enableFasterRaster(self):
+        self.raster_mode = self.raster_return_write
+        
+    def disableFasterRaster(self):
+        self.raster_mode = self.raster
