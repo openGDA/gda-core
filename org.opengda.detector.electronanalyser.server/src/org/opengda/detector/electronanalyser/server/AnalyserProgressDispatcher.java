4079d58ec7b3a186f2c0e21bcd3fdedb0eb7b3ea
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2013-03-22 09:49:17 +0000

    complete and add IObservable support

--- org.opengda.detector.electronanalyser.server/src/org/opengda/detector/electronanalyser/server/AnalyserProgressDispatcher.java
+++ org.opengda.detector.electronanalyser.server/src/org/opengda/detector/electronanalyser/server/AnalyserProgressDispatcher.java
@@ -1,30 +1,37 @@
 package org.opengda.detector.electronanalyser.server;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import gda.epics.connection.EpicsChannelManager;
 import gda.epics.connection.EpicsController;
 import gda.factory.Configurable;
 import gda.factory.FactoryException;
 import gda.factory.Findable;
-import gov.aps.jca.CAException;
+import gda.observable.IObservable;
+import gda.observable.IObserver;
+import gda.observable.ObservableComponent;
 import gov.aps.jca.dbr.DBR;
 import gov.aps.jca.dbr.DBR_Int;
 import gov.aps.jca.event.MonitorEvent;
 import gov.aps.jca.event.MonitorListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * notify the observer with progress value in percentage.
+ * @author fy65
+ *
+ */
 public class AnalyserProgressDispatcher implements MonitorListener,
-		Configurable, Findable {
-	private static final Logger logger=LoggerFactory.getLogger(AnalyserProgressDispatcher.class);
+		IObservable, Configurable, Findable {
+	private static final Logger logger = LoggerFactory
+			.getLogger(AnalyserProgressDispatcher.class);
 	private String name;
-	private VGScientaAnalyser analyser;
 	private EpicsController controller;
 	private EpicsChannelManager channelManager;
 	private String currentPointPv;
 	private int currentPoint;
 	private String totalPointsPv;
 	private int totalPoints;
+	private ObservableComponent observerComp = new ObservableComponent();;
 
 	public AnalyserProgressDispatcher() {
 		controller = EpicsController.getInstance();
@@ -44,16 +51,20 @@ public class AnalyserProgressDispatcher implements MonitorListener,
 	@Override
 	public void configure() throws FactoryException {
 		try {
-			controller.setMonitor(channelManager.createChannel(currentPointPv), this);
+			controller.setMonitor(channelManager.createChannel(currentPointPv),
+					this);
 		} catch (Exception e) {
-			logger.error("Cannot set up monitor of : "+ currentPointPv, e);
-			throw new FactoryException("Cannot set up monitor of "+ currentPointPv, e);
+			logger.error("Cannot set up monitor of : " + currentPointPv, e);
+			throw new FactoryException("Cannot set up monitor of "
+					+ currentPointPv, e);
 		}
 		try {
-			controller.setMonitor(channelManager.createChannel(totalPointsPv), new TotalPointsMonitor());
+			controller.setMonitor(channelManager.createChannel(totalPointsPv),
+					new TotalPointsMonitor());
 		} catch (Exception e) {
-			logger.error("Cannot set up monitor of : "+ totalPointsPv, e);
-			throw new FactoryException("Cannot set up monitor of "+ totalPointsPv, e);
+			logger.error("Cannot set up monitor of : " + totalPointsPv, e);
+			throw new FactoryException("Cannot set up monitor of "
+					+ totalPointsPv, e);
 		}
 	}
 
@@ -62,18 +73,34 @@ public class AnalyserProgressDispatcher implements MonitorListener,
 		DBR dbr=arg0.getDBR();
 		if (dbr.isINT()) {
 			currentPoint=((DBR_Int)dbr).getIntValue()[0];
+			observerComp.notifyIObservers(this, (currentPoint/totalPoints*100));
 		}
 	}
-	
+
 	private class TotalPointsMonitor implements MonitorListener {
 
 		@Override
 		public void monitorChanged(MonitorEvent arg0) {
-			DBR dbr=arg0.getDBR();
+			DBR dbr = arg0.getDBR();
 			if (dbr.isINT()) {
-				totalPoints=((DBR_Int)dbr).getIntValue()[0];
+				totalPoints = ((DBR_Int) dbr).getIntValue()[0];
 			}
 		}
+	}
+
+	@Override
+	public void addIObserver(IObserver observer) {
+		observerComp.addIObserver(observer);
+	}
+
+	@Override
+	public void deleteIObserver(IObserver observer) {
+		observerComp.deleteIObserver(observer);
+	}
+
+	@Override
+	public void deleteIObservers() {
+		observerComp.deleteIObservers();
 	}
 
 }
