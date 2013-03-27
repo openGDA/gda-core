99d59c85f2360d10e6a3ddbb96b0af73af4ae819
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-03-27 11:46:41 +0000

    PCOAutoContinuous - support setTriggermode

--- uk.ac.gda.epics/src/gda/device/detector/addetector/triggering/PCOAutoContinuous.java
+++ uk.ac.gda.epics/src/gda/device/detector/addetector/triggering/PCOAutoContinuous.java
@@ -39,7 +39,16 @@ public class PCOAutoContinuous extends SimpleAcquire {
 		this.adcMode = adcMode;
 	}
 
+	public int getTriggerMode() {
+		return triggerMode;
+	}
+
+	public void setTriggerMode(int triggerMode) {
+		this.triggerMode = triggerMode;
+	}
+
 	private final ADDriverPco adDriverPco;
+	private int triggerMode = PcoTriggerMode.AUTO.ordinal();
 
 	public PCOAutoContinuous(ADBase adBase, ADDriverPco adDriverPco) {
 		super(adBase, -0.1); // force acquireTime to be 0.
@@ -56,7 +65,7 @@ public class PCOAutoContinuous extends SimpleAcquire {
 
 		super.prepareForCollection(collectionTime, numImagesIgnored);
 		getAdBase().setImageMode(ImageMode.CONTINUOUS.ordinal());
-		getAdBase().setTriggerMode(PcoTriggerMode.AUTO.ordinal());
+		getAdBase().setTriggerMode(triggerMode);
 		adDriverPco.getAdcModePV().put(adcMode); // 2 adcs
 		adDriverPco.getTimeStampModePV().put(1); // BCD - if set to None then the image is blank. BCD means no timestamp
 													// on image
