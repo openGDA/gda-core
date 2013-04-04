533f596e36d4456146d11d73d0698bc68f93d9cf
Jacob Filik <jacob.filik@diamond.ac.uk>
2013-04-04 09:52:43 +0100

    Fixes to number box causing symptoms in DAWNSCI-580 (and more tests)

--- uk.ac.gda.common.rcp/test/uk/ac/gda/richbeans/components/scalebox/NumberBoxTest.java
+++ uk.ac.gda.common.rcp/test/uk/ac/gda/richbeans/components/scalebox/NumberBoxTest.java
@@ -29,8 +29,6 @@ import org.junit.Test;
 import swing2swt.layout.BorderLayout;
 
 public class NumberBoxTest {
-
-
 	
 	static private Display display;
 	static private Shell shell;
@@ -48,9 +46,43 @@ public class NumberBoxTest {
 		assertEquals("0.12345", expected,   box.getNumericValue(txt),1e-10);
 	}
 	
+	private void testBounds(int decimalPlaces, String newUnit, double expectedLow, double expectedHigh, String txtLow, String txtHigh){
+		ScaleBox boxLow = new ScaleBox(shell, SWT.NONE);
+		boxLow.setDecimalPlaces(decimalPlaces);
+		boxLow.setUnit(newUnit);
+		ScaleBox boxHigh = new ScaleBox(shell, SWT.NONE);
+		boxHigh.setDecimalPlaces(decimalPlaces);
+		boxHigh.setUnit(newUnit);
+		
+		boxLow.setMinimum(Double.NEGATIVE_INFINITY);
+		boxHigh.setMaximum(Double.POSITIVE_INFINITY);
+		
+		boxLow.setMaximum(boxHigh);
+		boxHigh.setMinimum(boxLow);
+		
+		boxLow.setValue(txtLow);
+		boxHigh.setValue(txtHigh);
+		
+		double min = boxHigh.getMinimum();
+		double max = boxLow.getMaximum();
+		
+		assertEquals("0.12345", expectedLow, min ,1e-10);
+		assertEquals("1234.5", expectedHigh, max ,1e-10);
+	}
+	
 	@Test
 	public void testDecimalPlaces0() {
 		testDecimalPlaces(0, null, 0., "0.12345");
+	}
+	
+	@Test
+	public void testInfinity() {
+		testDecimalPlaces(0, null, Double.POSITIVE_INFINITY, "Infinity");
+	}
+	
+	@Test
+	public void testNegativeInfinity() {
+		testDecimalPlaces(0, null, Double.NEGATIVE_INFINITY, "-Infinity");
 	}
 	
 	@Test
@@ -76,10 +108,40 @@ public class NumberBoxTest {
 	public void testDecimalPlaces5() {
 		testDecimalPlaces(5, null, 0.12345, "0.12345");
 	}
+	
+	@Test
+	public void test5Figures() {
+		testDecimalPlaces(5, null, 12345, "12345");
+	}
 
 	@Test
 	public void testDecimalPlaces0Units() {
 		testDecimalPlaces(0, "mm", 0., "0.12345 mm");
+	}
+	
+	@Test
+	public void test10Percent() {
+		testDecimalPlaces(5, "%", 10.000, "10.000 %");
+	}
+	
+	@Test
+	public void test10PercentSpaces() {
+		testDecimalPlaces(5, "%", 10.000, "10.000 %");
+	}
+	
+	@Test
+	public void testDecimalPlaces0UnitsNoSpace() {
+		testDecimalPlaces(0, "mm", 0., "0.12345mm");
+	}
+	
+	@Test
+	public void testDecimalPlaces5UnitsNoSpace() {
+		testDecimalPlaces(5, "mm", 0.12345, "0.12345mm");
+	}
+	
+	@Test
+	public void testDecimalPlaces0UnitsSomeSpace() {
+		testDecimalPlaces(5, "mm", 0.12345, "0.12345   mm");
 	}
 	
 	@Test
@@ -133,6 +195,51 @@ public class NumberBoxTest {
 	@Test
 	public void testDecimalPlaces5UnitsSpaceBeforeNoNumberBeforeDecPlace() {
 		testDecimalPlaces(5, "mm", .1234, " .1234 mm");
+	}
+	
+	@Test
+	public void testDecimalPlaces5UnitsPlusMinus() {
+		testDecimalPlaces(5, "mm", Double.NaN, "+-0.12345 mm");
+	}
+	
+	@Test
+	public void testDecimalPlaces5UnitsSpaceInNumber() {
+		testDecimalPlaces(5, "mm", Double.NaN, "0. 12345 mm");
+	}
+	
+	@Test
+	public void testDot() {
+		testDecimalPlaces(5, null , Double.NaN, ".");
+	}
+	
+	@Test
+	public void testDotAndUnits() {
+		testDecimalPlaces(5, null , Double.NaN, ". mm");
+	}
+	
+	@Test
+	public void testBounds() {
+		testBounds(5, "mm",0.12345,1234.5, "0.12345 mm", "1234.5 mm");
+	}
+	
+	@Test
+	public void testBoundsInfinite() {
+		testBounds(5, null,0.12345,Double.POSITIVE_INFINITY, "0.12345", "Infinity");
+	}
+	
+	@Test
+	public void testBoundsInfiniteUnits() {
+		testBounds(5, "mm" ,0.12345,Double.POSITIVE_INFINITY, "0.12345 mm", "Infinity mm");
+	}
+	
+	@Test
+	public void testBoundsNegativeInfinite() {
+		testBounds(5, null,Double.NEGATIVE_INFINITY, 0.12345,"-Infinity" , "0.12345");
+	}
+	
+	@Test
+	public void testBoundsNegativeInfiniteUnits() {
+		testBounds(5, "mm" ,Double.NEGATIVE_INFINITY, 0.12345,"-Infinity mm" , "0.12345 mm");
 	}
 
 }
