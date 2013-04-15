8a425b366cd88dfc5a4a71809cd3c6ca5ddddb92
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-04-15 12:21:11 +0100

    GDA-5509 Create Spring Factory Beans to aid creation of AreaDetector beans

---
+++ uk.ac.gda.epics/src/gda/spring/V17FactoryBeanBase.java
@@ -0,0 +1,53 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.spring;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Base FactoryBean to make the creation of an bean that implements an areaDetector plugin interface easier
+ */
+abstract public  class  V17FactoryBeanBase <T> implements FactoryBean<T>, InitializingBean, BeanNameAware{
+
+	protected String name;
+	protected T bean;
+
+	@Override
+	public void setBeanName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return bean.getClass();
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+	@Override
+	public T getObject() throws Exception {
+		return bean;
+	}
+
+}
---
+++ uk.ac.gda.epics/src/gda/spring/V17NDStatsFactoryBean.java
@@ -0,0 +1,39 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.spring;
+
+import gda.device.detector.areadetector.IPVProvider;
+import gda.device.detector.areadetector.v17.NDPluginBase;
+import gda.device.detector.areadetector.v17.NDStats;
+import gda.device.detector.areadetector.v17.impl.NDStatsImpl;
+/**
+ * FactoryBean to make the creation of an bean that implements NDStats easier
+ */
+public class V17NDStatsFactoryBean extends V17PluginFactoryBeanBase<NDStats>{
+
+	@Override
+	protected NDStats createObject(NDPluginBase pluginBase, IPVProvider pvProvider) throws Exception {
+		NDStatsImpl plugin = new NDStatsImpl();
+		plugin.setPluginBase(pluginBase);
+		plugin.setPvProvider(pvProvider);
+		plugin.afterPropertiesSet();
+		return plugin;
+	}
+
+}
