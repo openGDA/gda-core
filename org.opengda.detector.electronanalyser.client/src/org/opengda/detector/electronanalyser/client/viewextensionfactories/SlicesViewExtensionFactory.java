49c507b3342e70adbb07518e7c8e88fe9215de3b
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2013-04-16 08:55:09 +0100

    correct ID string

--- org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/viewextensionfactories/SlicesViewExtensionFactory.java
+++ org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/viewextensionfactories/SlicesViewExtensionFactory.java
@@ -3,6 +3,6 @@ package org.opengda.detector.electronanalyser.client.viewextensionfactories;
 import gda.rcp.views.ViewFactory;
 
 public class SlicesViewExtensionFactory extends ViewFactory {
-	public static final String ID = "org.opengda.detector.electronanalyser.client.views.spectrumview";
+	public static final String ID = "org.opengda.detector.electronanalyser.client.views.slicesview";
 
 }
