cf1d62bcd97bd7977b1e64dec5d65a0141b617ee
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2013-04-17 16:10:53 +0100

    sort out 4 different types of plot layouts

--- org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/SESPerspective.java
+++ org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/SESPerspective.java
@@ -29,10 +29,11 @@ public class SESPerspective implements IPerspectiveFactory {
 		layout.setFixed(false);
 
 		String editorArea = layout.getEditorArea();
-		layout.addView(SEQUENCEEDITOR, IPageLayout.LEFT, 0.75f, editorArea);
-		layout.addView(REGIONEDITOR, IPageLayout.RIGHT, 0.5f, editorArea);
-		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, (float)0.2, SEQUENCEEDITOR); //$NON-NLS-1$
+		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, (float)0.75, editorArea); //$NON-NLS-1$
         topLeft.addView(IPageLayout.ID_PROJECT_EXPLORER);
+        
+		layout.addView(SEQUENCEEDITOR, IPageLayout.RIGHT, 0.15f, "topLeft");
+        
 
 		IFolderLayout plotFolder = null;
 
@@ -40,34 +41,52 @@ public class SESPerspective implements IPerspectiveFactory {
 		if (plotLayoutString == null || plotLayoutString.isEmpty() 
 				|| ElectronAnalyserClientPlugin.STACK_PLOT.equals(plotLayoutString)) {
 			plotFolder = layout.createFolder("PlotFolder", IPageLayout.BOTTOM, 0.5f, SEQUENCEEDITOR);
+			layout.addView(REGIONEDITOR, IPageLayout.RIGHT, 0.7f, SEQUENCEEDITOR);
 
 			plotFolder.addView(SPECTRUMVIEW);
 			plotFolder.addView(EXTERNALIOVIEW);
 			plotFolder.addView(IMAGEVIEW);
 			plotFolder.addView(SLICEVIEW);
 
+			layout.addView(PROGRESSVIEW, IPageLayout.BOTTOM, 0.875f, "PlotFolder");
+			
 		} else if (ElectronAnalyserClientPlugin.TILE_QUAD.equals(plotLayoutString)) {
 			layout.addView(SPECTRUMVIEW, IPageLayout.BOTTOM, 0.5f, SEQUENCEEDITOR);
+			layout.addView(PROGRESSVIEW, IPageLayout.BOTTOM, 0.875f, SPECTRUMVIEW);
 			layout.addView(IMAGEVIEW, IPageLayout.RIGHT, 0.5f, SPECTRUMVIEW);
-			layout.addView(EXTERNALIOVIEW, IPageLayout.BOTTOM, 0.5f, SPECTRUMVIEW);
 			layout.addView(SLICEVIEW, IPageLayout.BOTTOM, 0.5f, IMAGEVIEW);
+			layout.addView(EXTERNALIOVIEW, IPageLayout.BOTTOM, 0.5f, SPECTRUMVIEW);
+			layout.addView(REGIONEDITOR, IPageLayout.RIGHT, 0.7f, SEQUENCEEDITOR);
 
 		} else if (ElectronAnalyserClientPlugin.TILE_VERTICAL.equals(plotLayoutString)) {
-			layout.addView(SPECTRUMVIEW, IPageLayout.BOTTOM, 0.5f, SEQUENCEEDITOR);
-			layout.addView(IMAGEVIEW, IPageLayout.BOTTOM, 0.25f, SPECTRUMVIEW);
-			layout.addView(EXTERNALIOVIEW, IPageLayout.BOTTOM, 0.33f, IMAGEVIEW);
-			layout.addView(SLICEVIEW, IPageLayout.BOTTOM, 0.5f, EXTERNALIOVIEW);
+			IFolderLayout topPlotfolder = layout.createFolder("topPlot", IPageLayout.BOTTOM, 0.5f, SEQUENCEEDITOR);
+			topPlotfolder.addView(SPECTRUMVIEW);
+			topPlotfolder.addView(IMAGEVIEW);
+			IFolderLayout bottomPlotFolder= layout.createFolder("bottomPlot", IPageLayout.BOTTOM, 0.45f, "topPlot");
+			bottomPlotFolder.addView(EXTERNALIOVIEW);
+			bottomPlotFolder.addView(SLICEVIEW);
+			layout.addView(PROGRESSVIEW, IPageLayout.BOTTOM, 0.8f, "bottomPlot");
+			layout.addView(REGIONEDITOR, IPageLayout.RIGHT, 0.7f, SEQUENCEEDITOR);
 			
 		} else if (ElectronAnalyserClientPlugin.TILE_HORIZONTAL.equals(plotLayoutString)) {
 			layout.addView(SPECTRUMVIEW, IPageLayout.BOTTOM, 0.5f, SEQUENCEEDITOR);
+			layout.addView(PROGRESSVIEW, IPageLayout.BOTTOM, 0.875f, SPECTRUMVIEW);
 			layout.addView(IMAGEVIEW, IPageLayout.RIGHT, 0.25f, SPECTRUMVIEW);
 			layout.addView(EXTERNALIOVIEW, IPageLayout.RIGHT, 0.33f, IMAGEVIEW);
 			layout.addView(SLICEVIEW, IPageLayout.RIGHT, 0.5f, EXTERNALIOVIEW);
+			layout.addView(REGIONEDITOR, IPageLayout.RIGHT, 0.7f, SEQUENCEEDITOR);
 		}
 
-		layout.addView(PROGRESSVIEW, IPageLayout.BOTTOM, 0.5f, REGIONEDITOR);
-		layout.addView(COMMANDQUEUEVIEW, IPageLayout.BOTTOM, 0.25f, REGIONEDITOR);
+		layout.addView(COMMANDQUEUEVIEW, IPageLayout.BOTTOM, 0.5f, "topLeft");
 		layout.setEditorAreaVisible(false);
+		
+		layout.addShowViewShortcut(SEQUENCEEDITOR);
+		layout.addShowViewShortcut(REGIONEDITOR);
+		layout.addShowViewShortcut(SPECTRUMVIEW);
+		layout.addShowViewShortcut(IMAGEVIEW);
+		layout.addShowViewShortcut(EXTERNALIOVIEW);
+		layout.addShowViewShortcut(SLICEVIEW);
+		layout.addShowViewShortcut(PROGRESSVIEW);
 	}
 
 }
