6229739c428eae3a0c101498f5a23f4cca2fd614
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2013-04-17 16:11:38 +0100

    make 'Open' context menu to open sequence file in sequence Editor View.

--- org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/actions/OpenSequenceActionProvider.java
+++ org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/actions/OpenSequenceActionProvider.java
@@ -1,21 +1,13 @@
 package org.opengda.detector.electronanalyser.client.actions;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.actions.OpenSystemEditorAction;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.eclipse.ui.navigator.ICommonMenuConstants;
-import org.opengda.detector.electronanalyser.client.sequenceeditor.IRegionDefinitionView;
-import org.opengda.detector.electronanalyser.client.viewextensionfactories.SequenceViewExtensionFactory;
+import org.eclipse.ui.navigator.ICommonViewerSite;
+import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 
 public class OpenSequenceActionProvider extends CommonActionProvider {
 
@@ -26,57 +18,26 @@ public class OpenSequenceActionProvider extends CommonActionProvider {
 	@Override
 	public void init(ICommonActionExtensionSite aSite) {
 		super.init(aSite);
-		openAction = new OpenAction(PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow().getActivePage());
+		ICommonViewerSite viewSite=aSite.getViewSite();
+		if (viewSite instanceof ICommonViewerWorkbenchSite) {
+			ICommonViewerWorkbenchSite workbenchSite=(ICommonViewerWorkbenchSite)viewSite;
+			openAction = new OpenAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
+		}
 	}
 
 	@Override
 	public void fillActionBars(IActionBars actionBars) {
 		super.fillActionBars(actionBars);
-		actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN,
-				openAction);
+		if (openAction.isEnabled()) {
+			actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN,openAction);
+		}
 	}
 
 	@Override
 	public void fillContextMenu(IMenuManager menu) {
 		super.fillContextMenu(menu);
-		menu.appendToGroup(ICommonMenuConstants.GROUP_OPEN, openAction);
-	}
-
-	public class OpenAction extends OpenSystemEditorAction {
-		private IWorkbenchPage page;
-
-		public OpenAction(IWorkbenchPage page) {
-			super(page);
-			this.page = page;
-		}
-
-		@Override
-		public void run() {
-			String seqFileName = null;
-			ISelection selection2 = PlatformUI.getWorkbench()
-					.getActiveWorkbenchWindow().getActivePage().getSelection();
-			if (selection2 instanceof IStructuredSelection) {
-				IStructuredSelection structSel = (IStructuredSelection) selection2;
-				if (structSel.getFirstElement() instanceof IFile) {
-					IFile seqFile = (IFile) structSel.getFirstElement();
-					seqFileName = seqFile.getLocation().toString();
-				}
-			}
-			IViewPart seqViewPart = null;
-			try {
-				seqViewPart = page.showView(SequenceViewExtensionFactory.ID);
-			} catch (PartInitException e) {
-				e.printStackTrace();
-			}
-			if (seqViewPart != null && seqFileName != null) {
-				if (seqViewPart instanceof IRegionDefinitionView) {
-					IRegionDefinitionView regionDefinitionView = (IRegionDefinitionView) seqViewPart;
-					regionDefinitionView.refreshTable(seqFileName, false);
-
-				}
-			}
+		if (openAction.isEnabled()) {
+			menu.appendToGroup(ICommonMenuConstants.GROUP_OPEN, openAction);
 		}
 	}
-
 }
