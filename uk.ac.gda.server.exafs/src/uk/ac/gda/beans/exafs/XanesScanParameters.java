c25d00c465a43786f24ef9fbaf3a57758278a8a4
Chris Coles <chris.coles@diamond.ac.uk>
2013-04-23 03:31:57 +0100

    XAS: added getter for final energy

--- uk.ac.gda.server.exafs/src/uk/ac/gda/beans/exafs/XanesScanParameters.java
+++ uk.ac.gda.server.exafs/src/uk/ac/gda/beans/exafs/XanesScanParameters.java
@@ -43,7 +43,11 @@ public class XanesScanParameters implements Serializable, IScanParameters {
 	public Double getFinalEnergy() {
 		return finalEnergy;
 	}
-
+	
+	public Double getInitialEnergy(){
+		return regions.get(0).getEnergy();
+	}
+	
 	/**
 	 * @param stopEnergy
 	 *            the stopEnergy to set
