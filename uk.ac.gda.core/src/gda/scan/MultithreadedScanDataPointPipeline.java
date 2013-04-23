4461744001c55d4e9e169c1266116dce3d0da80d
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-04-23 15:08:33 +0100

    make sure the shutdown thread calling to datawriters isn't interrupted
    SCATTER-128 may lead or relate to GDA-5479 MXGDA-985

--- uk.ac.gda.core/src/gda/scan/MultithreadedScanDataPointPipeline.java
+++ uk.ac.gda.core/src/gda/scan/MultithreadedScanDataPointPipeline.java
@@ -208,6 +208,13 @@ public class MultithreadedScanDataPointPipeline implements ScanDataPointPipeline
 		}
 		List<Runnable> remainingPointsInQueue = broadcasterQueue.shutdownNow();
 		try {
+			/**
+			 * This call clears the interrupted flag.
+			 * This is important so that the normal completeCollection() and 
+			 * other ordinary cleanup code can run and do file and terminal 
+			 * I/O which would fail (by design) in interrupted state. 
+			 */
+			boolean interrupted = Thread.interrupted();
 			getBroadcaster().shutdown();
 		} catch (Exception e) {
 
