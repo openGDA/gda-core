6d0c7b5c2f91924437a0115886cf5596c0a32b21
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2013-05-14 10:11:53 +0100

    add new event type for Region completion

---
+++ org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/selection/RegionRunCompletedSelection.java
@@ -0,0 +1,12 @@
+package org.opengda.detector.electronanalyser.client.selection;
+
+import org.eclipse.jface.viewers.ISelection;
+
+public class RegionRunCompletedSelection implements ISelection {
+
+	@Override
+	public boolean isEmpty() {
+		return true;
+	}
+
+}
