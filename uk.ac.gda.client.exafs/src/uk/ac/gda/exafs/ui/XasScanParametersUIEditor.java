f30b23b45c707b5c40d437aff93c29a3e308b696
chris coles <chris.coles@diamond.ac.uk>
2013-05-14 10:42:19 +0100

    XAS: Removed unused imports

--- uk.ac.gda.client.exafs/src/uk/ac/gda/exafs/ui/XasScanParametersUIEditor.java
+++ uk.ac.gda.client.exafs/src/uk/ac/gda/exafs/ui/XasScanParametersUIEditor.java
@@ -45,7 +45,6 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
