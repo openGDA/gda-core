a9d52905c56dd4b2d77960c5cce3b16fedf10e73
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-05-22 09:38:05 +0100

    GDA - Add getTitle to metadata_commands script

--- uk.ac.gda.core/scripts/gdascripts/metadata/metadata_commands.py
+++ uk.ac.gda.core/scripts/gdascripts/metadata/metadata_commands.py
@@ -4,3 +4,9 @@ def setTitle(title):
     Command to set the title that is recorded in the scan file
     """
     Finder.getInstance().find("GDAMetadata").setMetadataValue("title", title)
+
+def getTitle():
+    """
+    Command to get the title that is recorded in the scan file
+    """
+    return Finder.getInstance().find("GDAMetadata").getMetadataValue("title")
