2b85db08fbc374789c7472cd8475e46d75f7cd1c
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-06-05 11:28:45 +0100

    GDA - New Scan Interface to indicate scan is contiguous to user

---
+++ uk.ac.gda.core/src/gda/scan/ContiguousScan.java
@@ -0,0 +1,26 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.scan;
+
+/*
+ * Interface for a scan that looks to the user as a single scan but in implemented as a series of individual scans run one after the other
+ */
+public interface ContiguousScan {
+	int getNumberOfContiguousPoints();
+}
