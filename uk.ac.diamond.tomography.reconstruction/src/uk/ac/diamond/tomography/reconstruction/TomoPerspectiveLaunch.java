ff1ac5e0fee3e751b5d7f213955ba7c79afd487f
Mark Basham <mark.basham@diamond.ac.uk>
2013-06-06 16:13:52 +0100

    Added an intro item of the tomography GUI

---
+++ uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/TomoPerspectiveLaunch.java
@@ -0,0 +1,61 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.tomography.reconstruction;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+
+public class TomoPerspectiveLaunch implements IWorkbenchWindowActionDelegate {
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(IAction action) {
+		try {
+			PlatformUI.getWorkbench().showPerspective(
+					"uk.ac.diamond.tomography.reconstruction.perspective",
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+		} catch (WorkbenchException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
