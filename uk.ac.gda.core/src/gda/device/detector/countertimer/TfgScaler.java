5e4fc1cf49774d461921b21889438f0905c5d4c4
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2013-06-21 15:30:26 +0100

    ensure framesets cleared if scan aborts / fails

--- uk.ac.gda.core/src/gda/device/detector/countertimer/TfgScaler.java
+++ uk.ac.gda.core/src/gda/device/detector/countertimer/TfgScaler.java
@@ -211,9 +211,11 @@ public class TfgScaler extends TFGCounterTimer implements CounterTimer {
 
 	@Override
 	public void atCommandFailure() throws DeviceException {
-		if (frameSets.size() > 0) {
-			clearFrameSets();
-		}
+//		if (frameSets.size() > 0) {
+//			clearFrameSets();
+//		}
+		atScanEnd();
+		clearFrameSets();
 	}
 
 	@Override
