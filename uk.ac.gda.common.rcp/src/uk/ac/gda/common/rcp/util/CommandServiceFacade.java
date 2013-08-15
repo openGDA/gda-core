d1c65e9ecde16411fa8c9a384155cbab5db91adb
Paul Hathaway <paul.hathaway@diamond.ac.uk>
2013-08-15 14:43:09 +0100

    COMMON Add static convenience methods to CommandServiceFacade

--- uk.ac.gda.common.rcp/src/uk/ac/gda/common/rcp/util/CommandServiceFacade.java
+++ uk.ac.gda.common.rcp/src/uk/ac/gda/common/rcp/util/CommandServiceFacade.java
@@ -28,13 +28,18 @@ import org.eclipse.core.commands.NotEnabledException;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
 
 /**
  * A class for interacting with the ICommandService. This facade does not have to be
  * used but simply wraps up common code to stop it being written again.
  */
 public class CommandServiceFacade {
+
+	protected static IHandlerService hndService = null;
+	protected static ICommandService cmdService = null;
 
 	/**
 	 * Run a preconfigured command.
@@ -59,6 +64,52 @@ public class CommandServiceFacade {
         }
 			
 		return null;
+	}
+
+	public static void fireCommand(final Command command) {
+		try {
+			getHandlerService();
+			ExecutionEvent execEvent = hndService.createExecutionEvent(command,null);
+			command.executeWithChecks(execEvent);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (NotDefinedException e) {
+			e.printStackTrace();
+		} catch (NotEnabledException e) {
+			e.printStackTrace();
+		} catch (NotHandledException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void fireCommand(String commandId) {
+		getCommandService();
+		if(null!=cmdService) {
+			Command command = cmdService.getCommand(commandId);	
+			fireCommand(command);
+		}
+	}	
+	
+	public static ICommandService getCommandService() {
+		if(null==cmdService) {
+			initCommandService();
+		}
+		return cmdService;
+	}
+
+	public static IHandlerService getHandlerService() {
+		if(null==hndService) {
+			initCommandService();
+		}
+		return hndService;
+	}
+	
+	protected static boolean initCommandService() {
+		if(null==cmdService) {
+			hndService = (IHandlerService) PlatformUI.getWorkbench().getService(IHandlerService.class);
+			cmdService = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
+		}
+		return null!=cmdService;
 	}
 
 }
