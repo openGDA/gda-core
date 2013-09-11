9b71cfe195e562993c75c15b90ae6b93e40a75dc
Richard Fearn <richard.fearn@diamond.ac.uk>
2013-09-11 11:29:04 +0100

    MXGDA-1357 Add isEmpty method to HighestExitingFileMonitorSettings to indicate whether the fileTemplatePrefix/fileTemplate are empty
    LatestFileNameCompositeFactory treats these fields being empty as a special
    case

--- uk.ac.gda.api/src/gda/device/detectorfilemonitor/HighestExitingFileMonitorSettings.java
+++ uk.ac.gda.api/src/gda/device/detectorfilemonitor/HighestExitingFileMonitorSettings.java
@@ -58,6 +58,10 @@ public class HighestExitingFileMonitorSettings implements Serializable{
 		return templateInUse;
 	}
 
+	public boolean isEmpty() {
+		return fileTemplatePrefix.isEmpty() && fileTemplate.isEmpty();
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
