72ee1e6a01bb6162474afb8de332f5b263fe86d8
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-10-14 16:35:29 +0100

    add delegation class for smooth config upgrade

---
+++ uk.ac.gda.server.ncd.epics/src/uk/ac/gda/server/ncd/subdetector/ScalingAndOffsetParameters.java
@@ -0,0 +1,21 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.server.ncd.subdetector;
+
+public class ScalingAndOffsetParameters extends gda.device.currentamplifier.ScalingAndOffsetParameters { }
