0adedcca3d2ba082c4d9dccb76512f00ba028713
Peter Holloway <peter.holloway@diamond.ac.uk>
2013-12-18 12:42:14 +0000

    Added title entry box to NcdButtonPanelView
    Added title entry box and updater to reflect changes made from console.
    Minor required changes to ncdsim_beans_server.xml

---
+++ uk.ac.gda.client.ncd/src/gda/rcp/ncd/views/NcdButtonPanelUpdater.java
@@ -0,0 +1,287 @@
+///*-
+// * Copyright © 2009 Diamond Light Source Ltd.
+// *
+// * This file is part of GDA.
+// *
+// * GDA is free software: you can redistribute it and/or modify it under the
+// * terms of the GNU General Public License version 3 as published by the Free
+// * Software Foundation.
+// *
+// * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+// * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// * details.
+// *
+// * You should have received a copy of the GNU General Public License along
+// * with GDA. If not, see <http://www.gnu.org/licenses/>.
+// */
+//
+//package gda.rcp.ncd.views;
+//
+//import gda.data.PathConstructor;
+//import gda.data.metadata.GDAMetadataProvider;
+//import gda.data.metadata.Metadata;
+//import gda.device.Device;
+//import gda.device.TimerStatus;
+//import gda.factory.Finder;
+//import gda.jython.IAllScanDataPointsObserver;
+//import gda.jython.InterfaceProvider;
+//import gda.observable.IObservable;
+//import gda.observable.IObserver;
+//import gda.rcp.ncd.ExptDataModel;
+//import gda.rcp.ncd.NcdController;
+//import gda.scan.ScanDataPoint;
+//
+//import java.text.NumberFormat;
+//import java.util.Collection;
+//
+//import org.eclipse.swt.SWT;
+//import org.eclipse.swt.events.KeyEvent;
+//import org.eclipse.swt.widgets.Display;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import uk.ac.gda.server.ncd.detectorsystem.NcdDetectorSystem;
+//import uk.ac.gda.server.ncd.plotting.DetectorRates;
+//
+///**
+// * A system status display panel
+// */
+//public class NcdButtonPanelUpdater implements IObserver, IAllScanDataPointsObserver {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(NcdStatusUpdater.class);
+//
+//	private IObservable tfg;
+//	private ExptDataModel exptDataModel;
+//	private NcdButtonPanelView client;
+//	private String title;
+//	private Metadata metadata;
+//	private Device blaster;
+//
+//	/**
+//	 * Constructor
+//	 * 
+//	 * @param client
+//	 *            who to update
+//	 */
+//	public NcdButtonPanelUpdater(final NcdButtonPanelView client) {
+//		this.client = client;
+//
+//		exptDataModel = ExptDataModel.getInstance();
+//		if (exptDataModel != null) {
+//			exptDataModel.addIObserver(this);
+//		}
+//		tfg = NcdController.getInstance().getNcdDetectorSystem();
+//		if (tfg != null) {
+//			tfg.addIObserver(this);
+//		}
+//
+//		try {
+//			metadata = GDAMetadataProvider.getInstance();
+//			title = metadata.getMetadataValue("title");
+//
+//			blaster = Finder.getInstance().find("observableScanTitle");
+//			blaster.addIObserver(this);
+//
+//			meUpdate();
+//		} catch (Exception e) {
+//			logger.warn("could not find subdirectory metadata", e);
+//		}
+//
+//		InterfaceProvider.getScanDataPointProvider().addIScanDataPointObserver(this);
+//
+////		client.subDirectory.addKeyListener(new org.eclipse.swt.events.KeyAdapter() {
+////			@Override
+////			public void keyReleased(KeyEvent e) {
+////				super.keyReleased(e);
+////				if (e.character == SWT.CR) {
+////
+////					try {
+////						metadata.setMetadataValue("subdirectory", client.subDirectory.getText().trim());
+////					} catch (Exception e1) {
+////						client.subDirectory.setText("");
+////					}
+////				}
+////			}
+////		});
+//	}
+//
+//	@Override
+//	public void update(Object iObservable, Object arg) {
+//		if (client.titleEntry.isDisposed()) {
+//			tfg.deleteIObserver(this);
+//			blaster.deleteIObserver(this);
+//			exptDataModel.deleteIObserver(this);
+//			InterfaceProvider.getScanDataPointProvider().deleteIScanDataPointObserver(this);
+//			return;
+//		}
+//		Display.getDefault().asyncExec(new Updater(iObservable, arg));
+//	}
+//
+//	private void meUpdate() {
+//		client.titleEntry.setText(title);
+//		client.currentDirectory.setText(PathConstructor.createFromDefaultProperty());
+//		exptDataModel.setDirectory(client.currentDirectory.getText());
+//	}
+//
+//	private class Updater implements Runnable {
+//		private Object iObservable;
+//
+//		private Object arg;
+//
+//		/**
+//		 * @param iObservable
+//		 * @param arg
+//		 */
+//		public Updater(Object iObservable, Object arg) {
+//			this.iObservable = iObservable;
+//			this.arg = arg;
+//		}
+//
+//		@Override
+//		public void run() {
+//			if (arg != null) {
+//				if (arg instanceof TimerStatus) {
+//					TimerStatus ts = (TimerStatus) arg;
+//
+//					long diff = ts.getElapsedTime();
+//					NumberFormat nf = NumberFormat.getInstance();
+//					nf.setMinimumIntegerDigits(2);
+//					long hour = diff / 3600000;
+//					diff -= hour * 3600000;
+//					long min = diff / 60000;
+//					diff -= min * 60000;
+//					long sec = diff / 1000;
+//
+//					String text;
+//					text = nf.format(hour) + ":" + nf.format(min) + ":" + nf.format(sec);
+//					client.elapsedTime.setText(text);
+//
+//					client.frameStatus.setText(ts.getCurrentStatus());
+//					text = "" + ts.getCurrentFrame();
+//					client.frameNumber.setText(text);
+//					text = "" + ts.getCurrentCycle();
+//					client.cycleNumber.setText(text);
+//					text = "" + ts.getTotalCycles();
+//					client.totalCycleCount.setText(text);
+//
+//					client.progressBar.setSelection(ts.getPercentComplete());
+//				} else if (iObservable instanceof Device) {
+//					if (arg instanceof Collection<?>) {
+//						for (Object odr : (Collection<?>) arg) {
+//							if (odr instanceof DetectorRates) {
+//								DetectorRates dr = (DetectorRates) odr;
+//								if (NcdDetectorSystem.WAXS_DETECTOR.equalsIgnoreCase(dr.detType)) {
+//									client.waxsCountRate.setText(String.format("%,6.5G cps", dr.integratedCounts / dr.countingTime));
+//									client.waxsCount.setText(String.format("%,6.6G counts", dr.integratedCounts));
+//									client.waxsPeakRate.setText(String.format("%,6.5G cps", dr.maxCounts / dr.countingTime));
+//									client.waxsPeak.setText(String.format("%,6.6G counts", dr.maxCounts));
+//								} else if (NcdDetectorSystem.SAXS_DETECTOR.equalsIgnoreCase(dr.detType)) {
+//									client.saxsCountRate.setText(String.format("%,6.5G cps", dr.integratedCounts / dr.countingTime));
+//									client.saxsCount.setText(String.format("%,6.6G counts", dr.integratedCounts));
+//									client.saxsPeakRate.setText(String.format("%,6.5G cps", dr.maxCounts / dr.countingTime));
+//									client.saxsPeak.setText(String.format("%,6.6G counts", dr.maxCounts));
+//								}
+//							}
+//						}
+//					} else if (arg instanceof String) {
+//						subdirectory = arg.toString();
+//						meUpdate();
+//					}
+//				} else if (arg instanceof ScanDataPoint) {
+//					String filename = ((ScanDataPoint) arg).getCurrentFilename();
+//					client.scanFile.setText(filename);
+//				}
+//			} else if (iObservable instanceof ExptDataModel) {
+//				client.parameterFile.setText(exptDataModel.getFileName());
+//				client.currentDirectory.setText(exptDataModel.getDirectory());
+//			}
+//		}
+//	}
+//}
+
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.rcp.ncd.views;
+
+import org.eclipse.swt.widgets.Display;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gda.data.metadata.GDAMetadataProvider;
+import gda.data.metadata.Metadata;
+import gda.device.Device;
+import gda.device.DeviceException;
+import gda.factory.Finder;
+import gda.observable.IObserver;
+
+//Keep title as stored in metadata in sync with title input in NcdButtonPanel
+public class NcdButtonPanelUpdater implements IObserver {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NcdStatusUpdater.class);
+	private NcdButtonPanelView client;
+	private String title;
+	private Metadata metadata;
+	private Device blaster;
+	
+	public NcdButtonPanelUpdater(final NcdButtonPanelView client) {
+		this.client = client;
+		
+		try {
+			metadata = GDAMetadataProvider.getInstance();
+			title = metadata.getMetadataValue("title");
+			blaster = Finder.getInstance().find("observableScanTitle");
+			blaster.addIObserver(this);
+			update(new Object(), new Object());
+		} catch (DeviceException e) {
+			logger.error("Could not read metadata (scan title)", e);
+		}
+	}
+	
+	@Override
+	public void update(Object source, Object arg) {
+		update();
+	}
+		
+	public void update() {
+		if (client.titleEntry.isDisposed()) {
+			blaster.deleteIObserver(this);
+			return;
+		}
+		
+		Display.getDefault().asyncExec(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					title = metadata.getMetadataValue("title");
+					if (title.equals("") && client.hasTitleString()) {
+						metadata.setMetadataValue("title", client.getTitleString());
+						client.setTitleString("");
+					} else {
+						client.titleEntry.setText(title);
+					}
+				} catch (DeviceException e) {
+					logger.error("Could not access title metadata", e);
+				}
+			}
+		});
+		
+	}
+
+}
--- uk.ac.gda.client.ncd/src/gda/rcp/ncd/views/NcdButtonPanelView.java
+++ uk.ac.gda.client.ncd/src/gda/rcp/ncd/views/NcdButtonPanelView.java
@@ -37,17 +37,47 @@ import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.ViewPart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.swtdesigner.SWTResourceManager;
 
 import uk.ac.gda.util.ThreadManager;
 
 public class NcdButtonPanelView extends ViewPart {
+
+	private static final Logger logger = LoggerFactory.getLogger(NcdButtonPanelView.class);
+
+	protected Combo titleEntry;
+//	protected Text titleEntry;
+	private String titleString;
+	private Button startButton;
+	private Label validationMessage;
+	private Button retainTitle;
+	private String[] titleList;
+
+	public NcdButtonPanelView() {
+		this.titleString = "";
+		titleList = new String[] {};
+	}
 
 	/**
 	 * This class validates the title to ensure no illegal characters are in there (for xml).
@@ -73,20 +103,39 @@ public class NcdButtonPanelView extends ViewPart {
 		}
 	}
 
+	//run data acquisition with configured frameset
 	private SelectionListener startListener = new SelectionListener() {
 		@Override
 		public void widgetSelected(SelectionEvent e) {
-			try {
-				String text = GDAMetadataProvider.getInstance(true).getMetadataValue("title");
-				InputDialog dlg = new InputDialog(Display.getCurrent().getActiveShell(), "",
-						"Enter scan title (e.g. sample information)", text, new TitleValidator());
-				if (dlg.open() == Window.OK) {
-					// User clicked OK; update the label with the input
-					GDAMetadataProvider.getInstance().setMetadataValue("title", dlg.getValue().trim());
-				} else {
-					// cancel
-					return;
+			if (retainTitle.getSelection()) {
+				try {
+					titleString = GDAMetadataProvider.getInstance().getMetadataValue("title");
+				} catch (DeviceException e1) {
+					logger.error("Could not read metadata", e1);
 				}
+			} else {
+				titleString = "";
+			}
+
+
+			try {
+				//only show dialog if empty title
+				if (titleEntry.getText().trim().equals("")) {
+					InputDialog dlg = new InputDialog(Display.getCurrent().getActiveShell(), "",
+							"Enter scan title (e.g. sample information)", "", new TitleValidator());
+					if (dlg.open() == Window.OK) {
+						// User clicked OK; update the label with the input
+						GDAMetadataProvider.getInstance().setMetadataValue("title", dlg.getValue().trim());
+					} else {
+						// cancel
+						//if cancel, delete whitespace
+						titleEntry.setText("");
+						return;
+					}
+				}
+			
+				updateList(titleEntry.getText());
+				
 				//FIXME this need to capture the exceptions, but interface does not allow that
 				JythonServerFacade.getInstance().runCommand(
 						"gda.scan.StaticScan([" + NcdController.getInstance().getNcdDetectorSystem().getName()
@@ -100,13 +149,33 @@ public class NcdButtonPanelView extends ViewPart {
 				ErrorDialog.openError(Display.getCurrent().getActiveShell(), "DeviceException", "Error Starting Scan",
 						status);
 			}
+			
+			retainTitle.setSelection(false);
 		}
-
 		@Override
 		public void widgetDefaultSelected(SelectionEvent e) {
 		}
+	};	
+
+	//Update title metadata when focus is lost
+	private FocusListener titleUpdateListener = new FocusListener() {
+		@Override
+		public void focusGained(FocusEvent e) {
+		}
+
+		@Override
+		public void focusLost(FocusEvent e) {
+			try {
+				GDAMetadataProvider.getInstance(true).setMetadataValue("title", titleEntry.getText().trim());
+			} catch (DeviceException e1) {
+				logger.error("Could not set scan title from NcdButtonPanel", e1);
+			}
+		}
+			
+		
 	};
 
+	//Stop running script
 	private SelectionListener stopListener = new SelectionListener() {
 		@Override
 		public void widgetSelected(SelectionEvent e) {
@@ -140,36 +209,99 @@ public class NcdButtonPanelView extends ViewPart {
 		}
 	};
 
-	public NcdButtonPanelView() {
-	}
+	//Validate title input for xml
+	private ModifyListener titleValidateListener = new ModifyListener() {
+		@Override
+		public void modifyText(ModifyEvent e) {
+			validateTitle();
+		}
+	};
 
 	@SuppressWarnings("unused")
 	@Override
 	public void createPartControl(Composite parent) {
-		RowLayout rl_parent = new RowLayout(SWT.HORIZONTAL);
-		rl_parent.spacing = 5;
-		rl_parent.marginTop = 5;
-		rl_parent.marginRight = 5;
-		rl_parent.marginLeft = 5;
-		rl_parent.marginBottom = 5;
-		rl_parent.pack = false;
-		rl_parent.fill = true;
-		rl_parent.center = true;
-		parent.setLayout(rl_parent);
+		GridLayout gl_parent = new GridLayout(4, false);
+		gl_parent.verticalSpacing = 12;
+		GridData gridData = new GridData(SWT.LEFT, SWT.CENTER, true, false, 1, 1);
+		parent.setLayoutData(gridData);
+		parent.setLayout(gl_parent);
 
-		Button startButton = new Button(parent, SWT.NONE);
-		startButton.setText("Start");
-		startButton.setToolTipText("Run a data acquisition using the configured frameset");
-		startButton.addSelectionListener(startListener);
-
-		Button stopButton = new Button(parent, SWT.NONE);
-		stopButton.setText("Stop");
-		stopButton.setToolTipText("Stop detectors");
-		stopButton.addSelectionListener(stopListener);
-
+		{
+			startButton = new Button(parent, SWT.NONE);
+			startButton.setText("Start");
+			startButton.setToolTipText("Run a data acquisition using the configured frameset");
+			startButton.addSelectionListener(startListener);
+		}
+		{
+			Button stopButton = new Button(parent, SWT.NONE);
+			stopButton.setText("Stop");
+			stopButton.setToolTipText("Stop detectors");
+			stopButton.addSelectionListener(stopListener);
+		}
+		{
+			Group titleGroup = new Group(parent, SWT.BORDER);
+			GridData gd_titleGroup = new GridData(SWT.FILL, SWT.CENTER, true, false, 1,1);
+			titleGroup.setLayoutData(gd_titleGroup);
+			GridLayout gl_titleGroup = new GridLayout(2, false);
+			titleGroup.setLayout(gl_titleGroup);
+			titleGroup.setText("Title");
+			{
+				titleEntry = new Combo(titleGroup, SWT.NONE);
+				GridData gd_titleEntry = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+				titleEntry.setLayoutData(gd_titleEntry);
+				titleEntry.setItems(titleList);
+				titleEntry.setVisibleItemCount(6);
+				titleEntry.setToolTipText("Enter title or choose recent");
+				
+//				titleEntry = new Text(titleGroup, SWT.BORDER);
+//				GridData gd_titleEntry = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+//				titleEntry.setToolTipText("Title for the scan");
+//				titleEntry.setLayoutData(gd_titleEntry);
+//				titleEntry.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+			}
+			{
+				retainTitle = new Button(titleGroup, SWT.CHECK);
+				GridData gd_titleCheck = new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1);
+				retainTitle.setLayoutData(gd_titleCheck);
+				retainTitle.setToolTipText("Do not clear title after scan");
+				retainTitle.setText("Keep?");
+			}
+		}
+		
 		ArrayList<Findable> shutters = Finder.getInstance().listAllObjects("EnumPositioner");
 		for (Findable shutter : shutters) {
 			new ShutterGroup(parent, SWT.NONE, (EnumPositioner) shutter);
+		}
+		
+		try {
+			titleString = GDAMetadataProvider.getInstance(true).getMetadataValue("title");
+			titleEntry.setText(titleString);
+		} catch (DeviceException e) {
+			logger.error("Could not read title metadata", e);
+		}
+		{
+			titleEntry.addModifyListener(titleValidateListener );
+			titleEntry.addFocusListener(titleUpdateListener);
+		}
+		{
+			validationMessage = new Label(parent, SWT.NONE);
+			GridData gd_validationLabel = new GridData(SWT.FILL, SWT.RIGHT, false, false, 4, 1);
+			validationMessage.setLayoutData(gd_validationLabel);
+			validationMessage.setVisible(false);
+			validationMessage.setText("Title cannot contain < > or \\");
+		}
+		
+		new NcdButtonPanelUpdater(this);
+	}
+
+	protected void updateList(String text) {
+		int selectionIndex = titleEntry.indexOf(text);
+		if (selectionIndex != -1) {
+			titleEntry.remove(selectionIndex);
+		} 
+		titleEntry.add(text,0);
+		while (titleEntry.getItemCount() > 6) {
+			titleEntry.remove(6);
 		}
 	}
 
@@ -179,4 +311,38 @@ public class NcdButtonPanelView extends ViewPart {
 	@Override
 	public void setFocus() {
 	}
+
+	private void validateTitle() {
+		int valid = 1;
+		for (CharSequence ch : new CharSequence[] { ">", "<", "\\" }) {
+			if (titleEntry.getText().contains(ch)) {
+				valid = 0;
+			} 
+		}
+		if (valid != 1) {
+			validationMessage.setVisible(true);
+//			startButton.setEnabled(false);
+			startButton.setToolTipText("Title should not contain < > or \\");
+		} else {
+			validationMessage.setVisible(false);
+//			startButton.setEnabled(true);
+			startButton.setToolTipText("Run a data acquisition using the configured frameset");
+		}
+	}
+
+	public String getTitleString() {
+		return titleString;
+	}
+	
+	public void setTitleString(String title) {
+		this.titleString = title;
+	}
+
+	public boolean hasTitleString() {
+		if (titleString.equals("")) {
+			return false;
+		}
+		return true;
+	}
+	
 }
