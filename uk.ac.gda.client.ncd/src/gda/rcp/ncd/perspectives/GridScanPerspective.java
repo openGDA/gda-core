39a1c0a986c0de0a3dce43e803898f1355b64c60
Peter Holloway <peter.holloway@diamond.ac.uk>
2014-01-15 13:31:45 +0000

    Added grid tool view as default

--- uk.ac.gda.client.ncd/src/gda/rcp/ncd/perspectives/GridScanPerspective.java
+++ uk.ac.gda.client.ncd/src/gda/rcp/ncd/perspectives/GridScanPerspective.java
@@ -33,9 +33,11 @@ public class GridScanPerspective implements IPerspectiveFactory {
 			folderLayout.addView("uk.ac.gda.client.ncd.mappingthumb");
 		}
 		layout.addView("uk.ac.gda.rcp.views.dashboardView", IPageLayout.TOP, 0.15f, "uk.ac.gda.client.ncd.gridcameraview");
-		layout.addView("gda.rcp.jythonterminalview", IPageLayout.LEFT, 0.95f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("gda.rcp.jythonterminalview", IPageLayout.LEFT, 0.8f, IPageLayout.ID_EDITOR_AREA);
 		{
-			IFolderLayout folderLayout = layout.createFolder("folder_1", IPageLayout.BOTTOM, 0.87f, "gda.rcp.jythonterminalview");
+			IFolderLayout folderLayout = layout.createFolder("folder_1", IPageLayout.BOTTOM, 0.6f, "gda.rcp.jythonterminalview");
+		//	folderLayout.addView("org.dawb.workbench.plotting.views.toolPageView.2D");//uk.ac.gda.client.ncd.gridDetails");//, IPageLayout.BOTTOM, 0.4f, IPageLayout.ID_EDITOR_AREA);//"gda.rcp.jythonterminalview");
+			folderLayout.addView("org.dawb.workbench.plotting.views.toolPageView.fixed:org.dawb.workbench.plotting.tools.gridTool");//uk.ac.gda.client.ncd.gridDetails");//, IPageLayout.BOTTOM, 0.4f, IPageLayout.ID_EDITOR_AREA);//"gda.rcp.jythonterminalview");
 			folderLayout.addView("uk.ac.gda.client.ncd.NcdButtonPanelView");
 			folderLayout.addView("gda.rcp.views.baton.BatonView");
 		}
