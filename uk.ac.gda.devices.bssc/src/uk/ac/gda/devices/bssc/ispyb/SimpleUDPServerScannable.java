7e36adfb6d1aeb92fcc3f2cdd55ed19a3a1235f1
Conor Lehane <xlw00930@ws141.diamond.ac.uk>
2014-01-15 17:13:29 +0000

    Added new class SimpleUDPServerScannable which will receive ISpyB updates in the form of datagrams

---
+++ uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/ispyb/SimpleUDPServerScannable.java
@@ -0,0 +1,160 @@
+/*-
+ * Copyright © 2009 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.devices.bssc.ispyb;
+
+import gda.device.DeviceException;
+import gda.device.scannable.ScannableBase;
+import gda.device.scannable.ScannablePositionChangeEvent;
+import gda.factory.FactoryException;
+import gda.observable.IObserver;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+Scannable to receive UDP datagrams containing a string of format prefix:message. 
+The position is set the message part of the string.
+This is used to inform a gda server when a table in ISpy has been updated by a process on the network
+*/
+public class SimpleUDPServerScannable extends ScannableBase {
+	private static final Logger logger = LoggerFactory.getLogger(SimpleUDPServerScannable.class);
+	boolean running = true;
+	private int port=9876;
+	private String position="";
+	private String prefix="";
+
+	
+	@Override
+	public void configure() throws FactoryException {
+		super.configure();
+		setInputNames(new String[]{});
+		setExtraNames(new String[]{getName()});
+		if(isRunning())
+			startReading();
+	}
+	
+	@Override
+	public void asynchronousMoveTo(Object data) throws DeviceException {
+		if( data instanceof String){
+			position = (String)data;
+			notifyIObservers(this, new ScannablePositionChangeEvent(position));
+		}
+	}	
+	
+	@Override
+	public Object getPosition() throws DeviceException {
+		return position;
+	}
+
+	@Override
+	public boolean isBusy() throws DeviceException {
+		return false;
+	}	
+		
+
+	/**
+	 * @return true if the file is being monitored
+	 */
+	public boolean isRunning() {
+		return running;
+	}
+
+	/**
+	 * @param running
+	 */
+	public void setRunning(boolean running) {
+		if (running && !this.running) {
+			this.running = true;
+			startReading();
+		} else if (!running) {
+			this.running = false;
+		}
+	}
+
+	protected void startReading() {
+		Thread t = new Thread() {
+			@Override
+			public void run() {
+				try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+					
+					byte[] receiveData = new byte[1024];
+					
+					while (running) {
+						DatagramPacket receivePacket = new DatagramPacket(
+								receiveData, receiveData.length);
+						serverSocket.receive(receivePacket);
+						String s = new String(receivePacket.getData(),0,receivePacket.getLength(), "UTF-8");
+						String [] fields = s.split(":",2);
+						if(fields.length==2 && fields[0].equals(prefix))
+							asynchronousMoveTo(fields[1]);
+					}
+				} catch (Exception e) {
+					logger.error(e.getMessage(), e);
+				}
+			}
+		};
+		t.start();
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	/**
+	 * @param args
+	 * @throws FactoryException 
+	 */
+	public static void main(String[] args) throws FactoryException {
+		SimpleUDPServerScannable simpleUDPServer = new SimpleUDPServerScannable();
+		simpleUDPServer.setName("simpleUDPServer");
+		simpleUDPServer.setRunning(true);
+		simpleUDPServer.setPort(9877);
+		simpleUDPServer.setPrefix("simpleUDPServer");
+		simpleUDPServer.configure();
+		simpleUDPServer.addIObserver(new SimpleUDPReceiver());
+	}
+}
+
+class SimpleUDPReceiver implements IObserver {
+
+	@Override
+	public void update(Object theObserved, Object changeCode) {
+		if(changeCode instanceof ScannablePositionChangeEvent){
+			Object pos = ((ScannablePositionChangeEvent)changeCode).newPosition;
+			if( pos instanceof String){
+				System.out.println((String)pos);
+			}
+		}
+	}
+}
