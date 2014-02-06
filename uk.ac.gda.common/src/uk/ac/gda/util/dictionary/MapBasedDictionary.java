7ee58040cfb0c14e273da1eb8576f4144b3d161d
chris.coles@diamond.ac.uk <chris.coles@diamond.ac.uk>
2014-02-06 17:32:02 +0000

    Reduce warnings

--- uk.ac.gda.common/src/uk/ac/gda/util/dictionary/MapBasedDictionary.java
+++ uk.ac.gda.common/src/uk/ac/gda/util/dictionary/MapBasedDictionary.java
@@ -28,7 +28,8 @@ import java.util.Set;
 
 import org.springframework.util.Assert;
 
-@SuppressWarnings("all")
+
+@SuppressWarnings("rawtypes")
 public class MapBasedDictionary extends Dictionary implements Map {
 
 	private Map map;
@@ -56,10 +57,12 @@ public class MapBasedDictionary extends Dictionary implements Map {
 			this(col.iterator());
 		}
 
+		@Override
 		public boolean hasMoreElements() {
 			return it.hasNext();
 		}
 
+		@Override
 		public Object nextElement() {
 			return it.next();
 		}
@@ -98,36 +101,45 @@ public class MapBasedDictionary extends Dictionary implements Map {
 			putAll(dictionary);
 	}
 
+	@Override
 	public void clear() {
 		map.clear();
 	}
 
+	@Override
 	public boolean containsKey(Object key) {
 		return map.containsKey(key);
 	}
 
+	@Override
 	public boolean containsValue(Object value) {
 		return map.containsValue(value);
 	}
 
+	@Override
 	public Set entrySet() {
 		return map.entrySet();
 	}
 
+	@Override
 	public Object get(Object key) {
 		if (key == null)
 			throw new NullPointerException();
 		return map.get(key);
 	}
 
+	@Override
 	public boolean isEmpty() {
 		return map.isEmpty();
 	}
 
+	@Override
 	public Set keySet() {
 		return map.keySet();
 	}
 	
+	@SuppressWarnings("unchecked")
+	@Override
 	public Object put(Object key, Object value) {
 		if (key == null || value == null)
 			throw new NullPointerException();
@@ -135,10 +147,13 @@ public class MapBasedDictionary extends Dictionary implements Map {
 		return map.put(key, value);
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
 	public void putAll(Map t) {
 		map.putAll(t);
 	}
 
+	@SuppressWarnings("unchecked")
 	public void putAll(Dictionary dictionary) {
 		if (dictionary != null)
 			// copy the dictionary
@@ -148,6 +163,7 @@ public class MapBasedDictionary extends Dictionary implements Map {
 			}
 	}
 
+	@Override
 	public Object remove(Object key) {
 		if (key == null)
 			throw new NullPointerException();
@@ -155,32 +171,39 @@ public class MapBasedDictionary extends Dictionary implements Map {
 		return map.remove(key);
 	}
 
+	@Override
 	public int size() {
 		return map.size();
 	}
 
+	@Override
 	public Collection values() {
 		return map.values();
 	}
 
+	@Override
 	public Enumeration elements() {
 		return new IteratorBasedEnumeration(map.values());
 	}
 
+	@Override
 	public Enumeration keys() {
 		return new IteratorBasedEnumeration(map.keySet());
 	}
 
+	@Override
 	public String toString() {
 		return map.toString();
 	}
 
+	@Override
 	public boolean equals(Object obj) {
 		// this should work nicely since the Dictionary implementations inside
 		// the JDK are Maps also
 		return map.equals(obj);
 	}
 
+	@Override
 	public int hashCode() {
 		return map.hashCode();
 	}
