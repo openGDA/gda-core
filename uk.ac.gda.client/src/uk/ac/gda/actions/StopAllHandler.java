a61e0f17e0a4c0dab090b59b3cd49969b6a2c4e2
Richard Fearn <richard.fearn@diamond.ac.uk>
2014-02-11 13:41:00 +0000

    When "Panic Stop" is pressed, do the panic stop in a job
    I have no idea who made this change (on I24) but it seems reasonable...

--- uk.ac.gda.client/src/uk/ac/gda/actions/StopAllHandler.java
+++ uk.ac.gda.client/src/uk/ac/gda/actions/StopAllHandler.java
@@ -19,10 +19,16 @@
 package uk.ac.gda.actions;
 
 import gda.jython.JythonServerFacade;
+import gda.rcp.views.dashboard.DashboardView;
+import gda.rcp.views.dashboard.ServerObject;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 
 public class StopAllHandler extends AbstractHandler {
 
@@ -30,7 +36,16 @@ public class StopAllHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
-			JythonServerFacade.getInstance().panicStop();
+			Job job = new Job("Panic Stop...") {
+				@Override
+				protected IStatus run(IProgressMonitor monitor) {
+					JythonServerFacade.getInstance().panicStop();
+					return Status.OK_STATUS;
+				}
+			};
+			job.setUser(false);
+			job.schedule();
+			
 			return Boolean.TRUE;
 		} catch (Exception ne) {
 			throw new ExecutionException(ne.getMessage(), ne);
