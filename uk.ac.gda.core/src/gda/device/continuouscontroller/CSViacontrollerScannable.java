9bf4ed43c8b06b880ef8ec91584f8df4e7dc9b42
Rob Walton <rob.walton@diamond.ac.uk>
2014-02-25 12:46:21 +0000

    GDA-5863 (8.36.GDA-5863) I13J: Commit of local files found uncommited in workspace gda-core

---
+++ uk.ac.gda.core/src/gda/device/continuouscontroller/CSViacontrollerScannable.java
@@ -0,0 +1,125 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.continuouscontroller;
+
+import gda.device.DeviceException;
+import gda.device.scannable.ContinuouslyScannableViaController;
+import gda.device.scannable.PositionCallableProvider;
+import gda.device.scannable.PositionConvertorFunctions;
+import gda.device.scannable.SimpleScannable;
+import gda.scan.ScanPositionRecordable;
+import gda.scan.ScanPositionRecorder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class CSViacontrollerScannable extends SimpleScannable implements ContinuouslyScannableViaController, ScanPositionRecordable, PositionCallableProvider<Double>{
+	private static final Logger logger = LoggerFactory.getLogger(CSViacontrollerScannable.class);
+	ContinuousMoveController cmc;
+	
+
+	public ContinuousMoveController getCmc() {
+		return cmc;
+	}
+
+	public void setCmc(ContinuousMoveController cmc) {
+		this.cmc = cmc;
+	}
+
+	@Override
+	public Callable<Double> getPositionCallable() throws DeviceException {
+		return new Callable<Double>(){
+
+			@Override
+			public Double call() throws Exception {
+				return 0.;
+			}};
+	}
+
+	@Override
+	public void setOperatingContinuously(boolean b) throws DeviceException {
+	}
+
+	@Override
+	public boolean isOperatingContinously() {
+		return true;
+	}
+
+	@Override
+	public ContinuousMoveController getContinuousMoveController() {
+		return cmc;
+	}
+	
+	private ScanPositionRecorder recorder;
+	
+	@Override
+	public void asynchronousMoveTo(Object position) throws DeviceException {
+		if (recorder != null) {
+			recorder.addPositionToCurrentPoint(this, position);
+		} else {
+			addPoint(PositionConvertorFunctions.toDouble(externalToInternal(position)));
+		}
+	}
+	@Override
+	public Object getPosition() throws DeviceException {
+		if (isOperatingContinously()) {
+			Object[] pos = (Object[]) internalToExternal(new Double[]{getLastPointAdded()});
+			if (pos == null) {
+				// First point is in process of being added
+				return super.getPosition();
+			}
+			return pos[0];
+		}
+		return super.getPosition();
+	}	
+	
+	List<Double> points = null;
+
+
+	public void addPoint(Double point) {
+		if(points == null){
+			points = new ArrayList<Double>();
+		}
+		points.add(point);
+	}
+
+	public Double getLastPointAdded() {
+		if (points == null || points.size() == 0) {
+			logger.info(getName() + ".getLastPointAdded() returning null, as no points have yet been added");
+			return null;
+		}
+		return points.get(points.size() - 1);
+	}
+
+	@Override
+	public boolean isBusy() throws DeviceException {
+		return false;
+	}
+
+	@Override
+	public void setRecorder(ScanPositionRecorder recorder) {
+		this.recorder = recorder;
+		
+	}	
+	
+}
---
+++ uk.ac.gda.core/src/gda/device/continuouscontroller/CVScannable.java
@@ -0,0 +1,120 @@
+/*-
+ * Copyright © 2013 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.continuouscontroller;
+
+import gda.device.DeviceException;
+import gda.device.Scannable;
+import gda.device.scannable.ScannableBase;
+import gda.device.scannable.ScannableUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class CVScannable extends ScannableBase implements ConstantVelocityMoveController{
+	private static final Logger logger = LoggerFactory.getLogger(CVScannable.class);
+	
+	private double triggerPeriod;
+	private double end;
+	private double step;
+	private double start;
+	Scannable scannableBeingMoved;
+	
+	
+	public Scannable getScannableBeingMoved() {
+		return scannableBeingMoved;
+	}
+
+	public void setScannableBeingMoved(Scannable scannableBeingMoved) {
+		this.scannableBeingMoved = scannableBeingMoved;
+	}
+
+	@Override
+	public void setTriggerPeriod(double seconds) throws DeviceException {
+		logger.info("setTriggerPeriod:"+seconds);
+		triggerPeriod = seconds; //readout need to use readout time;
+
+	}
+
+	@Override
+	public int getNumberTriggers() {
+		logger.info("getNumberTriggers");
+		try {
+			return ScannableUtils.getNumberSteps(scannableBeingMoved, new Double(start),new Double(end),new Double(step))+1;
+		} catch (Exception e) {
+			logger.error("Error getting number of triggers", e);
+			return 0;
+		}
+	}
+
+	@Override
+	public double getTotalTime() throws DeviceException {
+		logger.info("getTotalTime");
+		return (getNumberTriggers() == 0) ? 0 : triggerPeriod * (getNumberTriggers() - 1);
+	}
+	@Override
+	public boolean isBusy() throws DeviceException {
+		return false;
+	}
+
+	@Override
+	public void prepareForMove() throws DeviceException, InterruptedException {
+		logger.info("prepareForMove");
+	}
+
+	@Override
+	public void startMove() throws DeviceException {
+		logger.info("startMove");
+	}
+
+	@Override
+	public boolean isMoving() throws DeviceException {
+		logger.info("isMoving");
+		return false;
+	}
+
+	@Override
+	public void waitWhileMoving() throws DeviceException, InterruptedException {
+		logger.info("waitWhileMoving");
+	}
+
+	@Override
+	public void stopAndReset() throws DeviceException, InterruptedException {
+		logger.info("stopAndReset");
+	}
+
+	@Override
+	public void setStart(double start) throws DeviceException {
+		logger.info("setStart:" + start); 
+		this.start = start;
+	}
+
+	@Override
+	public void setEnd(double end) throws DeviceException {
+		logger.info("setEnd:" + end);
+		this.end = end;
+	}
+
+	@Override
+	public void setStep(double step) throws DeviceException {
+		logger.info("setStep:"+ step);
+		this.step = step;
+
+	}
+	
+}
