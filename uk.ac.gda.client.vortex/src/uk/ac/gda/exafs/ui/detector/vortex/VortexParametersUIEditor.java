10b89dcc2023c9f8f64452892f5e6d9ef3bbb762
Chris Coles <chris.coles@diamond.ac.uk>
2014-06-03 23:52:01 +0100

    Vortex: fixed problem where elements couldn't be enabled/disabled properly.

--- uk.ac.gda.client.vortex/src/uk/ac/gda/exafs/ui/detector/vortex/VortexParametersUIEditor.java
+++ uk.ac.gda.client.vortex/src/uk/ac/gda/exafs/ui/detector/vortex/VortexParametersUIEditor.java
@@ -64,7 +64,7 @@ public class VortexParametersUIEditor extends DetectorEditor {
 		String tfgName = vortexParameters.getTfgName();
 		Detector xmapDetector = Finder.getInstance().find(detectorName);
 		Timer tfg = (Timer) Finder.getInstance().find(tfgName);
-		this.vortex = new Vortex(path, this.getSite(), parent, detectorList, xmapDetector, tfg);
+		this.vortex = new Vortex(path, this.getSite(), parent, detectorList, xmapDetector, tfg, vortexParameters);
 	}
 
 	@Override
