b4776eb409b1fa1befea3658153f2a2dec499bad
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-06-24 10:36:30 +0100

    close before finalise

--- uk.ac.gda.core/src/gda/data/scan/datawriter/NexusDataWriter.java
+++ uk.ac.gda.core/src/gda/data/scan/datawriter/NexusDataWriter.java
@@ -747,6 +747,7 @@ public class NexusDataWriter extends DataWriterBase implements DataWriter {
 		try {
 			if (file != null) {
 				file.flush();
+				file.close();
 				file.finalize();
 			}
 			if (createSrsFile) {
