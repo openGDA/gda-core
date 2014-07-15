88bf6d7eff1670585999d67a6e4a54b799d61327
matthew.gerring <matthew.gerring@gmail.com>
2014-07-15 15:12:03 +0100

    Changed package id to org.eclipse.dawnsci

--- uk.ac.gda.client.hrpd/src/uk/ac/gda/client/hrpd/views/LivePlotComposite.java
+++ uk.ac.gda.client.hrpd/src/uk/ac/gda/client/hrpd/views/LivePlotComposite.java
@@ -26,10 +26,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.dawnsci.plotting.api.IPlottingSystem;
-import org.dawnsci.plotting.api.PlotType;
-import org.dawnsci.plotting.api.PlottingFactory;
-import org.dawnsci.plotting.api.trace.ILineTrace;
+import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
+import org.eclipse.dawnsci.plotting.api.PlotType;
+import org.eclipse.dawnsci.plotting.api.PlottingFactory;
+import org.eclipse.dawnsci.plotting.api.trace.ILineTrace;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.jface.operation.IRunnableWithProgress;
