6d1c9ac02e72c3cf54b5fa28d2c9187ee0de6260
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-07-25 17:17:33 +0100

    fix org.eclipse.nebula.jface.cdatetime.CDateTimeCellEditor https://bugs.eclipse.org/bugs/show_bug.cgi?id=352693

---
+++ org.opengda.lde.ui/src/org/opengda/lde/ui/cdatetime/CDateTimeCellEditor.java
@@ -0,0 +1,33 @@
+package org.opengda.lde.ui.cdatetime;
+
+import org.eclipse.nebula.widgets.cdatetime.CDateTime;
+import org.eclipse.swt.events.FocusAdapter;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+
+public class CDateTimeCellEditor extends org.eclipse.nebula.jface.cdatetime.CDateTimeCellEditor {
+
+	public CDateTimeCellEditor(Composite parent) {
+		super(parent);
+	}
+	@Override
+	protected boolean dependsOnExternalFocusListener() {
+		return false;
+	}
+	@Override
+	protected Control createControl(Composite parent) {
+		final CDateTime cdt = (CDateTime) super.createControl(parent);
+		cdt.addFocusListener(new FocusAdapter() {
+			public void focusLost(FocusEvent e) {
+				if (!cdt.isOpen()) {
+					fireApplyEditorValue();
+					deactivate();
+				}
+			}
+		});
+		return cdt;
+	}
+
+}
