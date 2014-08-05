30f29c82c4b6ee0383a60055cc29eec705b85d8e
Jun Aishima <jun.aishima@diamond.ac.uk>
2014-08-05 15:29:38 +0100

    add script to create first, middle, last I/q plots

---
+++ uk.ac.gda.devices.bssc/scripts/create3Plots.py
@@ -0,0 +1,42 @@
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from matplotlib import cm
+import matplotlib
+matplotlib.use('Agg')
+import sys
+import os
+
+def create3Plots(reducedfile, output):
+	if reducedfile == None:
+		print "reducedfile must be defined"
+		sys.exit(1)
+
+	datapath="/entry1/detector_processing/Normalisation/data"
+	errorpath="/entry1/detector_processing/Normalisation/errors"
+	qpath = "/entry1/detector_result/q"
+	qerrorpath="/entry1/detector_result/q_errors"
+
+	#set up plot
+	import matplotlib.pyplot as plt
+
+	#set up data
+	import h5py
+	f=h5py.File(reducedfile)
+	reducedDataArray = f[datapath]
+	numberOfDatasets = len(reducedDataArray[0])
+	errorDataArray = f[errorpath]
+	qArray=f[qpath]
+	qErrorArray=f[qerrorpath]
+	filenamesToReturn = []
+	for i in (0, numberOfDatasets/2, numberOfDatasets-1):
+		fig = plt.figure(figsize=(6, 5))
+		ax = fig.add_subplot(1,1,1)
+		ax.semilogy()
+		ax.errorbar(qArray, reducedDataArray[0][i], yerr=errorDataArray[0][i], label="Plot for " + str(i+1) + "th I vs. q")
+		ax.set_xlabel(u"q (1/A)")
+		ax.set_ylabel('log I')
+		filename = os.path.join(output+ str(i)+ "plot.png")
+		fig.savefig(filename)
+		fig.clf()
+		filenamesToReturn.append(filename)
+	return filenamesToReturn
