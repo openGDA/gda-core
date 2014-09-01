16f4aa3dc5b9fff90204e6f34a2408c9cdd85696
Charles Mita <charles.mita@diamond.ac.uk>
2014-09-01 16:08:09 +0100

    Added SimpleFilePathConverter class.
    Performs file path substitution using StringUtils.replace().
    This is basically the same as UnixToWindowsFilePathConverter but doesn't
    change '/' to '\'.

---
+++ uk.ac.gda.common.test/src/uk/ac/gda/util/SimpleFilePathConverterTest.java
@@ -0,0 +1,46 @@
+/*-
+ * Copyright © 2011 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleFilePathConverterTest {
+
+	@Test
+	public void testConverttoInternal() throws Exception {
+		SimpleFilePathConverter converter = new SimpleFilePathConverter();
+		converter.setUserSubString("/dls/i13-1/data");
+		converter.setInternalSubString("/internalmount/test");
+		converter.afterPropertiesSet();
+		String internal = converter.converttoInternal("/dls/i13-1/data/2011/0-0/");
+		assertEquals("/internalmount/test/2011/0-0/", internal);
+	}
+
+	@Test
+	public void testConverttoExternal() throws Exception {
+		SimpleFilePathConverter converter = new SimpleFilePathConverter();
+		converter.setUserSubString("/dls/i13-1/data");
+		converter.setInternalSubString("/internalmount/test");
+		converter.afterPropertiesSet();
+		String external = converter.converttoExternal("/internalmount/test/2011/0-0/");
+		assertEquals("/dls/i13-1/data/2011/0-0/", external);
+	}
+}
