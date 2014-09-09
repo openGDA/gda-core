c7668af007e5901a820d92021534805673b16a30
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-09-09 11:03:26 +0100

    more running status icons

--- org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/ImageConstants.java
+++ org.opengda.detector.electronanalyser.client/src/org/opengda/detector/electronanalyser/client/ImageConstants.java
@@ -46,7 +46,7 @@ public class ImageConstants {
 
 
 	public static final String ICON_RUN_READY = "icons/waiting.gif";
-	public static final String ICON_RUNNING = "icons/youtube.gif";
+	public static final String ICON_RUNNING = "icons/ajax-loader.gif";
 	public static final String ICON_RUN_COMPLETE = "icons/complete_status.gif";
 	public static final String ICON_RUN_FAILURE = "icons/progress_error.gif";
 	public static final String ICON_INVALID = "icons/invalid.png";
