9fb5bc34f28dea288df57ede26cadda3e0d54e0b
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-10-15 13:23:50 +0100

    add calibration scannable

--- org.opengda.lde/src/org/opengda/lde/scannables/DataReductionScannable.java
+++ org.opengda.lde/src/org/opengda/lde/scannables/DataReductionScannable.java
@@ -1,6 +1,7 @@
 package org.opengda.lde.scannables;
 
 import gda.configuration.properties.LocalProperties;
+import gda.device.DeviceException;
 import gda.device.Scannable;
 import gda.device.scannable.DummyScannable;
 import gda.jython.InterfaceProvider;
@@ -9,6 +10,7 @@ import gda.jython.scriptcontroller.Scriptcontroller;
 import gda.util.OSCommandRunner;
 import gda.util.Sleep;
 
+import java.io.File;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -28,6 +30,7 @@ public class DataReductionScannable extends DummyScannable implements Scannable 
 	private String command;
 	private String filename=null;
 	private double timeout=30000; //30 seconds
+	private FileReferenceScannable currentCalibrationScannable;
 	@Override
 	public void atScanEnd() {
 		if (InterfaceProvider.getCurrentScanController().isFinishEarlyRequested()) {
@@ -45,8 +48,6 @@ public class DataReductionScannable extends DummyScannable implements Scannable 
 			}
 		}
 		filename=InterfaceProvider.getScanDataPointProvider().getLastScanDataPoint().getCurrentFilename();
-//		ScanInformation scaninfo=InterfaceProvider.getCurrentScanInformationHolder().getCurrentScanInformation();
-//		final String filename = scaninfo.getFilename();
 		logger.info("Starting data reduction processing on the cluster...");
 		InterfaceProvider.getTerminalPrinter().print("Starting data reduction processing on the cluster...");
 		if (isCalibrant()) {
@@ -93,7 +94,7 @@ public class DataReductionScannable extends DummyScannable implements Scannable 
 						} else {
 							osCommandRunner.logOutput();
 						}
-						return FilenameUtils.getPath(filename)+"processed/"+FilenameUtils.getBaseName(filename)+".xy";
+						return File.separator+FilenameUtils.getPath(filename)+"processed"+File.separator+FilenameUtils.getBaseName(filename)+".xy";
 					}
 				};
 				final ExecutorService executor = Executors.newFixedThreadPool(1);		
@@ -134,6 +135,11 @@ public class DataReductionScannable extends DummyScannable implements Scannable 
 	}
 
 	public void setCalibrant(boolean calibrant) {
+		try {
+			currentCalibrationScannable.moveTo("Undefined");
+		} catch (DeviceException e) {
+			logger.error("Failed to reset the data filename of current calibration scannable");
+		}
 		this.calibrant = calibrant;
 	}
 
@@ -144,6 +150,20 @@ public class DataReductionScannable extends DummyScannable implements Scannable 
 	public void setCurrentCalibrantDataFilename(
 			String current_Calibrant_Data_Filename) {
 		Current_Calibrant_Data_Filename = current_Calibrant_Data_Filename;
+		try {
+			currentCalibrationScannable.moveTo(filename);
+		} catch (DeviceException e) {
+			logger.error("failed to set the data file name for the current calibration scannable to {}", filename);
+		}
+	}
+
+	public FileReferenceScannable getCurrentCalibrationScannable() {
+		return currentCalibrationScannable;
+	}
+
+	public void setCurrentCalibrationScannable(
+			FileReferenceScannable currentCalibrationScannable) {
+		this.currentCalibrationScannable = currentCalibrationScannable;
 	}
 
 }
---
+++ org.opengda.lde/src/org/opengda/lde/scannables/FileReferenceScannable.java
@@ -0,0 +1,48 @@
+package org.opengda.lde.scannables;
+
+import gda.device.DeviceException;
+import gda.device.scannable.ScannableBase;
+
+public class FileReferenceScannable extends ScannableBase {
+
+	private String calibrationFilename="Undefined";
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public FileReferenceScannable() {
+	}
+	
+	public void configure() throws gda.factory.FactoryException {
+		if (!configured) {
+			setName(name);
+			setInputNames(new String[] { name });
+			setOutputFormat(new String[] { "%s" });
+			configured=true;
+		}
+	};
+	@Override
+	public Object getPosition() throws DeviceException {
+		return calibrationFilename;
+	}
+	@Override
+	public void asynchronousMoveTo(Object externalPosition)
+			throws DeviceException {
+		this.calibrationFilename=externalPosition.toString();
+	}
+	@Override
+	public boolean isBusy() throws DeviceException {
+		return false;
+	}
+	@Override
+	public String toFormattedString() {
+		try {
+			return getName() +": "+getPosition();
+		} catch (Exception e) {
+			throw new RuntimeException("Exception in " + getName() + ".toFormattedString()", e);
+		}
+	}
+}
