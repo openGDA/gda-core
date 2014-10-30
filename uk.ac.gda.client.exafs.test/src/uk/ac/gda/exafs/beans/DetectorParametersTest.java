974cffbba3f9f7cc1b1c3e389316fdb4cfaef2d9
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-10-30 14:55:55 +0000

    fix unit test - the bean has a new atribute which needed setting in the test

--- uk.ac.gda.client.exafs.test/src/uk/ac/gda/exafs/beans/DetectorParametersTest.java
+++ uk.ac.gda.client.exafs.test/src/uk/ac/gda/exafs/beans/DetectorParametersTest.java
@@ -117,6 +117,7 @@ public class DetectorParametersTest {
 		transmissionParameters.setDetectorType("Transmission");
 		transmissionParameters.setWorkingEnergy(7100.0);
 		transmissionParameters.setMythenEnergy(9000.0);
+		transmissionParameters.setMythenTime(10.0);
 		IonChamberParameters ionChamberParameters = new IonChamberParameters();
 		ionChamberParameters.setName("I0");
 		ionChamberParameters.setDeviceName("counterTimer01");
