377d99dd53e741b09f021af1f438e9f3f3604713
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-11-11 11:15:26 +0000

    XAS-172 tidy code

--- uk.ac.gda.client.microfocus/src/uk/ac/gda/client/microfocus/scan/FasterRasterMap.java
+++ uk.ac.gda.client.microfocus/src/uk/ac/gda/client/microfocus/scan/FasterRasterMap.java
@@ -74,6 +74,52 @@ public class FasterRasterMap extends RasterMap {
 		}
 		
 		return args;
+		
+//		ContinuousScan cs = new ContinuousScan(trajectoryMotor, mapScanParameters.getXStart(), mapScanParameters.getXEnd(), calculateNumberXPoints(), mapScanParameters.getRowTime(), detectorList) ;
+//		cs.setBiDirectional(true);
+//		
+//		//TODO have not done the custom settings for raster maps for the monitor objects
+//		
+//		Object[] args = new Object[] {yMotor, mapScanParameters.getYStart(), mapScanParameters.getYEnd(),  mapScanParameters.getYStepSize(), trajectoryBeamMonitor, cs};
+//		
+//		// add a Scannable, if defined, which fetches the motor readback values from the Epics Trajectory template after the trajectory completes.
+//		if (positionReader != null){
+//			args = ArrayUtils.add(args, positionReader);
+//		}
+//		
+//		return args;
+
+		
+//		// TODO test this on beamline - I am really not sure if this will work first time - the constructor is Object[]
+//		// and the dummy version does not work, so can only test on the beamline... yikes.
+//
+//		ScanPositionsTwoWay sptw = new ScanPositionsTwoWay(trajectoryMotor, mapScanParameters.getXStart(),
+//				mapScanParameters.getXEnd(), mapScanParameters.getXStepSize());
+//
+//		Double dwellTime = mapScanParameters.getRowTime() / calculateNumberXPoints();
+//		Object[] innerScanArgs = new Object[] { trajectoryMotor, sptw };
+//		innerScanArgs = ArrayUtils.addAll(innerScanArgs, detectorList);
+//		innerScanArgs = ArrayUtils.add(innerScanArgs, dwellTime);
+//
+//		TrajectoryScanLine tsl = new TrajectoryScanLine(innerScanArgs);
+//		tsl.setScanDataPointQueueLength(10000);
+//		tsl.setPositionCallableThreadPoolSize(10);
+//
+//		Object[] outerScanArgs = new Object[] { yMotor, mapScanParameters.getYStart(), mapScanParameters.getYEnd(),
+//				mapScanParameters.getYStepSize(), trajectoryBeamMonitor, tsl };
+//
+//		// add a Scannable, if defined, which fetches the motor readback values from the Epics Trajectory template after
+//		// the trajectory completes.
+//		if (positionReader != null) {
+//			outerScanArgs = ArrayUtils.add(outerScanArgs, positionReader);
+//		}
+//		
+//		
+////		I think that we could try using the ContinuousScan here - it would simply need modifying to accept a ScanPositionProvider instead of start. stop step but it should work....a
+////		
+////		if it does then both I18 raster maps could use ContinuousScan instead of faster raster using TrajectoryScanLine  :)
+//
+//		return outerScanArgs;
 	}
 
 	@Override
