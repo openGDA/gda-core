832e0922cf4837529cafe6ecb084099a024f2959
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-11-13 14:26:10 +0000

    separate out functionality observing file readability

--- uk.ac.gda.devices.xmap/src/gda/device/detector/xmap/XmapBufferedDetector.java
+++ uk.ac.gda.devices.xmap/src/gda/device/detector/xmap/XmapBufferedDetector.java
@@ -186,7 +186,7 @@ public class XmapBufferedDetector extends DetectorBase implements BufferedDetect
 				String beamline = LocalProperties.get("gda.factory.factoryName", "").toLowerCase();
 				lastFileName = lastFileName.replace("X:/", "/dls/" + beamline);
 								
-				waitForFileToBeReadable();
+				new XmapFileUtils(lastFileName).waitForFileToBeReadable();
 
 				createFileLoader();
 				try {
@@ -231,44 +231,7 @@ public class XmapBufferedDetector extends DetectorBase implements BufferedDetect
 		}
 	}
 
-	private void waitForFileToBeReadable() throws DeviceException, InterruptedException {
-		try {
-			int timeout = 5000; // ms
-			int timeWaited = 0; // ms
-			int waitTime = 500; // ms
-			
-			// first wait until the file can be seen on the filesystem
-			File existsTest = new File(lastFileName);
-			while (!existsTest.exists() && timeWaited < timeout){
-				Thread.sleep(waitTime);
-				timeWaited += waitTime;
-			}
-			
-			// then keep trying to read from it, as it may still being read to
-			while (true && timeWaited < timeout) {
-				try {
-					// if get here then it at leasts exists
-					BufferedInputStream fileTester = new BufferedInputStream(new FileInputStream(lastFileName));
-					fileTester.read(new byte[64]);
-					// if no exception then its OK to properly open the file
-					fileTester.close();
-					return;
-				} catch (FileNotFoundException e) {
-					logger.warn("FileNotFoundException while waiting for Xmap HDF5, will try again...");
-					Thread.sleep(waitTime);
-					timeWaited += waitTime;
-				} catch (IOException e) {
-					logger.warn("IOException while waiting for Xmap HDF5, will try again...");
-					Thread.sleep(waitTime);
-					timeWaited += waitTime;
-				}
-			}
-			
-			throw new DeviceException("Too many attempts / timeout while waiting for XMAP HDF5 file to be readable - " + lastFileName);
-		} catch (InterruptedException e) {
-			throw new InterruptedException("InterruptedException while waiting for XMAP HDF5 file to be readable - " + lastFileName);
-		}
-	}
+
 
 	private void createFileLoader() throws  Exception {
 		if (controller.isBufferedArrayPort())
