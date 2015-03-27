738016e4562b893a59507e5a4298ec38b0bcecc2
Peter Chang <peter.chang@diamond.ac.uk>
2015-03-27 09:43:24 +0000

    GDA-6113 eliminate most uses of constants and use new add API

--- uk.ac.gda.devices.mythen/src/gda/device/detector/mythen/MythenNexusImpl.java
+++ uk.ac.gda.devices.mythen/src/gda/device/detector/mythen/MythenNexusImpl.java
@@ -19,13 +19,12 @@
 package gda.device.detector.mythen;
 
 import gda.data.fileregistrar.FileRegistrarHelper;
+import gda.data.nexus.extractor.NexusGroupData;
 import gda.data.nexus.tree.NexusTreeProvider;
 import gda.device.DeviceException;
 import gda.device.detector.NXDetectorData;
 import gda.device.detector.NexusDetector;
 import gda.device.detector.mythen.data.MythenRawDataset;
-
-import gda.data.nexus.NexusGlobals;
 
 public class MythenNexusImpl extends MythenDetectorImpl implements NexusDetector {
 	
@@ -57,9 +56,8 @@ public class MythenNexusImpl extends MythenDetectorImpl implements NexusDetector
 	public NexusTreeProvider readout() throws DeviceException{
 		NXDetectorData thisFrame = new NXDetectorData(this);
 		thisFrame.addNote(getName(), "Raw file:" + rawFile.getName());
-		thisFrame.addData(getName(), new int[] { processedData.getLines().size() }, NexusGlobals.NX_FLOAT64, processedData.getCountArray(),
-				"counts", 1);
-		thisFrame.addAxis(getName(), "position", new int[] {processedData.getLines().size()}, NexusGlobals.NX_FLOAT64, processedData.getAngleArray(), 2, 1, "degrees", false);
+		thisFrame.addData(getName(), new NexusGroupData(processedData.getCountArray()), "counts", 1);
+		thisFrame.addAxis(getName(), "position", new NexusGroupData(processedData.getAngleArray()), 2, 1, "degrees", false);
 		thisFrame.setPlottableValue(getName(), 0.0);
 		thisFrame.setDoubleVals(new Double[]{0.0});
 		
