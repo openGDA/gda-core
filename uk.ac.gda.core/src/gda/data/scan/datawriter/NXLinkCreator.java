f7c8e8e13fc93a60af79c0c94393421740850496
Peter Chang <peter.chang@diamond.ac.uk>
2015-05-14 16:21:30 +0100

    GDA-6113 fix up relative paths, swapped parameters and remove old code
    Change-Id: Ibaef9443db37313f1e3d5de158c50247a5294bcd

--- uk.ac.gda.core/src/gda/data/scan/datawriter/NXLinkCreator.java
+++ uk.ac.gda.core/src/gda/data/scan/datawriter/NXLinkCreator.java
@@ -28,6 +28,7 @@ import ncsa.hdf.hdf5lib.H5;
 import ncsa.hdf.hdf5lib.HDF5Constants;
 
 import org.eclipse.dawnsci.analysis.api.tree.Node;
+import org.eclipse.dawnsci.analysis.api.tree.Tree;
 import org.eclipse.dawnsci.hdf5.nexus.NexusFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,24 +46,23 @@ public class NXLinkCreator {
 	}
 
 	public void makelinks(String filename) throws Exception {
-		NexusFile file = NexusUtils.createNexusFile(filename);
-		file.openToWrite(false);
+		NexusFile file = NexusUtils.openNexusFile(filename);
 
 		try {
 			for (SubEntryLink link : links) {
 				String value = link.target;
 				String key = link.key;
 				if (value.startsWith("nxfile")) { // create soft link to location in external file
-//					makelink(file, key, null, value);
 					file.linkExternal(new URI(value), key, false);
 				} else if (value.startsWith("#")) { // create soft link to current file
-//					makelink(file, key, null, "nxfile://" + filename + value);
 					file.linkExternal(new URI("nxfile://" + filename + value), key, false);
 				} else if (value.startsWith("!")) { // create external link
-					String unavailable = "";
 					String path = value.substring(1);
+					if (!path.startsWith(Tree.ROOT)) {
+						path = Tree.ROOT + path;
+					}
 					try {
-						file.link(value, key);
+						file.link(path, key);
 					} catch (Throwable t) {
 						path = NexusUtils.stripAugmentedPath(path);
 						int i = path.lastIndexOf(Node.SEPARATOR);
@@ -93,10 +93,12 @@ public class NXLinkCreator {
 							H5.H5Fclose(fid);
 						file.linkExternal(new URI(linkInfo), key, false);
 					}
-//					makelink(file, key, null, linkInfo);
 				} else { // create hard link
+					if (!value.startsWith(Tree.ROOT)) {
+						value = Tree.ROOT + value;
+					}
 					try {
-						file.link(key, value);
+						file.link(value, key);
 					} catch (Exception e) {
 						throw new Exception("Error making link for " + value + " in file " + file, e);
 					}
@@ -117,192 +119,6 @@ public class NXLinkCreator {
 			}
 		}
 	}
-
-//	/**
-//	 * Use visitor to go down NeXus file
-//	 */
-//	interface Visitor {
-//		/**
-//		 * Do things on arriving at any intermediate destination
-//		 * @param augmentedPath
-//		 * @throws Exception
-//		 */
-//		void arrive(String augmentedPath) throws Exception;
-//
-//		/**
-//		 * Do things when leaving intermediate destination on return leg 
-//		 * @throws Exception
-//		 */
-//		void depart() throws Exception;
-//
-//		/**
-//		 * Do things when leaving final destination
-//		 * @param part use null cannot find final destination 
-//		 * @param name
-//		 * @param nxClass
-//		 * @throws Exception
-//		 */
-//		void depart(String part, String name, String nxClass) throws Exception;
-//
-//		/**
-//		 * @return any payload from final destination 
-//		 */
-//		Object take();
-//	}
-//
-//	private static void visit(String path, Visitor visitor) throws Exception {
-//		
-//		String[] parts = path.split("/", 2);
-//		if (parts[0].isEmpty()) { // at top or bottom
-//			if (parts.length > 1) {
-//				visit(parts[1], visitor); // recurse down tree in file
-//				return;
-//			}
-//			visitor.depart(null, null, null); // final return
-//			return;
-//		}
-//		String[] subParts = parts[0].split(":");
-//		if (parts.length > 1) {
-//			visitor.arrive(subParts[0], subParts[1]);
-//			try {
-//				visit(parts[1], visitor); // recurse down tree in file
-//			} finally {
-//				visitor.depart();
-//			}
-//			return;
-//		}
-//		visitor.depart(parts[0], subParts[0], subParts.length > 1 ? subParts[1] : null);
-//	}
-//
-//	private static void makelink(final NexusFile file, String path, final NXlink link, final String url) throws Exception {
-//		visit(path, new Visitor() {
-//			@Override
-//			public void arrive(String augmentedPath) throws Exception {
-//				GroupNode group = file.getGroup(augmentedPath, false);
-//				
-//			}
-//			@Override
-//			public void arrive(String name, String nxClass) throws NexusException {
-//				if (!(file.groupdir().containsKey(name) && file.groupdir().get(name).equals(nxClass))) {
-//					try {
-//						// workaround because opengroup(x) sometimes fails after a call to makegroup(x)
-//						file.opengroup(name, nxClass);
-//						file.closegroup();
-//					} catch (Throwable e) {
-//						file.makegroup(name, nxClass);
-//					}
-//				}
-//				file.opengroup(name, nxClass);
-//			}
-//
-//			@Override
-//			public void depart() throws NexusException {
-//				file.closegroup();
-//			}
-//			
-//			@Override
-//			public void depart(String part, String name, String nxClass) throws NexusException {
-//				if (part == null)
-//					return;
-//
-//				if (link != null)
-//					file.makenamedlink(part, link);
-//				if (url != null)
-//					file.linkexternaldataset(part, url);
-//			}
-//
-//			@Override
-//			public Object take() {
-//				return null;
-//			}
-//		});
-//	}
-//
-//	private static NXlink getLink(final NexusFile file, String path) throws NexusException, Exception {
-//		Visitor v = new Visitor() {
-//			NXlink link = null;
-//			@Override
-//			public void arrive(String augmentedPath) throws Exception {
-//				GroupNode group = file.getGroup(augmentedPath, false);
-//				
-//			}
-//			public void arrive(String name, String nxClass) throws Exception {
-//				if( !(file.groupdir().containsKey(name) && file.groupdir().get(name).equals(nxClass))){
-//					throw new Exception("Item not found " + name + ":" + nxClass);
-//				}
-//				file.opengroup(name, nxClass);
-//			}
-//
-//			@Override
-//			public void depart(String part, String name, String nxClass) throws Exception {
-//				if (part == null)
-//					return;
-//
-//				if (nxClass.equals(NexusExtractor.NXDataClassName)) {
-//					try {
-//						file.opendata(name);
-//					} catch (NexusException ne) {
-//						throw new Exception("Error calling opendata for " + name, ne);
-//					}
-//					link = file.getdataID();
-//					file.closedata();
-//				}
-//			}
-//
-//			@Override
-//			public void depart() throws Exception {
-//				file.closegroup();
-//			}
-//
-//			@Override
-//			public Object take() {
-//				return link;
-//			}
-//		};
-//		visit(path, v);
-//		return (NXlink) v.take();
-//	}
-//
-//	private static String getExternalLinkInfo(final NexusFile file, String path, final String dataClassName, final String unavailable) throws NexusException, Exception {
-//		Visitor v = new Visitor() {
-//			String linkInfo = null;
-//			@Override
-//			public void arrive(String augmentedPath) throws Exception {
-//				// TODO Auto-generated method stub
-//				
-//			}
-//
-//			@Override
-//			public void arrive(String name, String nxClass) throws Exception {
-//				if( !(file.groupdir().containsKey(name) && file.groupdir().get(name).equals(nxClass))){
-//					throw new Exception("Item not found " + name + ":" + nxClass);
-//				}
-//				file.opengroup(name, nxClass);
-//			}
-//			
-//			@Override
-//			public void depart(String part, String name, String nxClass) throws Exception {
-//				if (part == null)	{
-//					linkInfo = null;
-//					return;
-//				}
-//				linkInfo = nxClass.equals(dataClassName) ? file.isexternaldataset("data") : unavailable;
-//			}
-//			
-//			@Override
-//			public void depart() throws Exception {
-//				file.closegroup();
-//			}
-//			
-//			@Override
-//			public Object take() {
-//				return linkInfo;
-//			}
-//		};
-//
-//		visit(path, v);
-//		return (String) v.take();
-//	}
 
 	/**
 	 * Define the key to be generated and its target
