5b105c981942c722c772bec3be540c519d79fa3b
James Mudd <james.mudd@diamond.ac.uk>
2015-06-01 15:31:15 +0100

    ARPES-332 Workaround for race condition causing a single frame acquition
    Also removed call to zeroSupplies which is removed in new analyser IOCs.
    Added some documentation.
    
    Change-Id: I494ba28dd3e3f9daf61e3907866bae9e4d993870

--- uk.ac.gda.arpes/scripts/arpesmonitor.py
+++ uk.ac.gda.arpes/scripts/arpesmonitor.py
@@ -1,25 +1,31 @@
 import gda.factory.Finder
+from time import sleep
 
 class ARPESMonitor:
+    ''' This class is designed to allow continuous acquisition for using a electron analyser
+    alignment of the experiment
+    '''
 
     def __init__(self):
         self.scienta = gda.factory.Finder.getInstance().listAllLocalObjects("uk.ac.gda.devices.vgscienta.VGScientaAnalyser")[0]
         self.configure()
 
     def configure(self):
+        # Change into fixed mode for alignment
         self.scienta.setFixedMode(True)
+        self.scienta.getNdProc().getPluginBase().setArrayCounter(0);
+        self.scienta.getNdProc().setResetFilter(1);
+        # Set exposures to 1 and change to continuous acquisition
         self.scienta.getAdBase().setNumExposures(1)
+        self.scienta.getAdBase().setImageModeWait(2)
+        # Pause to allow values to be set. Would be better to use setImageModeWait(ImageMode.CONTINUOUS)
+        # however the enum constant isn't available in jython see GDA-6152
+        sleep(0.25)
 
     def start(self):
         self.configure()
-        self.scienta.getAdBase().setImageMode(2)
-        self.scienta.getNdProc().getPluginBase().setArrayCounter(0);
-        self.scienta.getNdProc().setResetFilter(1);
         self.scienta.getAdBase().startAcquiring()
 
     def stop(self):
         self.scienta.getAdBase().stopAcquiring()
-        self.scienta.getAdBase().setImageMode(0)
-        self.scienta.zeroSupplies()
-
 #am=ARPESMonitor()
