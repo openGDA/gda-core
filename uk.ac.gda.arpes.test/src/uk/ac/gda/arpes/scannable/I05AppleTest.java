038da1df1787b744c1e08dd2d77ba11eee3f30ea
James Mudd <james.mudd@diamond.ac.uk>
2015-06-02 14:23:44 +0100

    ARPES-350 Stage 5 Update references after rename/move
    Change-Id: Id06120ba3200a7db2cdac37e50425938112a443b

--- uk.ac.gda.arpes.test/src/uk/ac/gda/arpes/scannable/I05AppleTest.java
+++ uk.ac.gda.arpes.test/src/uk/ac/gda/arpes/scannable/I05AppleTest.java
@@ -16,7 +16,7 @@
  * with GDA. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package uk.ac.gda.beamline.i05.scannable;
+package uk.ac.gda.arpes.scannable;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -34,8 +34,8 @@ import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import org.junit.Before;
 import org.junit.Test;
 
-import uk.ac.gda.beamline.i05.scannable.I05Apple.TrajectorySolver;
-import uk.ac.gda.beamline.i05.scannable.I05Apple.PGMove;
+import uk.ac.gda.arpes.scannable.I05Apple.TrajectorySolver;
+import uk.ac.gda.arpes.scannable.I05Apple.PGMove;
 
 public class I05AppleTest {
 
