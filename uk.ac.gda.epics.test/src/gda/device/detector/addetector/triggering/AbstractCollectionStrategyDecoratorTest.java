7cd35fff69f18c63bd7aa0ea8bde202869a85715
Mark Booth <Mark.Booth@diamond.ac.uk>
2015-06-05 12:04:04 +0100

    BLXV-185 (8.44) Refactor getDecorateesOfType to make it easier to use.
    Make it an instance rather than a class function.
    
    Also add tests to ensure that getDecorateesOfType does what we expect.
    
    Also, tidy up some comments and whitespace issues.
    
    Change-Id: I9812fad4417b2c65a53f76455b529656d725c702

---
+++ uk.ac.gda.epics.test/src/gda/device/detector/addetector/triggering/AbstractCollectionStrategyDecoratorTest.java
@@ -0,0 +1,70 @@
+/*-
+ * Copyright © 2015 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.detector.addetector.triggering;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbstractCollectionStrategyDecoratorTest {
+
+	private SimpleAcquire mockSimpleAcquire;
+	private UnsynchronisedExternalShutterDecorator unsynchronisedExternalShutterDecorator;
+	private MerlinColourModeDecorator merlinColourModeDecorator;
+
+	@Before
+	public void setUp() {
+		mockSimpleAcquire = mock(SimpleAcquire.class);
+		merlinColourModeDecorator = new MerlinColourModeDecorator();
+		unsynchronisedExternalShutterDecorator = new UnsynchronisedExternalShutterDecorator();
+	}
+
+	@Test
+	public void expectEmptyListWithNoIncompatibleDecorators() {
+		merlinColourModeDecorator.setDecoratee(mockSimpleAcquire);
+		List<UnsynchronisedExternalShutterNXCollectionStrategy> unsynchronisedExternalShutterNXCollectionStrategies =
+				merlinColourModeDecorator.getDecorateesOfType(UnsynchronisedExternalShutterNXCollectionStrategy.class);
+
+		assertEquals(0, unsynchronisedExternalShutterNXCollectionStrategies.size());
+	}
+
+	@Test
+	public void expectCorrectSizeListOnCompatibleInnerDecorator() {
+		unsynchronisedExternalShutterDecorator.setDecoratee(mockSimpleAcquire);
+		merlinColourModeDecorator.setDecoratee(unsynchronisedExternalShutterDecorator);
+		List<UnsynchronisedExternalShutterNXCollectionStrategy> unsynchronisedExternalShutterNXCollectionStrategies =
+				merlinColourModeDecorator.getDecorateesOfType(UnsynchronisedExternalShutterNXCollectionStrategy.class);
+
+		assertEquals(1, unsynchronisedExternalShutterNXCollectionStrategies.size());
+	}
+
+	@Test
+	public void expectCorrectSizeListOnCompatibleOuterDecorator() {
+		merlinColourModeDecorator.setDecoratee(mockSimpleAcquire);
+		unsynchronisedExternalShutterDecorator.setDecoratee(merlinColourModeDecorator);
+		List<UnsynchronisedExternalShutterNXCollectionStrategy> unsynchronisedExternalShutterNXCollectionStrategies =
+				unsynchronisedExternalShutterDecorator.getDecorateesOfType(UnsynchronisedExternalShutterNXCollectionStrategy.class);
+
+		assertEquals(1, unsynchronisedExternalShutterNXCollectionStrategies.size());
+	}
+}
