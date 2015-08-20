62aa28b37212272df0ca795b35d45c887248101b
Peter Holloway <peter.holloway@diamond.ac.uk>
2015-08-20 11:23:04 +0100

    Rely on scannable string format rather than getting positions directly
    Change-Id: I4bd4a423caced678ba04a254593c00cd9bd606b5

--- uk.ac.gda.core.test/src/gda/device/scannable/ScannableGroupTest.java
+++ uk.ac.gda.core.test/src/gda/device/scannable/ScannableGroupTest.java
@@ -254,8 +254,8 @@ public class ScannableGroupTest extends TestCase {
 		String[] outputFormat = group.getOutputFormat();
 		Object[] position = (Object[]) group.getPosition();
 
-		assertArraysEqual(new String[] {"input1", "input2","input3"}, inputNames);
-		assertArraysEqual(new String[] {"extra1", "extra2"}, extraNames);
+		assertArraysEqual(new String[] { "input1", "input2", "input3" }, inputNames);
+		assertArraysEqual(new String[] { "extra1", "extra2" }, extraNames);
 		// GDA-5794 from here
 		assertArraysEqual(new Object[] {10., 20., 10., 3., .901}, position);
 		assertArraysEqual(new String[] {"%1.1g", "%2.2g","%4.4g","%3.3f","%5.5g"}, outputFormat);
@@ -280,8 +280,8 @@ public class ScannableGroupTest extends TestCase {
 		String[] outputFormat = group.getOutputFormat();
 		Object[] position = (Object[]) group.getPosition();
 
-		assertArraysEqual(new String[] {"input1", "input2","input3"}, inputNames);
-		assertArraysEqual(new String[] {"extra1", "extra2"}, extraNames);
+		assertArraysEqual(new String[] { "input1", "input2", "input3" }, inputNames);
+		assertArraysEqual(new String[] { "extra1", "extra2" }, extraNames);
 		// GDA-5794 from here
 		assertArraysEqual(new Object[] {10., 20., 10., 3., .901}, position);
 		assertArraysEqual(new String[] {"%1.1g", "%2.2g","%4.4g","%3.3f","%5.5g"}, outputFormat);
