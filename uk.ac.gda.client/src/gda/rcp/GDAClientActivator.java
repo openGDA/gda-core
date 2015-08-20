cc901df436f6b63d2fa0cead97765d20390eeabc
Colin Palmer <colin.palmer@diamond.ac.uk>
2015-08-20 11:03:01 +0100

    Make getService() methods generic
    Change-Id: I46726bcbe04b032b5e38f81e2d7c5d8cf3408ff3

--- uk.ac.gda.client/src/gda/rcp/GDAClientActivator.java
+++ uk.ac.gda.client/src/gda/rcp/GDAClientActivator.java
@@ -153,12 +153,12 @@ public class GDAClientActivator extends AbstractUIPlugin {
 
 	/**
 	 * Method to get a service.
-	 * 
+	 *
 	 * @param serviceClass
 	 * @return any loaded OSGi service which the bundle can see.
 	 */
-	public static Object getService(Class<?> serviceClass) {
-		ServiceReference<?> ref = plugin.getBundle().getBundleContext().getServiceReference(serviceClass);
+	public static <T> T getService(Class<T> serviceClass) {
+		ServiceReference<T> ref = plugin.getBundle().getBundleContext().getServiceReference(serviceClass);
 		return plugin.getBundle().getBundleContext().getService(ref);
 	}
 
