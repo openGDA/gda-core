915463570c43395207c6e0c354095c0c71846bf7
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2015-10-01 15:36:09 +0100

    [Nexus] Initial implementation of nexus file builder API

---
+++ uk.ac.gda.nexus/src/gda/data/nexus/scan/NxDevice.java
@@ -0,0 +1,38 @@
+package gda.data.nexus.scan;
+
+import org.eclipse.dawnsci.nexus.NXobject;
+
+/**
+ * This interface defines a method that returns a new instance
+ * of the appropriate NeXus base class for this device.
+ *
+ * @param <D> the NeXus bass class for the device
+ */
+public interface NxDevice<D extends NXobject> {
+	
+	public enum DeviceType {
+		INSTRUMENT, SAMPLE
+	}
+	
+	/**
+	 * The {@link Class} of the NeXus object returned by
+	 * {@link #createBaseClassInstance()}, a
+	 * subclass of {@link NXobject}.
+	 * 
+	 * @return NeXus base class
+	 */
+	public Class<D> getNexusBaseClass();
+
+	/**
+	 * Returns a new instance of the appropriate NeXus base
+	 * class for this device.
+	 * 
+	 * @return new instance of NeXus base class
+	 */
+	public D createBaseClassInstance();
+	
+	public DeviceType getDeviceType();
+	
+	public String getName();
+	
+}
