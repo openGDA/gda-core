7030ee8247904af8e2d3c4aa7c9bbacc3fcbd58d
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-11-05 17:21:05 +0000

    Remote obselete usage of RemoteSubmission

--- uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/ui/actions/SubmitReconstruction.java
+++ uk.ac.diamond.tomography.reconstruction/src/uk/ac/diamond/tomography/reconstruction/ui/actions/SubmitReconstruction.java
@@ -21,7 +21,7 @@ package uk.ac.diamond.tomography.reconstruction.ui.actions;
 import java.net.URI;
 
 import org.dawb.common.ui.util.EclipseUtils;
-import org.dawnsci.commandserver.core.consumer.RemoteSubmission;
+import org.dawnsci.commandserver.core.ActiveMQServiceHolder;
 import org.dawnsci.commandserver.tomo.beans.TomoBean;
 import org.dawnsci.commandserver.ui.view.StatusQueueView;
 import org.eclipse.core.resources.IResource;
@@ -34,6 +34,8 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.scanning.api.event.IEventService;
+import org.eclipse.scanning.api.event.core.ISubmitter;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -44,51 +46,51 @@ import org.slf4j.LoggerFactory;
 
 /**
  * TODO Example class for an action to submit a reconstruction.
- * 
+ *
  * This action submits a file to be reconstructed using the ActiveMQ server.
  */
 public class SubmitReconstruction implements IWorkbenchWindowActionDelegate {
-	
+
     private Logger logger = LoggerFactory.getLogger(SubmitReconstruction.class);
-    
+
 	private IResource resource;
-	
+
 	@Override
 	public void run(IAction action) {
-		
+
 		if (resource==null) return;
-		
+
 		// TODO Probably want a wizard here - see RerunWizard used for Xia2 reruns.
-		final boolean ok = MessageDialog.openConfirm(Display.getDefault().getActiveShell(), 
+		final boolean ok = MessageDialog.openConfirm(Display.getDefault().getActiveShell(),
 				             "Confirm Test Reconstruction",
 				             "Would you like to run a test reconstruction on '"+resource.getName()+"'?");
-	
+
 	    if (!ok) return;
-	    
+
         try {
     	    final TomoBean tBean = new TomoBean();
     	    tBean.setProjectName("A test tomography reconstruction");
     	    tBean.setFileName(resource.getLocation().toOSString());
     	    tBean.setRunDirectory("/dls/p45/test/");
     	    tBean.setName("Test Recon "+resource.getName());
-    	    
+
     		IPreferenceStore store = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.dawnsci.commandserver.ui");
     		final String uri        = store.getString("org.dawnsci.commandserver.URI");
     		final String queueName  = "scisoft.tomo.SUBMISSION_QUEUE";
 
-    		final RemoteSubmission factory = new RemoteSubmission(new URI(uri));
-    		factory.setQueueName(queueName);
-    		
+			final IEventService service = ActiveMQServiceHolder.getEventService();
+			final ISubmitter<TomoBean> factory = service.createSubmitter(new URI(uri), queueName);
+
     		factory.submit(tBean, true);
 
     		final String secondId = StatusQueueView.createSecondaryId("org.dawnsci.commandserver.tomo", TomoBean.class.getName(), "scisoft.tomo.STATUS_QUEUE", "scisoft.tomo.STATUS_TOPIC", "scisoft.tomo.SUBMISSION_QUEUE");
 		    EclipseUtils.getPage().showView(StatusQueueView.ID, secondId, IWorkbenchPage.VIEW_VISIBLE);
-		
+
         } catch (Exception e) {
-        	
+
         	ErrorDialog.openError(Display.getDefault().getActiveShell(), "Cannot submit reconstruction", "Please contact your support representative.",
         			  new Status(IStatus.ERROR, "uk.ac.diamond.tomography.reconstruction", e.getMessage()));
-        	
+
         	logger.error(e.getMessage(), e);
 		}
 	}
