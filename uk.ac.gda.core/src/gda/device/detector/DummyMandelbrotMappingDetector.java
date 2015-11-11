ef77178c6bf8c37cb2d6177dfc7c1201163f8419
Colin Palmer <colin.palmer@diamond.ac.uk>
2015-11-11 20:42:38 +0000

    Normalise detector output to avoid steps in spectrum
    Change-Id: I84e0523a866679ee664c2c9e687d9c56535b35b9

--- uk.ac.gda.core/src/gda/device/detector/DummyMandelbrotMappingDetector.java
+++ uk.ac.gda.core/src/gda/device/detector/DummyMandelbrotMappingDetector.java
@@ -35,6 +35,9 @@ public class DummyMandelbrotMappingDetector extends DetectorBase implements Nexu
 
 	public static final String VALUE_NAME = "mandelbrot_value";
 
+	// Constants
+	private static final int MAX_ITERATIONS = 500;
+	private static final double ESCAPE_RADIUS = 10.0;
 	private static final int COLUMNS = 301;
 	private static final int ROWS = 241;
 	private static final int POINTS = 1000;
@@ -49,9 +52,6 @@ public class DummyMandelbrotMappingDetector extends DetectorBase implements Nexu
 	private Scannable xPos;
 	private Scannable yPos;
 	private OutputDimensions outputDimensions = OutputDimensions.TWO_D;
-
-	// TODO make this configurable (using some kind of detector parameters?)
-	private int maxIterations = 501;
 
 	public DummyMandelbrotMappingDetector() {
 		super();
@@ -105,18 +105,17 @@ public class DummyMandelbrotMappingDetector extends DetectorBase implements Nexu
 		final double a = ((Double) xPos.getPosition()).doubleValue();
 		final double b = ((Double) yPos.getPosition()).doubleValue();
 
-		// TODO normalise the value somehow?
-		int value = maxIterations - mandelbrot(a, b, maxIterations, 10.0);
+		double value = mandelbrot(a, b);
 
 		data = new NXDetectorData(this);
 		data.addData(getName(), VALUE_NAME, new NexusGroupData(value));
 		data.setPlottableValue(VALUE_NAME, Double.valueOf(value));
 
 		if (outputDimensions == OutputDimensions.ONE_D) {
-			int[] juliaSetLine = calculateJuliaSetLine(a, b, 0.0, 0.0, MAX_X, POINTS);
+			double[] juliaSetLine = calculateJuliaSetLine(a, b, 0.0, 0.0, MAX_X, POINTS);
 			data.addData(getName(), "data", new NexusGroupData(juliaSetLine), null, Integer.valueOf(1));
 		} else if (outputDimensions == OutputDimensions.TWO_D) {
-			int[][] juliaSet = calculateJuliaSet(a, b, COLUMNS, ROWS);
+			double[][] juliaSet = calculateJuliaSet(a, b, COLUMNS, ROWS);
 			data.addData(getName(), "data", new NexusGroupData(juliaSet), null, Integer.valueOf(1));
 		}
 
@@ -126,14 +125,14 @@ public class DummyMandelbrotMappingDetector extends DetectorBase implements Nexu
 	/**
 	 * Fill a Julia set around the origin for the value C = a + bi
 	 */
-	private int[][] calculateJuliaSet(final double a, final double b, int columns, int rows) {
+	private double[][] calculateJuliaSet(final double a, final double b, int columns, int rows) {
 		final double xStart = -MAX_X;
 		final double xStop = MAX_X;
 		final double yStart = -MAX_Y;
 		final double yStop = MAX_Y;
-		final double yStep = (yStop - yStart) / rows;
+		final double yStep = (yStop - yStart) / (rows - 1);
 		double y;
-		int[][] juliaSet = new int[rows][columns];
+		double[][] juliaSet = new double[rows][columns];
 		for (int yIndex = 0; yIndex < rows; yIndex++) {
 			y = yStart + yIndex * yStep;
 			juliaSet[yIndex] = calculateJuliaSetLine(a, b, y, xStart, xStop, columns);
@@ -144,14 +143,13 @@ public class DummyMandelbrotMappingDetector extends DetectorBase implements Nexu
 	/**
 	 * Fill a Julia set line between xStart and xStop at the given y value, for the value C = a + bi
 	 */
-	private int[] calculateJuliaSetLine(final double a, final double b, final double y, final double xStart, final double xStop, final int numPoints) {
-		final double xStep = (xStop - xStart) / numPoints;
+	private double[] calculateJuliaSetLine(final double a, final double b, final double y, final double xStart, final double xStop, final int numPoints) {
+		final double xStep = (xStop - xStart) / (numPoints - 1);
 		double x;
-		int[] juliaSetLine = new int[numPoints];
+		double[] juliaSetLine = new double[numPoints];
 		for (int xIndex = 0; xIndex < numPoints; xIndex++) {
 			x = xStart + xIndex * xStep;
-			// TODO normalise the value somehow?
-			juliaSetLine[xIndex] = maxIterations - julia(x, y, a, b, maxIterations, 10.0);
+			juliaSetLine[xIndex] = julia(x, y, a, b);
 		}
 		return juliaSetLine;
 	}
@@ -159,21 +157,34 @@ public class DummyMandelbrotMappingDetector extends DetectorBase implements Nexu
 	/**
 	 * Iterations of f(z) = z^2 + C, where z = x + yi, C = a + bi, and initial z = 0
 	 */
-	private int mandelbrot(final double a, final double b, final int maxIterations, final double escapeRadius) {
-		return julia(0.0, 0.0, a, b, maxIterations, escapeRadius);
+	private double mandelbrot(final double a, final double b) {
+		return julia(0.0, 0.0, a, b);
 	}
 
 	/**
 	 * Iterations of f(z) = z^2 + C, where z = x + yi and C = a + bi
 	 */
-	private int julia(double x, double y, final double a, final double b, final int maxIterations, final double escapeRadius) {
+	private double julia(double x, double y, final double a, final double b) {
 		int iteration = 0;
-		while (iteration < maxIterations && x * x + y * y < escapeRadius * escapeRadius) {
-			double tempX = x * x - y * y + a;
+		double xSquared, ySquared, tempX;
+		double escapeRadiusSquared = ESCAPE_RADIUS * ESCAPE_RADIUS;
+		do {
+			xSquared = x * x;
+			ySquared = y * y;
+			tempX = xSquared - ySquared + a;
 			y = 2 * x * y + b;
 			x = tempX;
 			iteration++;
+		} while (iteration < MAX_ITERATIONS && xSquared + ySquared < escapeRadiusSquared);
+
+		double modulus = Math.sqrt(x * x + y * y);
+
+		// If modulus > 1.0, normalise the result
+		// (Theoretically, I think this should make the value roughly independent of MAX_ITERATIONS and ESCAPE_RADIUS)
+		if (modulus > 1.0) {
+			return iteration - (Math.log(Math.log(modulus)) / Math.log(2.0));
 		}
+		// Otherwise just return the iteration count
 		return iteration;
 	}
 }
