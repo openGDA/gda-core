283bda927591124105729492d23e5034933c6c84
Peter Holloway <peter.holloway@diamond.ac.uk>
2015-11-30 17:18:10 +0000

    Calculate gaussian edge postion from error function not sum of gaussian
    Previous implementation was (negligably) quicker but returned different values depending on step size of scan
    
    Change-Id: I1f0b2e43f04b35eb57b9bcffc29e6803435e1657

--- uk.ac.gda.core/scripts/gdascripts/pd/pd_gaussian.py
+++ uk.ac.gda.core/scripts/gdascripts/pd/pd_gaussian.py
@@ -47,22 +47,42 @@ class GaussianY(ScannableMotionBase):
 		if self.height == None:
 			return self.background + self.gaussianX.getPosition()
 		val = self.background + self.height * exp( -1.0 *(((self.gaussianX.getPosition() - self.centre )**2)/ self.width) )
-		return val + self.height * self.noise * (0.5 - random.random())
+		return addNoise(val, self.height * self.noise)
 
 	def rawAsynchronousMoveTo(self,new_position):
 		pass
 
-class EdgeY(GaussianY):
-	def __init__(self, name, gaussianX, centre, width, height, background, noise=0):
-		self.cur_position = background
+class EdgeY(ScannableMotionBase):
+	def __init__(self, name, x, centre, width, start, end, noise=0):
 		self.setName(name)
 		self.setInputNames([name])
-		self.reset = background
-		self.gauss = GaussianY(name + "_gauss", gaussianX, centre, width, height, 0, noise)
+		self.gx = x
+		self.centre = centre
+		self.width = width
+		self.start = start
+		self.end = end
+		self.noise = noise
+
+	def rawIsBusy(self):
+		return False
 
 	def rawGetPosition(self):
-		self.cur_position += self.gauss.getPosition()
-		return self.cur_position
+		height = self.end - self.start
+		erf = approxErrorFunction((self.gx.getPosition() - self.centre)/self.width)
+		erf = (erf + 1)/2.0
+		return self.start + height * addNoise(erf, self.noise)
 
-	def atScanLineStart(self):
-		self.cur_position = self.reset
+	def rawAsynchronousMoveTo(self,new_position):
+		pass
+
+def approxErrorFunction(x):
+	#approximation based on https://en.wikipedia.org/wiki/Error_function#Numerical_approximation
+	t = 1.0/(1 + 0.5 * abs(x))
+	tau = t * exp(-x**2 - 1.265512223 + 1.00002368*t + 0.37409196*t**2 + 0.09678418*t**3 - 0.18628806*t**4 + 0.27886807*t**5 - 1.13520398*t**6 + 1.48851587*t**7 - 0.82215223*t**8 + 0.17087277*t**9)
+	if x < 0:
+		return tau - 1
+	else:
+		return 1 - tau
+
+def addNoise(x, noise):
+	return x + noise * (1 - 2 * random.random())
