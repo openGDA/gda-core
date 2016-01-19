c136159469a8d69c95103b809920d9f4147841c4
Richard Fearn <richard.fearn@diamond.ac.uk>
2016-01-19 11:05:07 +0000

    Revert "add write-only defaultPosition property for DummyMotor"
    This reverts commit edc3515397b9841279837ff7699a3c71799244f9.
    
    An initial position can already be specified using the 'position' property,
    so 'defaultPosition' is not required.
    
    The 'defaultPosition' property is only used on MX beamlines, and the GDA MX
    developers will fix those uses.
    
    Change-Id: Ic312edda1327bf20d8b5b09674f1b4a360d78876

--- uk.ac.gda.core/src/gda/device/motor/DummyMotor.java
+++ uk.ac.gda.core/src/gda/device/motor/DummyMotor.java
@@ -67,8 +67,6 @@ public class DummyMotor extends MotorBase implements Runnable, IObservable, Moto
 
 	private volatile double currentPosition;
 
-	private double defaultPosition = 0.0;
-
 	private volatile int status;
 
 	private double speed = 0;
@@ -138,7 +136,7 @@ public class DummyMotor extends MotorBase implements Runnable, IObservable, Moto
 		isInitialised = true;
 
 		runner.setName(getClass().getName() + " " + getName());
-		loadPosition(getName(), defaultPosition);
+		loadPosition(getName(), currentPosition);
 		logger.debug("Loaded motor position " + getPosition());
 		if (speed == 0.0) {
 			speed = getSlowSpeed();
@@ -287,10 +285,6 @@ public class DummyMotor extends MotorBase implements Runnable, IObservable, Moto
 	@Override
 	public void setPosition(double newPosition) {
 		currentPosition = newPosition;
-	}
-
-	public void setDefaultPosition(double defaultPosition) {
-		this.defaultPosition = defaultPosition;
 	}
 
 	/**
