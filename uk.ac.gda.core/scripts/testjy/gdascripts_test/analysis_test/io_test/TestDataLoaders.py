6cfde886d10858979a2258a85cfb8f7faaf39fef
Peter Chang <peter.chang@diamond.ac.uk>
2016-02-24 17:32:36 +0000

    Remove references to ConvertedTIFFImageLoader
    It is exactly the same as TIFFImageLoader
    
    Change-Id: Ibbe106307ac3cecc31a4c2717f16842b6c88fd5f

--- uk.ac.gda.core/scripts/testjy/gdascripts_test/analysis_test/io_test/TestDataLoaders.py
+++ uk.ac.gda.core/scripts/testjy/gdascripts_test/analysis_test/io_test/TestDataLoaders.py
@@ -2,13 +2,6 @@ from gdascripts.analysis.io.dataLoaders import loadImageIntoSFH
 from testjy.gdascripts_test.analysis_test.io_test.images import TESTFILE, IPP_XRAY_EYE_FILE
 from org.eclipse.dawnsci.analysis.api.io import ScanFileHolderException
 from gda.analysis.io import TIFFImageLoader
-try:
-# TODO remove all references to ConvertedTIFFImageLoader as it simply inherits
-# from TIFFImageLoader without any additional functionality
-	from gda.analysis.io import ConvertedTIFFImageLoader
-except ImportError:
-	print "ConvertedTIFFImageLoader can't be imported from PyDev tests without adding a reference to the gda.dls plugin"
-	ConvertedTIFFImageLoader = None
 import os.path
 import unittest
 
@@ -22,28 +15,14 @@ class TestImageLoading(unittest.TestCase):
 		sfh = loadImageIntoSFH(TESTFILE, TIFFImageLoader)
 		self.assert_(sfh is not None)
 	
-	def testLoadIntoSFHWithBodgedLoader(self):
-		# @Ignore if not running on a Diamond Linux box
-		if not os.path.exists("/dls_sw/apps/adsc2tiff/1.0/bin/tiff2tiff"):
-			return
-		sfh = loadImageIntoSFH(IPP_XRAY_EYE_FILE, ConvertedTIFFImageLoader)
+	def testLoadOddImageIntoSFHWithStandardTIFFLoader(self):
+		sfh = loadImageIntoSFH(IPP_XRAY_EYE_FILE, TIFFImageLoader)
 		self.assert_(sfh is not None)
 	
-	def testLoadIntoSFHWhereTheBodgerLoaderIsUsedAsAFallback(self):
-		# @Ignore if not running on a Diamond Linux box
-		if not os.path.exists("/dls_sw/apps/adsc2tiff/1.0/bin/tiff2tiff"):
-			return
+	def testLoadOddImageIntoSFH(self):
 		sfh = loadImageIntoSFH(IPP_XRAY_EYE_FILE)
 		self.assert_(sfh is not None)
 	
-	def testLoadIntoSFHWithBodgedLoaderFails(self):
-		# @Ignore if not running on a Diamond Linux box
-		if not os.path.exists("/dls_sw/apps/adsc2tiff/1.0/bin/tiff2tiff"):
-			return
-		if ConvertedTIFFImageLoader is None:
-			return 
-		self.assertRaises(ScanFileHolderException, loadImageIntoSFH, IPP_XRAY_EYE_FILE+'not here!', ConvertedTIFFImageLoader)
-
 	
 def suite():
 	suite = unittest.TestSuite()
