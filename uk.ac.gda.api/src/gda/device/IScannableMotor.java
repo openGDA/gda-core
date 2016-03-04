91245b4390b5174e1b9db1b6b8036bcf27323a12
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-03-04 11:11:37 +0000

    Add methods to IScannableMotor interface to aid testing
    This change makes it possible to substitute dummy objects with the same interface for testing purposes.
    
    Change-Id: I7b112197c7393aff95c637016b09034aefe42e81

--- uk.ac.gda.api/src/gda/device/IScannableMotor.java
+++ uk.ac.gda.api/src/gda/device/IScannableMotor.java
@@ -18,10 +18,38 @@
 
 package gda.device;
 
+
 /**
  *
  */
 public interface IScannableMotor extends Scannable {
+
+	/**
+	 * Sets the motor used by this scannable motor.
+	 *
+	 * @param motor
+	 *            the motor
+	 */
+	void setMotor(Motor motor);
+
+	/**
+	 * COPY_MOTOR_LIMITS_INTO_SCANNABLE_LIMITS Method required by scripts which need to access the real motor at times. Before the script could get the motor
+	 * name but now that the motor may be set by spring, scripts cannot get the underlying motor.
+	 *
+	 * @return Motor
+	 */
+	Motor getMotor();
+
+	/**
+	 * @param motorName
+	 *            The motorName to set.
+	 */
+	void setMotorName(String motorName);
+
+	/**
+	 * @return Returns the motorName.
+	 */
+	String getMotorName();
 
 	/**
 	 * @return speed of the motor
@@ -46,4 +74,60 @@ public interface IScannableMotor extends Scannable {
 	 */
 	double getUserOffset() throws DeviceException;
 
+	/**
+	 * Returns this motor's time to velocity.
+	 */
+	double getTimeToVelocity() throws DeviceException;
+
+	/**
+	 * Sets this motor's time to velocity.
+	 */
+	void setTimeToVelocity(double timeToVelocity) throws DeviceException;
+	/**
+	 * Set the speed of the underlying motor
+	 *
+	 * @param requiredSpeed
+	 *            in the motor's units
+	 * @throws DeviceException
+	 */
+	void setSpeed(double requiredSpeed) throws DeviceException;
+
+	/**
+	 * Set the position
+	 *
+	 * @throws DeviceException
+	 */
+	void setPosition(Object position) throws DeviceException;
+
+	/**
+	 * Returns the lower motor limit in its external representation. Null if there is no lower Motor limit.
+	 *
+	 * @return limit in external representation
+	 * @throws DeviceException
+	 */
+	Double getLowerMotorLimit() throws DeviceException;
+
+	/**
+	 * Returns the upper motor limit in its external representation. Null if there is no upper Motor limit.
+	 *
+	 * @return limit in external representation
+	 * @throws DeviceException
+	 */
+	Double getUpperMotorLimit() throws DeviceException;
+
+	/**
+	 * Returns the innermost (i.e. the most limiting) of the lower Scannable and Motor limits.
+	 *
+	 * @return the highest minimum limit, or null if neither are set.
+	 * @throws DeviceException
+	 */
+	Double getLowerInnerLimit() throws DeviceException;
+
+	/**
+	 * Returns the innermost (i.e. the most limiting) of the upper Scannable and Motor limits.
+	 *
+	 * @return the lowest maximum limit, or null if neither are set.
+	 * @throws DeviceException
+	 */
+	Double getUpperInnerLimit() throws DeviceException;
 }
