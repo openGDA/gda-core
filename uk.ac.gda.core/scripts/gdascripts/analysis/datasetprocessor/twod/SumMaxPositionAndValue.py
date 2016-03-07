f0e94e4833c0b194459c418e579cc023028d2fc0
Mark Booth <Mark.Booth@diamond.ac.uk>
2016-03-07 15:33:40 +0000

    I15_1-47 (8.51) Report a more user friendly error when Max fails on a dataset
    Change-Id: Id91eb32e85771ad1070de2c4c0d3f224ca9973f4

--- uk.ac.gda.core/scripts/gdascripts/analysis/datasetprocessor/twod/SumMaxPositionAndValue.py
+++ uk.ac.gda.core/scripts/gdascripts/analysis/datasetprocessor/twod/SumMaxPositionAndValue.py
@@ -1,5 +1,6 @@
 from TwodDataSetProcessor import TwodDataSetProcessor
 from org.slf4j import LoggerFactory
+from java.lang import UnsupportedOperationException
 
 class SumMaxPositionAndValue(TwodDataSetProcessor):
 	def __init__(self, name='max',
@@ -19,9 +20,16 @@ class SumMaxPositionAndValue(TwodDataSetProcessor):
 		#assert(dsxaxis==dsxsize)
 		#assert(dsyaxis is None)		# STUB
 		#assert(dsxaxis is None)		# STUB
-		summation = ds.sum()
-		maxval = ds.max()
-		yi, xi = ds.maxPos()
+		try:
+			summation = ds.sum()
+			maxval = ds.max()
+			yi, xi = ds.maxPos()
+		except UnsupportedOperationException, uoe:
+			print "*"*80
+			print "Unable to calculate stats on with dataset %d items per element: %s" % (ds.getElementsPerItem(), ds)
+			print "This could be caused by capturing a colour image rather than a monochrome one"
+			print "*"*80
+			raise uoe
 		# interpolate
 #		sfh = ScanFileHolder()
 #		dsxi = DataSet.arange(dsxsize)
