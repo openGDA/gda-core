a0b5660f4afe0e3b49a3e2c390870a3bfcbacdb9
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-04-22 15:51:24 +0100

    Advance version for next release (8.54)
    Change-Id: I19154b179225def5c8bec001a67ed21dde5e45f6

--- uk.ac.gda.api/src/gda/util/Version.java
+++ uk.ac.gda.api/src/gda/util/Version.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Version {
 
-	private static final String RELEASE_VER = "8.52.0";
+	private static final String RELEASE_VER = "8.54.0";
 
 	/**
 	 * Returns the release version as a String
