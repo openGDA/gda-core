36bd3730dbc536561429d372bfd027800f990637
James Mudd <james.mudd@diamond.ac.uk>
2016-05-09 16:26:54 +0100

    Update MappingScanNewStyleEventObserver to not use the map bean
    With the ability to get the NeXus file tree remotely this bean is not
    needed anymore only the file path is required.
    
    Also change to using the new EventConstants interface to refer to the
    STATUS_TOPIC constant, to remove a warning.
    
    Cleaned up the properties used for the dataserver
    
    Refactored file open and close constants
    
    Updated comments and logging to make clearer what is happening at the
    end of the scan
    
    Change-Id: Idf41b8064bf1b44933468959db1d6aa767f6a4e2

--- uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/MappingScanNewStyleEventObserver.java
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/MappingScanNewStyleEventObserver.java
@@ -20,15 +20,11 @@ package uk.ac.diamond.daq.mapping.ui.experiment;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.dawnsci.mapping.ui.datamodel.LiveDataBean;
-import org.dawnsci.mapping.ui.datamodel.MapBean;
-import org.dawnsci.mapping.ui.datamodel.MappedBlockBean;
-import org.dawnsci.mapping.ui.datamodel.MappedDataFileBean;
+import org.eclipse.scanning.api.event.EventConstants;
 import org.eclipse.scanning.api.event.EventException;
 import org.eclipse.scanning.api.event.IEventService;
 import org.eclipse.scanning.api.event.core.ISubscriber;
@@ -47,13 +43,13 @@ public class MappingScanNewStyleEventObserver implements IScanListener {
 
 	private static final Logger logger = LoggerFactory.getLogger(MappingScanNewStyleEventObserver.class);
 
+	// TODO These constants would ideally be defined somewhere in the Dawn mapping UI code
+	private static final String DAWNSCI_MAPPING_FILE_OPEN = "org/dawnsci/events/file/OPEN";
+	private static final String DAWNSCI_MAPPING_FILE_CLOSE = "org/dawnsci/events/file/CLOSE";
+
 	private IEventService eventService;
 	private ISubscriber<IScanListener> subscriber;
 	private EventAdmin eventAdmin;
-
-	private static final String NEXUS_PREFIX = "/entry/instrument/";
-
-
 
 	public void bindIEventService(IEventService eventService) {
 		logger.debug("bindIEventService called with {}", eventService.toString());
@@ -90,7 +86,7 @@ public class MappingScanNewStyleEventObserver implements IScanListener {
 
 		try {
 			final URI uri = new URI(LocalProperties.get(LocalProperties.GDA_ACTIVEMQ_BROKER_URI, ""));
-			subscriber = eventService.createSubscriber(uri, IEventService.STATUS_TOPIC);
+			subscriber = eventService.createSubscriber(uri, EventConstants.STATUS_TOPIC);
 			subscriber.addListener(this);
 		} catch (URISyntaxException | EventException e) {
 			logger.error("Could not subscribe to the event service", e);
@@ -106,90 +102,43 @@ public class MappingScanNewStyleEventObserver implements IScanListener {
 	@Override
 	public void scanStateChanged(ScanEvent event) {
 		ScanBean scanBean = event.getBean();
-		// FIXME This fails at the moment the scanbean doesn't have the file path (hardcoded in MappingScanSubmitter)
 		final String filePath = scanBean.getFilePath();
 		// Scan started
 		if (scanBean.scanStart() == true) {
 			logger.info("Pushing data to live visualisation from SWMR file: {}", filePath);
-			final MappedDataFileBean mappedDataFileBean = createMappingBeanDescription(scanBean, true);
-			if (mappedDataFileBean != null) {
-				// Create map holding the info needed to display the map
-				Map<String, Object> eventMap = new HashMap<String, Object>();
-				eventMap.put("path", filePath);
-				eventMap.put("map_bean", mappedDataFileBean);
-				eventAdmin.postEvent(new Event("org/dawnsci/events/file/OPEN", eventMap));
-			}
-		}
-		// Scan Ended swap out SWMR file for non-SWMR
-		if (scanBean.scanEnd() == true) {
-			logger.info("Switching SWMR file to non-SWMR: {}", filePath);
-			Map<String, Object> eventMap = new HashMap<String, Object>();
-			eventMap.put("path", filePath);
-			// Close the old file
-			eventAdmin.postEvent(new Event("org/dawnsci/events/file/CLOSE", eventMap));
-			// Reopen the non-SWMR file
-			eventAdmin.postEvent(new Event("org/dawnsci/events/file/OPEN", eventMap));
-		}
-	}
 
-	// TODO This method shouldn't be needed once we have self describing files
-	private MappedDataFileBean createMappingBeanDescription(ScanBean scanBean, boolean liveFile) {
-
-		MappedDataFileBean mappedDataFileBean = new MappedDataFileBean();
-
-		// Find all the detectors in the scan
-		Map<String, ?> detectors = scanBean.getScanRequest().getDetectors();
-		for (String detectorName : detectors.keySet()) {
-
-			MappedBlockBean mappedBlockBean = new MappedBlockBean();
-			String nexusDetectorPrefix = NEXUS_PREFIX + detectorName + "/";
-
-			List<String> axisNames = new ArrayList<>();
-			// Handle 2D rectangle scan
-			// FIXME Not sure if this is the best way to get the dimensions or if it is reliable
-			mappedBlockBean.setyDim(0);
-			mappedBlockBean.setxDim(1);
-			// FIXME This is using hard coded strings hopefully the correct NeXus wont need this
-			axisNames.add(NEXUS_PREFIX + MappingScanRequestHandler.Y_AXIS_NAME + "/value_demand");
-			axisNames.add(NEXUS_PREFIX + MappingScanRequestHandler.X_AXIS_NAME + "/value_demand");
-			axisNames.add(null);
-			axisNames.add(null);
-
-			mappedBlockBean.setAxes(axisNames.toArray(new String[axisNames.size()]));
-
-			// FIXME Hardcode to 4D ie 2D from scanning axis and 2D from detector
-			int scanDimensions = 2;
-			int detectorDimensions = 2;
-			mappedBlockBean.setRank(scanDimensions + detectorDimensions);
-			mappedBlockBean.setName(nexusDetectorPrefix + "data");
-
-			mappedDataFileBean.addBlock(mappedBlockBean);
-
-			MapBean mapBean = new MapBean();
-			mapBean.setName(nexusDetectorPrefix + "value"); // FIXME "value" is hard-coded to match MandelbrotDetector only!
-			mapBean.setParent(nexusDetectorPrefix + "data");
-			mappedDataFileBean.addMap(mapBean);
-		}
-
-		// This section add the live info to allow the SWMR visualisation
-		if (liveFile) {
-			// Check if the required properties are set
-			// FIXME There is a issue here that OSGi starts services before the properties are loaded so you don't get them
-			if (LocalProperties.get(LocalProperties.GDA_DATASERVER_HOST) == null || LocalProperties.getAsInt(LocalProperties.GDA_DATASERVER_PORT) == null) {
-				logger.error("Properties {} or {} are not set not live data can be provided!", LocalProperties.GDA_DATASERVER_HOST, LocalProperties.GDA_DATASERVER_PORT);
-			}
-			// else { // FIXME this else should handle the case where the properties are available
 			// Create the LiveDataBean
 			LiveDataBean liveDataBean = new LiveDataBean();
 
-			// TODO have coded defaults here which will work for testing but they should be removed
-			liveDataBean.setHost(LocalProperties.get(LocalProperties.GDA_DATASERVER_HOST, "localhost"));
-			liveDataBean.setPort(LocalProperties.getAsInt(LocalProperties.GDA_DATASERVER_PORT, 8690));
-			mappedDataFileBean.setLiveBean(liveDataBean);
-			// }
+			// Configure the liveDataBean with a host and port to reach a dataserver
+			liveDataBean.setHost(LocalProperties.get(LocalProperties.GDA_DATASERVER_HOST));
+			// Default the port to -1 so it can be checked next. An Integer is unboxed here to int
+			liveDataBean.setPort(LocalProperties.getAsInt(LocalProperties.GDA_DATASERVER_PORT, -1));
+			// Check the liveDataBean is valid
+			if (liveDataBean.getHost() == null || liveDataBean.getPort() == -1) {
+				logger.error("Live visualisation failed. The properties: {} or {} have not been set",
+						LocalProperties.GDA_DATASERVER_HOST, LocalProperties.GDA_DATASERVER_PORT);
+				// We can't do anything live at this point so return
+				return;
+			}
+
+			// Create map holding the info needed to display the map
+			Map<String, Object> eventMap = new HashMap<String, Object>();
+			eventMap.put("path", filePath);
+			eventMap.put("live_bean", liveDataBean);
+
+			// Send the event
+			eventAdmin.postEvent(new Event(DAWNSCI_MAPPING_FILE_OPEN, eventMap));
 		}
-
-		return mappedDataFileBean;
+		// Scan ended swap out remote SWMR file access for direct file access
+		if (scanBean.scanEnd() == true) {
+			logger.info("Switching from remote SWMR file to direct access: {}", filePath);
+			Map<String, Object> eventMap = new HashMap<String, Object>();
+			eventMap.put("path", filePath);
+			// Close the old remote file
+			eventAdmin.postEvent(new Event(DAWNSCI_MAPPING_FILE_CLOSE, eventMap));
+			// Reopen the file
+			eventAdmin.postEvent(new Event(DAWNSCI_MAPPING_FILE_OPEN, eventMap));
+		}
 	}
-
 }
