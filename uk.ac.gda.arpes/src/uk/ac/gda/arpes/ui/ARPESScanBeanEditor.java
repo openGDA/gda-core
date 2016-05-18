19fcaf609030a7a19fc79183d2b67ecba8a9be52
James Mudd <james.mudd@diamond.ac.uk>
2016-05-18 17:45:26 +0100

    I05-27 Fix .arpes editor "Save As" saved .xml not .arpes file
    Override the doSaveAs method to switch the hardcoded .xml extension to
    .arpes
    
    Change-Id: Ic7a2ea5b545d31933dc7261c2078bfaa627478bf

--- uk.ac.gda.arpes/src/uk/ac/gda/arpes/ui/ARPESScanBeanEditor.java
+++ uk.ac.gda.arpes/src/uk/ac/gda/arpes/ui/ARPESScanBeanEditor.java
@@ -18,13 +18,27 @@
 
 package uk.ac.gda.arpes.ui;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.ide.FileStoreEditorInput;
+import org.eclipse.ui.part.FileEditorInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.gda.arpes.beans.ARPESScanBean;
+import uk.ac.gda.common.rcp.util.EclipseUtils;
 import uk.ac.gda.richbeans.editors.RichBeanEditorPart;
 import uk.ac.gda.richbeans.editors.RichBeanMultiPageEditorPart;
 
@@ -70,5 +84,71 @@ public final class ARPESScanBeanEditor extends RichBeanMultiPageEditorPart {
 	public void replaceBean(Object object) {
 		this.editingBean = object;
 		linkUI();
+	}
+
+	/**
+	 * Override super class's doSaveAs() to remove switch hard coded .xml extension to .arpes in the filename.
+	 */
+	@Override
+	public void doSaveAs() {
+
+		final IFile currentiFile = EclipseUtils.getIFile(getEditorInput());
+		final IFolder folder = (currentiFile != null) ? (IFolder) currentiFile.getParent() : null;
+
+		final FileDialog dialog = new FileDialog(getSite().getShell(), SWT.SAVE);
+		dialog.setText("Save as ARPES file");
+		dialog.setFilterExtensions(new String[] { "*.arpes" });
+		final File currentFile = new File(this.path);
+		dialog.setFilterPath(currentFile.getParentFile().getAbsolutePath());
+		dialog.setFileName(currentFile.getName());
+
+		String newFile = dialog.open();
+		if (newFile != null) {
+
+			newFile = validateFileName(newFile);
+			if (newFile == null)
+				return;
+
+			final File file = new File(newFile);
+			if (file.exists()) {
+				final boolean ovr = MessageDialog.openQuestion(getSite().getShell(), "Confirm File Overwrite",
+						"The file '" + file.getName() + "' exists in '" + file.getParentFile().getName() + "'.\n\n" + "Would you like to overwrite it?");
+				if (!ovr)
+					return;
+			}
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				MessageDialog.openError(getSite().getShell(), "Cannot save file.",
+						"The file '" + file.getName() + "' cannot be created in '" + file.getParentFile().getName() + "'.\n\n" + e.getMessage());
+				return;
+			}
+			try {
+				if (!confirmFileNameChange(currentFile, file)) {
+					file.delete();
+					return;
+				}
+			} catch (Exception ne) {
+				logger.error("Cannot confirm name change", ne);
+				return;
+			}
+
+			IEditorInput input;
+			if (folder != null && folder.getLocation().toFile().equals(file.getParentFile())) {
+				final IFile ifile = folder.getFile(file.getName());
+				try {
+					ifile.refreshLocal(IResource.DEPTH_ZERO, null);
+				} catch (CoreException e) {
+					logger.error("Cannot refresh " + ifile, e);
+				}
+				input = new FileEditorInput(ifile);
+			} else {
+				input = new FileStoreEditorInput(EFS.getLocalFileSystem().fromLocalFile(file));
+			}
+
+			assignInput(input);
+			doSave(new NullProgressMonitor());
+			setDirty(false);
+		}
 	}
 }
