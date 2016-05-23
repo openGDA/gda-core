e767d0664c57213c4facef71ebea8dbcd753fb54
Colin Palmer <colin.palmer@diamond.ac.uk>
2016-05-23 15:38:50 +0100

    DAQ-180 Replace Mandelbrot preprocessor with Spring config for axis names
    Change-Id: Ie150a6a3fba2d5acc5db6fefe193c06ec934abc0

--- uk.ac.diamond.daq.mapping.test/src/uk/ac/diamond/daq/mapping/test/PreprocessorPluginTest.java
+++ uk.ac.diamond.daq.mapping.test/src/uk/ac/diamond/daq/mapping/test/PreprocessorPluginTest.java
@@ -26,7 +26,6 @@ import org.eclipse.scanning.api.event.scan.ScanBean;
 import org.eclipse.scanning.api.event.scan.ScanRequest;
 import org.eclipse.scanning.api.points.models.GridModel;
 import org.eclipse.scanning.api.scan.process.IPreprocessor;
-import org.eclipse.scanning.example.detector.MandelbrotModel;
 import org.eclipse.scanning.server.servlet.ScanServlet;
 import org.junit.After;
 import org.junit.Before;
@@ -34,7 +33,6 @@ import org.junit.Test;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
-import uk.ac.diamond.daq.mapping.impl.MandelbrotPreprocessor;
 import uk.ac.diamond.daq.mapping.impl.MappingStagePreprocessor;
 import uk.ac.gda.core.GDACoreActivator;
 
@@ -44,7 +42,6 @@ public class PreprocessorPluginTest {
 	private static final String CONFIGURED_Y_MOTOR_NAME = "y_motor";
 
 	private BundleContext bundleContext;
-	private ServiceRegistration<IPreprocessor> mandelbrotRegistration;
 	private ServiceRegistration<IPreprocessor> mappingRegistration;
 
 	private ScanServlet scanServlet;
@@ -54,11 +51,6 @@ public class PreprocessorPluginTest {
 		bundleContext = GDACoreActivator.getBundleContext();
 
 		// Set up the preprocessors. Normally this would be done by the Spring config.
-		MandelbrotPreprocessor mandelbrotPreprocessor = new MandelbrotPreprocessor();
-		mandelbrotPreprocessor.setRealAxisName(CONFIGURED_X_MOTOR_NAME);
-		mandelbrotPreprocessor.setImaginaryAxisName(CONFIGURED_Y_MOTOR_NAME);
-		mandelbrotRegistration = bundleContext.registerService(IPreprocessor.class, mandelbrotPreprocessor, null);
-
 		MappingStagePreprocessor mappingStagePreprocessor = new MappingStagePreprocessor();
 		mappingStagePreprocessor.setActiveFastScanAxis(CONFIGURED_X_MOTOR_NAME);
 		mappingStagePreprocessor.setActiveSlowScanAxis(CONFIGURED_Y_MOTOR_NAME);
@@ -70,7 +62,6 @@ public class PreprocessorPluginTest {
 	@After
 	public void tearDown() throws Exception {
 		scanServlet = null;
-		mandelbrotRegistration.unregister();
 		mappingRegistration.unregister();
 
 		// Ensure all the preprocessors have been removed
@@ -81,10 +72,6 @@ public class PreprocessorPluginTest {
 	@Test
 	public void axisNamesShouldBeReplaced() throws Exception {
 		ScanRequest<?> scanRequest = new ScanRequest<>();
-		MandelbrotModel mandelbrotModel = new MandelbrotModel();
-		mandelbrotModel.setRealAxisName("stage_x");
-		mandelbrotModel.setImaginaryAxisName("stage_y");
-		scanRequest.putDetector("mandelbrot_detector", mandelbrotModel);
 
 		GridModel gridModel = new GridModel();
 		gridModel.setFastAxisName("stage_x");
@@ -98,8 +85,6 @@ public class PreprocessorPluginTest {
 		// Ensure the names are different to begin with
 		assertNotEquals(CONFIGURED_X_MOTOR_NAME, gridModel.getFastAxisName());
 		assertNotEquals(CONFIGURED_Y_MOTOR_NAME, gridModel.getSlowAxisName());
-		assertNotEquals(CONFIGURED_X_MOTOR_NAME, mandelbrotModel.getRealAxisName());
-		assertNotEquals(CONFIGURED_Y_MOTOR_NAME, mandelbrotModel.getImaginaryAxisName());
 
 		// This next call is potentially very fragile. At the moment, it seems we can get away with calling
 		// createProcess() without most of the scanning-related services or ActiveMQ available, and the servlet still
@@ -109,7 +94,5 @@ public class PreprocessorPluginTest {
 		// Check the names have been correctly changed by the preprocessors
 		assertEquals(CONFIGURED_X_MOTOR_NAME, gridModel.getFastAxisName());
 		assertEquals(CONFIGURED_Y_MOTOR_NAME, gridModel.getSlowAxisName());
-		assertEquals(CONFIGURED_X_MOTOR_NAME, mandelbrotModel.getRealAxisName());
-		assertEquals(CONFIGURED_Y_MOTOR_NAME, mandelbrotModel.getImaginaryAxisName());
 	}
 }
