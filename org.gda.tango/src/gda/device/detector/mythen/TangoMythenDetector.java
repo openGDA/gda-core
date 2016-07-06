d330ade02c2e0979d0449644ddbe123ad3a8b162
Peter Chang <peter.chang@diamond.ac.uk>
2016-07-06 11:53:17 +0100

    Refactor to new January namespace
    Change-Id: I723fb548ab7072f311460be1015f5dad8d57a1a3

--- org.gda.tango/src/gda/device/detector/mythen/TangoMythenDetector.java
+++ org.gda.tango/src/gda/device/detector/mythen/TangoMythenDetector.java
@@ -19,8 +19,8 @@
 
 package gda.device.detector.mythen;
 
-import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
-import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
+import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.DatasetFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
