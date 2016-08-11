8dfdb06c4d6e886102f9db469097f56d7f56ec81
Peter Holloway <peter.holloway@diamond.ac.uk>
2016-08-11 16:41:59 +0100

    Prevent NPE when saving default HPLC/BSSC files

--- uk.ac.gda.devices.hatsaxs/src/uk/ac/gda/devices/hatsaxs/HatsaxsUtils.java
+++ uk.ac.gda.devices.hatsaxs/src/uk/ac/gda/devices/hatsaxs/HatsaxsUtils.java
@@ -20,7 +20,16 @@ package uk.ac.gda.devices.hatsaxs;
 
 import gda.configuration.properties.LocalProperties;
 import gda.data.PathConstructor;
+import gda.rcp.DataProject;
+
 import java.io.File;
+import java.net.URI;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.runtime.CoreException;
 
 public class HatsaxsUtils {
 
@@ -50,4 +59,22 @@ public class HatsaxsUtils {
 		return getHplcFileFromName(DEFAULT_FILE_NAME);
 	}
 	
+	public static void refreshXmlDirectory() {
+		IProject project = DataProject.getDataProjectIfExists();
+		if (project != null) {
+			try {
+				IWorkspace wksp = project.getWorkspace();
+				IWorkspaceRoot root = wksp.getRoot();
+				URI xml = new File(getXmlDirectory()).toURI();
+				IContainer[] containers = root.findContainersForLocationURI(xml);
+				if (containers.length > 0) {
+					containers[0].refreshLocal(2, null);
+				}
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
