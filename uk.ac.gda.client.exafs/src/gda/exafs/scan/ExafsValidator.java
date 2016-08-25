9192df1e779cf8240f8fca6533656c0569560408
Iain Hall <iain.hall@diamond.ac.uk>
2016-08-25 12:32:34 +0100

    I20-50 : Remove ROI check for Medipix in detector validator.
    ROI check for Medipix detector has been removed since this is now configured using an XML file, the same as the other detectors.
    
    Change-Id: I468b9e7f8953695f71600ab826d1c129b4e1aaa3

--- uk.ac.gda.client.exafs/src/gda/exafs/scan/ExafsValidator.java
+++ uk.ac.gda.client.exafs/src/gda/exafs/scan/ExafsValidator.java
@@ -29,7 +29,6 @@ import org.slf4j.LoggerFactory;
 
 import gda.configuration.properties.LocalProperties;
 import gda.device.Scannable;
-import gda.device.detector.nxdetector.roi.PlotServerROISelectionProvider;
 import gda.factory.Findable;
 import gda.factory.Finder;
 import gda.util.Element;
@@ -158,35 +157,14 @@ public abstract class ExafsValidator extends AbstractValidator {
 			return errors;
 		}
 
-		if (iDetectorParams.getExperimentType().equalsIgnoreCase(DetectorParameters.FLUORESCENCE_TYPE)) {
-			if (iDetectorParams.getFluorescenceParameters().getConfigFileName() == null
-					|| iDetectorParams.getFluorescenceParameters().getConfigFileName().isEmpty()) {
+		String fileName = "";
+		if (iDetectorParams.getExperimentType().equalsIgnoreCase(DetectorParameters.FLUORESCENCE_TYPE))
+			fileName = iDetectorParams.getFluorescenceParameters().getConfigFileName();
+		else if (iDetectorParams.getExperimentType().equalsIgnoreCase(DetectorParameters.XES_TYPE))
+			fileName = iDetectorParams.getXesParameters().getConfigFileName();
+
+		if (fileName == null || fileName.isEmpty())
 				errors.add(new InvalidBeanMessage("Fluorescence detector XML configuration file not specified!"));
-			}
-		} else if (iDetectorParams.getExperimentType().equalsIgnoreCase(DetectorParameters.XES_TYPE)) {
-
-			if (iDetectorParams.getXesParameters().getDetectorType().equals("Medipix")) {
-				// See if ROI is set for medipix and set warning message if needed. imh 2/2/2016
-				// Works well but not from built client, causes errors :
-				// java.lang.NoClassDefFoundError: gda/device/detector/nxdetector/roi/PlotServerROISelectionProvider
-				// Caused by: java.lang.ClassNotFoundException: gda.device.detector.nxdetector.roi.PlotServerROISelectionProvider
-				// Dependency issue?!
-
-				try {
-					PlotServerROISelectionProvider roiProvider = new PlotServerROISelectionProvider("medipix", 1);
-					if (roiProvider.getRoi(0) == null)
-						errors.add(new InvalidBeanMessage("\nMedipix does not have any ROIs set\n"));
-				} catch (Exception e1) {
-					errors.add(new InvalidBeanMessage("Problem getting Medipix ROI information"));
-					logger.info(e1.toString());
-				}
-
-			} else if (iDetectorParams.getXesParameters().getConfigFileName() == null
-					|| iDetectorParams.getXesParameters().getConfigFileName().isEmpty()) {
-				// Display warning only if not using Medipix - since this is not currently configured using an xml file. imh 1/2/2016
-				errors.add(new InvalidBeanMessage("Fluorescence detector XML configuration file not specified!"));
-			}
-		}
 
 		if (bean != null) {
 			setFileName(errors, bean.getDetectorFileName());
