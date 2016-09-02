fbf20983096abae76836f6a07b9ffacf8cdef03a
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-09-02 18:04:47 +0100

    Add dawn new intro contribution (plugin only used by DAWN)
    Change-Id: Ia89c697b30fafba4f0f4375dc93db81cd3ab5f1b

---
+++ uk.ac.diamond.tomography.reconstruction.dawn/src/uk/ac/diamond/tomography/reconstruction/dawn/TomoPerspectiveLaunch.java
@@ -0,0 +1,11 @@
+package uk.ac.diamond.tomography.reconstruction.dawn;
+
+import org.dawb.common.ui.perspective.AbstractPerspectiveLaunch;
+
+public class TomoPerspectiveLaunch extends AbstractPerspectiveLaunch {
+
+	@Override
+	public String getID() {
+		return "uk.ac.diamond.tomography.reconstruction.perspective";
+	}
+}
