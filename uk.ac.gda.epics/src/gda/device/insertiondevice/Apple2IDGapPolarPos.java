95b29222b4f7452167fec2548103e4f6382f7977
fajinyuan <fajin.yuan@diamond.ac.uk>
2016-09-19 14:35:51 +0100

    Refactor codes: 1. pull out PolarisationMode as it was defined in 2 places 2. update Polarisation mode to what Scientists requested 3. pull up getPolarisationMode() methods to IApple2ID interface so to remove duplicated implementation in both control and scannable. 4. update unit test 5. make maximum gap configurable so can be used for different beamline 6. add access to idcontrol so up level object can use these limits in PVs without need to get them from somewhere else.
    Change-Id: I8cf0c19d9604bdd15f8cd455d0dd149dcdbf17ae

--- uk.ac.gda.epics/src/gda/device/insertiondevice/Apple2IDGapPolarPos.java
+++ uk.ac.gda.epics/src/gda/device/insertiondevice/Apple2IDGapPolarPos.java
@@ -23,45 +23,40 @@ import java.util.List;
 import gda.device.DeviceException;
 
 /**
- * Scannable to allow control of an AppleII ID by specifying gap, polarisation and motor position
+ * Scannable to allow control of an AppleII ID by specifying gap, polarisation mode and phase motor position.
  * <p>
- * This is a relatively low-level control, allowing you to specify ID gap, polarisation and motor position. Beamlines may prefer to have two scannables to allow
- * you to specify energy and polarisation separately and will also adjust the PGM as appropriate.
+ * This class expects as input an array of elements: [&ltgap_in_mm&gt, &ltpolarisation_mode&gt, &ltphase_motor_position_in_mm&gt]
  * <p>
- * This class expects as input an array of elements: [<gap in mm>, <polarisation>, <motor position in mm>]
+ * Polarisation Mode can be:<br>
+ * <ul>
+ * <li>LH (Linear Horizontal)<br>
+ * <li>LV (Linear Vertical)<br>
+ * <li>CR (Circular Right)<br>
+ * <li>CL (Circular Left)<br>
+ * <li>LAP (Linear Angular Positive)<br>
+ * <li>LAN (Linear Angular Negative)
+ * </ul>
  * <p>
- * Polarisation can be:<br>
- * LH (horizontal)<br>
- * C (circular)<br>
- * L1 (linear 1)<br>
- * L2 (linear 2)
+ * Motor position will be used to set the positions of the individual soft motors as described in http://confluence.diamond.ac.uk/display/I21/Insertion+device
  * <p>
- * Motor position will be used to set the positions of the individual soft motors as described in
- * http://confluence.diamond.ac.uk/display/I21/i21+software+setup#i21softwaresetup-Insertiondevice%28ID%29
- * <p>
- * If polarisation is LH, the motor position parameter will be ignored, but must be supplied.
- * <p>
- * Vertical polarisation is a special case of circular polarisation, where motor position is the maximum allowed value.
+ * If polarisation mode is LH or LV, the phase motor position parameter will be ignored, but must be supplied.
  * <p>
  * Control of the ID is delegated to a lower-level control that implements IApple2ID.
+ *
+ * @see Apple2IDPolarisationMode
  */
 public class Apple2IDGapPolarPos extends Apple2IDScannableBase {
-
-	// See comment above for description of possible modes
-	private enum PolarisationMode {
-		LH, C, L1, L2, UNKNOWN
-	}
 
 	public Apple2IDGapPolarPos() {
 		setInputNames(new String[] { "gap", "polarisation", "motorPos" });
 		setExtraNames(new String[] { "mode", "enabled", "topOuterMotor", "topInnerMotor", "bottomOuterMotor", "bottomInnerMotor" });
-		setOutputFormat(new String[] { "%5.3fmm", "%s", "%5.3fmm", "%s", "%5.3fmm", "%5.3fmm", "%5.3fmm", "%5.3fmm", "%5.3fmm" });
+		setOutputFormat(new String[] { "%5.3fmm", "%s", "%5.3fmm", "%s", "%b", "%5.3fmm", "%5.3fmm", "%5.3fmm", "%5.3fmm" });
 	}
 
 	@Override
 	public void rawAsynchronousMoveTo(Object position) throws DeviceException {
 
-		PolarisationMode mode;
+		Apple2IDPolarisationMode mode;
 		double motorPos = 0;
 
 		// Parse parameters
@@ -77,9 +72,9 @@ public class Apple2IDGapPolarPos extends Apple2IDScannableBase {
 		final double gap = parseParamToDouble(params[0]);
 
 		try {
-			mode = PolarisationMode.valueOf(params[1].toString().toUpperCase());
+			mode = Apple2IDPolarisationMode.valueOf(params[1].toString().toUpperCase());
 		} catch (Exception ex) {
-			throw new DeviceException(String.format("invalid mode: must be LH, C, L1 or L2"));
+			throw new DeviceException(String.format("invalid mode: must be LH, LV, CR, CL, LAP or LAN"));
 		}
 
 		motorPos = parseParamToDouble(params[2]);
@@ -88,13 +83,19 @@ public class Apple2IDGapPolarPos extends Apple2IDScannableBase {
 		case LH:
 			controller.asynchronousMoveTo(new Apple2IDPosition(gap, 0, 0, 0, 0));
 			break;
-		case C:
+		case LV:
+			controller.asynchronousMoveTo(new Apple2IDPosition(gap, controller.getMaxPhaseMotorPos(), 0, 0, controller.getMaxPhaseMotorPos()));
+			break;
+		case CR:
 			controller.asynchronousMoveTo(new Apple2IDPosition(gap, motorPos, 0, 0, motorPos));
 			break;
-		case L1:
+		case CL:
+			controller.asynchronousMoveTo(new Apple2IDPosition(gap, -motorPos, 0, 0, -motorPos));
+			break;
+		case LAP:
 			controller.asynchronousMoveTo(new Apple2IDPosition(gap, motorPos, 0, 0, -motorPos));
 			break;
-		case L2:
+		case LAN:
 			controller.asynchronousMoveTo(new Apple2IDPosition(gap, 0, motorPos, -motorPos, 0));
 			break;
 		default:
@@ -106,11 +107,10 @@ public class Apple2IDGapPolarPos extends Apple2IDScannableBase {
 	public Object rawGetPosition() throws DeviceException {
 		final Object[] result = new Object[9];
 		final Apple2IDPosition position = controller.getPosition();
-		final PolarisationMode polarisationMode = getPolarisationMode(position.topOuterPos, position.topInnerPos, position.bottomOuterPos,
-				position.bottomInnerPos);
+		final Apple2IDPolarisationMode polarisationMode = controller.getPolarisationMode(position);
 		result[0] = position.gap;
 		result[1] = polarisationMode.toString();
-		result[2] = (polarisationMode == PolarisationMode.L2) ? position.topInnerPos : position.topOuterPos;
+		result[2] = (polarisationMode == Apple2IDPolarisationMode.LAN) ? position.topInnerPos : position.topOuterPos;
 		result[3] = controller.getIDMode();
 		result[4] = controller.isEnabled();
 		result[5] = position.topOuterPos;
@@ -118,25 +118,5 @@ public class Apple2IDGapPolarPos extends Apple2IDScannableBase {
 		result[7] = position.bottomOuterPos;
 		result[8] = position.bottomInnerPos;
 		return result;
-	}
-
-	// Get polarisation mode. This is currently the same code as in Apple2IDBase, but the definition of polarisation may change in future,
-	// so the two functions are kept separate.
-	private PolarisationMode getPolarisationMode(double topOuterPos, double topInnerPos, double bottomOuterPos, double bottomInnerPos) {
-
-		if (motorPositionsEqual(topOuterPos, 0) && motorPositionsEqual(topInnerPos, 0) && motorPositionsEqual(bottomOuterPos, 0)
-				&& motorPositionsEqual(bottomInnerPos, 0)) {
-			return PolarisationMode.LH;
-		}
-		if (motorPositionsEqual(topOuterPos, bottomInnerPos) && motorPositionsEqual(topInnerPos, 0) && motorPositionsEqual(bottomOuterPos, 0)) {
-			return PolarisationMode.C;
-		}
-		if (motorPositionsEqual(topOuterPos, -bottomInnerPos) && motorPositionsEqual(topInnerPos, 0) && motorPositionsEqual(bottomOuterPos, 0)) {
-			return PolarisationMode.L1;
-		}
-		if (motorPositionsEqual(topInnerPos, -bottomOuterPos) && motorPositionsEqual(topOuterPos, 0) && motorPositionsEqual(bottomInnerPos, 0)) {
-			return PolarisationMode.L2;
-		}
-		return PolarisationMode.UNKNOWN;
 	}
 }
