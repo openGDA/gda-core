554505c9ce9e3702abaaa0e5aa81f24cb74a946c
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2016-09-21 16:15:03 +0100

    [DAQ-329] move gui generator to daq-platform
    Change-Id: I6cc529d1e4b8cff3438833303383e7ee8145cedd

---
+++ uk.ac.diamond.daq.guigenerator.test/src/uk/ac/diamond/daq/guigenerator/test/ListStructuredContentProviderTest.java
@@ -0,0 +1,53 @@
+/*-
+ * Copyright © 2016 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.daq.guigenerator.test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import uk.ac.diamond.daq.guigenerator.ListStructuredContentProvider;
+
+public class ListStructuredContentProviderTest {
+	@Test
+	public void testConvertsListToArray(){
+		assertArrayEquals(
+				new Object[]{1,2,3,4},
+				new ListStructuredContentProvider().getElements(Arrays.asList(1,2,3,4))
+		);
+	}
+
+	@Test
+	public void testConvertsNullToEmptyArray(){
+		assertArrayEquals(
+				new Object[]{},
+				new ListStructuredContentProvider().getElements(null)
+		);
+	}
+
+	@Test
+	public void testConvertsOtherThingsToEmptyArray(){
+		assertArrayEquals(
+				new Object[]{},
+				new ListStructuredContentProvider().getElements("not a list")
+		);
+	}
+}
