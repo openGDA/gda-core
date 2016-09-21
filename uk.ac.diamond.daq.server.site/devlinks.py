4176d27f1f1f068d36effccce59eee7427ccbfa0
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2016-09-21 15:30:12 +0100

    Remove the need to copy script folders to dls_root on server export
    Python script folders (e.g. uk.ac.gda.core/scripts) are currently copied
    to the dls_root pseudo workspace parent during server export so that
    they are consistently accessible in the case when running from
    eclipse. It would be better if they remained under their associated
    plugins in the exported product and the intermediate path differences
    were managed by the application. This would increase the ability to
    remove dls_root completely which is one of the goals in replacing the
    current latching mechanism.
    
    This part of the change 
    
    1. Updates buckminster file to remove no longer needed copying of
    folders to dls_root.
    2. Adds the new target to produce the development export.
    3. Adds a script to perform the script folder linking when the
    development target is used.
    4. Ensures that the already identified script folders don't get jarred
    during export
    
    Change-Id: Ib05ecc9ff3c4e6f1382dd65b941b6514c6da5e56

---
+++ uk.ac.diamond.daq.server.site/devlinks.py
@@ -0,0 +1,104 @@
+""" This script is only run when exporting a Development GDA Server product from the uk.ac.diamond.server.site using Buckminster
+Do not attempt to use it for other purposes as the destination of the links it creates will likely not be present.
+
+The script finds all the bundles that contain python script locations defined using the uk.ac.diamond.daq.jython.api.scriptLocations
+extension point (i.e. those that should be available to all configurations). These folders are copied to their associated bundle
+folders by the basic export process, however for a development export they must be replaced by links back to the corresponding
+folders under workspace_git. In order to  do this, the script uses the exported plugins folder supplied by the calling Ant task
+plus the script location value(s) parsed from the identified file to create a relative path to the copied folder that includes the
+generated bundle version for the exported product. This folder is then deleted and replaced by a symlink that points back to the
+source folder in workspace_git. The utilities folder is processed in the same way to handle script sources that are not plugins but
+nevertheless need to be available (e.g. diffcalc)
+
+Arguments:
+1                The path to the exported plugins directory (usually supplied by Ant)
+
+The following folder structure is assumed for the plugin scripts:
+
+    < some_workspace_parent >
+         \
+          \__< some repo parent (e.g. workspace_git) >
+           \           \
+            \           \_______< this plugin's repo >
+             \           \                         \
+              \           \                         \__< this plugin (i.e. uk.ac.diamond.daq.server.site) >
+               \           \___< plugin AA's repo >
+                \                                 \____< plugin AA source root >
+                 \                                                   \___________< 0..n intermediate folders >
+                  \__< some servers root >                                                  \_________________< a scripts folder >
+                              \
+                               \__< the server being exported >
+                                                       \________< plugins_folder (passed in) >
+                                                                            \________________< exported versioned plugin AA root >
+
+
+when complete there will be a scripts folder link(s) at the same relative path below < exported versioned plugin AA root > that point
+back to each corresponding < a scripts folder > beneath < plugin AA source root > for each plugin that uses the extension point."""
+
+import subprocess
+import os
+import sys
+import glob
+import shutil
+import xml.etree.ElementTree
+
+up_one = ".." + os.sep
+
+""" Removes the script folder at root/exported_path and replaces it with a link to
+<git_repo_parent>/path where <git_repo_parent> is at the same level as the servers
+folder containing the exported server build. The number of levels between this and
+the script folder is specified by depth whilst root specifies whether it is below.
+the plugins or utilities folder."""
+
+def write_link(root, path, exported_path, depth):
+    target_test = up_one * 2 + path                                             # make the basic workspace script folder path relative to here
+    if os.path.exists(target_test) and os.path.isdir(target_test):              # so its existence can be checked
+        path_offset = up_one * depth +  up_one                                  # calculate the offset from the exported script folder's parent to the git parent
+        ws_git_name = os.path.basename(os.path.realpath(up_one * 2)) + os.sep   # read the name of the git parent
+        link_target = path_offset + ws_git_name + path                          # build the equivalent relative path from the exported plugin folder
+        lfrom = os.path.join(root, exported_path)                               # build the full path of the exported script folder/link
+
+        shutil.rmtree(lfrom, True)                                              # remove the exported script folder and its subfolders
+        if os.path.islink(lfrom):                                               # in case it wasn't there get rid of any links that might be instead:
+             os.remove(lfrom)
+        os.symlink(link_target, lfrom)                                          # make the replacement symlink
+
+
+if len(sys.argv) != 2:                                                          # script name plus plugins path
+    print "ERROR: plugins.dir argument must be supplied"
+    sys.exit(1)
+
+plugins_folder = sys.argv[1]
+roots = [plugins_folder, plugins_folder + os.sep + up_one + "utilities"]
+
+# Grep for the plugin.xml files of all the bundles that define script locations via the corresponding extension point
+process = subprocess.Popen('grep -Rl --include="plugin.xml" "jython.api.scriptLocations" ' + 2 * up_one, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+stdout, stderr = process.communicate()
+plugins_file_list = stdout.split('\n')
+
+
+""" Iterate over the plugin list building the required links to link back
+from the exported product's plugin/utilities folder to the relevant script folder,
+then delete the exported folder and replace it with the link."""
+
+for plugin_file in plugins_file_list:
+    if len(plugin_file) > 0:
+        xml_plugin_root = xml.etree.ElementTree.parse(plugin_file).getroot()                        # parse the current plugin.xml file
+        workspace_script_folder_paths = []
+        for location_tag in xml_plugin_root.findall("./extension/location"):
+            workspace_script_folder_paths.append(location_tag.get("folder"))
+
+        for workspace_script_folder_path in workspace_script_folder_paths:
+            tokens = workspace_script_folder_path.split(os.sep)
+            for root in roots:
+                if root == plugins_folder and len(tokens) > 1:                                      # for plugins, token[0] is XX_git and token[1] is the plugin name
+                    path_matcher = os.path.join(root, tokens[1] + "_*")                             # build matcher for versioned plugin path from the plugin name
+                    versioned_list = glob.glob(path_matcher)                                        # and get a list of matching folders (will match 0 or 1)
+                    if len(versioned_list) > 0:
+                        versioned = versioned_list[0]                                               # resolve the path of the exported versioned folder
+                        tokens[1] = versioned.split(os.sep)[-1]                                     # use the last path element to replace the unversioned folder name
+                        product_versioned_script_folder = str(os.path.join(*tokens[1:]))            # create the new relative path by chopping off the git repo
+
+                        write_link(root, workspace_script_folder_path, product_versioned_script_folder, len(tokens))
+                elif os.path.exists(os.path.join(root, workspace_script_folder_path)):
+                    write_link(root, workspace_script_folder_path, workspace_script_folder_path, len(tokens) + 1)
