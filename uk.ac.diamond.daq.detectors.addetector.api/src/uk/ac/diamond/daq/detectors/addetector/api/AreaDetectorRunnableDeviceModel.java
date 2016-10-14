2cacf7cbd80e31e11bdd0754148a396c36fe2bc1
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2016-10-14 10:32:55 +0100

    make AreaDetectorRunnableDeviceModel extends AbstractDetectorModel
    Change-Id: I6b38f540eee685b1ee00f3e7bfa4ecb01010b41a

--- uk.ac.diamond.daq.detectors.addetector.api/src/uk/ac/diamond/daq/detectors/addetector/api/AreaDetectorRunnableDeviceModel.java
+++ uk.ac.diamond.daq.detectors.addetector.api/src/uk/ac/diamond/daq/detectors/addetector/api/AreaDetectorRunnableDeviceModel.java
@@ -1,62 +1,12 @@
 package uk.ac.diamond.daq.detectors.addetector.api;
 
-import org.eclipse.scanning.api.device.models.IDetectorModel;
+import org.eclipse.scanning.api.device.models.AbstractDetectorModel;
 
 /**
  * Very simple model for initially getting a area detector to run in the new scanning.
  *
  * @author James Mudd
  */
-public class AreaDetectorRunnableDeviceModel implements IDetectorModel {
-
-	private String name;
-	private double exposureTime; // seconds
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public double getExposureTime() {
-		return exposureTime;
-	}
-
-	public void setExposureTime(double exposureTime) {
-		this.exposureTime = exposureTime;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(exposureTime);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AreaDetectorRunnableDeviceModel other = (AreaDetectorRunnableDeviceModel) obj;
-		if (Double.doubleToLongBits(exposureTime) != Double.doubleToLongBits(other.exposureTime))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
+public class AreaDetectorRunnableDeviceModel extends AbstractDetectorModel {
 
 }
