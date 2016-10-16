3276da0798d9f84cdaa39afb8431b2ac79a8fc18
Jonathan Blakes <jonathan.blakes@diamond.ac.uk>
2016-10-16 16:49:21 +0100

    Split uk.ac.gda.bimorph bundle into into scripts and UI
    bimorph.ui topic for updating client features to uk.ac.gda.bimorph.ui
    
    Change-Id: Idff870ead47215c03c2d10a6e64b89deab125993

--- uk.ac.gda.bimorph.ui/src/uk/ac/gda/bimorph/ui/BimorphParameters.java
+++ uk.ac.gda.bimorph.ui/src/uk/ac/gda/bimorph/ui/BimorphParameters.java
@@ -16,7 +16,7 @@
  * with GDA. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package uk.ac.gda.bimorph;
+package uk.ac.gda.bimorph.ui;
 
 import java.io.Serializable;
 import java.net.URL;
--- uk.ac.gda.bimorph.ui/src/uk/ac/gda/bimorph/ui/BimorphParametersComposite.java
+++ uk.ac.gda.bimorph.ui/src/uk/ac/gda/bimorph/ui/BimorphParametersComposite.java
@@ -16,7 +16,7 @@
  * with GDA. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package uk.ac.gda.bimorph;
+package uk.ac.gda.bimorph.ui;
 
 import org.eclipse.richbeans.api.event.ValueEvent;
 import org.eclipse.richbeans.api.event.ValueListener;
