fbe417bbcfd2d7cf6d3f03ba09f559d4d88cc587
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2016-11-09 15:39:46 +0000

    Fix problem where config scripts missing from python path
    Due to part of change d64b7d5 which prevented script path duplication
    after a reset_namespace the config and user script paths were being
    deleted from the python path. This reverts that change and achieves its
    aim by checking that the path list doesn't already contain a path before
    merging it. Thus the path will only ever get added once.
    
    Change-Id: I60cf7bd241574b86615ffabc2984d33e6e05d154

--- uk.ac.gda.core/src/gda/jython/ScriptProject.java
+++ uk.ac.gda.core/src/gda/jython/ScriptProject.java
@@ -66,4 +66,40 @@ public class ScriptProject {
 	public Boolean isCoreProject() {
 		return type == ScriptProjectType.CORE;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ScriptProject other = (ScriptProject) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (path == null) {
+			if (other.path != null)
+				return false;
+		} else if (!path.equals(other.path))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+
 }
