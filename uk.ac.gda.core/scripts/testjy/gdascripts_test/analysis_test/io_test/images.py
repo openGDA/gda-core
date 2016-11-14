79e738cc03890950a62d4f5b6b115d7d2b3f2507
James Mudd <james.mudd@diamond.ac.uk>
2016-11-14 13:41:09 +0000

    DASCTEST-301 Remove explicit Jython version checking in test code
    This change means this code can work with 2.5 and 2.7, it will be safe
    to merge in preparation for moving to 2.7.
    
    To do this, switch to an approach which only requires os. This removes
    the need for pathresolver.py which is removed.
    
    Change-Id: I11a07cd89d33601d6754c0dff45e792b4d889ebf

--- uk.ac.gda.core/scripts/testjy/gdascripts_test/analysis_test/io_test/images.py
+++ uk.ac.gda.core/scripts/testjy/gdascripts_test/analysis_test/io_test/images.py
@@ -1,14 +1,7 @@
-from testjy.test_util.pathresolver import replacePathSeparater
+import os
 
-import __init__
-import sys
+THISDIR = os.path.dirname(__file__)
 
-THISDIR = __init__.__file__
-if int(sys.version_info[1]) == 2:
-    THISDIR = THISDIR.split('__init__.py')[0] # Jython 2.2
-elif int(sys.version_info[1]) == 5:
-    THISDIR = THISDIR.split('__init__$py.class')[0] # Jython 2.5
-
-TESTFILE = replacePathSeparater(THISDIR + 'blurred_gauss_with_spots.tif') # made in gimp
-IPP_XRAY_EYE_FILE = replacePathSeparater(THISDIR + 'image_pro_plus_tif_from_xray_eye.TIF') # actual data
-FOCUSED_BEAM = replacePathSeparater(THISDIR + 'one_micron_focused_beam_from_xrayeye_via_ipp.TIF')
+TESTFILE = os.path.join(THISDIR, 'blurred_gauss_with_spots.tif') # made in gimp
+IPP_XRAY_EYE_FILE = os.path.join(THISDIR, 'image_pro_plus_tif_from_xray_eye.TIF') # actual data
+FOCUSED_BEAM = os.path.join(THISDIR, 'one_micron_focused_beam_from_xrayeye_via_ipp.TIF')
