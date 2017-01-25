83b23fff8a88057aaa7abf0cdcb4024fd29ffe4e
James Mudd <james.mudd@diamond.ac.uk>
2017-01-25 13:59:21 +0000

    Remove pointless prefix's in logging statements
    Also remove incorrect "g.d.d.a.v.i.FfmpegStreamImp" strings presumably
    copied into here!
    
    Increased logging level to error for these exceptions you about to throw
    anyway so stuff is about to go badly.
    
    Change-Id: I05ec2c0f498dfb2f142eefaa1cd7232bf8f7b7fb

--- uk.ac.gda.epics/src/gda/device/detector/areadetector/v17/impl/NDFileImpl.java
+++ uk.ac.gda.epics/src/gda/device/detector/areadetector/v17/impl/NDFileImpl.java
@@ -18,6 +18,13 @@
 
 package gda.device.detector.areadetector.v17.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+
 import gda.configuration.epics.ConfigurationNotFoundException;
 import gda.configuration.epics.Configurator;
 import gda.configuration.epics.EpicsConfiguration;
@@ -37,14 +44,6 @@ import gov.aps.jca.Channel;
 import gov.aps.jca.TimeoutException;
 import gov.aps.jca.event.PutEvent;
 import gov.aps.jca.event.PutListener;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
-
 import uk.ac.gda.util.FilePathConverter;
 
 public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
@@ -197,7 +196,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return new String(EPICS_CONTROLLER.cagetByteArray(getChannel(FilePath))).trim();
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFilePath", ex);
+			logger.error("Cannot getFilePath", ex);
 			throw ex;
 		}
 	}
@@ -215,7 +214,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			byte[] bytes = (getInternalPath(filepath) + '\0').getBytes();
 			EPICS_CONTROLLER.caputWait(channel, bytes);
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setFilePath", ex);
+			logger.error("Cannot setFilePath", ex);
 			throw ex;
 		}
 	}
@@ -230,7 +229,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				String filepath = new String(EPICS_CONTROLLER.cagetByteArray(channel)).trim();
 				return filePathConverter != null ? filePathConverter.converttoExternal(filepath)  : filepath;
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFilePath_RBV", ex);
+			logger.error("Cannot getFilePath_RBV", ex);
 			throw ex;
 		}
 	}
@@ -246,7 +245,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return new String(EPICS_CONTROLLER.cagetByteArray(getChannel(FileName))).trim();
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileName", ex);
+			logger.error("Cannot getFileName", ex);
 			throw ex;
 		}
 	}
@@ -263,7 +262,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(FileName), (filename + '\0').getBytes());
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setFileName", ex);
+			logger.error("Cannot setFileName", ex);
 			throw ex;
 		}
 	}
@@ -280,7 +279,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return new String(EPICS_CONTROLLER.cagetByteArray(getChannel(FileName_RBV))).trim();
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileName_RBV", ex);
+			logger.error("Cannot getFileName_RBV", ex);
 			throw ex;
 		}
 	}
@@ -296,7 +295,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetInt(getChannel(FileNumber));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileNumber", ex);
+			logger.error("Cannot getFileNumber", ex);
 			throw ex;
 		}
 	}
@@ -313,7 +312,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(FileNumber), filenumber);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setFileNumber", ex);
+			logger.error("Cannot setFileNumber", ex);
 			throw ex;
 		}
 	}
@@ -329,7 +328,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetInt(getChannel(FileNumber_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileNumber_RBV", ex);
+			logger.error("Cannot getFileNumber_RBV", ex);
 			throw ex;
 		}
 	}
@@ -345,7 +344,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(AutoIncrement));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getAutoIncrement", ex);
+			logger.error("Cannot getAutoIncrement", ex);
 			throw ex;
 		}
 	}
@@ -362,7 +361,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(AutoIncrement), autoincrement);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setAutoIncrement", ex);
+			logger.error("Cannot setAutoIncrement", ex);
 			throw ex;
 		}
 	}
@@ -378,7 +377,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(AutoIncrement_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getAutoIncrement_RBV", ex);
+			logger.error("Cannot getAutoIncrement_RBV", ex);
 			throw ex;
 		}
 	}
@@ -395,7 +394,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return new String(EPICS_CONTROLLER.cagetByteArray((getChannel(FileTemplate)))).trim();
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileTemplate", ex);
+			logger.error("Cannot getFileTemplate", ex);
 			throw ex;
 		}
 	}
@@ -413,7 +412,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(FileTemplate), (filetemplate + '\0').getBytes());
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setFileTemplate", ex);
+			logger.error("Cannot setFileTemplate", ex);
 			throw ex;
 		}
 	}
@@ -430,7 +429,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return new String(EPICS_CONTROLLER.cagetByteArray(getChannel(FileTemplate_RBV))).trim();
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileTemplate_RBV", ex);
+			logger.error("Cannot getFileTemplate_RBV", ex);
 			throw ex;
 		}
 	}
@@ -445,7 +444,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			String filepath = new String(EPICS_CONTROLLER.cagetByteArray(channel)).trim();
 			return filePathConverter != null ? filePathConverter.converttoExternal(filepath)  : filepath;
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFullFileName_RBV", ex);
+			logger.error("Cannot getFullFileName_RBV", ex);
 			throw ex;
 		}
 	}
@@ -461,7 +460,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(AutoSave));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getAutoSave", ex);
+			logger.error("Cannot getAutoSave", ex);
 			throw ex;
 		}
 	}
@@ -478,7 +477,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(AutoSave), autosave);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setAutoSave", ex);
+			logger.error("Cannot setAutoSave", ex);
 			throw ex;
 		}
 	}
@@ -494,7 +493,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(AutoSave_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getAutoSave_RBV", ex);
+			logger.error("Cannot getAutoSave_RBV", ex);
 			throw ex;
 		}
 	}
@@ -510,7 +509,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(WriteFile));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getWriteFile", ex);
+			logger.error("Cannot getWriteFile", ex);
 			throw ex;
 		}
 	}
@@ -527,7 +526,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caput(getChannel(WriteFile), writefile);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setWriteFile", ex);
+			logger.error("Cannot setWriteFile", ex);
 			throw ex;
 		}
 	}
@@ -543,7 +542,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(WriteFile_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getWriteFile_RBV", ex);
+			logger.error("Cannot getWriteFile_RBV", ex);
 			throw ex;
 		}
 	}
@@ -559,7 +558,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(ReadFile));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getReadFile", ex);
+			logger.error("Cannot getReadFile", ex);
 			throw ex;
 		}
 	}
@@ -576,7 +575,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caput(getChannel(ReadFile), readfile);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setReadFile", ex);
+			logger.error("Cannot setReadFile", ex);
 			throw ex;
 		}
 	}
@@ -592,7 +591,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(ReadFile_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getReadFile_RBV", ex);
+			logger.error("Cannot getReadFile_RBV", ex);
 			throw ex;
 		}
 	}
@@ -608,7 +607,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(FileFormat));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileFormat", ex);
+			logger.error("Cannot getFileFormat", ex);
 			throw ex;
 		}
 	}
@@ -625,7 +624,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caput(getChannel(FileFormat), fileformat);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setFileFormat", ex);
+			logger.error("Cannot setFileFormat", ex);
 			throw ex;
 		}
 	}
@@ -641,7 +640,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(FileFormat_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileFormat_RBV", ex);
+			logger.error("Cannot getFileFormat_RBV", ex);
 			throw ex;
 		}
 	}
@@ -657,7 +656,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(FileWriteMode));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileWriteMode", ex);
+			logger.error("Cannot getFileWriteMode", ex);
 			throw ex;
 		}
 	}
@@ -677,7 +676,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(FileWriteMode), filewritemode);
 			}
 		} catch (Exception ex) {
-			logger.warn("Cannot setFileWriteMode", ex);
+			logger.error("Cannot setFileWriteMode", ex);
 			throw ex;
 		}
 	}
@@ -693,7 +692,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(FileWriteMode_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getFileWriteMode_RBV", ex);
+			logger.error("Cannot getFileWriteMode_RBV", ex);
 			throw ex;
 		}
 	}
@@ -709,7 +708,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(Capture));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getCapture", ex);
+			logger.error("Cannot getCapture", ex);
 			throw ex;
 		}
 	}
@@ -784,7 +783,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetEnum(getChannel(Capture_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getCapture_RBV", ex);
+			logger.error("Cannot getCapture_RBV", ex);
 			throw ex;
 		}
 	}
@@ -800,7 +799,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetInt(getChannel(NumCapture));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getNumCapture", ex);
+			logger.error("Cannot getNumCapture", ex);
 			throw ex;
 		}
 	}
@@ -817,7 +816,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 				EPICS_CONTROLLER.caputWait(getChannel(NumCapture), numcapture);
 			}
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.FfmpegStreamImpl-> Cannot setNumCapture", ex);
+			logger.error("Cannot setNumCapture", ex);
 			throw ex;
 		}
 	}
@@ -833,7 +832,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetInt(getChannel(NumCapture_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getNumCapture_RBV", ex);
+			logger.error("Cannot getNumCapture_RBV", ex);
 			throw ex;
 		}
 	}
@@ -849,7 +848,7 @@ public class NDFileImpl extends NDBaseImpl implements InitializingBean, NDFile {
 			}
 			return EPICS_CONTROLLER.cagetInt(getChannel(NumCaptured_RBV));
 		} catch (Exception ex) {
-			logger.warn("g.d.d.a.v.i.NDFileImpl-> Cannot getNumCaptured_RBV", ex);
+			logger.error("Cannot getNumCaptured_RBV", ex);
 			throw ex;
 		}
 	}
