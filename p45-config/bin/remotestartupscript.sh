47aef41606712588f74bdea115e0f2797bf38006
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2017-02-27 16:06:17 +0000

    Make use of centralised remote startup script
    A template of how to use the centralised remote startup script added to
    dls-config allowing custom commands to first be executed. Could easily
    be moved into /config/etc/live if that were to become the standard
    location for these, but that would require update to the authorized_keys written by CFEngine plus adjustment of the relative path.
    
    Change-Id: Ib3de6821fd13cb6093defdf8b5353502007911fd

--- p45-config/bin/remotestartupscript.sh
+++ p45-config/bin/remotestartupscript.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 # This script is only invoked when user gda2 ssh's to the control machine. It is run by an entry in gda's ~/.ssh/authorized_keys
+# Because of this, no environment variables (other than SSH_ORIGINAL_COMMAND potentially) have been set when it executes.
+#
+# If you wish to use the dls-config common /remote/startupscript.sh you must source it, performing any custom operations beforehand.
+# If you don't have any custom operations then you can just invoke the common script directly from your authorized_keys file.
+#
+# If not using the common script, you will need to set GDA_NO_PROMPT and GDA_IN_REMOTE_STARTUP to true and initialise your Bash
+# environment before invoking the main gda script to get correct startup behaviour. Also please add "< /dev/null > /dev/null 2>&1"
+# to the end of the line that calls the gda script to prevent ssh from hanging incorrectly due to an unclosed stream.
 
-here_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# This script is run as a single command by ssh, so we need to set up our environment
-# See http://stackoverflow.com/questions/216202/why-does-an-ssh-remote-command-get-fewer-environment-variables-then-when-run-man
-. /usr/share/Modules/init/bash
 
-# There is no user or screen to prompt or display pop-ups
-export GDA_NO_PROMPT=true
-
-# Set an environment variable to indicate we came through the remote startup script, so that we can error if we attempt to do this recursively
-export GDA_IN_REMOTE_STARTUP=true
-
-if [[ -n "${SSH_ORIGINAL_COMMAND}" ]]; then
-    if [[ "${SSH_ORIGINAL_COMMAND}" == *"restart"* ]]; then
-        SSH_ORIGINAL_COMMAND="${SSH_ORIGINAL_COMMAND/restart/--restart}"
-    elif [[ "${SSH_ORIGINAL_COMMAND}" == *"start"* ]]; then
-        SSH_ORIGINAL_COMMAND="${SSH_ORIGINAL_COMMAND/start/--start}"
-    elif [[ "${SSH_ORIGINAL_COMMAND}" == *"stop"* ]]; then
-        SSH_ORIGINAL_COMMAND="${SSH_ORIGINAL_COMMAND/stop/--stop}"
-    fi
-    ${here_dir}/gda  ${SSH_ORIGINAL_COMMAND} --mode=live servers < /dev/null > /dev/null 2>&1      # should prevent the ssh command that calls this script from not returning
-else
-    ${here_dir}/gda --restart --mode=live servers < /dev/null > /dev/null 2>&1
-fi
+# If you want any special behaviour add it above this line
+here_location="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${here_location}/../../../gda-diamond.git/dls-config/live/gda-servers-startup-script.sh  # Derive dls-config relative path as appropriate
