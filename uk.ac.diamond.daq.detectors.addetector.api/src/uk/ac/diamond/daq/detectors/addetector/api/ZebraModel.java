d6d843cfc69b10146b42ec3d6a2b534c24052ff1
Anthony Hull <anthony.hull@diamond.ac.uk>
2017-03-09 10:42:00 +0000

    I14-198 RunnableDevice interface for Zebra box
    Change-Id: I149004cdf63844546fe826e05d3421a591df76ef

---
+++ uk.ac.diamond.daq.detectors.addetector.api/src/uk/ac/diamond/daq/detectors/addetector/api/ZebraModel.java
@@ -0,0 +1,44 @@
+/*-
+ * Copyright © 2017 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.daq.detectors.addetector.api;
+
+import org.eclipse.scanning.api.device.models.IDetectorModel;
+
+public class ZebraModel implements IDetectorModel {
+
+	private String name;
+	private double exposureTime;
+
+	@Override
+	public String getName() {
+		return name;
+	}
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public double getExposureTime() {
+		return exposureTime;
+	}
+	@Override
+	public void setExposureTime(double exposureTime) {
+		this.exposureTime = exposureTime;
+	}
+}
