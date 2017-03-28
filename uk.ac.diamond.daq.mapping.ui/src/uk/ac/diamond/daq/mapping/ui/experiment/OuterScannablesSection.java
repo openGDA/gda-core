e5123946fb023c3ebb823013e96190ca66eac911
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2017-03-28 15:44:15 +0100

    [DAQ-417] add support for MultiStep models to OuterScannablesSection
    Change-Id: I8ba9f4b8435fd6d6c0939d97f7b69a959c90d73a

--- uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/OuterScannablesSection.java
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/OuterScannablesSection.java
@@ -19,6 +19,7 @@
 package uk.ac.diamond.daq.mapping.ui.experiment;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -35,6 +36,7 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.scanning.api.points.models.ArrayModel;
 import org.eclipse.scanning.api.points.models.IScanPathModel;
+import org.eclipse.scanning.api.points.models.MultiStepModel;
 import org.eclipse.scanning.api.points.models.StepModel;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
@@ -63,25 +65,44 @@ class OuterScannablesSection extends AbstractMappingSection {
 				return convertStepModel((StepModel) fromObject);
 			} else if (fromObject instanceof ArrayModel) {
 				return convertArrayModel((ArrayModel) fromObject);
+			} else if (fromObject instanceof MultiStepModel) {
+				return convertMultiStepModel((MultiStepModel) fromObject);
 			} else {
 				// We only expect path model types that can be created from this GUI
 				throw new IllegalArgumentException("Unknown model type: " + fromObject.getClass());
 			}
 		}
 
-		private Object convertStepModel(StepModel stepModel) {
+		private String convertStepModel(StepModel stepModel) {
 			StringBuilder stringBuilder = new StringBuilder();
+			appendStepModel(stepModel, stringBuilder);
+
+			return stringBuilder.toString();
+		}
+
+		private void appendStepModel(StepModel stepModel, StringBuilder stringBuilder) {
 			stringBuilder.append(doubleToString(stepModel.getStart()));
 			stringBuilder.append(' ');
 			stringBuilder.append(doubleToString(stepModel.getStop()));
 			stringBuilder.append(' ');
 			stringBuilder.append(doubleToString(stepModel.getStep()));
+		}
+
+		private String convertMultiStepModel(MultiStepModel multiStepModel) {
+			Iterator<StepModel> iter = multiStepModel.getStepModels().iterator();
+
+			StringBuilder stringBuilder = new StringBuilder();
+			while (iter.hasNext()) {
+				appendStepModel(iter.next(), stringBuilder);
+				if (iter.hasNext()) {
+					stringBuilder.append(";");
+				}
+			}
 
 			return stringBuilder.toString();
 		}
 
-		private Object convertArrayModel(IScanPathModel scanPath) {
-			ArrayModel arrayModel = (ArrayModel) scanPath;
+		private String convertArrayModel(ArrayModel arrayModel) {
 			double[] positions = arrayModel.getPositions();
 			StringBuilder stringBuilder = new StringBuilder();
 			for (int i = 0; i < positions.length; i++) {
@@ -102,10 +123,12 @@ class OuterScannablesSection extends AbstractMappingSection {
 
 	/**
 	 * Converter for converting a string to a path model. The string specifies the path of the
-	 * outer scannable. It can either be:
+	 * outer scannable. It can be:
 	 * <ul>
-	 *   <li>A comma separated list of points, i.e. pos1,pos2,pos3,pos4,...</li>
-	 *   <li>One or more step ranges separated by spaces, i.e. start stop step
+	 *   <li>A comma separated list of values, i.e. pos1,pos2,pos3,pos4,...</li>
+	 *   <li>A single step range with start stop and step values separated by spaces, i.e. start stop step</li>
+	 *   <li>Multiple step ranges with each step range separated by a semi-colon, i.e.
+	 *      start1 stop1 step1; start2 stop2 step2</li>
 	 * </ul>
 	 * <p>If the string contains a comma, it is interpreted a sequence of points, otherwise
 	 * as one or more ranges.
@@ -125,6 +148,8 @@ class OuterScannablesSection extends AbstractMappingSection {
 			try {
 				if (text.contains(",")) {
 					return convertStringToArrayModel(text);
+				} else if (text.contains(";")) {
+					return convertStringToMultiStepModel(text);
 				} else {
 					return convertStringToStepModel(text);
 				}
@@ -133,7 +158,7 @@ class OuterScannablesSection extends AbstractMappingSection {
 			}
 		}
 
-		private Object convertStringToStepModel(String text) {
+		private StepModel convertStringToStepModel(String text) {
 			String[] startStopStep= text.split(" ");
 			if (startStopStep.length == 3) {
 				StepModel stepModel = new StepModel();
@@ -146,7 +171,22 @@ class OuterScannablesSection extends AbstractMappingSection {
 			return null;
 		}
 
-		private Object convertStringToArrayModel(String text) {
+		private MultiStepModel convertStringToMultiStepModel(String text) {
+			MultiStepModel multiStepModel = new MultiStepModel();
+			multiStepModel.setName(scannableName);
+			String[] stepModelStrs = text.split(";");
+			for (String stepModelStr : stepModelStrs) {
+				StepModel stepModel = convertStringToStepModel(stepModelStr.trim());
+				if (stepModel == null) {
+					return null;
+				}
+				multiStepModel.addRange(stepModel);
+			}
+
+			return multiStepModel;
+		}
+
+		private ArrayModel convertStringToArrayModel(String text) {
 			String[] strings = text.split(",");
 			double[] positions = new double[strings.length];
 			for (int index = 0; index < strings.length; index++) {
@@ -191,8 +231,9 @@ class OuterScannablesSection extends AbstractMappingSection {
 
 			// FIXME make a proper widget for this?
 			Text axisText = new Text(otherScanAxesComposite, SWT.BORDER);
-			axisText.setToolTipText("<start stop step>");
-			axisText.setToolTipText("<start stop step> or <pos1,pos2,pos3,pos4...>");
+			axisText.setToolTipText("A range <start stop step>\n"
+					+ "or a list of points <pos1,pos2,pos3,pos4...>\n"
+					+ "or a list of ranges <start1 stop1 step1; start2 stop2 step2>");
 			GridDataFactory.fillDefaults().grab(true, false).applyTo(axisText);
 			IObservableValue axisTextValue = WidgetProperties.text(SWT.Modify).observe(axisText);
 			bindScanPathModelToTextField(scannableAxisParameters, axisTextValue);
