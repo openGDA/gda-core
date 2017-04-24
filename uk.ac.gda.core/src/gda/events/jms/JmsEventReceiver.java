f53d68b3e0b3c01f5ce20052ee782ee2ddb69644
James Mudd <james.mudd@diamond.ac.uk>
2017-04-24 17:12:21 +0100

    DAQ-515 Improvements to JmsEventReceiver
    - Change to using gda.util.Serializer to allow message object classes to
      be deserialized in an OSGi environment.
    - Replace EventTopicListener by MessageDispatcher to make the code
      clearer. Move the logic for setting up the consumer into the subscribe
      method.
    - Make variables final where possible.
    - Implements disconnect so consumers are closed when its called.
    - Add checking of the sending timestamp and log a warning if old
      messages are being received.
    - Add logging and improve exception handling.
    
    Change-Id: Ib220aad254662acbd41ccd33ff3a8f1d135dea94

--- uk.ac.gda.core/src/gda/events/jms/JmsEventReceiver.java
+++ uk.ac.gda.core/src/gda/events/jms/JmsEventReceiver.java
@@ -19,10 +19,8 @@
 
 package gda.events.jms;
 
-import gda.factory.corba.util.EventReceiver;
-import gda.factory.corba.util.EventSubscriber;
-import gda.factory.corba.util.Filter;
-import gda.factory.corba.util.NameFilter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -31,10 +29,31 @@ import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
 import javax.jms.Topic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gda.factory.corba.util.EventReceiver;
+import gda.factory.corba.util.EventSubscriber;
+import gda.factory.corba.util.Filter;
+import gda.factory.corba.util.NameFilter;
+import gda.util.Serializer;
+
 /**
- * An {@link EventReceiver} that receives messages using JMS.
+ * An {@link EventReceiver} that receives messages using JMS. This class was significantly rewritten as part of DAQ-515
  */
 public class JmsEventReceiver extends JmsClient implements EventReceiver {
+
+	private static final Logger logger = LoggerFactory.getLogger(JmsEventReceiver.class);
+
+	/**
+	 * Messages received older than this will generate a warning.
+	 */
+	private static final long MESSAGE_AGE_WARNING_MILLS = 1000;
+
+	/**
+	 * Store a list of consumers so they can be disconnected
+	 */
+	private final List<MessageConsumer> consumers = new CopyOnWriteArrayList<>();
 
 	/**
 	 * Creates a JMS event receiver.
@@ -43,52 +62,101 @@ public class JmsEventReceiver extends JmsClient implements EventReceiver {
 		try {
 			createSession();
 			connection.start();
+			logger.info("Created new session: '{}'", session);
 		} catch (JMSException e) {
-			throw new RuntimeException("Unable to create JMS event receiver", e);
+			logger.error("Unable to create JMS event receiver", e);
 		}
 	}
 
-	@SuppressWarnings("unused")
 	@Override
-	public void subscribe(EventSubscriber eventSubscriber, Filter filter) {
+	public void subscribe(final EventSubscriber eventSubscriber, final Filter filter) {
 		try {
-			NameFilter nf = (NameFilter) filter;
-			String name = nf.getName();
-			String channelName = TOPIC_PREFIX + name;
-			Topic topic = session.createTopic(channelName);
-			new EventTopicListener(topic, eventSubscriber);
+			// TODO The only Filter type we have is NameFilter so this cast is definitely safe at the moment but maybe
+			// we should consider redesigning or removing this Filter system.
+			final NameFilter nf = (NameFilter) filter;
+			final String name = nf.getName();
+			final String topicName = TOPIC_PREFIX + name;
+			final Topic topic = session.createTopic(topicName);
+
+			final MessageConsumer consumer = session.createConsumer(topic);
+
+			// Add the listener that handles received messages
+			consumer.setMessageListener(new MessageDispatcher(eventSubscriber));
+
+			// Store the consumer for disconnecting
+			consumers.add(consumer);
+
+			logger.debug("Subscribed to events on topic: '{}' with consumer: '{}'", topicName, consumer);
 		} catch (JMSException e) {
-			throw new RuntimeException("Could not subscribe", e);
+			throw new RuntimeException("Could not subscribe to topic: '{}'", e);
 		}
 	}
 
-	class EventTopicListener implements MessageListener {
+	/**
+	 * This handles messages received from JMS. It checks the message is valid, deserializes it and pushes it out to
+	 * subscribers.
+	 */
+	private class MessageDispatcher implements MessageListener {
 
-		private EventSubscriber eventSubscriber;
+		private final EventSubscriber eventSubscriber;
 
-		public EventTopicListener(Topic topic, EventSubscriber eventSubscriber) throws JMSException {
+		public MessageDispatcher(EventSubscriber eventSubscriber) {
 			this.eventSubscriber = eventSubscriber;
-			MessageConsumer consumer = session.createConsumer(topic);
-			consumer.setMessageListener(this);
 		}
 
 		@Override
-		public void onMessage(Message message) {
-			if (message instanceof ObjectMessage) {
-				try {
-					ObjectMessage objectMessage = (ObjectMessage) message;
-					Object o = objectMessage.getObject();
-					eventSubscriber.inform(o);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+		public void onMessage(final Message message) {
+			// If its not a ObjectMessage fail
+			if (!(message instanceof ObjectMessage)) {
+				logger.error("Received unexpected message: '{}'", message);
+				return; // We can't handle this message
 			}
-		}
+			// It is a ObjectMessage so cast it
+			final ObjectMessage objectMessage = (ObjectMessage) message;
 
+			// Get the data out of the message
+			final Object serializedObject;
+			final long sendingTimestamp;
+			try {
+				// Get the serialized object back out of the message
+				serializedObject = objectMessage.getObject();
+
+				// Get the sending timestamp
+				sendingTimestamp = objectMessage.getJMSTimestamp();
+			} catch (JMSException e) {
+				logger.error("Error handling received message: '{}'", message, e);
+				return; // We can't continue
+			}
+
+			if (!(serializedObject instanceof byte[])) {
+				logger.error("Received a message that was not a serialized object");
+				return; // We can't handle this case
+			}
+			// Deserialize back to the actual message object - Using the GDA serializer which can see the classes
+			final Object messageObject = Serializer.toObject((byte[]) serializedObject);
+
+			// Warn about receiving old messages. This suggests a communication issue or very high message rate.
+			final long messageAgeMillis = System.currentTimeMillis() - sendingTimestamp;
+			if (messageAgeMillis > MESSAGE_AGE_WARNING_MILLS) {
+				logger.warn("Message received is older than {} ms. Age is: {} ms", MESSAGE_AGE_WARNING_MILLS, messageAgeMillis);
+			}
+
+			// Inform subscribers
+			eventSubscriber.inform(messageObject);
+		}
 	}
 
 	@Override
 	public void disconnect() {
+		logger.debug("Disconnecting...");
+		for (MessageConsumer consumer : consumers) {
+			try {
+				logger.trace("Closing consumer: '{}'...", consumer);
+				consumer.close();
+			} catch (JMSException e) {
+				logger.error("Error closing connection", e);
+			}
+		}
 	}
 
 }
