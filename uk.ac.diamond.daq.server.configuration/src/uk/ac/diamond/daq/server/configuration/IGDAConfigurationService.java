34d47c36e7ac4059756683d8181b4f022263d01f
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2017-04-28 14:17:53 +0100

    Remove redundant static and some tabs
    Nested enums are implicitly static.
    
    Change-Id: Ib14ffcd83a6fd8f5dc1fdccd4042a3fb6e07a152

--- uk.ac.diamond.daq.server.configuration/src/uk/ac/diamond/daq/server/configuration/IGDAConfigurationService.java
+++ uk.ac.diamond.daq.server.configuration/src/uk/ac/diamond/daq/server/configuration/IGDAConfigurationService.java
@@ -6,26 +6,26 @@ import uk.ac.diamond.daq.server.configuration.commands.ObjectServerCommand;
 import uk.ac.diamond.daq.server.configuration.commands.SubProcessCommand;
 
 public interface IGDAConfigurationService {
-	
+
 	void loadConfiguration();
-	
+
 	String getMode();
-	
+
 	String[] getProfiles();
-	
+
 	SubProcessCommand getNameServerCommand();
-	
+
 	SubProcessCommand getLogServerCommand();
-	
+
 	SubProcessCommand getEventServerCommand();
-	
+
 	List<ObjectServerCommand> getObjectServerCommands();
-	
+
 	void setInstanceConfigRoot(final String path);
-	
+
 	String getInstanceConfigRoot();
-	 
-	public static enum ServerType {
+
+	public enum ServerType {
 		NAME,
 		LOG,
 		EVENT;
