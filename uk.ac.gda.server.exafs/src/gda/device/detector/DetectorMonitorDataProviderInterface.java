768de89eac5032287dbfbf08db0b4888fe9b09bd
Iain Hall <iain.hall@diamond.ac.uk>
2017-05-11 13:54:43 +0100

    B18-126 : Move data collection for 'Detector Rates View' to server.
    A new server side class DetectorMonitorDataProvider has been created to 
    provide the data used in the 'Detector Rates' view; the data collection in XspressMonitorView and XMapMonitorView classes is now carried out using 
    the new class. This refactoring enables explcit control over 
    data collection for the rate view from the server.
    The thread handling in MonitorViewBase has also been simplified - the collection
    thread is started when the 'start' button is pressed and ends when 'stop' button
    is pressed, the data provider prevents collection, or an exception is thrown.
    A 'set collection time' button has also been added to the rate view gui.
    
    Change-Id: I9c03135ebe54b766137d44cf1b9b89be700db1e5

---
+++ uk.ac.gda.server.exafs/src/gda/device/detector/DetectorMonitorDataProviderInterface.java
@@ -0,0 +1,41 @@
+/*-
+ * Copyright © 2017 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.detector;
+
+import gda.device.DeviceException;
+import gda.device.detector.DetectorMonitorDataProvider.COLLECTION_TYPES;
+import gda.factory.Findable;
+import gda.observable.IObservable;
+
+public interface DetectorMonitorDataProviderInterface extends Findable, IObservable {
+
+	public double getCollectionTime();
+
+	public void setCollectionTime(double collectionTime);
+
+	public Double[] getIonChamberValues(COLLECTION_TYPES type) throws Exception;
+
+	public Double[] getFluoDetectorCountRatesAndDeadTimes(COLLECTION_TYPES type) throws DeviceException;
+
+	public int getNumElements(COLLECTION_TYPES type) throws DeviceException;
+
+	boolean getCollectionAllowed();
+
+	void setCollectionAllowed(boolean collectionAllowed);
+}
