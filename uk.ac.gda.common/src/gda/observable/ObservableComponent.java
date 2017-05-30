8bc4eee97cb2e888b71a21fa7936e002455dbd3e
James Mudd <james.mudd@diamond.ac.uk>
2017-05-30 09:42:51 +0100

    Add logging to IObservable to identify which observers are slow to handle events
    The default time to trigger this warning is 100 ms which can be
    configured using a property "gda.observable.notificationWarningTime"
    
    Also:
    - Improve the JavaDoc's
    - Add TODO to remove workaround to avoid calling toString
    
    Change-Id: Id5e34e4f41cb6117bf880a2b96b53492eaa0fe6c

--- uk.ac.gda.common/src/gda/observable/ObservableComponent.java
+++ uk.ac.gda.common/src/gda/observable/ObservableComponent.java
@@ -25,13 +25,30 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gda.configuration.properties.LocalProperties;
+
 /**
- * A Component that may be used by Observable objects to maintain its list of IObservers DO NOT synchronize the methods of this class as doing so in the past
- * led to deadlocks when used with DOFS and AbosulteMove objects
+ * A Component that may be used by Observable objects to maintain a set of IObservers.
+ * <p>
+ * It is thread safe due to the use of a {@link CopyOnWriteArraySet} being used to hold the {@link IObserver}'s.
  */
 public class ObservableComponent implements IObservable, IIsBeingObserved {
-	private final Set<IObserver> myIObservers = new CopyOnWriteArraySet<>();
 	private static final Logger logger = LoggerFactory.getLogger(ObservableComponent.class);
+	private static final String TIME_TO_UPDATE_WARNING_PROPERTY = "gda.observable.notificationWarningTime";
+	private static final int TIME_TO_UPDATE_WARNING_DEFAULT_MILLIS = 100; //ms
+
+	/**
+	 * This is the time in ms, which if exceeded while updating an observer of an event will cause a warning to be logged. It can be set by the property
+	 * {@value #TIME_TO_UPDATE_WARNING_PROPERTY}. If not set it defaults to {@value #TIME_TO_UPDATE_WARNING_DEFAULT_MILLIS} ms.
+	 */
+	private static final long TIME_TO_UPDATE_WARNING_MILLIS = LocalProperties.getAsInt(TIME_TO_UPDATE_WARNING_PROPERTY, TIME_TO_UPDATE_WARNING_DEFAULT_MILLIS);
+
+	/**
+	 * This is the set of observer that will be notified of events. {@link CopyOnWriteArraySet} is chosen here because although it's slow to write to the set
+	 * (i.e. when adding or removing observer) it's very fast to iterate though the set. Typically modifying observers in done infrequently compared to the
+	 * number of events sent, this offers good performance.
+	 */
+	private final Set<IObserver> myIObservers = new CopyOnWriteArraySet<>();
 
 	/**
 	 * Add an observer to the list of observers providing that the list does not already contain an instance of the observer on it. {@inheritDoc}
@@ -89,7 +106,6 @@ public class ObservableComponent implements IObservable, IIsBeingObserved {
 		for (IObserver anIObserver : myIObservers) {
 			try {
 				sendEventToObserver(anIObserver, theObserved, changeCode);
-
 			} catch (Exception ex) {
 				// Don't log the full stack trace to keep the log tidier, full trace is in debug if enabled
 				logger.error("swallowing exception: {}", ex.toString());
@@ -99,6 +115,8 @@ public class ObservableComponent implements IObservable, IIsBeingObserved {
 					 * Try to log something useful about the args of update which caused the exception to be thrown, but beware: ServerSideEventDispatcher.update
 					 * details how calling toString (including via {} in logger) on Scannables can cause deadlocks...so just use class names to describe problem.
 					 */
+					// TODO Probably this workaround to avoid calling toString in no longer required. Scannable.toFormattedString was added.
+					// Additionally this class now no longer contains explicit synchronized methods due to the use of CopyOnWriteArraySet
 					final String anIObserverDescription = anIObserver == null ? "null" : anIObserver.getClass().getName();
 					final String theObservedDescription = theObserved == null ? "null" : theObserved.getClass().getName();
 
@@ -109,8 +127,31 @@ public class ObservableComponent implements IObservable, IIsBeingObserved {
 		}
 	}
 
-	protected void sendEventToObserver(IObserver anIObserver, Object theObserved, Object changeCode) {
+	/**
+	 * This is the method which actually updates {@link IObserver}s with new events.
+	 *
+	 * @param anIObserver
+	 *            The {@link IObserver} to update i.e. the sink of events
+	 * @param theObserved
+	 *            The {@link IObservable} being observed i.e. the source of events
+	 * @param changeCode
+	 *            The event to be updated about
+	 */
+	protected void sendEventToObserver(final IObserver anIObserver, final Object theObserved, final Object changeCode) {
+		// Store the start time
+		final long startMills = System.currentTimeMillis();
+
+		// Actually do the update
 		anIObserver.update(theObserved, changeCode);
+
+		// Now do some performance analysis and possibly logging
+		final long endMillis = System.currentTimeMillis();
+		final long timeToUpdate = endMillis - startMills;
+		if (timeToUpdate > TIME_TO_UPDATE_WARNING_MILLIS) { // Took "too long" to update
+			logger.warn("Notifying observer '{}' of the event '{}' from '{}' took {} ms", anIObserver, changeCode, theObserved, timeToUpdate);
+		} else if (logger.isTraceEnabled()) { // if trace is enabled and not already logged, log the performance of the event
+			logger.trace("Notifying observer '{}' of the event '{}' from '{}' took {} ms", anIObserver, changeCode, theObserved, timeToUpdate);
+		}
 	}
 
 	@Override
