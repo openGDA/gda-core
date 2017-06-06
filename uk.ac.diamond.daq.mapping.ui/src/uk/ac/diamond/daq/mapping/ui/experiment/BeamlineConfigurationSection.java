2aa41089bfe090279a5232547d497aabe295708c
Douglas Winter <douglas.winter@diamond.ac.uk>
2017-06-06 16:36:12 +0100

    [DAQ-660] Display in Mapping view scannables configured with Configure Beamline
    This change creates a read-only text to the right of existing 
    Beamline Configuration label and button, showing the scannables
    manually configured with this tool.
    A tooltip with the same content is also set since there is only 
    room for four lines in the composite.
    
    Change-Id: Ia664459147ab52109cb6537b7f3503052d1678f6

--- uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/BeamlineConfigurationSection.java
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/BeamlineConfigurationSection.java
@@ -19,6 +19,11 @@
 package uk.ac.diamond.daq.mapping.ui.experiment;
 
 import java.net.URI;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
@@ -27,9 +32,12 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.scanning.api.device.IScannableDeviceService;
 import org.eclipse.scanning.api.event.IEventService;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,19 +51,42 @@ import uk.ac.diamond.daq.mapping.api.IMappingExperimentBean;
 public class BeamlineConfigurationSection extends AbstractMappingSection {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BeamlineConfigurationSection.class);
+	private Text summaryText;
+	private Composite configSummaryComposite;
+	private static final int MAX_TXT_LINES = 3;
+	private Map<String, Object> configuredScannables;
+	private DecimalFormat format = new DecimalFormat("##########0.0###");
 
 	@Override
 	public void createControls(Composite parent) {
 		Composite beamlineConfigComposite = new Composite(parent, SWT.NONE);
-		GridDataFactory.fillDefaults().grab(true, false).applyTo(beamlineConfigComposite);
-		final int configColumns = 2;
-		GridLayoutFactory.swtDefaults().numColumns(configColumns).applyTo(beamlineConfigComposite);
-		Label beamlineConfigLabel = new Label(beamlineConfigComposite, SWT.NONE);
-		beamlineConfigLabel.setText("Beamline Configuration");
-		GridDataFactory.fillDefaults().span(configColumns, 1).applyTo(beamlineConfigLabel);
-		Button editBeamlineConfigButton = new Button(beamlineConfigComposite, SWT.PUSH);
+		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false).applyTo(beamlineConfigComposite);
+		GridDataFactory.fillDefaults().grab(false, false).applyTo(beamlineConfigComposite);
+
+		Composite configLabelAndButtonComposite = new Composite(beamlineConfigComposite,SWT.NONE);
+		GridLayoutFactory.swtDefaults().numColumns(1).equalWidth(false).applyTo(configLabelAndButtonComposite);
+
+		Label configLabel = new Label(configLabelAndButtonComposite, SWT.NONE);
+		configLabel.setText("Beamline Configuration");
+
+		Button editBeamlineConfigButton = new Button(configLabelAndButtonComposite, SWT.PUSH);
 		editBeamlineConfigButton.setText("Configure Beamline...");
 		editBeamlineConfigButton.addListener(SWT.Selection, event -> editBeamlineConfiguration());
+
+		configSummaryComposite = new Composite(beamlineConfigComposite, SWT.NONE);
+		configSummaryComposite.setVisible(false);
+		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false).applyTo(configSummaryComposite);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(configSummaryComposite);
+
+		final Label separator = new Label(configSummaryComposite, SWT.SEPARATOR | SWT.VERTICAL);
+		GridDataFactory.fillDefaults().hint(SWT.DEFAULT, configLabelAndButtonComposite.getSize().y).grab(false, false).applyTo(separator);
+
+		summaryText = new Text(configSummaryComposite, SWT.MULTI | SWT.READ_ONLY);
+		summaryText.setLayoutData(new GridData(SWT.FILL,SWT.FILL,true,true));
+		summaryText.setForeground(Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY));
+
+		configuredScannables = getMappingBean().getBeamlineConfiguration();
+		updateConfiguredScannableSummary(configuredScannables);
 	}
 
 	private void editBeamlineConfiguration() {
@@ -66,7 +97,9 @@ public class BeamlineConfigurationSection extends AbstractMappingSection {
 			dialog.setInitialBeamlineConfiguration(mappingBean.getBeamlineConfiguration());
 			dialog.create();
 			if (dialog.open() == Window.OK) {
-				mappingBean.setBeamlineConfiguration(dialog.getModifiedBeamlineConfiguration());
+				configuredScannables = dialog.getModifiedBeamlineConfiguration();
+				mappingBean.setBeamlineConfiguration(configuredScannables);
+				updateConfiguredScannableSummary(configuredScannables);
 			}
 		} catch (Exception e) {
 			LOGGER.error("Could not edit beamline configuration", e);
@@ -78,6 +111,28 @@ public class BeamlineConfigurationSection extends AbstractMappingSection {
 		IEventService eventService = getService(IEventService.class);
 		URI jmsURI = new URI(LocalProperties.getActiveMQBrokerURI());
 		return  eventService.createRemoteService(jmsURI, IScannableDeviceService.class);
+	}
+
+	private void updateConfiguredScannableSummary(Map<String, Object> configured) {
+
+		try {
+			List<String> txt = new ArrayList<>();
+			for (Map.Entry<String,Object> entry : configured.entrySet()) {
+				String key = entry.getKey();
+				Object value = entry.getValue();
+				txt.add(key+" = "+(value instanceof Number ? format.format(value) : value ));
+			}
+			summaryText.setToolTipText(txt.stream().collect(Collectors.joining("\n")));
+			if (txt.size()>MAX_TXT_LINES) {
+				txt = txt.subList(0, MAX_TXT_LINES);
+				txt.set(MAX_TXT_LINES-1, ".....");
+			}
+			summaryText.setText(txt.stream().collect(Collectors.joining("\n")));
+			configSummaryComposite.setVisible( !txt.isEmpty()? true : false);
+
+		} catch (NullPointerException npe) {
+			LOGGER.debug("MappingExperimentBean.beamlineConfiguration is null on startup in new workspace: "+npe);
+		}
 	}
 
 }
