af0857378b61973d2679327218373e1a550ebbf5
Peter Holloway <peter.holloway@diamond.ac.uk>
2017-06-14 12:33:30 +0100

    BSSC and HPLC script beamline updates

--- uk.ac.gda.devices.bssc/scripts/BSSC.py
+++ uk.ac.gda.devices.bssc/scripts/BSSC.py
@@ -1,4 +1,5 @@
 import datetime, time, sys, os
+from java.util import HashMap
 from gda.data import PathConstructor
 import gda.factory.Finder
 from uk.ac.gda.devices.bssc.beans import BSSCSessionBean
@@ -7,11 +8,18 @@ import gda.jython.commands.ScannableCommands
 from gda.jython import InterfaceProvider
 from gda.commandqueue import JythonScriptProgressProvider
 from gda.jython.commands.GeneralCommands import pause
+from tfgsetup import fs
+from cStringIO import StringIO
+from gdascripts.pd.epics_pds import DisplayEpicsPVClass, SingleEpicsPositionerClass
+
+SAMPLE_HOLD = True
 
 class BSSCRun:
     def __init__(self, beanFile):
+        self.__version__ = '1.00'
         finder = gda.factory.Finder.getInstance()
         find = finder.find
+        self.holdsample = SAMPLE_HOLD
         self.samplevolume = 20
         self.beanFile = beanFile
         self.bean = BSSCSessionBean.createFromXML(beanFile)
@@ -21,9 +29,11 @@ class BSSCRun:
         self.shutter = find("shutter")
         self.bsscscannable = find("bsscscannable")
         self.processing = find("bssc_proc")
-        self.energy = float(find("dcm_energy").getPosition())
+        #self.energy = float(find("dcm_energy").getPosition())
         self.sampleConcentration = find("sample_concentration")
         self.sampleName = find("samplename")
+        self.sample_type = find('sample_type')
+        self.sample_environment = find('sample_environment')
 
         self.meta = GDAMetadataProvider.getInstance()
 
@@ -43,6 +53,41 @@ class BSSCRun:
 
         currentVisit = GDAMetadataProvider.getInstance().getMetadataValue("visit")
         self.totalSteps = self.overheadsteps + self.bean.getMeasurements().size() * self.stepspersample
+
+    def getFastShutter(self):
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+        fs()
+        sys.stdout = old_stdout
+        if mystdout.getvalue() == 'fs: Open\n':
+            self.reportProgress('Fast shutter is open')
+            return True
+        else:
+            self.reportProgress('Fast shutter is closed')
+            return False
+
+    def setFastShutter(self, command='Open'):
+        if command in ['Open', 'Close']:
+            self.reportProgress('Setting fast shutter to '+command)
+            fs(command)
+            
+        else:
+            self.reportProgress('setFastValve function requires either Close or Open as input')
+
+    def armFastValve(self):
+        try:
+            fv1.getPosition()
+        except:
+            fv1 = SingleEpicsPositionerClass('fv1', 'BL21B-VA-FVALV-01:CON', 'BL21B-VA-FVALV-01:STA', 'BL21B-VA-FVALV-01:STA', 'BL21B-VA-FVALV-01:CON', 'mm', '%d')
+        if not fv1.getPosition() == 3.0:
+            fv1(3.0)
+
+
+    def setHoldSample(self, holdsample):
+        if type(holdsample) == type(True):
+            self.holdsample = holdsample
+        else:
+            self.reportProgress("ERROR: setHoldSample input should be either True or False")
 
     def reportProgress(self, message):
         self.progresscounter += 1
@@ -67,17 +112,17 @@ class BSSCRun:
             time.sleep(2)
             raise
         time.sleep(0.1)
-        
+
     def checkDevice(self):
         self.bssc.setEnableVolumeDetectionInWell(True)
         print "Plates in Sample Changer: ", self.bssc.getPlatesIDs()
-    
+
     def getStorageTemperature(self):
         if not self.isSimulation:
             return self.bssc.getTemperatureSampleStorage()
         else:
             return -300.0
-            
+
     def getExposureTemperature(self):
         if not self.isSimulation:
             return self.bssc.getTemperatureSEU()
@@ -87,33 +132,33 @@ class BSSCRun:
     def setExposureTemperature(self, temperature):
         if not self.isSimulation:
             self.monitorAsynchronousMethod(self.bssc.waitTemperatureSEU((temperature)))
-    
+
     def clean(self):
         self.monitorAsynchronousMethod(self.bssc.clean())
-    
+
     def setupTfg(self, frames, tpf):
         self.tfg.clearFrameSets()
-        self.tfg.addFrameSet(frames, 50, tpf * 1000, 0, 251, 0, 0);
+        self.tfg.addFrameSet(frames, 50, tpf * 1000, int('00000100', 2), int('11111111', 2), 0, 0);
         self.tfg.loadFrameSets()
         return frames * (tpf + 0.05)
-    
+
     def setTitle(self, title):
         GDAMetadataProvider.getInstance().setMetadataValue("title", title)
-    
+
     def loadWell(self, location):
         self.monitorAsynchronousMethod(self.bssc.fill(location.getPlate(), location.getRowAsInt(), location.getColumn(), self.samplevolume))
-        
+
     def unloadIntoWell(self, location):
         self.monitorAsynchronousMethod(self.bssc.recuperate(location.getPlate(), location.getRowAsInt(), location.getColumn()))
-    
+
     def doTheScan(self, scannables):
         scan = gda.scan.StaticScan(scannables)
         scan.runScan()
-        return scan.getDataWriter().getCurrentFileName() 
-    
+        return scan.getDataWriter().getCurrentFileName()
+
     def expose(self, duration):
         speed = self.samplevolume / duration
-        if speed >= 5 and speed <= 6000: 
+        if speed >= 5 and speed <= 6000:
             # simulation reports these limits
             taskid = self.bssc.push(self.samplevolume, speed)
             filename = self.doTheScan(self.scannables)
@@ -122,7 +167,7 @@ class BSSCRun:
             print "sample speed %5.1f outside allowed range, will do static exposure" % speed
             filename = self.doTheScan(self.scannables)
         return filename
-            
+
     def openShutter(self):
         self.shutter.asynchronousMoveTo("Open")
 
@@ -130,12 +175,17 @@ class BSSCRun:
         self.shutter.asynchronousMoveTo("Close")
 
     def measureSample(self, titration, duration):
-        if titration != None:     
+        if titration != None:
             self.reportProgress("Cleaning before Sample")
             pause()
             self.clean()
             self.reportProgress("Sucking in Sample")
             self.loadWell(titration.getLocation())
+            if self.holdsample:
+                self.reportProgress("Turning on sample position feedback")
+                self.bssc.setLiquidPositionFixed(self.holdsample)
+            else:
+                self.reportProgress("Sample position feedback is off.")
             self.reportProgress("Exposing Sample")
 
             self.setTitle("Sample: %s (Location %s)" % (titration.getSampleName(), titration.getLocation().toString()))
@@ -158,20 +208,27 @@ class BSSCRun:
         self.setExposureTemperature(titration.getExposureTemperature())
         self.reportProgress("Setting Up Time Frame Generator")
         return self.setupTfg(titration.getFrames(), titration.getTimePerFrame())
-        
+
     def run(self, processing=True):
+        self.reportProgress('Running BSSC script version '+self.__version__)
         self.reportProgress("Initialising");
         self.checkDevice()
+        if self.getFastShutter():
+            self.setFastShutter('Close')
         self.bssc.setSampleType("green")
         self.reportProgress("Opening Shutter")
+        self.armFastValve()
         self.openShutter()
+        self.sample_environment('BSSC')
         for titration in self.bean.getMeasurements():
             self.meta.setMetadataValue('visit', titration.getVisit())
             print  "\n== Running Titration " + titration.getSampleName() + " =="
+            self.sample_type('buffer' if titration.buffer else 'sample+buffer')
             duration = self.setUpRobotAndDetector(titration)
             samplefile = self.measureSample(titration, duration)
             titration.datafilename = samplefile
-            self.processing.triggerProcessing(samplefile)
+            # if processing:
+            #     self.processing.triggerProcessing(samplefile)
 
         self.reportProgress("\nClosing shutter")
         self.closeShutter()
@@ -179,6 +236,8 @@ class BSSCRun:
         self.bssc.setViscosityLevel("high")
         self.clean()
         self.exportFinalBeans()
+        self.sample_environment('Manual')
+        self.sample_type('sample')
 
     def exportFinalBeans(self):
         """The samples are updated to include the datafile paths
--- uk.ac.gda.devices.hplc/scripts/HPLC.py
+++ uk.ac.gda.devices.hplc/scripts/HPLC.py
@@ -1,17 +1,229 @@
+'''
+Created on April 28th, 2017
+
+@author: nathan
+'''
 from uk.ac.gda.devices.hplc.beans import HplcSessionBean
 from gda.commandqueue import JythonScriptProgressProvider
+from gdascripts.pd.epics_pds import DisplayEpicsPVClass, SingleEpicsPositionerClass
+from gda.data.metadata import GDAMetadataProvider
+from gda.scan import StaticScan
+from tfgsetup import fs
 from time import sleep
+import logging
+import gda.factory.Finder
+import sys
+from cStringIO import StringIO
+from math import ceil
+from datetime import datetime
+
 
 class HPLC(object):
+    """Handles collection of HPLC SAXS data in GDA/Jython
+
+    The class is called via the 'Queue Experiment' button on the
+    <filename.hplc> xml file in the HPLC setup perspective in GDA.
+    The class has various functions to check shutters etc on the 
+    beamline as well as measuring the data.
+    """
+    
     def __init__(self, filename):
+        self.__version__ = '1.00'
         self.hplcFile = filename
         self.bean = HplcSessionBean.createFromXML(filename)
+        finder = gda.factory.Finder.getInstance()
+        find = finder.find
+        self.shutter = find('shutter')
+        self.sample_type = find('sample_type')
+        self.sampleName = find("samplename")
+        self.environment = find("sample_environment")
+        self.tfg = finder.listAllLocalObjects("gda.device.Timer")[0]
+        self.ncddetectors = finder.listAllLocalObjects("uk.ac.gda.server.ncd.detectorsystem.NcdDetectorSystem")[0]
+        #CREATE A LOGGER
+        self.logger = logging.getLogger('HPLC')
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(module)s: %(message)s',"[%Y-%m-%d %H:%M:%S]")
+        streamhandler = logging.StreamHandler()
+        streamhandler.setFormatter(formatter)
+        if len(self.logger.handlers) == 0:
+            self.logger.addHandler(streamhandler)
+        self.logger.info('HPLC class version '+self.__version__+' was instantiated')
+        
+    def setupTfg(self, frames, readout_time, tpf):
+        self.tfg.clearFrameSets()
+        self.tfg.addFrameSet(frames, readout_time * 1000, tpf * 1000, int('00000100', 2), int('11111111', 2), 0, 0)#note, byte order is reversed!
+        self.tfg.loadFrameSets()
+        return frames * (tpf + readout_time)
+    
+    def setTitle(self, title):
+        GDAMetadataProvider.getInstance().setMetadataValue("title", title)
+        
+    def setSampleType(self, type='sample'):
+        if type in ['sample', 'sample+buffer', 'buffer']:
+            self.sample_type(type)
+        else:
+            self.logger.error('Sample type should be one of: sample, sample+buffer, buffer.')
+            
+    def setEnvironment(self, type='HPLC'):
+        if type in ['BSSC', 'HPLC', 'Manual']:
+            self.environment(type)
+        else:
+            self.logger.error('Environment type should be one of: HPLC, BSSC, Manual.')
+
+    def armFastValve(self):
+        try:
+            fv1.getPosition()
+        except:
+            fv1 = SingleEpicsPositionerClass('fv1', 'BL21B-VA-FVALV-01:CON', 'BL21B-VA-FVALV-01:STA', 'BL21B-VA-FVALV-01:STA', 'BL21B-VA-FVALV-01:CON', 'mm', '%d')
+        if not fv1.getPosition() == 3.0:
+            fv1(3.0)
+
+
+    def getSearchStatus(self):
+        try:
+            eh_search_status.getPosition()
+        except:
+            eh_search_status=DisplayEpicsPVClass('eh_search_status', 'BL21B-PS-IOC-01:M11:LOP', 'units', '%d')
+
+        if eh_search_status.getPosition() != 0.0:
+            return False
+        else:
+            return True
+
+    def getSafetyShutter(self):
+        if self.shutter.getPosition() == 'Open':
+            return True
+        else:
+            return False
+
+    def setSafetyShutter(self, command='Open'):
+        levels = ['Open', 'Close']
+        if command in levels:
+            self.shutter(command)
+            self.logger.info('Setting safety shutter to '+command)
+        else:
+            self.logger.error("setSafetyShutter function requires either 'Open' or 'Close' as input")
+
+            
+    def getHplcValve(self):
+        try:
+            hplcvalve_status.getPosition()
+        except:
+            hplcvalve_status=DisplayEpicsPVClass('hplcvalve_status', 'BL21B-EA-HPLC-01:MOD1:VALVE:CTRL', 'units', '%d')
+        if hplcvalve_status.getPosition() == 1.0:
+            return True
+        else:
+            return False
+                                  
+    def getFastShutter(self):
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = StringIO()
+        fs()
+        sys.stdout = old_stdout
+        if mystdout.getvalue() == 'fs: Open\n':
+            self.logger.info('Fast shutter is open')
+            return True
+        else:
+            self.logger.info('Fast shutter is closed')
+            return False
+
+    def setFastShutter(self, command='Open'):
+        if command in ['Open', 'Close']:
+            self.logger.info('Setting fast shutter to '+command)
+            fs(command)
+            
+        else:
+            self.logger.error('setFastValve function requires either Close or Open as input')
+
+    def getInjectSignal(self):
+        cutoff = 3.0
+        signal = 3.05
+        try:
+            signal = inject_signal.getPosition()
+        except:
+            inject_signal=DisplayEpicsPVClass('inject_signal', 'BL21B-EA-ENV-01:HPLC:TRIG', 'units', '%.3e')
+            signal = inject_signal.getPosition()
+            
+        if signal < cutoff:
+            return True
+        else:
+            return False
+            
+        
+
     def run(self, processing=True):
-        if processing == "true": processing = True
-        elif processing == "false": processing = False
-        print "working with %s" % self.hplcFile
-        print "processing is %s" %processing
-        for i, b in enumerate(self.bean.measurements):
-            print b.sampleName
-            JythonScriptProgressProvider.sendProgress(100*i/float(len(self.bean.measurements)), "Processing %s" %b.sampleName)
-            sleep(2)
+        try:
+            if not self.getSafetyShutter():
+                if self.getSearchStatus():
+                    #self.armFastValve()
+                    self.setSafetyShutter('Open')
+                else:
+                    self.logger.error('Search the hutch you crazy fool!')
+                    raise EnvironmentError('The script terminated early because the hutch is not searched.')
+                
+            if not self.getHplcValve():
+                self.logger.error('The HPLC static/flow valve is in static position, this may be caused by a loss of vacuum in the sample section.')
+                raise EnvironmentError('The script terminated early because the HPLC static/flow valve is in the static position')
+            
+            if self.getFastShutter():
+                self.logger.info('Fast shutter was open, closing it so the tfg can take care of it during collection')
+                self.setFastShutter('Close')
+                
+            self.setEnvironment('HPLC')
+            self.setSampleType('sample+buffer')
+
+            for i, b in enumerate(self.bean.measurements):
+                self.logger.info('---- STARTING RUN '+str(i)+' of '+str(len(self.bean.measurements))+': SAMPLE: '+b.getSampleName()+' ----')
+                readout_time = 0.1
+                exposure_time = b.getTimePerFrame()
+                pre_run_delay = 120
+                
+                #GET THE RUN TIME
+                try:
+                    runtime = int(b.getComment())
+                except:
+                    self.logger.error('The run time of the experiment is set in the comment box, must be an integer and is in minutes, using 30 mins as a default')
+                    runtime = 30
+                number_of_images = int(ceil(runtime * 60.0 / ( readout_time + exposure_time)))
+                
+                #Set up run parameters
+                self.setupTfg(number_of_images, readout_time, exposure_time)
+                self.setTitle(b.getSampleName())
+                
+                #Get the inject signal
+                self.logger.info('Waiting for injection signal from HPLC')
+                starttime = datetime.now()
+                elapsed_time = 0
+                found_signal = False
+                while elapsed_time < pre_run_delay:
+                    if self.getInjectSignal():
+                        self.logger.info('Found injection signal')
+                        found_signal = True
+                        break
+                    else:
+                        elapsed_time = (datetime.now() - starttime).seconds
+                    sleep(0.1)
+                if not found_signal:
+                    self.logger.info('Did not find the inject signal, will proceed anyway.')
+                    
+                #Start the data collection
+                self.logger.info('Starting collection of '+str(number_of_images)+'x'+str(exposure_time)+' second exposures')
+                StaticScan([self.ncddetectors]).run()
+                self.logger.info('Finished collecting '+b.getSampleName())
+            self.setSafetyShutter('Close')
+            self.logger.info('SCRIPT FINISHED NORMALLY')
+            #namespace:
+            #b.getLocation().getRow()
+            #b.getLocation().getColumn()
+            #b.getSampleName()
+            #b.getConcentration()
+            #b.getMolecularWeight()
+            #b.getTimePerFrame()
+            #b.getBuffers()
+            #b.getComment()
+            #b.getVisit()
+            #b.getUsername()
+            #JythonScriptProgressProvider.sendProgress(100*i/float(len(self.bean.measurements)))
+        except:
+            self.logger.error('SCRIPT TERMINATED IN ERROR')
+
