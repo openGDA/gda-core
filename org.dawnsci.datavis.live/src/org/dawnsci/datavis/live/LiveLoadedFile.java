fe6002a3cd3ff34eebb8e2795a791018349256e0
Jacob Filik <jacob.filik@diamond.ac.uk>
2017-07-10 14:57:18 +0100

    Include axes in datasets from NXdata

--- org.dawnsci.datavis.live/src/org/dawnsci/datavis/live/LiveLoadedFile.java
+++ org.dawnsci.datavis.live/src/org/dawnsci/datavis/live/LiveLoadedFile.java
@@ -1,6 +1,7 @@
 package org.dawnsci.datavis.live;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -12,25 +13,34 @@ import org.dawnsci.datavis.model.LoadedFile;
 import org.dawnsci.datavis.model.NDimensions;
 import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
 import org.eclipse.dawnsci.analysis.api.io.ILoaderService;
+import org.eclipse.dawnsci.analysis.api.processing.NXCite;
+import org.eclipse.dawnsci.analysis.api.tree.Attribute;
 import org.eclipse.dawnsci.analysis.api.tree.DataNode;
+import org.eclipse.dawnsci.analysis.api.tree.GroupNode;
 import org.eclipse.dawnsci.analysis.api.tree.IFindInTree;
 import org.eclipse.dawnsci.analysis.api.tree.Node;
 import org.eclipse.dawnsci.analysis.api.tree.NodeLink;
 import org.eclipse.dawnsci.analysis.api.tree.Tree;
 import org.eclipse.dawnsci.analysis.api.tree.TreeUtils;
 import org.eclipse.dawnsci.analysis.tree.TreeToMapUtils;
+import org.eclipse.january.MetadataException;
 import org.eclipse.january.dataset.Dataset;
+import org.eclipse.january.dataset.IDataset;
 import org.eclipse.january.dataset.IDatasetConnector;
 import org.eclipse.january.dataset.IDynamicDataset;
 import org.eclipse.january.dataset.ILazyDataset;
 import org.eclipse.january.dataset.IRemoteData;
 import org.eclipse.january.dataset.LazyDatasetBase;
+import org.eclipse.january.dataset.StringDataset;
+import org.eclipse.january.metadata.AxesMetadata;
 import org.eclipse.january.metadata.IMetadata;
 import org.eclipse.january.metadata.Metadata;
+import org.eclipse.january.metadata.MetadataFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.diamond.scisoft.analysis.io.DataHolder;
+import uk.ac.diamond.scisoft.analysis.io.NexusTreeUtils;
 
 public class LiveLoadedFile extends LoadedFile implements IRefreshable {
 
@@ -76,6 +86,8 @@ public class LiveLoadedFile extends LoadedFile implements IRefreshable {
 	private static void fillDataHolder(IDataHolder dh, Map<String, NodeLink> result, Tree tree, Map<String, DataOptions> options, LoadedFile file, String path, String host, int port) {
 		IMetadata md = dh.getMetadata();
 		
+		Map<String, String[]> axesMap = new HashMap<>();
+		
 		for (Entry<String, NodeLink> s : result.entrySet()) {
 			
 			String name = "/"+s.getKey();
@@ -96,21 +108,96 @@ public class LiveLoadedFile extends LoadedFile implements IRefreshable {
 				DataOptions d = new DataOptions(name, file);
 				options.put(d.getName(),d);
 			}
+			
+			String[] split = name.split("/");
+			
+			String shortName = split[split.length-1];
+			
+			String[] axes = getAxes(dataset,s.getValue(), shortName);
+			if (axes != null) axesMap.put(name, axes);
 		
 		}
 		
 		dh.setMetadata(md);
 		
 		dh.setTree(tree);
+		
+		try {
+			buildAxes(axesMap, dh);
+		} catch (MetadataException e) {
+			logger.error("Could not build AxesMetadata!", e);
+		}
 	}
 	
-//	private static void addAxesMetadata(IDynamicDataset d, NodeLink l){
-//		Node s = l.getSource();
-//		//should axes be found in Finder?
-////		if (s instanceof GroupNode) {
-////			((GroupNode)s).
-////		}
-//	}
+	private static void buildAxes(Map<String, String[]> map, IDataHolder dh) throws MetadataException {
+		
+		for (Entry<String, String[]> entry : map.entrySet()) {
+			String name = entry.getKey();
+			if (dh.contains(name)){
+				int[] shape = dh.getLazyDataset(name).getShape();
+				String[] names = entry.getValue();
+				AxesMetadata m = null;
+				int index = name.lastIndexOf("/");
+				
+				for (int i = 0 ; (i < shape.length && i < names.length) ; i++) {
+					
+					String fullName = name.substring(0, index+1) + names[i];
+					if (dh.contains(fullName)){
+						if (m == null) m = MetadataFactory.createMetadata(AxesMetadata.class, shape.length);
+						ILazyDataset ax = dh.getLazyDataset(fullName);
+						ax.setName(names[i]);
+						m.addAxis(i, dh.getLazyDataset(fullName));
+					}
+					
+				}
+			
+				if (m != null) {
+					ILazyDataset lazyDataset = dh.getLazyDataset(name);
+					lazyDataset.addMetadata(m);
+				}
+			}
+		}
+		
+	}
+	
+	private static String[] getAxes(IDynamicDataset d, NodeLink l, String name){
+		Node s = l.getSource();
+
+		if (s instanceof GroupNode) {
+			GroupNode g = (GroupNode)s;
+			if (getNXClass(g).equals(NexusTreeUtils.NX_DATA)) {
+				String signal = getNXSignal(g);
+				if (signal != null && signal.equals(name)) {
+					return getNXaxes(g);
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	private static String getNXClass(GroupNode g){
+		Attribute attribute = g.getAttribute(NexusTreeUtils.NX_CLASS);
+		if (attribute != null) return attribute.getFirstElement();
+		return "";
+	}
+	
+	private static String getNXSignal(GroupNode g){
+		Attribute attribute = g.getAttribute(NexusTreeUtils.NX_SIGNAL);
+		if (attribute != null) return attribute.getFirstElement();
+		return null;
+	}
+	
+	private static String[] getNXaxes(GroupNode g){
+		Attribute attribute = g.getAttribute(NexusTreeUtils.NX_AXES);
+		if (attribute == null) return null;
+		IDataset value = attribute.getValue();
+		if (value instanceof StringDataset) {
+			return ((StringDataset)value).getData();
+		}
+		return null;
+	}
+	
 	
 	private static Tree getTree(String path, String host, int port){
 		IRemoteData rd = ServiceManager.getRemoteDatasetService().createRemoteData(host, port);
@@ -201,7 +288,18 @@ public class LiveLoadedFile extends LoadedFile implements IRefreshable {
 	
 	private void addNonStringDataset(ILazyDataset lazyDataset, String name){
 		if (lazyDataset instanceof IDynamicDataset) {
-			((IDynamicDataset)lazyDataset).refreshShape();
+			try {
+				((IDynamicDataset)lazyDataset).refreshShape();
+				AxesMetadata ax = lazyDataset.getFirstMetadata(AxesMetadata.class);
+				if (ax != null) {
+					int[] refresh = ax.refresh(lazyDataset.getShape());
+					((IDynamicDataset) lazyDataset).resize(refresh);
+				}
+
+
+			}catch (Exception e) {
+				logger.error("Could not refresh dataset:" + lazyDataset.getName(),e);
+			}
 		}
 		if (lazyDataset != null && ((LazyDatasetBase)lazyDataset).getDType() != Dataset.STRING) {
 			DataOptions d = new DataOptions(name, this);
@@ -213,9 +311,18 @@ public class LiveLoadedFile extends LoadedFile implements IRefreshable {
 		IDataHolder dh = dataHolder.get();
 		String[] names = dh.getNames();
 		for (String n : names) {
-			ILazyDataset lazyDataset = dh.getLazyDataset(n);
-			if (lazyDataset instanceof IDynamicDataset) {
-				((IDynamicDataset)lazyDataset).refreshShape();
+			try {
+				ILazyDataset lazyDataset = dh.getLazyDataset(n);
+				if (lazyDataset instanceof IDynamicDataset) {
+					((IDynamicDataset)lazyDataset).refreshShape();
+					AxesMetadata ax = lazyDataset.getFirstMetadata(AxesMetadata.class);
+					if (ax != null) {
+						int[] refresh = ax.refresh(lazyDataset.getShape());
+						((IDynamicDataset) lazyDataset).resize(refresh);
+					}
+				}
+			}catch (Exception e) {
+				logger.error("Could not refresh dataset: " + n,e);
 			}
 		}
 	}
