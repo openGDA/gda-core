0b60d3802ba49eba47c97ccf2f8085cf2e520963
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-09-18 15:09:26 +0100

    I06-329 shared device access control for multiple end stations beamline.
    - support EPICS CONTROLLER PV implementation which changes by shutter operation.
    - restrict to asynchronousMoveTo enforce only
    - remove exception for beamline staffs (PBS changed his mind on this.)
    
    Change-Id: Iaa8ab02113217634be4ef82413084e3f2567f67c

---
+++ uk.ac.gda.epics/src/gda/aop/DeviceAccess.java
@@ -0,0 +1,69 @@
+/*-
+ * Copyright © 2017 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.aop;
+
+import java.util.Arrays;
+
+import gda.device.DeviceException;
+import gda.device.enumpositioner.EpicsSimpleMbbinary;
+
+public class DeviceAccess {
+	/**
+	 * access to EPICS CONTROLLER PV
+	 */
+	private EpicsSimpleMbbinary controller;
+
+	private String endStation;
+
+	public boolean hasAccess() throws DeviceException {
+		String trim = ((String)controller.getPosition()).trim();
+		return trim.equals(endStation);
+	}
+
+	public void init() throws Exception {
+		if (endStation == null || endStation.isEmpty()) {
+			throw new IllegalStateException("endStation is not set.");
+		}
+		if (controller == null) {
+			throw new IllegalStateException("Epics Controller scannable is not set.");
+		}
+
+		String[] positions = controller.getPositions();
+		if (!Arrays.asList(positions).contains(endStation)) {
+			throw new IllegalArgumentException("Value of endStation is must be one of "+positions);
+		}
+	}
+
+	public EpicsSimpleMbbinary getController() {
+		return controller;
+	}
+
+	public void setController(EpicsSimpleMbbinary controller) {
+		this.controller = controller;
+	}
+
+	public String getEndStation() {
+		return endStation;
+	}
+
+	public void setEndStation(String endStation) {
+		this.endStation = endStation;
+	}
+
+}
