c4d78b5702fad5a3729d36739cccfa3966630598
James Mudd <james.mudd@diamond.ac.uk>
2017-10-02 12:51:51 +0100

    DAQ-836 Cleanup of LocalProperties
    - Remove the use of the LocalProperties constructor which is now private
    - Remove commented out code
    - Return value without local variable
    
    Change-Id: Ic4345eb0691833d7335456f6673d33c7b815c7a5

--- uk.ac.gda.core/src/gda/util/LocalPropertiesPropertySource.java
+++ uk.ac.gda.core/src/gda/util/LocalPropertiesPropertySource.java
@@ -7,15 +7,12 @@ import gda.configuration.properties.LocalProperties;
 public class LocalPropertiesPropertySource extends PropertySource<LocalProperties> {
 
 	public LocalPropertiesPropertySource() {
-		super("LocalProperties", new LocalProperties());
+		super("LocalProperties");
 	}
 
 	@Override
 	public String getProperty(String name) {
-//		System.out.println("getting property " + name);
-		final String value = LocalProperties.get(name);
-//		System.out.println("returning value " + value);
-		return value;
+		return LocalProperties.get(name);
 	}
 
 }
