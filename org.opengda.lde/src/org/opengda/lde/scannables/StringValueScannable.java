64ef71731e5f1af2da130645a8f5f040cd5e0969
Anthony Hull <anthony.hull@diamond.ac.uk>
2017-11-03 00:10:25 +0000

    [DAQ-982] More consistent behaviour of toFormattedString()
    - Log exceptions as warnings
    - Always return a string: do not throw a RuntimeException

--- org.opengda.lde/src/org/opengda/lde/scannables/StringValueScannable.java
+++ org.opengda.lde/src/org/opengda/lde/scannables/StringValueScannable.java
@@ -1,7 +1,9 @@
 package org.opengda.lde.scannables;
 
-import gda.device.DeviceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import gda.device.DeviceException;
 import gda.device.scannable.ScannableBase;
 import gda.device.scannable.corba.impl.ScannableAdapter;
 import gda.device.scannable.corba.impl.ScannableImpl;
@@ -12,9 +14,11 @@ import gda.factory.corba.util.CorbaImplClass;
 @CorbaImplClass(ScannableImpl.class)
 public class StringValueScannable extends ScannableBase {
 
+	private static final Logger logger = LoggerFactory.getLogger(StringValueScannable.class);
+
 	private String value="Undefined";
 	private String name;
-	
+
 	@Override
 	public String getName() {
 		return name;
@@ -25,7 +29,7 @@ public class StringValueScannable extends ScannableBase {
 	}
 	public StringValueScannable() {
 	}
-	
+
 	@Override
 	public void configure() throws gda.factory.FactoryException {
 		if (!configured) {
@@ -51,9 +55,10 @@ public class StringValueScannable extends ScannableBase {
 	@Override
 	public String toFormattedString() {
 		try {
-			return getName() +": "+getPosition();
+			return getName() + " : " + getPosition();
 		} catch (Exception e) {
-			throw new RuntimeException("Exception in " + getName() + ".toFormattedString()", e);
+			logger.warn("Exception formatting position for {}", getName(), e);
+			return valueUnavailableString();
 		}
 	}
 }
