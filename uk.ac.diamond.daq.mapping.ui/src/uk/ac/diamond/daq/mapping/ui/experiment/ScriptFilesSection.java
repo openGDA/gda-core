b4ca0e0e9146f97841ebee5a1587190240b9c4e7
Douglas Winter <douglas.winter@diamond.ac.uk>
2017-12-04 14:48:44 +0000

    DAQ-676 Add scripts summary to Mapping UI when scripts are selected
    In the style (roughly) of BeamlineConfigurationSection
    
    Change-Id: Iae764ed6a453ee475fe1bd3f0bb5da4699a6c9ee

--- uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/ScriptFilesSection.java
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/ScriptFilesSection.java
@@ -18,18 +18,27 @@
 
 package uk.ac.diamond.daq.mapping.ui.experiment;
 
+import java.io.File;
+import java.util.Objects;
+
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.richbeans.api.generator.IGuiGeneratorService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+import uk.ac.diamond.daq.mapping.api.IScriptFiles;
 
 /**
  * A section to configure the script files to run before and/or after a scan.
  */
 public class ScriptFilesSection extends AbstractMappingSection {
+
+	private Text summaryText;
 
 	@Override
 	public boolean shouldShow() {
@@ -41,12 +50,17 @@ public class ScriptFilesSection extends AbstractMappingSection {
 	@Override
 	public void createControls(Composite parent) {
 		final Composite scriptsComposite = new Composite(parent, SWT.NONE);
-		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false).applyTo(scriptsComposite);
+		GridLayoutFactory.swtDefaults().numColumns(3).equalWidth(false).applyTo(scriptsComposite);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(scriptsComposite);
 
-		final Label scriptsLabel = new Label(scriptsComposite, SWT.NONE);
-		scriptsLabel.setText("Script Files");
-		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).grab(true, false).applyTo(scriptsLabel);
+		new Label(scriptsComposite, SWT.NONE).setText("Script Files");
+
+		Composite scriptsSummaryComposite = new Composite(scriptsComposite, SWT.NONE);
+		GridLayoutFactory.swtDefaults().numColumns(2).equalWidth(false).applyTo(scriptsSummaryComposite);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(scriptsSummaryComposite);
+		summaryText = new Text(scriptsSummaryComposite, SWT.MULTI | SWT.READ_ONLY);
+		GridDataFactory.swtDefaults().grab(true, false).applyTo(summaryText);
+		summaryText.setForeground(Display.getDefault().getSystemColor(SWT.COLOR_DARK_GRAY));
 
 		final Button editScriptsButton = new Button(scriptsComposite, SWT.PUSH);
 		editScriptsButton.setImage(MappingExperimentUtils.getImage("icons/pencil.png"));
@@ -54,9 +68,28 @@ public class ScriptFilesSection extends AbstractMappingSection {
 		GridDataFactory.swtDefaults().align(SWT.TRAIL, SWT.CENTER).applyTo(editScriptsButton);
 
 		final IGuiGeneratorService guiGenerator = getService(IGuiGeneratorService.class);
-		editScriptsButton.addListener(SWT.Selection, event ->
-			guiGenerator.openDialog(getMappingBean().getScriptFiles(), parent.getShell(), "Select Script Files")
-		);
+		editScriptsButton.addListener(SWT.Selection, event -> {
+			guiGenerator.openDialog(getMappingBean().getScriptFiles(), parent.getShell(), "Select Script Files");
+			updateSummaryText();
+		});
+
+		updateSummaryText();
+	}
+
+	private void updateSummaryText() {
+		IScriptFiles scripts = getMappingBean().getScriptFiles();
+		StringBuilder summary = new StringBuilder();
+		if (Objects.nonNull(scripts.getBeforeScanScript()) && !scripts.getBeforeScanScript().isEmpty()) {
+			summary.append("before: " + getScriptName(scripts.getBeforeScanScript())+";");
+		}
+		if (Objects.nonNull(scripts.getAfterScanScript()) && !scripts.getAfterScanScript().isEmpty()) {
+			summary.append("after: " + getScriptName(scripts.getAfterScanScript()));
+		}
+		summaryText.setText(summary.toString());
+	}
+
+	private String getScriptName(String path) {
+		return new File(path).getName();
 	}
 
 }
