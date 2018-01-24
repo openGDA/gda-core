1d13f550c325e051f6dee5cd9a4ff77b3a8585d8
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-01-24 12:48:15 +0000

    Use DummyMotor instead of TotalDummyMotor in example config (twojaw.py)
    TotalDummyMotor has been deleted
    
    Change-Id: I37026d674fe930b3371eaed3084c4d2f9ba216af

--- example-config/scripts/twojaw.py
+++ example-config/scripts/twojaw.py
@@ -9,10 +9,10 @@
 
 from gda.device.scannable import PseudoDevice
 from gda.device.scannable import ScannableMotor
-from gda.device.motor import TotalDummyMotor
+from gda.device.motor import DummyMotor
 
 class twojawslits(PseudoDevice):
-    
+
     def __init__(self,name,slit1,slit2):
         self.setName(name)                                         # required
         self.setInputNames(["gap","offset"])                       # required
@@ -20,35 +20,34 @@ class twojawslits(PseudoDevice):
         self.setOutputFormat(["%5.2g","%5.2g","%5.2g","%5.2g"])    # required
         self.slit1 = slit1
         self.slit2 = slit2
-        
+
     #
     # Required to fulfil the Scannable interface.
     # Returns an array of numbers defined by the inputNames and extraNames arrays.
-    #    
+    #
     def getPosition(self):
         return [self.calcGap(),self.calcOffset(),self.slit1(),self.slit2()]
-    
+
     #
     # Required to fulfil the Scannable interface.
     # Operates this scannable based on an array of numbers defined by the inputNames array
     #
     def asynchronousMoveTo(self,newPosition):
-        
         if newPosition.__len__() != 2:
             raise Exception("position array of wrong length!")
-        
+
         if newPosition[0] == None:
             newPosition[0] = self.calcGap()
-        
+
         if newPosition[1] == None:
             newPosition[1] = self.calcOffset()
-            
+
         motor1Target = newPosition[1] - (newPosition[0] / 2.)
         motor2Target = newPosition[1] + (newPosition[0] / 2.)
-        
+
         self.slit1.asynchronousMoveTo(motor1Target)
         self.slit2.asynchronousMoveTo(motor2Target)
-        
+
     #
     # Required to fulfil the Scannable interface.
     # Returns a boolean to say if this Scannable is in a state to have its asychronousMoveTo called.
@@ -62,23 +61,27 @@ class twojawslits(PseudoDevice):
     #
     def calcGap(self):
         return self.slit2() - self.slit1()
-    
+
     #
-    # internal to this example - calculates the slit centre relative to 0 
+    # internal to this example - calculates the slit centre relative to 0
     # (assuming the two motors are using the same coordinate system and motor2 is at a higher position than motor 1)
     #
     def calcOffset(self):
         return self.calcGap() / 2 + self.slit1()
-  
-s1motor1 = TotalDummyMotor()
+
+s1motor1 = DummyMotor()
 s1motor1.setName("s1motor1")
+s1motor1.setSpeed(10)
+s1motor1.configure()
 s1slit1 = ScannableMotor()
 s1slit1.setName("s1slit1")
 s1slit1.setMotor(s1motor1)
 s1slit1.configure()
 
-s1motor2 = TotalDummyMotor()
-s1motor2.setName("s1motor2")  
+s1motor2 = DummyMotor()
+s1motor2.setName("s1motor2")
+s1motor2.setSpeed(10)
+s1motor2.configure()
 s1slit2 = ScannableMotor()
 s1slit2.setName("s1slit2")
 s1slit2.setMotor(s1motor2)
@@ -98,4 +101,3 @@ s1 = twojawslits("s1",s1slit1,s1slit2)
 #
 # scan s1 [0,0] [1,0] [0.1,None]
 #
-        
