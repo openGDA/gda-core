d771539360672e842d58772aefa1668ccdc12ec1
Chris Sharpe <chris.sharpe@diamond.ac.uk>
2018-02-20 14:56:32 +0000

    i16-139: Simplify logic in asynchronousMoveTo for DeferredScannableGroup.
    *This in effect reverts to a previous commit, a7ec24b.
    
    *A hack was put in place to counter the problem with lost motor records in a deferred scan.This was caused by motor jitter on i16's diffractometer. The new control layer sends a PV directly to motors, and not through the motor record.
    
    * A new EPICS class EpicsMotorDirect is created for this purpose.
    
    Change-Id: Id0aeb0afaf103db90c6794d32b74517e898e3346

--- uk.ac.gda.core/src/gda/device/scannable/scannablegroup/DeferredScannableGroup.java
+++ uk.ac.gda.core/src/gda/device/scannable/scannablegroup/DeferredScannableGroup.java
@@ -23,9 +23,6 @@ import org.slf4j.LoggerFactory;
 
 import gda.device.ControlPoint;
 import gda.device.DeviceException;
-import gda.device.Scannable;
-import gda.device.scannable.PositionConvertorFunctions;
-import gda.device.scannable.ScannableMotor;
 import gda.factory.FactoryException;
 import gda.factory.Finder;
 
@@ -37,14 +34,8 @@ import gda.factory.Finder;
  */
 public class DeferredScannableGroup extends CoordinatedScannableGroup {
 	private static final Logger logger = LoggerFactory.getLogger(DeferredScannableGroup.class);
-
 	private ControlPoint deferredControlPoint;
-
 	String deferredControlPointName;
-
-	private ControlPoint numberToMoveControlPoint;
-
-	private ControlPoint checkStartControlPoint;
 
 	private int deferOnValue = 1;
 
@@ -70,93 +61,15 @@ public class DeferredScannableGroup extends CoordinatedScannableGroup {
 	 */
 	@Override
 	public void asynchronousMoveTo(Object position) throws DeviceException {
-		ControlPoint moveCp = getNumberToMoveControlPoint();
-		ControlPoint startCp = getCheckStartControlPoint();
-		boolean validate = false; // perform validation steps using numberToMove and checkStart control points
-		if (moveCp != null || startCp != null) {
-			if (startCp == null || moveCp == null) {
-				throw new DeviceException(
-						"Require neither or both numberToMoveControlPoint and checkStartControlPoint to be set");
-			}
-			validate = true;
+		setDefer(true);
+		try {
+			super.asynchronousMoveTo(position);
+		} catch (Exception e) {
+			logger.error("Exception while moving deferred scannable group, stopping all axes in group and setting defer flag off",e);
+			stop();
+			throw new DeviceException("Exception while triggering deferred scannable group move:\n " + e.getMessage(), e);
 		}
-		int attemptsRemaining = validate ? 60 : 1;
-
-		while (attemptsRemaining-- > 0) {
-			Double[] current = PositionConvertorFunctions.toDoubleArray(getPosition());
-			Double[] target = PositionConvertorFunctions.toDoubleArray(position);
-
-			for (int i = 0; i < target.length; i++) {
-				if (target[i] == null) {
-					continue;
-				}
-				Scannable scn = ((CoordinatedChildScannable) getGroupMembers().get(i)).delegate;
-				double tolerance = scn instanceof ScannableMotor ?
-						((ScannableMotor) scn).getDemandPositionTolerance() : 0;
-				if (Math.abs(current[i] - target[i]) < tolerance) {
-					target[i] = null;
-				}
-			}
-
-			if (isLogDefFlagChangesAsInfo()) {
-				logger.info("[[[" +getName() + ": defer ON");
-			}
-			setDefer(true);
-			if (validate) {
-				int toMove = testGroupMove(target);
-				logger.info("Telling defer move system that we will move " + toMove + " axes");
-				getNumberToMoveControlPoint().setValue(toMove);
-			}
-			// sometimes a move request can fail due to axis vibration
-			// this is *very rare*, but can waste a night of beamtime
-			// we handle move request failure and try again a limited number of times
-			int moveRequestsRemaining = 10;
-			while (moveRequestsRemaining-- > 0) {
-				try {
-					super.asynchronousMoveTo(target);
-					break;
-				} catch (Exception e) {
-					// A move request can fail if the PID loop gets stuck trying to correct an axis
-					// Commence horrible hack to handle axis vibration
-					if (moveRequestsRemaining > 0) {
-						logger.info("Move request failed, retrying. " + moveRequestsRemaining + " retries left");
-						stop();
-						try {
-							Thread.sleep(100);
-						} catch (InterruptedException e1) {
-							throw new DeviceException(e1);
-						}
-						continue;
-					}
-					logger.error("Move request failed after all retry attempts; stopping axes and setting defer flag off");
-					stop();
-					throw new DeviceException("Exception while triggering deferred scannable group move", e);
-				}
-			}
-			if (isLogDefFlagChangesAsInfo()) {
-				logger.info("]]]" + getName() + ": defer OFF");
-			}
-			setDefer(false);
-			if (validate) {
-				double value = getCheckStartControlPoint().getValue();
-				boolean succeeded = Math.abs(value) < 1;
-				if (succeeded) {
-					break;
-				}
-				// attempt failed
-				if (attemptsRemaining > 0) {
-					logger.warn("Deferred move system failed; trying again after 1 second, " +
-							attemptsRemaining + " attempts remaining");
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						throw new DeviceException(e);
-					}
-				} else {
-					throw new DeviceException("Deferred move system failed");
-				}
-			}
-		}
+		setDefer(false);
 	}
 
 	@Override
@@ -170,8 +83,10 @@ public class DeferredScannableGroup extends CoordinatedScannableGroup {
 	 */
 	public void setDefer(boolean deferFlag) throws DeviceException {
 		if (deferFlag == true) {
+			logger.info(getName() + ": defer ON");
 			deferredControlPoint.setValue(deferOnValue);
 		} else {
+			logger.info(getName() + ": defer OFF");
 			deferredControlPoint.setValue(0);
 		}
 	}
@@ -224,22 +139,6 @@ public class DeferredScannableGroup extends CoordinatedScannableGroup {
 	 */
 	public void setDeferredControlPointName(String deferredControlPointName) {
 		this.deferredControlPointName = deferredControlPointName;
-	}
-
-	public ControlPoint getNumberToMoveControlPoint() {
-		return numberToMoveControlPoint;
-	}
-
-	public void setNumberToMoveControlPoint(ControlPoint numberToMoveControlPoint) {
-		this.numberToMoveControlPoint = numberToMoveControlPoint;
-	}
-
-	public ControlPoint getCheckStartControlPoint() {
-		return checkStartControlPoint;
-	}
-
-	public void setCheckStartControlPoint(ControlPoint checkStartControlPoint) {
-		this.checkStartControlPoint = checkStartControlPoint;
 	}
 
 	public boolean isLogDefFlagChangesAsInfo() {
