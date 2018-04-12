openGDA/gda-core

commit ff4f33af40c77b3f1b3b3c901c35bda636a19280

B18-188 : Make element range for 'Element edge editor' depend on local properties
This update sets the atomic element range for the 'Element edge editor'
(i.e. 'element name' and 'edge' combo boxes in scan setup GUI) using 
'gda.exafs.element.min' and 'gda.exafs.element.max' local properties. These
properties are already used for the ExafsValidator to validate the GUI
parameters, and so this change makes the GUI consistent with the allowed
element range.

Change-Id: I848a4de1c17cada61c4d7c60b52c9d64751473f8

uk.ac.gda.client.exafs/src/gda/exafs/scan/ExafsValidator.java
@@ -45,6 +45,7 @@ import uk.ac.gda.beans.validation.AbstractValidator;
 import uk.ac.gda.beans.validation.InvalidBeanMessage;
 import uk.ac.gda.devices.detector.FluorescenceDetector;
 import uk.ac.gda.devices.detector.FluorescenceDetectorParameters;
+import uk.ac.gda.exafs.ui.ElementEdgeEditor;
 import uk.ac.gda.exafs.ui.data.ScanObject;
 import uk.ac.gda.util.beans.xml.XMLHelpers;
 
@@ -251,8 +252,8 @@ public abstract class ExafsValidator extends AbstractValidator {
 			errors.add(new InvalidBeanMessage(e.getMessage()));
 		}
 
-		String minElement = LocalProperties.get("gda.exafs.element.min", "P");
-		String maxElement = LocalProperties.get("gda.exafs.element.max", "U");
+		String minElement = LocalProperties.get(ElementEdgeEditor.EXAFS_MIN_ELEMENT_PROP, "P");
+		String maxElement = LocalProperties.get(ElementEdgeEditor.EXAFS_MIN_ELEMENT_PROP, "U");
 		if (!Arrays.asList(Element.getSortedEdgeSymbols(minElement, maxElement)).contains(x.getElement())) {
 			errors.add(new InvalidBeanMessage("The element '" + x.getElement()
 					+ "' is not currently allowed to be scanned."));

uk.ac.gda.client.exafs/src/uk/ac/gda/exafs/ui/ElementEdgeEditor.java
@@ -18,14 +18,6 @@
 
 package uk.ac.gda.exafs.ui;
 
-import gda.configuration.properties.LocalProperties;
-import gda.exafs.scan.ExafsScanPointCreator;
-import gda.exafs.scan.ExafsScanPointCreatorException;
-import gda.exafs.scan.ExafsTimeEstimator;
-import gda.exafs.scan.XanesScanPointCreator;
-import gda.jython.JythonServerFacade;
-import gda.util.exafs.Element;
-
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.text.DateFormat;
@@ -77,6 +69,13 @@ import org.python.core.PyObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import gda.configuration.properties.LocalProperties;
+import gda.exafs.scan.ExafsScanPointCreator;
+import gda.exafs.scan.ExafsScanPointCreatorException;
+import gda.exafs.scan.ExafsTimeEstimator;
+import gda.exafs.scan.XanesScanPointCreator;
+import gda.jython.JythonServerFacade;
+import gda.util.exafs.Element;
 import uk.ac.diamond.scisoft.analysis.axis.AxisValues;
 import uk.ac.gda.beans.exafs.XanesScanParameters;
 import uk.ac.gda.beans.exafs.XasScanParameters;
@@ -114,6 +113,11 @@ public abstract class ElementEdgeEditor extends RichBeanEditorPart {
 	protected boolean updateValueAllowed = true;
 	private Color red, black;
 	protected PlotUpdateJob plotUpdateJob;
+
+	public static final String EXAFS_MIN_ELEMENT_PROP = "gda.exafs.element.min";
+	public static final String EXAFS_MAX_ELEMENT_PROP = "gda.exafs.element.max";
+	private String minAllowedElementSymbol = LocalProperties.get(EXAFS_MIN_ELEMENT_PROP,"P");
+	private String maxAllowedElementSymbol = LocalProperties.get(EXAFS_MAX_ELEMENT_PROP,"Np");
 
 	public ElementEdgeEditor(String path, URL mappingURL, DirtyContainer dirtyContainer, Object editingBean) {
 		super(path, mappingURL, dirtyContainer, editingBean);
@@ -322,7 +326,7 @@ public abstract class ElementEdgeEditor extends RichBeanEditorPart {
 	 * @throws Exception
 	 */
 	protected SchemaReader setupElementAndEdge(final String objectName) throws Exception {
-		element.setItems(Element.getSortedEdgeSymbols("P", "Np"));
+		element.setItems(Element.getSortedEdgeSymbols(minAllowedElementSymbol, maxAllowedElementSymbol));
 
 		SchemaReader reader = new SchemaReader(XasScanParameters.schemaUrl);
 		List<String> choices = reader.getAllowedChoices(objectName, "edge");
@@ -644,5 +648,13 @@ public abstract class ElementEdgeEditor extends RichBeanEditorPart {
 
 	public ComboWrapper getElement() {
 		return element;
+	}
+
+	public String getMinAllowedElementSymbol() {
+		return minAllowedElementSymbol;
+	}
+
+	public String getMaxAllowedElemenetSymbol() {
+		return maxAllowedElementSymbol;
 	}
 }

commit adafc96ac4cb96b05227f1f821d92a2eb449900e

I20 : Bugfix for incorrect element range in Xanes validator
This fixes a bug introduced by change ff4f33af where the min and max elements
allowed for Xanes scans is set to the same element if the local property 
EXAFS_MIN_ELEMENT_PROP has been defined.

Change-Id: Ibbbd762d1ff29c3041d90e3626d30eee0996beed

uk.ac.gda.client.exafs/src/gda/exafs/scan/ExafsValidator.java
@@ -253,7 +253,7 @@ public abstract class ExafsValidator extends AbstractValidator {
 		}
 
 		String minElement = LocalProperties.get(ElementEdgeEditor.EXAFS_MIN_ELEMENT_PROP, "P");
-		String maxElement = LocalProperties.get(ElementEdgeEditor.EXAFS_MIN_ELEMENT_PROP, "U");
+		String maxElement = LocalProperties.get(ElementEdgeEditor.EXAFS_MAX_ELEMENT_PROP, "U");
 		if (!Arrays.asList(Element.getSortedEdgeSymbols(minElement, maxElement)).contains(x.getElement())) {
 			errors.add(new InvalidBeanMessage("The element '" + x.getElement()
 					+ "' is not currently allowed to be scanned."));