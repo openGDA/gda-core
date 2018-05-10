8331cca074932581fa227df2374535a919da6371
Olly King <olly.king@diamond.ac.uk>
2018-05-10 10:48:19 +0100

    DAQ-1313 Deletes p45-config, p45 plugin, p45.site and p45.feature from gda-core
    - As part of move of p45-config from gda-core to gda-diamond
    
    
    Change-Id: I2273121b6bb4502e1d7d1b9d17ca881c322e76d1

--- p45-config/scripts/localStation.py
+++
@@ -1,9 +0,0 @@
-# localStation.py
-# For beamline specific initialisation code.
-#
-from mapping_scan_commands import (
-    mscan, detector, scan_request, submit, step, grid, circ, poly, rect)
-
-print "Installing standard scans with processing"
-from gdascripts.scan.installStandardScansWithProcessing import * #@UnusedWildImport
-scan_processor.rootNamespaceDict = globals()
