b5fa2c2c81f5ccd4387c4f41fd613427de20fc7d
Matthew Webber <matthew.webber@diamond.ac.uk>
2018-06-04 13:28:13 +0100

    Set version number for next release (9.9)
    Change-Id: I34bf515a6dc12b48d3ae551dc8b79ee84ec065ec

--- uk.ac.gda.api/src/gda/util/Version.java
+++ uk.ac.gda.api/src/gda/util/Version.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Version {
 
-	private static final String RELEASE_VER = "9.8.0";
+	private static final String RELEASE_VER = "9.9.0pre";
 
 	/**
 	 * Returns the release version as a String
