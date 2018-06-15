72efe314c4bec553c366e4dd2b69359d54e2b063
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-06-15 11:38:34 +0100

    [DAQ-1463] Improve unit tests for EpicsMCASimple
    Test the return value of functions rather than just calling them.
    
    Change-Id: I951996ececac4bc873268d7f945e5b4d41fd3420

--- uk.ac.gda.epics.test/src/gda/device/detector/analyser/EpicsMCASimpleTest.java
+++ uk.ac.gda.epics.test/src/gda/device/detector/analyser/EpicsMCASimpleTest.java
@@ -20,100 +20,159 @@ package gda.device.detector.analyser;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import gda.device.DeviceException;
-import gda.device.epicsdevice.EpicsDevice;
-import gda.device.epicsdevice.FindableEpicsDevice;
-import gda.factory.FactoryException;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.HashMap;
-import java.util.Vector;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- *
- */
+import gda.device.epicsdevice.EpicsDevice;
+import gda.device.epicsdevice.FindableEpicsDevice;
+
 public class EpicsMCASimpleTest {
 
-	/**
-	 * Test method for {@link gda.device.detector.analyser.EpicsMCASimple#configure()}.
-	 * @throws FactoryException
-	 * @throws DeviceException
-	 */
-	@Test
-	public void testConfigure() throws FactoryException, DeviceException {
-		HashMap<String, String> recordPVs = new HashMap<String, String>();
+	private static final HashMap<String, String> recordPVs = new HashMap<String, String>();
+
+	private EpicsMCASimple mcaSimpleDevice;
+
+	@BeforeClass
+	public static void setUpClass() {
 		recordPVs.put("RECORD", "BL0XI-DI-DET-01:aim_adc1");
-		EpicsDevice mca = new EpicsDevice("mca_ed", recordPVs, true);
-		mca.configure();
-		FindableEpicsDevice epicsDevice = new FindableEpicsDevice("test", mca);
-		EpicsMCASimple dev = new EpicsMCASimple();
-		dev.setEpicsDevice(epicsDevice);
-		dev.configure();
-		@SuppressWarnings("unused")
-		Object obj = dev.getCalibrationParameters();
-		obj = dev.getData();
-		obj = dev.getElapsedParameters();
-		obj = dev.getRegionsOfInterest();
-		obj = dev.getRegionsOfInterestCount();
-		dev.dispose();
-		obj = dev.getRegionsOfInterest();
-
-		double dwellTime1 = dev.getDwellTime();
-		dev.setDwellTime(1.0);
-		// double dwellTime2 = dev.getDwellTime();
-		// The dwell time appears to be changed automatically to 0
-		// assertEquals(dwellTime2, 1.0);
-		dev.setDwellTime(dwellTime1);
-
-		long channels1 = dev.getNumberOfChannels();
-		try {
-			dev.setNumberOfChannels(channels1 / 2);
-			long channels2 = dev.getNumberOfChannels();
-			assertEquals(channels1 / 2, channels2);
-		} finally {
-			dev.setNumberOfChannels(channels1);
-		}
-
-		EpicsMCAPresets presets1 = (EpicsMCAPresets) dev.getPresets();
-		try {
-			EpicsMCAPresets presets2 = new EpicsMCAPresets((float) 1.0, (float) 2.0, 1, 2, 3, 4);
-			dev.setPresets(presets2);
-			EpicsMCAPresets presets3 = (EpicsMCAPresets) dev.getPresets();
-			assertEquals(presets2, presets3);
-		} finally {
-			dev.setPresets(presets1);
-		}
-
-		EpicsMCARegionOfInterest[] regionsOfInterest1 = (EpicsMCARegionOfInterest[]) dev.getRegionsOfInterest();
-		try {
-			Vector<EpicsMCARegionOfInterest> _regionsOfInterest2 = new Vector<EpicsMCARegionOfInterest>();
-			for (Integer i = 0; i < dev.getNumberOfRegions(); i++) {
-				_regionsOfInterest2.add(new EpicsMCARegionOfInterest(i, i, i * 2, i / 2, i, i.toString()));
-			}
-			EpicsMCARegionOfInterest[] regionsOfInterest2 = _regionsOfInterest2
-					.toArray(new EpicsMCARegionOfInterest[0]);
-			dev.setRegionsOfInterest(regionsOfInterest2);
-			EpicsMCARegionOfInterest[] regionsOfInterest3 = (EpicsMCARegionOfInterest[]) dev.getRegionsOfInterest();
-			assertArrayEquals(regionsOfInterest2, regionsOfInterest3);
-		} finally {
-			dev.setRegionsOfInterest(regionsOfInterest1);
-		}
-
-		int[] data = (int[]) dev.getData();
-		try {
-			int len = data.length;
-			int[] data1 = new int[len];
-			for (int i = 0; i < len; i++) {
-				data1[i] = i;
-			}
-			dev.setData(data1);
-			int[] data2 = (int[]) dev.getData();
-			assertArrayEquals(data1, data2);
-		} finally {
-			dev.setData(data);
-		}
-
 	}
 
+	@Before
+	public void setUp() throws Exception {
+		final EpicsDevice mcaDevice = new EpicsDevice("mca_ed", recordPVs, true);
+		mcaDevice.configure();
+		final FindableEpicsDevice epicsDevice = new FindableEpicsDevice("test", mcaDevice);
+
+		mcaSimpleDevice = new EpicsMCASimple();
+		mcaSimpleDevice.setName("mcaSimpleDevice");
+		mcaSimpleDevice.setEpicsDevice(epicsDevice);
+		mcaSimpleDevice.configure();
+	}
+
+	@Test
+	public void testGetCalibrationParameters() throws Exception {
+		final EpicsMCACalibration calibrationParams = (EpicsMCACalibration) mcaSimpleDevice.getCalibrationParameters();
+		assertEquals("EGU", calibrationParams.getEngineeringUnits());
+		assertEquals(1.0, calibrationParams.getCalibrationOffset(), 0.0001);
+		assertEquals(0.0, calibrationParams.getCalibrationQuadratic(), 0.0001);
+		assertEquals(1.0, calibrationParams.getCalibrationSlope(), 0.0001);
+		assertEquals(0.0, calibrationParams.getTwoThetaAngle(), 0.0001);
+	}
+
+	@Test
+	public void testGetData() throws Exception {
+		final int[] data = (int[]) mcaSimpleDevice.getData();
+		assertEquals(2048, data.length);
+		for (int i = 0; i < data.length; i++) {
+			assertEquals(i, data[i]);
+		}
+	}
+
+	@Test
+	public void testSetData() throws Exception {
+		final int dataLen = ((int[]) mcaSimpleDevice.getData()).length;
+		final int[] newData = new int[dataLen];
+		for (int i = 0; i < dataLen; i++) {
+			newData[i] = i;
+		}
+		mcaSimpleDevice.setData(newData);
+		assertArrayEquals(newData, (int[]) mcaSimpleDevice.getData());
+	}
+
+	@Test
+	public void testGetElapsedParameters() throws Exception {
+		final float[] elapsedParams = (float[]) mcaSimpleDevice.getElapsedParameters();
+		assertEquals(2, elapsedParams.length);
+		assertEquals(1.1, elapsedParams[0], 0.001);
+		assertEquals(1.0, elapsedParams[1], 0.001);
+	}
+
+	@Test
+	public void testGetRegionsOfInterest() throws Exception {
+		final EpicsMCARegionOfInterest[] mcaRois = (EpicsMCARegionOfInterest[]) mcaSimpleDevice.getRegionsOfInterest();
+		assertEquals(32, mcaRois.length);
+		for (int i = 0; i < mcaRois.length; i++) {
+			final EpicsMCARegionOfInterest mcaRoi = mcaRois[i];
+			assertEquals(i, mcaRoi.getRegionIndex());
+			assertEquals(-1d, mcaRoi.getRegionLow(), 0.001);
+			assertEquals(-1d, mcaRoi.getRegionHigh(), 0.001);
+			assertEquals(0, mcaRoi.getRegionBackground());
+			assertEquals(1d, mcaRoi.getRegionPreset(), 0.001);
+			assertEquals(Integer.toString(i), mcaRoi.getRegionName());
+		}
+	}
+
+	@Test
+	public void testSetRegionsOfInterest() throws Exception {
+		final EpicsMCARegionOfInterest[] newRegionsOfInterest = new EpicsMCARegionOfInterest[mcaSimpleDevice.getNumberOfRegions()];
+		for (Integer i = 0; i < mcaSimpleDevice.getNumberOfRegions(); i++) {
+			newRegionsOfInterest[i] = new EpicsMCARegionOfInterest(i, i, i * 2, i / 2, i, i.toString());
+		}
+		mcaSimpleDevice.setRegionsOfInterest(newRegionsOfInterest);
+		assertArrayEquals(newRegionsOfInterest, (EpicsMCARegionOfInterest[]) mcaSimpleDevice.getRegionsOfInterest());
+	}
+
+	@Test
+	public void testGetRegionsOfInterestCount() throws Exception {
+		final double[][] roiCount = mcaSimpleDevice.getRegionsOfInterestCount();
+		assertEquals(32, roiCount.length);
+		for (int i = 0; i < roiCount.length; i++) {
+			final double[] valuesForRegion = roiCount[i];
+			assertEquals(2, valuesForRegion.length);
+			assertEquals(i * 1000, valuesForRegion[0], 0.001);
+			assertEquals(i * 1000, valuesForRegion[1], 0.001);
+		}
+	}
+
+	@Test
+	public void testDispose() throws Exception {
+		mcaSimpleDevice.dispose();
+		// Dispose does nothing in dummy mode, so functions should still return values
+		assertNotNull(mcaSimpleDevice.getRegionsOfInterest());
+	}
+
+	@Test
+	public void testGetDwellTme() throws Exception {
+		assertEquals(1.0, mcaSimpleDevice.getDwellTime(), 0.001);
+	}
+
+	@Test
+	public void testSetDwellTime() throws Exception {
+		mcaSimpleDevice.setDwellTime(1.5);
+		assertEquals(1.5, mcaSimpleDevice.getDwellTime(), 0.001);
+	}
+
+	@Test
+	public void testGetNumberOfChannels() throws Exception {
+		assertEquals(2048, mcaSimpleDevice.getNumberOfChannels());
+	}
+
+	@Test
+	public void testSetNumberOfChannels() throws Exception {
+		mcaSimpleDevice.setNumberOfChannels(100);
+		assertEquals(100, mcaSimpleDevice.getNumberOfChannels());
+	}
+
+	@Test
+	public void testGetPresets() throws Exception {
+		final EpicsMCAPresets presets = (EpicsMCAPresets) mcaSimpleDevice.getPresets();
+		assertEquals(1.0, presets.getPresetRealTime(), 0.001);
+		assertEquals(1.0, presets.getPresetLiveTime(), 0.001);
+		assertEquals(1, presets.getPresetCounts());
+		assertEquals(1, presets.getPresetCountlow());
+		assertEquals(1, presets.getPresetCountHigh());
+		assertEquals(1, presets.getPresetSweeps());
+	}
+
+	@Test
+	public void testSetPresets() throws Exception {
+		final EpicsMCAPresets newPresets = new EpicsMCAPresets((float) 1.5, (float) 2.0, 1, 2, 3, 4);
+		mcaSimpleDevice.setPresets(newPresets);
+		assertEquals(newPresets, mcaSimpleDevice.getPresets());
+	}
 }
