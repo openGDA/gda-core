2b807ea2f514ed8ba036a69e223d83d1f3f7f283
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-07-03 12:13:12 +0100

    [DAQ-1502] Improve CoupledScannableTest
    - rationalise dummy/mock objects
    - add more tests
    
    Change-Id: I328d6b28e43f4704c31a3f51681e80e068779c55

--- uk.ac.gda.core.test/src/gda/device/scannable/CoupledScannableTest.java
+++ uk.ac.gda.core.test/src/gda/device/scannable/CoupledScannableTest.java
@@ -19,64 +19,86 @@
 
 package gda.device.scannable;
 
+import static org.hamcrest.Matchers.any;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.jscience.physics.quantities.Quantity;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Matchers;
 
+import gda.TestHelpers;
 import gda.device.DeviceException;
 import gda.device.Scannable;
 import gda.device.ScannableMotionUnits;
+import gda.factory.Factory;
 import gda.factory.FactoryException;
+import gda.factory.Finder;
 import gda.function.Function;
+import gda.observable.IObserver;
 import gda.util.QuantityFactory;
 
 public class CoupledScannableTest {
+
+	private ScannableMotionUnits dummyScannable1;
+	private ScannableMotionUnits dummyScannable2;
+
+	private Function mockFunction1;
+	private Function mockFunction2;
+
+	@Before
+	public void setUp() throws Exception {
+		dummyScannable1 = new DummyUnitsScannable("s1", 0, "mm", "mm");
+		dummyScannable2 = new DummyUnitsScannable("s2", 0, "nm", "nm");
+
+		mockFunction1 = mock(Function.class);
+		when(mockFunction1.evaluate(Matchers.any(Quantity.class))).thenReturn(QuantityFactory.createFromString("12.3 cm"));
+
+		mockFunction2 = mock(Function.class);
+		when(mockFunction2.evaluate(Matchers.any(Quantity.class))).thenReturn(QuantityFactory.createFromString("78.9 nm"));
+	}
+
 	/*
 	 * test that position returned takes into account changes in the user units of the first scannable
 	 */
 	@Test
 	public void testUseFirstScannableUnits() throws DeviceException, FactoryException {
-		ScannableMotionUnits bsx = new DummyUnitsScannable("bsx", 0, "mm", "mm");
-
-		CoupledScannable scannable = new CoupledScannable();
-		scannable.setScannables(new Scannable[]{bsx});
+		final CoupledScannable scannable = new CoupledScannable();
+		scannable.setScannables(new Scannable[] { dummyScannable1 , dummyScannable2});
 		scannable.setName("test");
 		scannable.configure();
 
-		bsx.moveTo(1.0);
-		Object posWithUserUnits_mm = scannable.getPosition();
-		assertEquals("Should have same value as position of bsx as user unit set to that of bsx", 1.0, posWithUserUnits_mm);
+		dummyScannable1.moveTo(1.0);
+		final Object posWithUserUnits_mm = scannable.getPosition();
+		assertEquals("Should have same value as position of scannable1 as user unit set to that of scannable1", 1.0, posWithUserUnits_mm);
 
-		bsx.setUserUnits("m");
+		dummyScannable1.setUserUnits("m");
 
-		Object posWithUserUnits_m = scannable.getPosition();
+		final Object posWithUserUnits_m = scannable.getPosition();
 
 		assertEquals(posWithUserUnits_mm, posWithUserUnits_m);
 	}
 
 	@Test
 	public void testUseFirstScannableUnitsButWithInitialUnitsSet() throws DeviceException, FactoryException {
-		ScannableMotionUnits bsx = new DummyUnitsScannable("bsx", 0, "mm", "mm");
-
-		CoupledScannable scannable = new CoupledScannable();
-		scannable.setScannables(new Scannable[]{bsx});
+		final CoupledScannable scannable = new CoupledScannable();
+		scannable.setScannables(new Scannable[] { dummyScannable1, dummyScannable2 });
 		scannable.setName("test");
 		scannable.setInitialUserUnits("micron");
 		scannable.configure();
 
-		bsx.moveTo(1.0);
-		Object posWithUserUnits_mm = scannable.getPosition();
-		assertEquals("Should have value of 1000 user unit set to micro whilst bsx is mm", 1000.0, posWithUserUnits_mm);
+		dummyScannable1.moveTo(1.0);
+		final Object posWithUserUnits_mm = scannable.getPosition();
+		assertEquals("Should have value of 1000 user unit set to micro whilst scannable1 is mm", 1000.0, posWithUserUnits_mm);
 
-		bsx.setUserUnits("m");
+		dummyScannable1.setUserUnits("m");
 
-		Object posWithUserUnits_m = scannable.getPosition();
+		final Object posWithUserUnits_m = scannable.getPosition();
 
 		assertEquals(posWithUserUnits_mm, posWithUserUnits_m);
 	}
@@ -88,16 +110,11 @@ public class CoupledScannableTest {
 	 */
 	@Test
 	public void testPassCorrectUnits() throws DeviceException, FactoryException {
-		final ScannableMotionUnits scannable = new DummyUnitsScannable("s1", 0, "mm", "mm");
-
-		final Function function = mock(Function.class);
-		when(function.evaluate(Matchers.any(Quantity.class))).thenReturn(QuantityFactory.createFromString("12.3 cm"));
-
 		final CoupledScannable coupled = new CoupledScannable();
 		coupled.setName("testCoupledScannable");
 		coupled.setUserUnits("eV");
-		coupled.setScannables(new Scannable[] { scannable });
-		coupled.setFunctions(new Function[] { function });
+		coupled.setScannables(new Scannable[] { dummyScannable1 });
+		coupled.setFunctions(new Function[] { mockFunction1 });
 		coupled.configure();
 
 		coupled.moveTo(798.34);
@@ -106,12 +123,117 @@ public class CoupledScannableTest {
 
 		// Verify that the input units of the coupled scannable (i.e. eV) are passed to the evaluation function for the
 		// component scannable
-		verify(function).evaluate(quantityCaptor.capture());
+		verify(mockFunction1).evaluate(quantityCaptor.capture());
 		assertEquals(798.34, quantityCaptor.getValue().getAmount(), 0.0001);
 		assertEquals("eV", quantityCaptor.getValue().getUnit().toString());
 
 		// Verify that the units for the component move have been handled correctly
-		assertEquals("mm", scannable.getHardwareUnitString());
-		assertEquals(123.0, (Double) scannable.getPosition(), 0.0001);
+		assertEquals("mm", dummyScannable1.getHardwareUnitString());
+		assertEquals(123.0, (Double) dummyScannable1.getPosition(), 0.0001);
+	}
+
+	@Test
+	public void testSetScannablesDirectly() throws Exception {
+		final CoupledScannable coupled = new CoupledScannable();
+		coupled.setName("testCoupledScannable");
+		coupled.setUserUnits("mm");
+		coupled.setScannables(new Scannable[] { dummyScannable1, dummyScannable2 });
+		coupled.setFunctions(new Function[] { mockFunction1, mockFunction2 });
+		coupled.configure();
+
+		coupled.moveTo(15.7);
+
+		assertEquals("mm", dummyScannable1.getHardwareUnitString());
+		assertEquals(123.0, (Double) dummyScannable1.getPosition(), 0.0001);
+
+		assertEquals("nm", dummyScannable2.getHardwareUnitString());
+		assertEquals(78.9, (Double) dummyScannable2.getPosition(), 0.0001);
+	}
+
+	@Test
+	public void testSetScannablesFromFinder() throws Exception {
+		final Factory testFactory = TestHelpers.createTestFactory("testFactory");
+		testFactory.addFindable(dummyScannable1);
+		testFactory.addFindable(dummyScannable2);
+
+		Finder.getInstance().addFactory(testFactory);
+
+		final CoupledScannable coupled = new CoupledScannable();
+		coupled.setName("testCoupledScannable");
+		coupled.setUserUnits("mm");
+		coupled.setScannableNames(new String[] { "s1", "s2" });
+		coupled.setFunctions(new Function[] { mockFunction1, mockFunction2 });
+		coupled.configure();
+
+		coupled.moveTo(15.7);
+
+		assertEquals("mm", dummyScannable1.getHardwareUnitString());
+		assertEquals(123.0, (Double) dummyScannable1.getPosition(), 0.0001);
+
+		assertEquals("nm", dummyScannable2.getHardwareUnitString());
+		assertEquals(78.9, (Double) dummyScannable2.getPosition(), 0.0001);
+	}
+
+	@Test(expected = FactoryException.class)
+	public void testUnequalNumbersOfScannablesAndFunctions() throws Exception {
+		final CoupledScannable coupled = new CoupledScannable();
+		coupled.setName("testCoupledScannable");
+		coupled.setUserUnits("mm");
+		coupled.setScannables(new Scannable[] { dummyScannable1, dummyScannable2 });
+		coupled.setFunctions(new Function[] { mockFunction1 });
+		coupled.configure();
+	}
+
+	@Test
+	public void testNoFunctions() throws Exception {
+		final CoupledScannable coupled = new CoupledScannable();
+		coupled.setName("testCoupledScannable");
+		coupled.setUserUnits("mm");
+		coupled.setScannables(new Scannable[] { dummyScannable1, dummyScannable2 });
+		coupled.configure();
+
+		coupled.moveTo(15.7);
+
+		// If there are no functions, all scannables are moved to the same position
+		assertEquals("mm", dummyScannable1.getHardwareUnitString());
+		assertEquals(15.7, (Double) dummyScannable1.getPosition(), 0.0001);
+
+		assertEquals("nm", dummyScannable2.getHardwareUnitString());
+		assertEquals(15.7, (Double) dummyScannable2.getPosition(), 0.0001);
+	}
+
+	@Test(expected = DeviceException.class)
+	public void testThrowsExceptionIfMoveFails() throws Exception {
+		dummyScannable1.setUpperGdaLimits(120.0); // so move to 123.0 will fail
+
+		final CoupledScannable coupled = new CoupledScannable();
+		coupled.setName("testCoupledScannable");
+		coupled.setUserUnits("mm");
+		coupled.setScannables(new Scannable[] { dummyScannable1, dummyScannable2 });
+		coupled.setFunctions(new Function[] { mockFunction1, mockFunction2 });
+		coupled.configure();
+
+		coupled.moveTo(15.7);
+	}
+
+	@Test
+	public void testNotifiesObservers() throws Exception {
+		final IObserver observer = mock(IObserver.class);
+
+		final CoupledScannable coupled = new CoupledScannable();
+		coupled.setName("testCoupledScannable");
+		coupled.setUserUnits("mm");
+		coupled.setScannables(new Scannable[] { dummyScannable1, dummyScannable2 });
+		coupled.addIObserver(observer);
+		coupled.configure();
+
+		coupled.moveTo(15.7);
+
+		// Notifies observers only when all scannables are idle
+		((DummyUnitsScannable) dummyScannable1).notifyIObservers(dummyScannable1, ScannableStatus.IDLE);
+		verify(observer, never()).update(any(Object.class), any(Object.class));
+
+		((DummyUnitsScannable) dummyScannable2).notifyIObservers(dummyScannable2, ScannableStatus.IDLE);
+		verify(observer).update(coupled, ScannableStatus.IDLE);
 	}
 }
