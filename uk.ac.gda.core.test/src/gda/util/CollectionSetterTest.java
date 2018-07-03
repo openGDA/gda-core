a54d6a8ee472469be86efe7527d1bd5ad63617d9
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-07-03 15:16:18 +0100

    [DAQ-1502] CollectionSetterTest: use standard functions to create lists
    Change-Id: I89a77789ac29712131a8327b5c35963cebb970c3

--- uk.ac.gda.core.test/src/gda/util/CollectionSetterTest.java
+++ uk.ac.gda.core.test/src/gda/util/CollectionSetterTest.java
@@ -19,11 +19,10 @@
 
 package gda.util;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -40,7 +39,6 @@ import gda.function.IdentityFunction;
 import gda.util.converters.util.RangeConverterNameProvider;
 import gda.util.converters.util.RangeandConverterNameHolder;
 
-
 /**
  * Tests that the newly-introduced setters for collection fields that are
  */
@@ -51,7 +49,7 @@ public class CollectionSetterTest {
 		GdaMetadata gm = new GdaMetadata();
 		assertTrue(gm.getMetadataEntries().isEmpty());
 		IMetadataEntry entry = new StoredMetadataEntry("name", "value");
-		gm.setMetadataEntries(arrayListOf(entry));
+		gm.setMetadataEntries(asList(entry));
 		assertEquals(1, gm.getMetadataEntries().size());
 	}
 
@@ -60,7 +58,7 @@ public class CollectionSetterTest {
 		NexusMetadataReader nmr = new NexusMetadataReader();
 		assertTrue(nmr.getNexusMetadataEntries().isEmpty());
 		NexusMetadataEntry entry = new NexusMetadataEntry();
-		nmr.setNexusMetadataEntries(arrayListOf(entry));
+		nmr.setNexusMetadataEntries(asList(entry));
 		assertEquals(1, nmr.getNexusMetadataEntries().size());
 	}
 
@@ -68,7 +66,7 @@ public class CollectionSetterTest {
 	public void testScannableGroupMemberNames() {
 		ScannableGroup sg = new ScannableGroup();
 		assertEquals(0, sg.getGroupMemberNames().length);
-		sg.setGroupMemberNames(arrayListOf("one", "two"));
+		sg.setGroupMemberNames(asList("one", "two"));
 		assertEquals(2, sg.getGroupMemberNames().length);
 	}
 
@@ -76,7 +74,7 @@ public class CollectionSetterTest {
 	public void testCoupledScannableScannableNames() {
 		CoupledScannable cs = new CoupledScannable();
 		assertNull(cs.getScannableNames());
-		cs.setScannableNames(arrayListOf("one", "two", "three"));
+		cs.setScannableNames(asList("one", "two", "three"));
 		assertEquals(3, cs.getScannableNames().size());
 	}
 
@@ -85,7 +83,7 @@ public class CollectionSetterTest {
 		CoupledScannable cs = new CoupledScannable();
 		assertEquals(0, cs.getFunctions().size());
 		Function function = new IdentityFunction();
-		cs.setFunctions(arrayListOf(function));
+		cs.setFunctions(asList(function));
 		assertEquals(1, cs.getFunctions().size());
 	}
 
@@ -93,7 +91,7 @@ public class CollectionSetterTest {
 	public void testDummyEnumPositioner() {
 		DummyEnumPositioner dep = new DummyEnumPositioner();
 		assertEquals(0, dep.getPositionsList().size());
-		dep.setPositions(arrayOf("1", "2", "3"));
+		dep.setPositions(asList("1", "2", "3"));
 		assertEquals(3, dep.getPositionsList().size());
 	}
 
@@ -103,20 +101,8 @@ public class CollectionSetterTest {
 		assertEquals(0, rcnp.getConverterList().size());
 		RangeandConverterNameHolder conv1 = new RangeandConverterNameHolder("one", "one", 1.0, 2.0);
 		RangeandConverterNameHolder conv2 = new RangeandConverterNameHolder("two", "two", 3.0, 4.0);
-		rcnp.setConverters(arrayListOf(conv1, conv2));
+		rcnp.setConverters(asList(conv1, conv2));
 		assertEquals(2, rcnp.getConverterList().size());
-	}
-
-	private <T> T[] arrayOf(@SuppressWarnings("unchecked") T... items) {
-		return items;
-	}
-
-	private <T> ArrayList<T> arrayListOf(@SuppressWarnings("unchecked") T... items) {
-		ArrayList<T> list = new ArrayList<T>();
-		for (T i : items) {
-			list.add(i);
-		}
-		return list;
 	}
 
 }
