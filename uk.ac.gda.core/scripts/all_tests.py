ad3c028842a39565e8b28cd670802835e338dcc7
Richard Fearn <richard.fearn@diamond.ac.uk>
2018-07-13 13:41:49 +0100

    MXGDA-3111 uk.ac.gda.core: wrap JyUnit test suite in project-specific TestSuite subclass
    Otherwise Jenkins can't properly display the standard output of the tests.
    
    Change-Id: Ife726b488c11376150632628c2bbe29107b5c0b1

--- uk.ac.gda.core/scripts/all_tests.py
+++ uk.ac.gda.core/scripts/all_tests.py
@@ -7,7 +7,16 @@ def suite():
 	
 	suite = unittest.TestSuite()
 	suite.addTest(testjy.all_tests.suite())
+	suite = UkAcGdaCoreTestSuite.wrap(suite)
 	return suite
+
+class UkAcGdaCoreTestSuite(unittest.TestSuite):
+	
+	@staticmethod
+	def wrap(suite):
+		new_suite = UkAcGdaCoreTestSuite()
+		new_suite.addTest(suite)
+		return new_suite
 
 if __name__ == '__main__':
 	print sys.path
