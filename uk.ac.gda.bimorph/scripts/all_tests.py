25c3f7704dee0ad571683ef41b5ce72a36a06a86
Richard Fearn <richard.fearn@diamond.ac.uk>
2018-07-13 13:42:22 +0100

    MXGDA-3111 uk.ac.gda.bimorph: wrap JyUnit test suite in project-specific TestSuite subclass
    Otherwise Jenkins can't properly display the standard output of the tests.
    
    Change-Id: I9160a97966b21f9b88889133cb7ac2f67b6d3dae

--- uk.ac.gda.bimorph/scripts/all_tests.py
+++ uk.ac.gda.bimorph/scripts/all_tests.py
@@ -7,7 +7,16 @@ def suite():
 	
 	suite = unittest.TestSuite()
 	suite.addTest(test.all_tests.suite())
+	suite = UkAcGdaBimorphTestSuite.wrap(suite)
 	return suite
+
+class UkAcGdaBimorphTestSuite(unittest.TestSuite):
+	
+	@staticmethod
+	def wrap(suite):
+		new_suite = UkAcGdaBimorphTestSuite()
+		new_suite.addTest(suite)
+		return new_suite
 
 if __name__ == '__main__':
 	print sys.path
