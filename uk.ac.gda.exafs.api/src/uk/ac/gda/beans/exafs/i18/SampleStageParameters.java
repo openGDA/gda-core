bc5fed00d8050a060ecaabf52cbf3d5a01475e4e
Douglas Winter <douglas.winter@diamond.ac.uk>
2018-07-17 10:31:44 +0100

    Remove 'disable' flag from SampleStageParameters
    Didn't really work anyway
    
    Change-Id: If0a0237ccfd0cd5fa76f5067b07ae7e52ec185b1

--- uk.ac.gda.exafs.api/src/uk/ac/gda/beans/exafs/i18/SampleStageParameters.java
+++ uk.ac.gda.exafs.api/src/uk/ac/gda/beans/exafs/i18/SampleStageParameters.java
@@ -24,54 +24,7 @@ public class SampleStageParameters implements Serializable {
 	private Double x;
 	private Double y;
 	private Double z;
-	private Boolean disable = false;
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		SampleStageParameters other = (SampleStageParameters) obj;
-		if (x == null) {
-			if (other.x != null) {
-				return false;
-			}
-		} else if (!x.equals(other.x)) {
-			return false;
-		}
-		if (y == null) {
-			if (other.y != null) {
-				return false;
-			}
-		} else if (!y.equals(other.y)) {
-			return false;
-		}
-		if (z == null) {
-			if (other.z != null) {
-				return false;
-			}
-		} else if (!z.equals(other.z)) {
-			return false;
-		}
-		return true;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-		+ ((x == null) ? 0 : x.hashCode());
-		result = prime * result
-		+ ((y == null) ? 0 : y.hashCode());
-		result = prime * result
-		+ ((z == null) ? 0 : z.hashCode());
-		return result;
-	}
+
 	public void setX(Double x) {
 		this.x = x;
 	}
@@ -90,11 +43,40 @@ public class SampleStageParameters implements Serializable {
 	public Double getZ() {
 		return z;
 	}
-	public Boolean getDisable() {
-		return disable;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((x == null) ? 0 : x.hashCode());
+		result = prime * result + ((y == null) ? 0 : y.hashCode());
+		result = prime * result + ((z == null) ? 0 : z.hashCode());
+		return result;
 	}
-	public void setDisable(Boolean disable) {
-		this.disable = disable;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SampleStageParameters other = (SampleStageParameters) obj;
+		if (x == null) {
+			if (other.x != null)
+				return false;
+		} else if (!x.equals(other.x))
+			return false;
+		if (y == null) {
+			if (other.y != null)
+				return false;
+		} else if (!y.equals(other.y))
+			return false;
+		if (z == null) {
+			if (other.z != null)
+				return false;
+		} else if (!z.equals(other.z))
+			return false;
+		return true;
 	}
 
 }
