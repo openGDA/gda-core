openGDA/gda-core

commit 1768eb745cf8de2056d25ff2bade5538278b3421

DAQ-1423 Clean up use of "command_server" in gda-core
...mostly by using Finder::findSingleton rather than finding by name.
Where the name has remained, it is addressed as Jython.SERVER_NAME

Change-Id: I394dec525621bd098aec1706334cf411a4c4ba75

uk.ac.gda.core/scripts/gdascripts/utils.py
@@ -5,6 +5,7 @@
 from gda.epics import CAClient
 from gda.factory import Finder
 from contextlib import contextmanager
+from gda.jython import JythonServerFacade
 from gda.jython.commands import ScannableCommands
 
 import logging
@@ -92,7 +93,7 @@ def default_scannables(*scn):
 		ScannableCommands.remove_default(*new_scannables)
 
 
-command_server = Finder.getInstance().find('command_server')
+command_server = JythonServerFacade.getInstance()
 
 def jobs():
 	"""

uk.ac.gda.api/src/gda/jython/InterfaceProvider.java
@@ -21,7 +21,6 @@ package gda.jython;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import gda.factory.Findable;
 import gda.factory.Finder;
 import gda.jython.commandinfo.ICommandThreadInfoProvider;
 import gda.jython.completion.TextCompleter;
@@ -157,11 +156,7 @@ public final class InterfaceProvider {
 	 * @return JythonServer
 	 */
 	private static LocalJython getLocalJythonServerFromFinder() {
-		Findable obj = Finder.getInstance().find(Jython.SERVER_NAME);
-		if (obj != null && obj instanceof LocalJython) {
-			return (LocalJython) obj;
-		}
-		throw new IllegalStateException("Unable to find local object of type LocalJython called " + Jython.SERVER_NAME);
+		return Finder.getInstance().findSingleton(LocalJython.class);
 	}
 
 	/**

uk.ac.gda.api/src/gda/jython/JythonServerFacade.java
@@ -121,7 +121,7 @@ public class JythonServerFacade implements IObserver, JSFObserver, IScanStatusHo
 	 * @throws InstantiationException
 	 */
 	private JythonServerFacade() throws InstantiationException {
-		this((Jython) Finder.getInstance().find("command_server"));
+		this(Finder.getInstance().findSingleton(Jython.class));
 	}
 
 	/**

uk.ac.gda.client/src/uk/ac/gda/rcp/views/sample/MetadataUpdater.java
@@ -21,8 +21,6 @@ package uk.ac.gda.rcp.views.sample;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-import java.util.StringTokenizer;
-import java.util.Vector;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.FocusEvent;
@@ -42,6 +40,7 @@ import gda.device.DeviceException;
 import gda.factory.Finder;
 import gda.jython.IJythonServerStatusObserver;
 import gda.jython.IScanDataPointObserver;
+import gda.jython.InterfaceProvider;
 import gda.jython.JythonServerFacade;
 import gda.jython.JythonServerStatus;
 import gda.jython.commandinfo.CommandThreadEvent;
@@ -184,15 +183,6 @@ public class MetadataUpdater implements IObserver, IScanDataPointObserver, IJyth
 			this.arg = arg;
 		}
 
-		private List<Integer> parseScanDimensions(String string) {
-			StringTokenizer st = new StringTokenizer(string, "[], ");
-			List<Integer> sd = new Vector<Integer>();
-			while (st.hasMoreTokens()) {
-				sd.add(Integer.valueOf(st.nextToken()));
-			}
-			return sd;
-		}
-
 		private Integer multiply(Collection<Integer> c) {
 			int a = 1;
 			for (Integer integer : c) {
@@ -305,8 +295,11 @@ public class MetadataUpdater implements IObserver, IScanDataPointObserver, IJyth
 						client.scanStatus.setText("RUNNING");
 						started = new Date();
 						client.elapsedTime.setText("00:00:00");
-						scanstring = jsf.evaluateCommand("finder.find(\"command_server\").getCurrentScanInformation().getDimensions().tolist()");
-						scandimensions = parseScanDimensions(scanstring);
+						int[] dimensions = InterfaceProvider.getCurrentScanInformationHolder().getCurrentScanInformation().getDimensions();
+						scandimensions.clear();
+						for (Integer dim : dimensions) {
+							scandimensions.add(dim);
+						}
 						totalScanPoints = multiply(scandimensions);
 						break;
 					default:

uk.ac.gda.core.test/src/gda/data/fileregistrar/FileRegistrarUnitTest.java
@@ -34,6 +34,7 @@ import java.util.concurrent.TimeoutException;
 import org.eclipse.january.dataset.Dataset;
 import org.eclipse.scanning.api.scan.IFilePathService;
 import org.eclipse.scanning.api.scan.IScanService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -41,7 +42,10 @@ import org.mockito.ArgumentCaptor;
 import gda.TestHelpers;
 import gda.data.scan.datawriter.IDataWriterExtender;
 import gda.device.Detector;
+import gda.factory.Factory;
 import gda.factory.FactoryException;
+import gda.factory.Finder;
+import gda.jython.Jython;
 import gda.scan.IScanDataPoint;
 
 public class FileRegistrarUnitTest {
@@ -87,6 +91,19 @@ public class FileRegistrarUnitTest {
 
 		filePathService = mock(IFilePathService.class);
 		FileRegistrarServiceHolder.setFilePathService(filePathService);
+	}
+
+	@Before
+	public void addMockJythonToFinder() {
+		Jython jython = mock(Jython.class);
+		Factory factory = mock(Factory.class);
+		when(factory.getFindables()).thenReturn(Arrays.asList(jython));
+		Finder.getInstance().addFactory(factory);
+	}
+
+	@After
+	public void cleanUpFinder() {
+		Finder.getInstance().removeAllFactories();
 	}
 
 	@SuppressWarnings("unused")

uk.ac.gda.core.test/src/gda/data/metadata/XMLIcatTest.java
@@ -21,17 +21,25 @@ package gda.data.metadata;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import gda.configuration.properties.LocalProperties;
-import gda.data.metadata.icat.Icat;
-import gda.data.metadata.icat.IcatProvider;
-import gda.jython.authenticator.UserAuthentication;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import gda.configuration.properties.LocalProperties;
+import gda.data.metadata.icat.Icat;
+import gda.data.metadata.icat.IcatProvider;
+import gda.factory.Factory;
+import gda.factory.Finder;
+import gda.jython.Jython;
+import gda.jython.authenticator.UserAuthentication;
 
 /**
  *
@@ -62,6 +70,16 @@ public class XMLIcatTest {
 		System.setProperty("user.name", "abc123");
 		UserAuthentication.setToUseOSAuthentication();
 
+		Jython jython = mock(Jython.class);
+		Factory factory = mock(Factory.class);
+		when(factory.getFindables()).thenReturn(Arrays.asList(jython));
+		Finder.getInstance().addFactory(factory);
+
+	}
+
+	@After
+	public void cleanUpFinder() {
+		Finder.getInstance().removeAllFactories();
 	}
 
 	/**

uk.ac.gda.core/scripts/gdascripts/scan/trajscans.py
@@ -1,4 +1,3 @@
-from gda.factory import Finder
 from gda.scan import TrajectoryScanLine, ConcurrentScan, \
     ConstantVelocityScanLine
 
@@ -11,12 +10,12 @@ POSITION_CALLABLE_THREADPOOL_SIZE = 4
 
 def setDefaultScannables(new):
     # TODO: Replace this lot with a call to create a Scan with no defaults.
-    cs = Finder.getInstance().find("command_server")
-    original = list(cs.getDefaultScannables())
+    from gda.jython.commands.ScannableCommands import get_defaults, add_default, remove_default
+    original = get_defaults()
     for scn in original:
-        cs.removeDefault(scn)
+        remove_default(scn)
     for scn in new:
-        cs.addDefault(scn)
+        add_default(scn)
     return original
 
 

uk.ac.gda.core/src/gda/commandqueue/JythonCommandCommandProvider.java
@@ -18,9 +18,6 @@
 
 package gda.commandqueue;
 
-import gda.factory.Finder;
-import gda.jython.JythonServer;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -30,6 +27,9 @@ import java.io.PrintWriter;
 import java.io.Serializable;
 
 import org.springframework.util.StringUtils;
+
+import gda.factory.Finder;
+import gda.jython.JythonServer;
 
 public class JythonCommandCommandProvider implements CommandProvider, Serializable{
 
@@ -57,7 +57,7 @@ public class JythonCommandCommandProvider implements CommandProvider, Serializab
 			String[] split = trim.split(" ");
 			if( split.length ==2){
 				String scriptName = split[1].replaceAll("[\"\']", "");
-				JythonServer server = (JythonServer) Finder.getInstance().find(JythonServer.SERVERNAME);
+				JythonServer server = Finder.getInstance().findSingleton(JythonServer.class);
 				scriptPath = server.getJythonScriptPaths().pathToScript(scriptName);
 				if (scriptPath == null) {
 					throw new FileNotFoundException("Could not run " + scriptName + " script. File not found in " + server.getJythonScriptPaths().description() + ".");

commit 59fde02e084c4b671a9191697de31b1457d2e493

Import JythonServer instead of the Facade to fix bug
This is a bug I introduced in commit 1768eb7. This is used in the server
so Facade should not be used (and in fact would fail!).

Additional improvement: move import inside function that requires it.

Change-Id: Iacf316b38971101966ad6a5ccdebbf28524cbef6

uk.ac.gda.core/scripts/gdascripts/utils.py
@@ -5,7 +5,7 @@
 from gda.epics import CAClient
 from gda.factory import Finder
 from contextlib import contextmanager
-from gda.jython import JythonServerFacade
+from gda.jython import Jython
 from gda.jython.commands import ScannableCommands
 
 import logging
@@ -93,14 +93,15 @@ def default_scannables(*scn):
 		ScannableCommands.remove_default(*new_scannables)
 
 
-command_server = JythonServerFacade.getInstance()
-
 def jobs():
+	
 	"""
 	Print a string showing the threads running in the command server.
 
 	Only shows the live threads.
 	"""
+	command_server = Finder.getInstance().find(Jython.SERVER_NAME)
+	
 	logger.debug("jobs() called")
 
 	s = "%-10s %-12s %-8s %-11s %s\n" % ('DATE', 'TIME', 'TYPE', 'INTERRUPTED', 'COMMAND')