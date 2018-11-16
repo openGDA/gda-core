351aab9588346dcc6ebd6445f44aa9fab0ae8c61
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-11-16 18:04:59 +0000

    [DAQ-1797] Make DummyEnumPositioner behave more like Epics positioner classes
    - Don't do move instantaneously: let it take a (configurable) time
    - Allow motion to be stopped
    - Send (only) EnumPositionerStatus events
    
    Change-Id: I5f38d7964e56a243de5b7e96d6e19d6834a63ff5

--- uk.ac.gda.core/src/gda/device/enumpositioner/DummyEnumPositioner.java
+++ uk.ac.gda.core/src/gda/device/enumpositioner/DummyEnumPositioner.java
@@ -19,10 +19,16 @@
 
 package gda.device.enumpositioner;
 
+import static gda.device.EnumPositionerStatus.IDLE;
+import static gda.device.EnumPositionerStatus.MOVING;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gda.device.DeviceException;
 import gda.device.EnumPositioner;
 import gda.device.EnumPositionerStatus;
-import gda.device.scannable.ScannablePositionChangeEvent;
+import uk.ac.diamond.daq.concurrent.Async;
 import uk.ac.gda.api.remoting.ServiceInterface;
 
 /**
@@ -30,11 +36,29 @@ import uk.ac.gda.api.remoting.ServiceInterface;
  */
 @ServiceInterface(EnumPositioner.class)
 public class DummyEnumPositioner extends EditableEnumPositionerBase {
+
+	private static final Logger logger = LoggerFactory.getLogger(DummyEnumPositioner.class);
+
+	private static final String INVALID_POSITION_MESSAGE = "Position '%s' is invalid for %s";
+
+	/** time in ms between polling for stop request */
+	private static final int POLLING_TIME = 100;
+
+	/** default time in ms to perform move */
+	private static final int DEFAULT_MOVE_TIME = 500;
+
 	private int currentPositionIndex = 0;
+
+	/** Time (in ms) to perform move */
+	private long timeToMove = DEFAULT_MOVE_TIME;
+
+	private volatile EnumPositionerStatus currentStatus = IDLE;
+	private volatile boolean inPos = true;
+	private volatile boolean stopRequested = false;
 
 	@Override
 	public void configure() {
-		this.inputNames = new String[]{getName()};
+		this.inputNames = new String[] { getName() };
 		setConfigured(true);
 	}
 
@@ -52,7 +76,16 @@ public class DummyEnumPositioner extends EditableEnumPositionerBase {
 
 	@Override
 	public void rawAsynchronousMoveTo(Object position) throws DeviceException {
-		moveTo(position);
+		// Set status to "moving" in case the positioner is polled before the async task starts
+		setStatus(MOVING);
+		Async.execute(() -> {
+			try {
+				moveTo(position);
+			} catch (DeviceException e) {
+				logger.error("Error moving {} to position {}", getName(), position, e);
+				setStatus(IDLE);
+			}
+		});
 	}
 
 	@Override
@@ -62,43 +95,78 @@ public class DummyEnumPositioner extends EditableEnumPositionerBase {
 
 	@Override
 	public EnumPositionerStatus getStatus() throws DeviceException {
-		return EnumPositionerStatus.IDLE;
+		return currentStatus;
 	}
 
 	@Override
 	public void stop() throws DeviceException {
-		// do nothing
+		stopRequested = true;
 	}
 
 	@Override
 	public void moveTo(Object position) throws DeviceException {
-
 		final String positionString = position.toString();
+		stopRequested = false;
+
+		// Check validity of the requested position
+		if (!containsPosition(positionString)) {
+			setStatus(IDLE);
+			throw new DeviceException(String.format(INVALID_POSITION_MESSAGE, positionString, getName()));
+		}
 
 		// find in the positionNames array the index of the string
-		if (containsPosition(positionString) ) {
-			if( !getPosition().equals(positionString)){
-				currentPositionIndex = getPositionIndex(positionString);
-				this.notifyIObservers(this, getStatus());
-				this.notifyIObservers(this, getPosition());
-				this.notifyIObservers(this, new ScannablePositionChangeEvent(getPosition()));
+		if (!getPosition().equals(positionString)) {
+			inPos = false;
+			setStatus(MOVING);
+			logger.debug("{} moving to {}", getName(), positionString);
+
+			final long numPolls = timeToMove / POLLING_TIME;
+			for (int i = 0; i < numPolls; i++) {
+				if (stopRequested) {
+					break;
+				}
+				try {
+					Thread.sleep(POLLING_TIME);
+				} catch (InterruptedException e) {
+					logger.error("Interrupted during simulated move", e);
+					Thread.currentThread().interrupt();
+				}
 			}
-			return;
+
+			if (!stopRequested) {
+				currentPositionIndex = getPositionIndex(positionString);
+				inPos = true;
+			}
 		}
-		// if get here then wrong position name supplied
-		throw new DeviceException("Position called: " + positionString + " not found.");
+		setStatus(IDLE);
+	}
+
+	private void setStatus(EnumPositionerStatus status) {
+		if (status != currentStatus) {
+			currentStatus = status;
+			notifyIObservers(this, status);
+		}
 	}
 
 	@Override
 	public boolean isInPos() throws DeviceException {
-		// Moves happen immediately, so this will always be true
-		return true;
+		return inPos;
 	}
 
 	public void setPosition(String position) throws DeviceException {
-		if (!isConfigured()) {
-			moveTo(position);
+		final int positionIndex = getPositionIndex(position);
+		if (positionIndex < 0) {
+			throw new DeviceException(String.format(INVALID_POSITION_MESSAGE, position, getName()));
 		}
+		currentPositionIndex = positionIndex;
+	}
+
+	public void setTimeToMove(long timeToMove) {
+		this.timeToMove = timeToMove;
+	}
+
+	public long getTimeToMove() {
+		return timeToMove;
 	}
 
 }
