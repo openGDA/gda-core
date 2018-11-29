aafedcb4d68e6f54936f01d1409ee85d03cdffa9
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-11-29 10:38:41 +0000

    [I08-192] Exit slit config: allow user to change the tweak amount
    for mirror & slit
    
    Replace existing controls with NudgePositionerComposite
    
    Change-Id: Ib10ea54ac21adda99b1b6e794678d3647aad49bf

--- uk.ac.diamond.daq.client.gui.exitslit.configuration/src/uk/ac/diamond/daq/client/gui/exitslit/configuration/ConfigureExitSlitsNudgeMotor.java
+++ uk.ac.diamond.daq.client.gui.exitslit.configuration/src/uk/ac/diamond/daq/client/gui/exitslit/configuration/ConfigureExitSlitsNudgeMotor.java
@@ -18,29 +18,20 @@
 
 package uk.ac.diamond.daq.client.gui.exitslit.configuration;
 
-import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
 import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.COLOUR_WHITE;
-import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.MOTOR_ERROR;
-import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.SCANNABLE_UPDATE_PERIOD;
-import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.createButton;
 import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.createInstructionLabel;
 import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.createSeparator;
-import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.createStopButton;
-import static uk.ac.diamond.daq.client.gui.exitslit.configuration.ConfigureExitSlitsUtils.displayError;
-
-import java.text.DecimalFormat;
 
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import gda.device.DeviceException;
 import gda.device.IScannableMotor;
-import uk.ac.gda.dls.client.views.ReadonlyScannableComposite;
+import gda.rcp.views.NudgePositionerComposite;
 
 /**
  * Composite to handle tweaking a motor position until the user is satisfied that the position is correct.
@@ -52,62 +43,28 @@ public class ConfigureExitSlitsNudgeMotor extends ConfigureExitSlitsComposite {
 
 	private final IScannableMotor motor;
 
-	private final Button btnMinus;
-	private final Button btnPlus;
-
-	private static final int COLUMNS = 4;
-
 	public ConfigureExitSlitsNudgeMotor(Composite parent, String title, String description, String instructions,
-			IScannableMotor motor, String motorDescription, double tweakAmount) {
+			IScannableMotor motor, String displayName, double initialTweakAmount) {
 		super(parent, title, description);
 		this.motor = motor;
 
-		GridLayoutFactory.swtDefaults().numColumns(COLUMNS).applyTo(this);
+		GridLayoutFactory.swtDefaults().applyTo(this);
 		setBackground(COLOUR_WHITE);
 
 		// Instructions
-		GridDataFactory.swtDefaults().span(COLUMNS, 1).applyTo(createInstructionLabel(this, instructions));
-		GridDataFactory.fillDefaults().span(COLUMNS, 1).grab(true,  false).applyTo(createSeparator(this));
+		GridDataFactory.swtDefaults().applyTo(createInstructionLabel(this, instructions));
+		GridDataFactory.fillDefaults().grab(true,  false).applyTo(createSeparator(this));
 
 		// Motor position with buttons to adjust it
-		final DecimalFormat motorPositionFormat = new DecimalFormat("#.#####");
-		final String motorPositionString = motorPositionFormat.format(tweakAmount);
-
-		final ReadonlyScannableComposite motorPositionComposite = new ReadonlyScannableComposite(this, SWT.NONE, motor, motorDescription, "", 3, true);
-		GridDataFactory.swtDefaults().applyTo(motorPositionComposite);
-		motorPositionComposite.setBackground(COLOUR_WHITE);
-		motorPositionComposite.setMinPeriodMS(SCANNABLE_UPDATE_PERIOD);
-
-		btnMinus = createButton(this, "-", String.format("Decrement value by %s", motorPositionString));
-		GridDataFactory.swtDefaults().hint(NUDGE_BUTTON_WIDTH, SWT.DEFAULT).applyTo(btnMinus);
-		btnMinus.addSelectionListener(widgetSelectedAdapter(e -> moveBy(-tweakAmount)));
-
-		btnPlus = createButton(this, "+", String.format("Increment value by %s", motorPositionString));
-		GridDataFactory.swtDefaults().hint(NUDGE_BUTTON_WIDTH, SWT.DEFAULT).applyTo(btnPlus);
-		btnPlus.addSelectionListener(widgetSelectedAdapter(e -> moveBy(tweakAmount)));
-
-		final Button btnStop = createStopButton(this);
-		btnStop.addSelectionListener(widgetSelectedAdapter(e -> stopMotor()));
+		final NudgePositionerComposite motorPositionComposite = new NudgePositionerComposite(this,
+				SWT.HORIZONTAL | SWT.READ_ONLY);
+		GridDataFactory.fillDefaults().applyTo(motorPositionComposite);
+		motorPositionComposite.setScannable(motor);
+		motorPositionComposite.setIncrement(initialTweakAmount);
+		motorPositionComposite.setIncrementTextWidth(50);
+		motorPositionComposite.setDisplayName(displayName);
 
 		updateButtons();
-	}
-
-	private void moveBy(double amount) {
-		try {
-			final double newPosition = (double) motor.getPosition() + amount;
-			motor.asynchronousMoveTo(newPosition);
-		} catch (DeviceException e) {
-			final String message = String.format("Error moving motor %s", motor.getName());
-			displayError(MOTOR_ERROR, message, e, logger);
-		}
-	}
-
-	private void stopMotor() {
-		try {
-			motor.stop();
-		} catch (DeviceException e) {
-			displayError(MOTOR_ERROR, "Error stopping motor", e, logger);
-		}
 	}
 
 	@Override
@@ -131,15 +88,7 @@ public class ConfigureExitSlitsNudgeMotor extends ConfigureExitSlitsComposite {
 
 	@Override
 	protected void updateButtons() {
-		if (btnMinus != null && btnPlus != null) {
-			try {
-				final boolean moving = motor.isBusy();
-				btnMinus.setEnabled(!moving);
-				btnPlus.setEnabled(!moving);
-			} catch (DeviceException e) {
-				logger.error("Error acccessing motor {}", motor.getName(), e);
-			}
-		}
+		// Nothing to do - buttons managed by composite
 	}
 
 	@Override
