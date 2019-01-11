d3647abba1a87de27aef976df85e64b46b1c2bbd
Chris Kelley <chris.kelley@diamond.ac.uk>
2019-01-11 10:57:20 +0000

    Remove unused imports
    Change-Id: I3f55ad9e2788e1ca28de1a2890ad97e5381d85ea

--- plugins/uk.ac.gda.client.live.stream.controls/src/uk/ac/gda/client/live/stream/controls/utils/CountDownTimer.java
+++ plugins/uk.ac.gda.client.live.stream.controls/src/uk/ac/gda/client/live/stream/controls/utils/CountDownTimer.java
@@ -2,7 +2,6 @@ package uk.ac.gda.client.live.stream.controls.utils;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 /**
