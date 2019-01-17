c89d04a37e56101d908775a38e0260c41834604b
James Mudd <james.mudd@diamond.ac.uk>
2019-01-17 11:26:03 +0000

    Update Version after 9.11 release
    Change-Id: I2f0cea05a96e780dbbd4921b58ac8c238e01295a

--- uk.ac.gda.api/src/gda/util/Version.java
+++ uk.ac.gda.api/src/gda/util/Version.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Version {
 
-	private static final String RELEASE_VER = "9.11.0";
+	private static final String RELEASE_VER = "9.12.0pre";
 
 	/**
 	 * Returns the release version as a String
