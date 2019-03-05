13d4879c608c86fc9fab5fb39023ad511ab10b62
Chris Kelley <chris.kelley@diamond.ac.uk>
2019-03-05 11:20:37 +0000

    Automatic on-save cleanup in DummySwtVideoReceiver
    Change-Id: I5e25d9efe7f76fabf1fcd852092fc78bb84ea84a

--- uk.ac.gda.video/src/gda/images/camera/DummySwtVideoReceiver.java
+++ uk.ac.gda.video/src/gda/images/camera/DummySwtVideoReceiver.java
@@ -18,8 +18,6 @@
 
 package gda.images.camera;
 
-import gda.device.DeviceException;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
@@ -27,55 +25,57 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Display;
 
+import gda.device.DeviceException;
+
 public class DummySwtVideoReceiver extends DummyVideoReceiverBase<ImageData>{
 
 	private boolean drawGridlines;
-	
+
 	public void setDrawGridlines(boolean drawGridlines) {
 		this.drawGridlines = drawGridlines;
 	}
-	
+
 	private boolean showBottomRight;
-	
+
 	public void setShowBottomRight(boolean showBottomRight) {
 		this.showBottomRight = showBottomRight;
 	}
-	
+
 	private Image image;
-	
+
 	private GC gc;
-	
+
 	private Color backgroundColour;
-	
+
 	private Color gridLineColour;
-	
+
 	private Color circleColour;
-	
+
 	@Override
 	protected void createBlankImage() {
 		Display display = Display.getDefault();
 		image = new Image(display, imageSize.width, imageSize.height);
 		gc = new GC(image);
-		
+
 		backgroundColour = display.getSystemColor(SWT.COLOR_GRAY);
 		gridLineColour = display.getSystemColor(SWT.COLOR_BLACK);
 		circleColour = display.getSystemColor(SWT.COLOR_RED);
-		
+
 		gc.setBackground(backgroundColour);
 		gc.fillRectangle(0, 0, imageSize.width, imageSize.height);
 	}
-	
+
 	@Override
 	protected ImageData updateImage() {
-		
+
 		// erase old circle
 		gc.setBackground(backgroundColour);
 		gc.fillOval(circleX-2, circleY-2, CIRCLE_SIZE+4, CIRCLE_SIZE+4);
-		
+
 		// move circle to new position
 		circleX += circleDeltaX;
 		circleY += circleDeltaY;
-		
+
 		// check whether circle has move beyond edge of image
 		if (circleY < 0 || circleY >= (imageSize.height - CIRCLE_SIZE)) {
 			circleY -= circleDeltaY * 2;
@@ -85,42 +85,42 @@ public class DummySwtVideoReceiver extends DummyVideoReceiverBase<ImageData>{
 			circleX -= circleDeltaX * 2;
 			circleDeltaX = -circleDeltaX;
 		}
-		
+
 		// draw new circle
 		gc.setBackground(circleColour);
 		gc.fillOval(circleX, circleY, CIRCLE_SIZE, CIRCLE_SIZE);
-		
+
 		if (drawGridlines) {
-			
+
 			gc.setForeground(gridLineColour);
 			gc.setBackground(backgroundColour);
-			
+
 			for (int x=0; x<=imageSize.width; x+=100) {
 				gc.drawLine(x, 0, x, imageSize.height);
 				gc.drawText(String.format("x=%d", x), x+5, 50);
 			}
-			
+
 			for (int y=0; y<=imageSize.height; y+=100) {
 				gc.drawLine(0, y, imageSize.width, y);
 				gc.drawText(String.format("y=%d", y), 30, y+2);
 			}
 		}
-		
+
 		if (showBottomRight) {
-			
+
 			// arrow pointing to bottom right corner
 			gc.drawLine(imageSize.width-30, imageSize.height-30, imageSize.width-10, imageSize.height-10);
 			gc.setBackground(gridLineColour);
 			gc.fillPolygon(new int[] {imageSize.width-5, imageSize.height-20, imageSize.width-20, imageSize.height-5, imageSize.width-5, imageSize.height-5});
-			
+
 			// coordinates of bottom right
 			gc.setBackground(backgroundColour);
 			gc.drawText(String.format("(%d, %d)", imageSize.width, imageSize.height), imageSize.width-80, imageSize.height-50);
 		}
-		
+
 		return image.getImageData();
 	}
-	
+
 	@Override
 	public ImageData getImage() throws DeviceException {
 		return image.getImageData();
