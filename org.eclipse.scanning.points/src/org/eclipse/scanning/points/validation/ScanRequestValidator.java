39cceae596f9e88a80bc506be23f97545a51ae6a
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2019-03-07 15:00:32 +0000

    [DAQ-2056] refactoring ScanRequestValidator.validateMalcolmRules
    Change-Id: I5f7a59e91da1516bf929019d9d4babb1ec3ae430

--- org.eclipse.scanning.points/src/org/eclipse/scanning/points/validation/ScanRequestValidator.java
+++ org.eclipse.scanning.points/src/org/eclipse/scanning/points/validation/ScanRequestValidator.java
@@ -13,8 +13,9 @@ package org.eclipse.scanning.points.validation;
 
 import java.lang.reflect.Field;
 import java.text.MessageFormat;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.beanutils.BeanMap;
 import org.eclipse.scanning.api.IValidator;
@@ -90,50 +91,50 @@ class ScanRequestValidator implements IValidator<ScanRequest<?>> {
 	}
 
 	private void validateMalcolmRules(Map<String, Object> dmodels)  throws ValidationException, ScanningException {
-		ScanMode scanMode = dmodels.values().stream().anyMatch(IMalcolmModel.class::isInstance) ?
+		// we can't validate without a validation service
+		IRunnableDeviceService runnableDeviceService = ValidatorService.getRunnableDeviceService();
+		if (runnableDeviceService == null) return;
+
+		// if the scan includes a malcolm device, then it is a hardware scan
+		final ScanMode scanMode = dmodels.values().stream().anyMatch(IMalcolmModel.class::isInstance) ?
 				ScanMode.HARDWARE : ScanMode.SOFTWARE;
 
-		IRunnableDeviceService dservice = ValidatorService.getRunnableDeviceService();
-		if (dservice!=null) {
-			Map<DeviceRole, Integer> deviceRoleCount = new HashMap<>();
-			for (DeviceRole role : DeviceRole.values()) deviceRoleCount.put(role, 0);
-			for (String name : dmodels.keySet()) {
-				DeviceRole role = null;
-				DeviceInformation<?> info = dservice.getDeviceInformation(name);
-				if (info==null) {
-					try {
-						final IRunnableDevice<?> device = dservice.createRunnableDevice(dmodels.get(name));
-						if (device.getRole()==DeviceRole.PROCESSING) {
-							role = device.getRole();
-						} else {
-							// Only processing may be created on the fly, the others must have names.
-							throw new ValidationException("Detector '"+name+"' cannot be found!");
-						}
-					} catch (ScanningException ne) {
-						throw ne; // If we cannot make a device with this model, the scan request is not valid.
-					}
-				} else {
-					// devices that can run either as a standard hardware detector or as a hardware
-					// triggered detector will be switched to the appropriate role according to the scan type
-					if (!info.getSupportedScanModes().contains(scanMode)) {
-						throw new ValidationException(MessageFormat.format("The device ''{0}'' does not support a {1} scan",
-								info.getName(), scanMode.toString().toLowerCase()));
-					}
-					role = info.getDeviceRole();
-				}
+		final Map<DeviceRole, Integer> deviceRoleCount = new EnumMap<>(DeviceRole.class);
+		for (DeviceRole role : DeviceRole.values()) deviceRoleCount.put(role, 0);
+		for (Entry<String, Object> entry : dmodels.entrySet()) {
+			final String name = entry.getKey();
+			final Object model = entry.getValue();
+			final DeviceInformation<?> info = runnableDeviceService.getDeviceInformation(name);
 
-				if (role==null) throw new ValidationException("Detector '"+name+"' cannot be found!");
-				Integer c = deviceRoleCount.get(role);
-				deviceRoleCount.put(role, ++c);
+			DeviceRole role = null;
+			if (info == null) {
+				final IRunnableDevice<?> device = runnableDeviceService.createRunnableDevice(model);
+				if (device.getRole() != DeviceRole.PROCESSING) {
+					// Only processing may be created on the fly, the others must have names.
+					throw new ValidationException("Detector '"+name+"' cannot be found!");
+				}
+				role = DeviceRole.PROCESSING;
+			} else {
+				// devices that can run either as a standard hardware detector or as a hardware
+				// triggered detector will be switched to the appropriate role according to the scan type
+				if (!info.getSupportedScanModes().contains(scanMode)) {
+					throw new ValidationException(MessageFormat.format("The device ''{0}'' does not support a {1} scan",
+							info.getName(), scanMode.toString().toLowerCase()));
+				}
+				role = info.getDeviceRole();
 			}
-			if (deviceRoleCount.get(DeviceRole.MALCOLM)>1) {
-				throw new ValidationException("Only one malcolm device may be used per scan.");
-			}
+
+			if (role==null) throw new ValidationException("Detector '"+name+"' cannot be found!");
+			Integer c = deviceRoleCount.get(role);
+			deviceRoleCount.put(role, ++c);
+		}
+
+		if (deviceRoleCount.get(DeviceRole.MALCOLM) > 1) {
+			throw new ValidationException("Only one malcolm device may be used per scan.");
 		}
 	}
 
 	private void validateDetectors(Map<String, Object> dmodels) throws ValidationException, InstantiationException, IllegalAccessException {
-
 		// All the models must validate too
 		for (Object model : dmodels.values()) {
 			IValidator<Object> validator = vservice.getValidator(model);
