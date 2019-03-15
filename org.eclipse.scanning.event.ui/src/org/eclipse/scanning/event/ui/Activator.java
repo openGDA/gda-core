b0acf64710cee08f14bc4579e3bdc8311fe58f50
Chris Kelley <chris.kelley@diamond.ac.uk>
2019-03-15 17:09:45 +0000

    Remove redundant null check
    Also fixes up the formatting
    
    Change-Id: I06b842f3cf88887aa8e71d9e0da4c76502c8cb8b

--- org.eclipse.scanning.event.ui/src/org/eclipse/scanning/event/ui/Activator.java
+++ org.eclipse.scanning.event.ui/src/org/eclipse/scanning/event/ui/Activator.java
@@ -64,29 +64,28 @@ public class Activator extends AbstractUIPlugin {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
 
+	public static final String getJmsUri() {
 
-
-
-    public static final String getJmsUri() {
-
-	String uri = null;
-		if (uri == null) uri = getNovelCommandPreference(CommandConstants.JMS_URI);
-	    if (uri == null) uri = CommandConstants.getScanningBrokerUri();
+	String uri = getNovelCommandPreference(CommandConstants.JMS_URI);
+	if (uri == null) {
+		uri = CommandConstants.getScanningBrokerUri();
+	}
 		return uri;
 	}
 
-
-    /**
-     * Get the command value if it has been changed by the user.
-     * @param key
-     * @return
-     */
-    private static final String getNovelCommandPreference(String key) {
+	/**
+	 * Get the command value if it has been changed by the user.
+	 * @param key
+	 * @return
+	 */
+	private static final String getNovelCommandPreference(String key) {
 		final IPreferenceStore store = getDefault().getPreferenceStore();
-	String val = store.getString(key);
-	String def = store.getDefaultString(key);
-	if (!val.equals(def)) return val;
-	return null;
-     }
+		String val = store.getString(key);
+		String def = store.getDefaultString(key);
+		if (!val.equals(def)) {
+			return val;
+		}
+		return null;
+	}
 
 }
