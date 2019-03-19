72cd99ed4437515156f66f53f2a7830cc1aa132e
Anthony Hull <anthony.hull@diamond.ac.uk>
2019-03-19 16:16:18 +0000

    [I14-616] Improve button to change scan types
    - Define in XML
    - Move to main toolbar and menu
    - Show text as well as icon
    
    Change-Id: Ib2048aa0ff27de83081e7b9e3de105a36d44a842

---
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/SelectScanTypeHandler.java
@@ -0,0 +1,89 @@
+/*-
+ * Copyright © 2019 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.daq.mapping.ui.experiment;
+
+import static java.util.Arrays.asList;
+
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ListDialog;
+
+/**
+ * Command handler to show a dialogue that allows the user to change the type of scan submitted to GDA
+ * <p>
+ * It will only be active if the {@link MappingExperimentView}, defined by the {@link MappingViewConfiguration} bean,
+ * contains a {@link SubmitScanSelector} containing more than one choice of {@link SubmitScanSection} (or subclass
+ * thereof).
+ */
+public class SelectScanTypeHandler extends AbstractHandler implements IHandler {
+
+	private static final String VIEW_ID = "uk.ac.diamond.daq.mapping.ui.experiment.mappingExperimentView";
+
+	private boolean initialised = false;
+	private boolean enabled = false;
+	private SubmitScanSelector submitScanSelector;
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final List<String> descriptions = submitScanSelector.getDescriptions();
+		final int currentSelection = submitScanSelector.getCurrentSectionIndex();
+
+		final ListDialog dialog = new ListDialog(Display.getDefault().getActiveShell());
+		dialog.setTitle("Choose the scan type");
+		dialog.setContentProvider(new ArrayContentProvider());
+		dialog.setLabelProvider(new LabelProvider());
+		dialog.setInput(submitScanSelector.getDescriptions());
+		// Select the name of the section that is currently visible
+		dialog.setInitialElementSelections(asList(descriptions.get(currentSelection)));
+
+		if (dialog.open() == Window.OK) {
+			final Object[] result = dialog.getResult();
+			submitScanSelector.showSection(descriptions.indexOf(result[0]));
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		if (!initialised) {
+			final EPartService partService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(EPartService.class);
+			final MPart mappingViewPart = partService.findPart(VIEW_ID);
+			if (mappingViewPart != null) {
+				final MappingExperimentView mappingExperimentView = (MappingExperimentView) mappingViewPart.getObject();
+				submitScanSelector = mappingExperimentView.getSection(SubmitScanSelector.class);
+				if (submitScanSelector != null && submitScanSelector.getNumberOfSections() > 1) {
+					enabled = true;
+				}
+			}
+			initialised = true;
+		}
+		return enabled;
+	}
+}
