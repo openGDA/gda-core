fc06d53f0b9d68790c4319d5429d694109418874
Anthony Hull <anthony.hull@diamond.ac.uk>
2019-05-21 14:45:06 +0100

    [I14-600] Add Standards scan mapping support in a new plugin
    Change-Id: Ib7c188a653bc8d99e148aba87de174f0fdd5d6eb

---
+++ uk.ac.diamond.daq.mapping.standards.ui/src/uk/ac/diamond/daq/mapping/standards/ui/StandardsScanConfig.java
@@ -0,0 +1,41 @@
+/*-
+ * Copyright © 2019 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.daq.mapping.standards.ui;
+
+import uk.ac.diamond.daq.osgi.OsgiService;
+
+/**
+ * Configuration for {@link StandardsScanView}
+ */
+@OsgiService(StandardsScanConfig.class)
+public class StandardsScanConfig {
+	/**
+	 * Name of the scannable to be used for the standards scan
+	 */
+	private String scannableName;
+
+	public String getScannableName() {
+		return scannableName;
+	}
+
+	public void setScannableName(String scannableName) {
+		this.scannableName = scannableName;
+	}
+
+}
