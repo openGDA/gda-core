366e3d6098645ee0211de869acec4ce591fdcd5d
Peter Holloway <peter.holloway@diamond.ac.uk>
2019-07-31 14:47:33 +0100

    Add ConfigurableAware interface
    Add hooks that allow beans to perform actions at the start or end of GDA's
    configuration process. This allows methods that require all beans to
    have been configured to be seperated from the configuration process.
    
    Change-Id: I976bbd257f460c31f261312491919a7597d31c14

---
+++ uk.ac.gda.api/src/gda/factory/ConfigurableAware.java
@@ -0,0 +1,59 @@
+/*-
+ * Copyright © 2019 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.factory;
+
+/**
+ * An additional interface in the Configurable system that allows other beans to be aware
+ * of the configuration process. Some actions required at start up need to be able to guarantee that all
+ * configurables have been configured. This is not possible during an objects own configure as it cannot
+ * control the order in which it will be called.
+ * <p>
+ * All methods default to no-ops to allow classes to only implement the required hooks.
+ */
+public interface ConfigurableAware {
+
+	/**
+	 * Called before the configuration process starts when no beans have been configured.<p>
+	 * Order in which implementations of this method are called is undetermined.
+	 */
+	default void preConfigure() {}
+
+	/**
+	 * Called before each bean is configured.
+	 * @param bean about to be configured.
+	 */
+	@SuppressWarnings("unused") //bean intended to be used by implementations
+	default void preConfigureBean(Configurable bean) {}
+
+	/**
+	 * Called after each bean is configured.
+	 * @param bean that has just been configured.
+	 * @param error any exception raised during configure - null if successful
+	 */
+	@SuppressWarnings("unused") //bean intended to be used by implementations
+	default void postConfigureBean(Configurable bean, Exception error) {}
+
+	/**
+	 * Called after the configuration process is complete.<p>
+	 * When this is called, all beans are guaranteed to have been configured
+	 * although the order in which implementations of this method are called is
+	 * undetermined
+	 */
+	default void postConfigure() {}
+}
