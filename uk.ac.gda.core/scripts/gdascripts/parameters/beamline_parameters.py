f847693b140139f7769fa01992099fae53783585
Paul V Hathaway <paul.hathaway@diamond.ac.uk>
2019-08-06 16:53:32 +0100

    Reformat beamline_parameters.py
    Replace spaces with tabs
    Add blank lines between methods
    Reformat non-named parameters to python lower_case
    
    Change-Id: Id4ab9d10c6e3ce9f8ad423ac6b2c84acbd07d112

--- uk.ac.gda.core/scripts/gdascripts/parameters/beamline_parameters.py
+++ uk.ac.gda.core/scripts/gdascripts/parameters/beamline_parameters.py
@@ -6,175 +6,207 @@ JYTHON_NAMESPACE_MAPPING_FILE_PROPERTY = 'gda.jython.namespaceMappingFile'
 FINDER_NAME_MAPPING_FILE_PROPERTY      = 'gda.jython.finderNameMappingFile'
 BEAMLINE_PARAMETERS_FILE_PROPERTY      = 'gda.jython.beamlineParametersFile'
 
-def readDictionaryFromFile(mappingFile, mapping):
-    
-    lines = read_lines_from_file(mappingFile)
-    
-    for line in lines:
-        
-        # remove comment, if any
-        line = line.split("#")[0].strip()
-        
-        whitespace = ' \t'
-        separators = '='
-        
-        if line: # don't process empty lines
-            
-            limit = len(line)
-            key_length = 0
-            value_start = limit
-            has_separator = False
-            
-            # determine the key
-            while key_length < limit:
-                c = line[key_length]
-                if c in separators:
-                    # separator means we've reached the end of the key
-                    value_start = key_length + 1
-                    has_separator = True
-                    break
-                elif c in whitespace:
-                    # whitespace means we've reached the end of the key
-                    value_start = key_length + 1
-                    # key/value separated by whitespace - no separator
-                    break
-                key_length += 1
-            
-            # determine the value
-            while value_start < limit:
-                c = line[value_start]
-                if c not in whitespace: # skip past whitespace
-                    if not has_separator and c in separators:
-                        # found separator, and hadn't already found one; skip
-                        # past it
-                        has_separator = True
-                    else:
-                        # found first character of value
-                        break
-                value_start += 1
-            
-            key = line[:key_length]
-            value = line[value_start:]
-            mapping[key] = value
-    return mapping
+def readDictionaryFromFile(mapping_file, mapping):
+	lines = read_lines_from_file(mapping_file)
+	for line in lines:
+		# remove comment, if any
+		line = line.split("#")[0].strip()
+		whitespace = ' \t'
+		separators = '='
+		if line: # don't process empty lines
+			limit = len(line)
+			key_length = 0
+			value_start = limit
+			has_separator = False
+
+			# determine the key
+			while key_length < limit:
+				c = line[key_length]
+				if c in separators:
+					# separator means we've reached the end of the key
+					value_start = key_length + 1
+					has_separator = True
+					break
+				elif c in whitespace:
+					# whitespace means we've reached the end of the key
+					value_start = key_length + 1
+					# key/value separated by whitespace - no separator
+					break
+				key_length += 1
+
+			# determine the value
+			while value_start < limit:
+				c = line[value_start]
+				if c not in whitespace: # skip past whitespace
+					if not has_separator and c in separators:
+						# found separator, and hadn't already found one; skip
+						# past it
+						has_separator = True
+					else:
+						# found first character of value
+						break
+				value_start += 1
+
+			key = line[:key_length]
+			value = line[value_start:]
+			mapping[key] = value
+	return mapping
+
 
 def read_lines_from_file(filename):
-    f = open(filename, "r")
-    lines = f.readlines()
-    f.close()
-    lines = map(remove_trailing_newlines, lines)
-    return lines
+	f = open(filename, "r")
+	lines = f.readlines()
+	f.close()
+	lines = map(remove_trailing_newlines, lines)
+	return lines
+
 
 remove_trailing_newlines = lambda l: l.rstrip("\r\n")
 
+
 class JythonNameSpaceMapping:
-    ''''''
-    def __init__(self, commandServer=None ):
-        if commandServer==None:
-            commandServer = InterfaceProvider.getJythonNamespace()
-        self.reload(commandServer)
-    def __getitem__(self, itemName):
-        return self.__getattr__(itemName)
-    def __getattr__(self, attrName):
-        if attrName.startswith('__') and attrName.endswith('__'):
-            raise AttributeError
-        if( self.jythonNamespaceMapping.has_key(attrName)) :
-            nameInNameSpace = self.jythonNamespaceMapping[attrName]
-        else:
-            nameInNameSpace = attrName
-        try:
-            return self.commandServer.getFromJythonNamespace(nameInNameSpace)
-        except:
-            msg =  "Error getting value for item named " + nameInNameSpace + " in jythonNamespace for attribute " +attrName
-            # print out now as it is not output by the Jython terminal
-            print msg
-            raise AttributeError, msg
-    def reload(self,commandServer):
-        self.commandServer = commandServer
-        self.jythonNamespaceMapping={}
-        self.jythonNamespaceMapFilePath = LocalProperties.get(JYTHON_NAMESPACE_MAPPING_FILE_PROPERTY)
-        if(self.jythonNamespaceMapFilePath != None):
-            self.jythonNamespaceMapping = readDictionaryFromFile(self.jythonNamespaceMapFilePath, self.jythonNamespaceMapping)
+	''''''
+	def __init__(self, commandServer=None ):
+		if commandServer==None:
+			commandServer = InterfaceProvider.getJythonNamespace()
+		self.reload(commandServer)
+
+
+	def __getitem__(self, item_name):
+		return self.__getattr__(item_name)
+
+
+	def __getattr__(self, attr_name):
+		if attr_name.startswith('__') and attr_name.endswith('__'):
+			raise AttributeError
+		if( self.jythonNamespaceMapping.has_key(attr_name)) :
+			name_in_name_space = self.jythonNamespaceMapping[attr_name]
+		else:
+			name_in_name_space = attr_name
+		try:
+			return self.commandServer.getFromJythonNamespace(name_in_name_space)
+		except:
+			msg =  "Error getting value for item named " + name_in_name_space + " in jythonNamespace for attribute " +attr_name
+			# print out now as it is not output by the Jython terminal
+			print msg
+			raise AttributeError, msg
+
+
+	def reload(self, command_server):
+		self.commandServer = command_server
+		self.jythonNamespaceMapping={}
+		self.jythonNamespaceMapFilePath = LocalProperties.get(JYTHON_NAMESPACE_MAPPING_FILE_PROPERTY)
+		if(self.jythonNamespaceMapFilePath != None):
+			self.jythonNamespaceMapping = readDictionaryFromFile(self.jythonNamespaceMapFilePath, self.jythonNamespaceMapping)
+
 
 class FinderNameMapping:
-    ''''''
-    def __init__(self, finder=None):
-        if finder == None:
-            finder = Finder.getInstance()
-        self.reload(finder)
-    def __getitem__(self, itemName):
-        return self.__getattr__(itemName)
-    def __repr__(self):
-        return "FinderNameMapping(finder=%s)" % self.finder
-    def __getattr__(self, attrName):
-        if attrName.startswith('__') and attrName.endswith('__'):
-            raise AttributeError
-        if( self.finderNameMapping.has_key(attrName)) :
-            nameToFind = self.finderNameMapping[attrName]
-        else:
-            nameToFind = attrName
-        try:
-            obj = self.finder.find(nameToFind)
-        except:
-            # print out now as it is not output by the Jython terminal
-            msg = "Error getting value for item named " + nameToFind + " in finderNameMap for attribute " + attrName
-            print msg
-            raise AttributeError, msg
-        if( obj == None):
-            msg = "Error getting value for item named " + nameToFind + " in finderNameMap for attribute " + attrName
-            print msg
-            raise AttributeError, msg
-        return obj
-    def reload(self,finder):
-        self.finder = finder
-        self.finderNameMapping={}
-        self.finderNameMapFilePath = LocalProperties.get(FINDER_NAME_MAPPING_FILE_PROPERTY)
-        if(self.finderNameMapFilePath != None):
-            self.finderNameMapping = readDictionaryFromFile(self.finderNameMapFilePath, self.finderNameMapping)
+	''''''
+	def __init__(self, finder=None):
+		if finder == None:
+			finder = Finder.getInstance()
+		self.reload(finder)
+
+
+	def __getitem__(self, item_name):
+		return self.__getattr__(item_name)
+
+
+	def __repr__(self):
+		return "FinderNameMapping(finder=%s)" % self.finder
+
+
+	def __getattr__(self, attr_name):
+		if attr_name.startswith('__') and attr_name.endswith('__'):
+			raise AttributeError
+		if( self.finderNameMapping.has_key(attr_name)) :
+			name_to_find = self.finderNameMapping[attr_name]
+		else:
+			name_to_find = attr_name
+		try:
+			obj = self.finder.find(name_to_find)
+		except:
+			# print out now as it is not output by the Jython terminal
+			msg = "Error getting value for item named " + name_to_find + " in finderNameMap for attribute " + attr_name
+			print(msg)
+			raise AttributeError, msg
+		if( obj == None):
+			msg = "Error getting value for item named " + name_to_find + " in finderNameMap for attribute " + attr_name
+			print(msg)
+			raise AttributeError, msg
+		return obj
+
+
+	def reload(self,finder):
+		self.finder = finder
+		self.finderNameMapping={}
+		self.finderNameMapFilePath = LocalProperties.get(FINDER_NAME_MAPPING_FILE_PROPERTY)
+		if(self.finderNameMapFilePath != None):
+			self.finderNameMapping = readDictionaryFromFile(self.finderNameMapFilePath, self.finderNameMapping)
+
 
 class DictionaryWrapper:
-    def __init__(self, map):
-        self.map = map
-    def __getitem__(self, itemName):
-        return self.__getattr__(itemName)
-    def __getattr__(self, attrName):
-        if( self.map.has_key(attrName)) :
-            return self.map[attrName]
-        else:
-            raise AttributeError, attrName
-        
+
+	def __init__(self, source_map):
+		self.map = source_map
+
+
+	def __getitem__(self, item_name):
+		return self.__getattr__(item_name)
+
+
+	def __getattr__(self, attr_name):
+		if( self.map.has_key(attr_name)) :
+			return self.map[attr_name]
+		else:
+			raise AttributeError, attr_name
+
+
 class Parameters:
-    def __init__(self, parametersFilePath=None ):
-        self.reload(parametersFilePath=parametersFilePath)
-    def __getitem__(self, itemName):
-        return self.__getattr__(itemName)
-    def __getattr__(self, attrName):
-        if( self.parameters.has_key(attrName)) :
-            return self.parameters[attrName]
-        else:
-            raise AttributeError, attrName
-    def getValueOrNone(self, attrName):
-        if( self.parameters.has_key(attrName)) :
-            return self.parameters[attrName]
-        else:
-            return None
-    def reload(self, parametersFilePath=None):
-        if parametersFilePath == None:
-            self.parametersFilePath = LocalProperties.get(BEAMLINE_PARAMETERS_FILE_PROPERTY)
-            if(self.parametersFilePath == None):
-                raise AttributeError, "property " + BEAMLINE_PARAMETERS_FILE_PROPERTY + " is not set"
-        else:
-            self.parametersFilePath = parametersFilePath
-        self.parameters={}
-        self.parameters = readDictionaryFromFile(self.parametersFilePath, self.parameters)
-    def getValueFromObjectOrNone(self, attrName, object):
-        if( self.parameters.has_key(attrName)) :
-            return object.__getattr__(self.parameters[attrName])
-        else:
-            return None
-    def saveToFile(self, filePath):
-        f = open(filePath,"w")
-        for key,item in self.parameters.items():
-            f.write(key + "=" + str(item) + "\n")
-        f.close()
+
+	def __init__(self, parameters_file_path=None ):
+		self.reload(parameters_file_path=parameters_file_path)
+
+
+	def __getitem__(self, item_name):
+		return self.__getattr__(item_name)
+
+
+	def __getattr__(self, attr_name):
+		if( self.parameters.has_key(attr_name)) :
+			return self.parameters[attr_name]
+		else:
+			raise AttributeError, attr_name
+
+
+	def getValueOrNone(self, attr_name):
+		if( self.parameters.has_key(attr_name)) :
+			return self.parameters[attr_name]
+		else:
+			return None
+
+
+	def reload(self, parameters_file_path=None):
+		if parameters_file_path == None:
+			self.parametersFilePath = LocalProperties.get(BEAMLINE_PARAMETERS_FILE_PROPERTY)
+			if(self.parametersFilePath == None):
+				raise AttributeError, "property " + BEAMLINE_PARAMETERS_FILE_PROPERTY + " is not set"
+		else:
+			self.parametersFilePath = parameters_file_path
+		self.parameters={}
+		self.parameters = readDictionaryFromFile(self.parametersFilePath, self.parameters)
+
+
+	def getValueFromObjectOrNone(self, attr_name, source):
+		if( self.parameters.has_key(attr_name)) :
+			return source.__getattr__(self.parameters[attr_name])
+		else:
+			return None
+
+
+	def saveToFile(self, file_path):
+		f = open(file_path,"w")
+		for key,item in self.parameters.items():
+			f.write(key + "=" + str(item) + "\n")
+		f.close()
 
