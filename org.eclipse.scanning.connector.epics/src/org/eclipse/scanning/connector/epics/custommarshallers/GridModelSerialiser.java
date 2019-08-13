6693b387487e2fbf007decfbf0e49d96add1d085
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2019-08-13 10:02:16 +0100

    Revert "[DAQ-1710] add vertical orientation to GridModelSerializer/Deserializer"
    This reverts commit b2e221a4384388af67924a6b227d655669367907.
    
    Reason for revert: Has bugs that break mapping perspective so not ready for release
    
    Change-Id: Ide64497630c7667040efb4021592b05b41cca185

--- org.eclipse.scanning.connector.epics/src/org/eclipse/scanning/connector/epics/custommarshallers/GridModelSerialiser.java
+++ org.eclipse.scanning.connector.epics/src/org/eclipse/scanning/connector/epics/custommarshallers/GridModelSerialiser.java
@@ -26,7 +26,6 @@ import org.epics.pvmarshaller.marshaller.serialisers.Serialiser;
 /**
  * Custom serialiser for Grid model.
  * TODO - make this non 'test' and finalise custom serialisation strategy for models
- * TODO: DAQ-2324 This class is almost certainly not used. It should be deleted.
  * @author Matt Taylor
  *
  */
@@ -38,13 +37,11 @@ public class GridModelSerialiser implements IPVStructureSerialiser<GridModel> {
 
 		Structure structure = fieldCreate.createFieldBuilder().
 			add("name", ScalarType.pvString).
-			add("xAxisName", ScalarType.pvString).
-			add("yAxisName", ScalarType.pvString).
-			add("xAxisPoints", ScalarType.pvInt).
-			add("yAxisPoints", ScalarType.pvInt).
+			add("fastAxisName", ScalarType.pvString).
+			add("slowAxisName", ScalarType.pvString).
+			add("fastAxisPoints", ScalarType.pvInt).
+			add("slowAxisPoints", ScalarType.pvInt).
 			add("snake", ScalarType.pvBoolean).
-			add("continuous", ScalarType.pvBoolean).
-			add("verticalOrientation", ScalarType.pvBoolean).
 			setId("GridModel").
 			createStructure();
 		return structure;
@@ -64,10 +61,6 @@ public class GridModelSerialiser implements IPVStructureSerialiser<GridModel> {
 		slowAxisPoints.put(model.getYAxisPoints());
 		PVBoolean snake = pvStructure.getSubField(PVBoolean.class, "snake");
 		snake.put(model.isSnake());
-		PVBoolean continuous = pvStructure.getSubField(PVBoolean.class, "continuous");
-		continuous.put(model.isContinuous());
-		PVBoolean verticalOrientation = pvStructure.getSubField(PVBoolean.class, "verticalOrientation");
-		verticalOrientation.put(model.isVerticalOrientation());
 	}
 
 }
