ec5c70688b2c050cef1abd44a54b23ff0187b9e3
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2019-08-15 10:42:34 +0100

    DAQ-2332 Update version numbers post release
    To 9.15.0 or 9.15.0pre as appropriate
    
    Change-Id: Id2aaa0905e26bc14483b9b983696851824c7f30b

--- uk.ac.gda.api/src/gda/util/Version.java
+++ uk.ac.gda.api/src/gda/util/Version.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Version {
 
-	private static final String RELEASE_VER = "9.14.0";
+	private static final String RELEASE_VER = "9.15.0pre";
 
 	/**
 	 * Returns the release version as a String
