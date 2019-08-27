9e206cdb47ec06ffd746405fdfc45622c0515aa1
Chris Kelley <chris.kelley@diamond.ac.uk>
2019-08-27 17:33:34 +0100

    Remove unused imports
    Change-Id: I31ad82e608f2711aa7dfa22b53ac2a44717f3dce

--- uk.ac.diamond.daq.server/src/uk/ac/diamond/daq/server/GDAServerApplication.java
+++ uk.ac.diamond.daq.server/src/uk/ac/diamond/daq/server/GDAServerApplication.java
@@ -2,8 +2,6 @@ package uk.ac.diamond.daq.server;
 
 import static uk.ac.diamond.daq.server.configuration.IGDAConfigurationService.ServerType.LOG;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.ServerSocket;
