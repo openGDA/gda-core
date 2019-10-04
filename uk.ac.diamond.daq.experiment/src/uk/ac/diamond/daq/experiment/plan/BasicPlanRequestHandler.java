5ed8ba60eefd90ffe9e9564be3a7f67fa755e0a4
Douglas Winter <douglas.winter@diamond.ac.uk>
2019-10-04 13:31:13 +0100

    Plan Request Handler now submits job to ExecutorService
    In a previous, unrelated commit this was erroneously changed so that the
    plan was started in the same thread. Though the current implementation
    of IPlan::start is nonblocking, the same is not necessarily true for
    implementations of experiment drivers.
    
    Change-Id: I66a4b2a4d1bed5266fd92b3f0e200192081998c1

--- uk.ac.diamond.daq.experiment/src/uk/ac/diamond/daq/experiment/plan/BasicPlanRequestHandler.java
+++ uk.ac.diamond.daq.experiment/src/uk/ac/diamond/daq/experiment/plan/BasicPlanRequestHandler.java
@@ -1,7 +1,10 @@
 package uk.ac.diamond.daq.experiment.plan;
 
+import java.util.concurrent.ExecutorService;
+
 import gda.device.DeviceException;
 import gda.factory.FindableBase;
+import uk.ac.diamond.daq.concurrent.ExecutorFactory;
 import uk.ac.diamond.daq.experiment.api.plan.IPlan;
 import uk.ac.diamond.daq.experiment.api.remote.PlanRequest;
 import uk.ac.diamond.daq.experiment.api.remote.PlanRequestHandler;
@@ -10,11 +13,13 @@ import uk.ac.gda.api.remoting.ServiceInterface;
 @ServiceInterface(PlanRequestHandler.class)
 public class BasicPlanRequestHandler extends FindableBase implements PlanRequestHandler {
 	
+	private ExecutorService executor = ExecutorFactory.singleThread();
+	
 	@Override
 	public void submit(PlanRequest planRequest) throws DeviceException {
 		PlanRequestParser planRequestParser = new PlanRequestParser();
 		IPlan plan = planRequestParser.parsePlanRequest(planRequest);
-		plan.start();
+		executor.submit(plan::start);
 	}
 
 }
