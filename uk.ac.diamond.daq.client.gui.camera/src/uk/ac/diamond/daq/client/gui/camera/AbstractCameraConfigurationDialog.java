81eacaf5999778168d840fd05b738652fd148139
Maurizio Nagni <maurizio.nagni@diamond.ac.uk>
2019-12-05 14:16:01 +0000

    DAQ-2596 CameraConfigurationView - Absorption Tool
    Implements absorption tool following K11 feedback.
    Calculating the ratio of two ROI intensities
    
    Change-Id: I3d7e6ec772aa50ef269edee1d6db1ebf37128490

--- uk.ac.diamond.daq.client.gui.camera/src/uk/ac/diamond/daq/client/gui/camera/AbstractCameraConfigurationDialog.java
+++ uk.ac.diamond.daq.client.gui.camera/src/uk/ac/diamond/daq/client/gui/camera/AbstractCameraConfigurationDialog.java
@@ -9,8 +9,13 @@ import org.eclipse.jface.layout.RowLayoutFactory;
 import org.eclipse.scanning.api.event.core.IConnection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Sash;
 import org.eclipse.swt.widgets.Shell;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +29,14 @@ import uk.ac.diamond.daq.client.gui.camera.liveview.LiveViewCompositeFactory;
 import uk.ac.gda.ui.tool.ClientMessages;
 import uk.ac.gda.ui.tool.ClientSWTElements;
 
+/**
+ * Composes the overall layout for the Camera Configuration 
+ * 
+ * @author Eliot Hall
+ * @author Maurizio Nagni
+ *
+ * @param <T>
+ */
 public abstract class AbstractCameraConfigurationDialog<T extends AbstractCameraConfigurationController> {
 
 	private static final int BUTTON_WIDTH = 80;
@@ -33,6 +46,11 @@ public abstract class AbstractCameraConfigurationDialog<T extends AbstractCamera
 	protected final T controller;
 	private IConnection liveStreamConnection;
 	protected CameraImageComposite cameraImageComposite;
+
+	private Composite internalArea;
+	private Sash sash;
+	private Composite topArea;
+	private Composite bottomArea;
 
 	private static final Logger logger = LoggerFactory.getLogger(AbstractCameraConfigurationDialog.class);
 
@@ -46,28 +64,84 @@ public abstract class AbstractCameraConfigurationDialog<T extends AbstractCamera
 			this.shell = Shell.class.cast(composite);
 			this.parent = null;
 			this.controller = controller;
-			GridLayoutFactory.fillDefaults().applyTo(shell);			
+			GridLayoutFactory.fillDefaults().applyTo(shell);
 		} else {
 			this.shell = composite.getShell();
 			this.parent = composite;
-			this.controller = controller;	
+			this.controller = controller;
 		}
 	}
 
-	public void createComposite(boolean closable) throws DeviceException {
+//	public void createComposite(boolean closable) throws DeviceException {
+//		Composite intParent = getParent() == null ? getShell() : getParent();
+//		UUID uuid = UUID.randomUUID();
+//		intParent.setData(CompositeFactory.COMPOSITE_ROOT, uuid);
+//		intParent.setBackground(intParent.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+//
+//		CompositeFactory cf = new LiveViewCompositeFactory<>(getController());
+//		cf.createComposite(intParent, SWT.NONE);
+//
+//		cf = createTabFactory();
+//		Composite tabs = cf.createComposite(intParent, SWT.NONE);
+//		CTabFolder cTab = CTabFolder.class.cast(tabs.getData(TabFolderBuilder.CTAB_FOLDER));
+//		GridDataFactory.fillDefaults().grab(true, false).applyTo(cTab);
+//		createLoadSaveComposite(intParent, closable);
+//	}
+
+	public Composite createComposite(boolean closable) throws DeviceException {
 		Composite intParent = getParent() == null ? getShell() : getParent();
 		UUID uuid = UUID.randomUUID();
 		intParent.setData(CompositeFactory.COMPOSITE_ROOT, uuid);
 		intParent.setBackground(intParent.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 
-		CompositeFactory cf = new LiveViewCompositeFactory<>(getController());
-		cf.createComposite(intParent, SWT.NONE);
-
+		internalArea = ClientSWTElements.createComposite(intParent, SWT.NONE, 1);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(internalArea);
+		sash = new Sash(internalArea, SWT.HORIZONTAL);
+		
+		CompositeFactory cf = new LiveViewCompositeFactory();
+		topArea = cf.createComposite(internalArea, SWT.NONE);
+		
 		cf = createTabFactory();
-		Composite tabs = cf.createComposite(intParent, SWT.NONE);
-		CTabFolder cTab = CTabFolder.class.cast(tabs.getData(TabFolderBuilder.CTAB_FOLDER));
+		bottomArea = cf.createComposite(internalArea, SWT.NONE);
+		CTabFolder cTab = CTabFolder.class.cast(bottomArea.getData(TabFolderBuilder.CTAB_FOLDER));
+
+		final FormLayout form = new FormLayout();
+		internalArea.setLayout(form);
+
+		FormData topAreaData = new FormData();
+		topAreaData.left = new FormAttachment(0, 0);
+		topAreaData.right = new FormAttachment(100, 0);
+		topAreaData.top = new FormAttachment(0, 0);
+		topAreaData.bottom = new FormAttachment(sash, 0);
+		topArea.setLayoutData(topAreaData);
+
+		final int maxPercent = 80;
+		final FormData sashData = new FormData();
+		sashData.left = new FormAttachment(0, 0);
+		sashData.right = new FormAttachment(100, 0);
+		sashData.top = new FormAttachment(maxPercent, 0);
+		sash.setLayoutData(sashData);
+		
+		FormData bottomAreaData = new FormData();
+		bottomAreaData.top = new FormAttachment(sash, 0);
+		bottomAreaData.bottom = new FormAttachment(100, 0);
+		bottomAreaData.left = new FormAttachment(0, 0);
+		bottomAreaData.right = new FormAttachment(100, 0);
+		bottomArea.setLayoutData(bottomAreaData);
+
+		sash.addListener(SWT.Selection, e -> {
+			Rectangle sashRect = sash.getBounds();
+			Rectangle shellRect = internalArea.getClientArea();
+			int newPercent = (int)(((double)sashRect.y)/((double)shellRect.height)*100);
+			if (e.y != sashRect.y) {
+				sashData.top = new FormAttachment(Math.min(maxPercent, newPercent), 0);
+				internalArea.layout(true, true);
+				bottomArea.layout(true, true);				
+			}
+		});
+
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(cTab);
-		createLoadSaveComposite(intParent, closable);
+		return internalArea;
 	}
 
 	public T getController() {
--- uk.ac.diamond.daq.client.gui.camera/src/uk/ac/diamond/daq/client/gui/camera/liveview/CameraImageComposite.java
+++ uk.ac.diamond.daq.client.gui.camera/src/uk/ac/diamond/daq/client/gui/camera/liveview/CameraImageComposite.java
@@ -1,16 +1,6 @@
 package uk.ac.diamond.daq.client.gui.camera.liveview;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.dawnsci.analysis.api.roi.IRectangularROI;
-import org.eclipse.dawnsci.analysis.dataset.roi.RectangularROI;
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
-import org.eclipse.dawnsci.plotting.api.region.IROIListener;
-import org.eclipse.dawnsci.plotting.api.region.ROIEvent;
-import org.eclipse.dawnsci.plotting.api.trace.ITrace;
-import org.eclipse.january.dataset.Dataset;
-import org.eclipse.january.dataset.DatasetUtils;
-import org.eclipse.january.dataset.IndexIterator;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
@@ -20,134 +10,23 @@ import org.slf4j.LoggerFactory;
 
 import com.swtdesigner.SWTResourceManager;
 
-import uk.ac.diamond.daq.client.gui.camera.controller.AbstractCameraConfigurationController;
-import uk.ac.diamond.daq.client.gui.camera.controller.CameraConfigurationAdapter;
-import uk.ac.diamond.daq.client.gui.camera.controller.CameraConfigurationMode;
+import uk.ac.diamond.daq.client.gui.camera.listener.ROIListener;
 import uk.ac.gda.client.exception.GDAClientException;
-import uk.ac.gda.client.live.stream.LiveStreamException;
-import uk.ac.gda.client.live.stream.handlers.SnapshotData;
 import uk.ac.gda.client.live.stream.view.LivePlottingComposite;
 
 /**
+ * Instantiates the elements for the CameraConfiguration top area  
  * 
- * 
- * @author Eliot Hall
  * @author Maurzio Nagni
  */
 public class CameraImageComposite extends Composite {
-	private static final Logger log = LoggerFactory.getLogger(CameraImageComposite.class);
+	private static final Logger logger = LoggerFactory.getLogger(CameraImageComposite.class);
 
-	private AbstractCameraConfigurationController controller;
 	private LivePlottingComposite plottingComposite;
 	private IPlottingSystem<Composite> plottingSystem;
-	private boolean frozen = false;
 
-	private SnapshotData lastSnapshot;
-
-	private DrawableRegion roiSelectionRegion;
-	private DrawableRegion highFluxRegion;
-	private DrawableRegion lowFluxRegion;
-
-	private class ROIListener implements IROIListener {
-		@Override
-		public void roiDragged(ROIEvent event) {
-			// Do nothing
-		}
-
-		@Override
-		public void roiChanged(ROIEvent event) {
-			RectangularROI roi = ((RectangularROI) event.getROI().getBounds()).copy();
-			RectangularROI currentRoi = controller.getCurrentRoi();
-
-			roi.setPoint(roi.getIntPoint()[0] + currentRoi.getIntPoint()[0],
-					roi.getIntPoint()[1] + currentRoi.getIntPoint()[1]);
-			controller.setROI(roi);
-		}
-
-		@Override
-		public void roiSelected(ROIEvent event) {
-			// do nothing
-		}
-	}
-
-	private class RegionListener implements IROIListener {
-		@Override
-		public void roiDragged(ROIEvent event) {
-			// do nothing
-		}
-
-		@Override
-		public void roiChanged(ROIEvent evt) {
-			calculateRatio();
-		}
-
-		@Override
-		public void roiSelected(ROIEvent evt) {
-			// do nothing
-		}
-
-	}
-
-	public class ModeListener extends CameraConfigurationAdapter {
-		private void refreshSnapshot(boolean reconnect) throws Exception {
-			if (reconnect) {
-				plottingComposite.activatePlottingSystem();
-			}
-			ITrace liveTrace = plottingComposite.getITrace();
-			lastSnapshot = new SnapshotData("Adsorption Snapshot", liveTrace.getData().clone());
-			plottingComposite.disconnect();
-			plottingSystem.clear();
-			plottingSystem.createPlot2D(lastSnapshot.getDataset(), null, "Snap!", new NullProgressMonitor());
-			plottingSystem.setTitle(lastSnapshot.getTitle());
-		}
-
-		@Override
-		public void refreshSnapshot() {
-			try {
-				refreshSnapshot(true);
-				calculateRatio();
-			} catch (Exception e) {
-				log.error("Unable to open connection", e);
-			}
-		}
-
-		@Override
-		public void setCameraConfigurationMode(CameraConfigurationMode cameraConfigurationMode) {
-			try {
-				if (cameraConfigurationMode == CameraConfigurationMode.absorption && !frozen) {
-					refreshSnapshot(false);
-					frozen = true;
-
-					roiSelectionRegion.setActive(false);
-
-					highFluxRegion.setActive(true);
-					lowFluxRegion.setActive(true);
-					calculateRatio();
-				} else if (cameraConfigurationMode == CameraConfigurationMode.exposure && frozen) {
-					plottingComposite.activatePlottingSystem();
-					frozen = false;
-
-					highFluxRegion.setActive(false);
-					lowFluxRegion.setActive(false);
-
-					roiSelectionRegion.setActive(true);
-				}
-			} catch (LiveStreamException e) {
-				MessageDialog.openError(plottingComposite.getShell(), "Imaging Camera",
-						"Error connecting to imaging camera");
-				log.error("Error connecting to imaging camera", e);
-			} catch (Exception e) {
-				log.error("Error recreating ROIs", e);
-			}
-		}
-	}
-
-	public CameraImageComposite(Composite parent, AbstractCameraConfigurationController controller, int style) throws GDAClientException {
+	public CameraImageComposite(Composite parent, int style) throws GDAClientException {
 		super(parent, style);
-		this.controller = controller;
-		ModeListener modeListener = new ModeListener();
-		controller.addListener(modeListener);
-
 		GridLayoutFactory.fillDefaults().numColumns(1).applyTo(this);
 
 		plottingComposite = new LivePlottingComposite(this, SWT.NONE, "Live View", null);
@@ -155,60 +34,14 @@ public class CameraImageComposite extends Composite {
 		plottingSystem = plottingComposite.getPlottingSystem();
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(plottingComposite);
 
-		roiSelectionRegion = new DrawableRegion(plottingSystem, SWTResourceManager.getColor(SWT.COLOR_GREEN), "ROI",
-				new ROIListener());
-		highFluxRegion = new DrawableRegion(plottingSystem, SWTResourceManager.getColor(SWT.COLOR_RED), "High Flux",
-				new RegionListener());
-		lowFluxRegion = new DrawableRegion(plottingSystem, SWTResourceManager.getColor(SWT.COLOR_BLUE), "Low Flux",
-				new RegionListener());
-
+		DrawableRegion roiSelectionRegion = new DrawableRegion(plottingSystem, SWTResourceManager.getColor(SWT.COLOR_GREEN), "ROI",
+		new ROIListener(this, plottingComposite));		
 		roiSelectionRegion.setActive(true);
-
-		addListener(SWT.Dispose, e -> {
-			plottingComposite.disconnect();
-			controller.removeListener(modeListener);
-		});
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(this);
+		logger.debug("CameraImageComposite created");
 	}
 
 	public IPlottingSystem<Composite> getPlottingSystem() {
 		return plottingSystem;
-	}
-
-	private int getRawData(DrawableRegion regionWrapper) {
-		IRectangularROI roi = regionWrapper.getRegion();
-		int[] xy = roi.getIntPoint();
-		int[] length = roi.getIntLengths();
-
-		int[] start = new int[] { xy[0], xy[1] };
-		int[] end = new int[] { xy[0] + length[0], xy[1] + length[1] };
-		int[] step = new int[] { 1, 1 };
-
-		Dataset dataset = DatasetUtils.convertToDataset(lastSnapshot.getDataset().getSliceView(start, end, step));
-
-		double val = 0;
-		int count = 0;
-		IndexIterator iterator = dataset.getIterator();
-		while (iterator.hasNext()) {
-			double value = dataset.getElementDoubleAbs(iterator.index);
-			if (!Double.isNaN(value)) {
-				val += value;
-				count++;
-			}
-		}
-		if (count == 0) {
-			count = 1;
-		}
-		return (int) Math.round(val / count);
-	}
-
-	private void calculateRatio() {
-		if (lowFluxRegion.getRegion() == null || highFluxRegion.getRegion() == null || lastSnapshot == null) {
-			return;
-		}
-
-		int low = getRawData(lowFluxRegion);
-		int high = getRawData(highFluxRegion);
-		controller.calculateRatio(high, low);
 	}
 }
