40429d98cbdc923c461934f0d85f0bea821a258d
Peter Holloway <peter.holloway@diamond.ac.uk>
2020-02-10 12:44:09 +0000

    Move ElogEntry static methods to top of class
    Mixing them in with the instance methods was confusing.
    
    Change-Id: Iff50894c0a5148bd6263ee1975424bfa0bf4efd0

--- uk.ac.gda.core/src/gda/util/ElogEntry.java
+++ uk.ac.gda.core/src/gda/util/ElogEntry.java
@@ -64,6 +64,128 @@ public class ElogEntry {
 	private String logID;
 	private String groupID;
 
+	/**
+	 * Creates an ELog entry. Default ELog server is "http://rdb.pri.diamond.ac.uk/devl/php/elog/cs_logentryext_bl.php"
+	 * which is the development database. "http://rdb.pri.diamond.ac.uk/php/elog/cs_logentryext_bl.php" is the
+	 * production database. The java.properties file contains the property "gda.elog.targeturl" which can be set to be
+	 * either the development or production databases.
+	 *
+	 * @param title
+	 *            The ELog title
+	 * @param content
+	 *            The ELog content
+	 * @param userID
+	 *            The user ID e.g. epics or gda or abc12345
+	 * @param visit
+	 *            The visit number
+	 * @param logID
+	 *            The type of log book, The log book ID: Beam Lines: - BLB16, BLB23, BLI02, BLI03, BLI04, BLI06, BLI11,
+	 *            BLI16, BLI18, BLI19, BLI22, BLI24, BLI15, DAG = Data Acquisition, EHC = Experimental Hall
+	 *            Coordinators, OM = Optics and Meteorology, OPR = Operations, E
+	 * @param groupID
+	 *            The group sending the ELog, DA = Data Acquisition, EHC = Experimental Hall Coordinators, OM = Optics
+	 *            and Meteorology, OPR = Operations CS = Control Systems, GroupID Can also be a beam line,
+	 * @param fileLocations
+	 *            The image file names with path to upload
+	 * @throws ELogEntryException
+	 */
+	public static void post(String title, String content, String userID, String visit, String logID, String groupID,
+			String[] fileLocations) throws ELogEntryException {
+		String targetURL = POST_UPLOAD_URL;
+		try {
+			String entryType = "41";// entry type is always a log (41)
+			String titleForPost = visit == null ? title : "Visit: " + visit + " - " + title;
+
+			MultipartEntityBuilder request = MultipartEntityBuilder.create()
+					.addTextBody("txtTITLE", titleForPost)
+					.addTextBody("txtCONTENT", content)
+					.addTextBody("txtLOGBOOKID", logID)
+					.addTextBody("txtGROUPID", groupID)
+					.addTextBody("txtENTRYTYPEID", entryType)
+					.addTextBody("txtUSERID", userID);
+
+			if(fileLocations != null){
+				for (int i = 1; i < fileLocations.length + 1; i++) {
+					File targetFile = new File(fileLocations[i - 1]);
+					request = request.addBinaryBody("userfile" + i,
+							targetFile,
+							ContentType.create("image/png"),
+							targetFile.getName());
+				}
+			}
+
+			HttpEntity entity = request.build();
+			targetURL  = LocalProperties.get("gda.elog.targeturl", POST_UPLOAD_URL);
+			HttpPost httpPost = new HttpPost(targetURL);
+			httpPost.setEntity(entity);
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			CloseableHttpResponse response = httpClient.execute(httpPost);
+
+			try {
+				String responseString = EntityUtils.toString(response.getEntity());
+				System.out.println(responseString);
+				if (!responseString.contains("New Log Entry ID")) {
+					throw new ELogEntryException("Upload failed, status=" + response.getStatusLine().getStatusCode()
+						+ " response="+responseString
+						+ " targetURL = " + targetURL
+						+ " titleForPost = " + titleForPost
+						+ " logID = " + logID
+						+ " groupID = " + groupID
+						+ " entryType = " + entryType
+						+ " userID = " + userID);
+				}
+			} finally {
+				response.close();
+				httpClient.close();
+			}
+		} catch (ELogEntryException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new ELogEntryException("Error in ELogger.  Database:" + targetURL, e);
+		}
+	}
+
+	/**
+	 * Async version of post @see ElogEntry.post
+	 *
+	 * @param title
+	 * @param content
+	 * @param userID
+	 * @param visit
+	 * @param logID
+	 * @param groupID
+	 * @param fileLocations
+	 */
+	public static void postAsyn(final String title, final String content, final String userID, final String visit,
+			final String logID, final String groupID, final String[] fileLocations) {
+
+		Async.execute(() -> {
+				try {
+					ElogEntry.post(title, content, userID, visit, logID, groupID, fileLocations);
+				} catch (Exception e) {
+					logger.error("Error posting ElogEntry", e);
+				}},
+				"ElogEntry: %s", title);
+	}
+
+	/**
+	 * Create a logger event of the class gda.util.ElogEntry which can be used
+	 * as a filter in logback configuration
+	 * The resultant message is of the form visit + "%%" + title + "%%" + content
+	 * which is understood by the associated ELogAppender class.
+	 * @param title
+	 * @param content
+	 */
+	public static void postViaLogger(String title, String content) {
+		String visit;
+		try {
+			visit = GDAMetadataProvider.getInstance().getMetadataValue("visit");
+		} catch (DeviceException e) {
+			visit = "unknown";
+		}
+		logger.info("{}%%{}%%{}", visit, title, content);
+	}
+
 	public ElogEntry(String title, String userID, String visit, String logID,
 			String groupID) {
 		this.parts = new ArrayList<ElogEntry.PostPart>();
@@ -255,128 +377,6 @@ public class ElogEntry {
 		}
 	}
 
-
-	/**
-	 * Creates an ELog entry. Default ELog server is "http://rdb.pri.diamond.ac.uk/devl/php/elog/cs_logentryext_bl.php"
-	 * which is the development database. "http://rdb.pri.diamond.ac.uk/php/elog/cs_logentryext_bl.php" is the
-	 * production database. The java.properties file contains the property "gda.elog.targeturl" which can be set to be
-	 * either the development or production databases.
-	 *
-	 * @param title
-	 *            The ELog title
-	 * @param content
-	 *            The ELog content
-	 * @param userID
-	 *            The user ID e.g. epics or gda or abc12345
-	 * @param visit
-	 *            The visit number
-	 * @param logID
-	 *            The type of log book, The log book ID: Beam Lines: - BLB16, BLB23, BLI02, BLI03, BLI04, BLI06, BLI11,
-	 *            BLI16, BLI18, BLI19, BLI22, BLI24, BLI15, DAG = Data Acquisition, EHC = Experimental Hall
-	 *            Coordinators, OM = Optics and Meteorology, OPR = Operations, E
-	 * @param groupID
-	 *            The group sending the ELog, DA = Data Acquisition, EHC = Experimental Hall Coordinators, OM = Optics
-	 *            and Meteorology, OPR = Operations CS = Control Systems, GroupID Can also be a beam line,
-	 * @param fileLocations
-	 *            The image file names with path to upload
-	 * @throws ELogEntryException
-	 */
-	public static void post(String title, String content, String userID, String visit, String logID, String groupID,
-			String[] fileLocations) throws ELogEntryException {
-		String targetURL = POST_UPLOAD_URL;
-		try {
-			String entryType = "41";// entry type is always a log (41)
-			String titleForPost = visit == null ? title : "Visit: " + visit + " - " + title;
-
-			MultipartEntityBuilder request = MultipartEntityBuilder.create()
-					.addTextBody("txtTITLE", titleForPost)
-					.addTextBody("txtCONTENT", content)
-					.addTextBody("txtLOGBOOKID", logID)
-					.addTextBody("txtGROUPID", groupID)
-					.addTextBody("txtENTRYTYPEID", entryType)
-					.addTextBody("txtUSERID", userID);
-
-			if(fileLocations != null){
-				for (int i = 1; i < fileLocations.length + 1; i++) {
-					File targetFile = new File(fileLocations[i - 1]);
-					request = request.addBinaryBody("userfile" + i,
-							targetFile,
-							ContentType.create("image/png"),
-							targetFile.getName());
-				}
-			}
-
-			HttpEntity entity = request.build();
-			targetURL  = LocalProperties.get("gda.elog.targeturl", POST_UPLOAD_URL);
-			HttpPost httpPost = new HttpPost(targetURL);
-			httpPost.setEntity(entity);
-			CloseableHttpClient httpClient = HttpClients.createDefault();
-			CloseableHttpResponse response = httpClient.execute(httpPost);
-
-			try {
-				String responseString = EntityUtils.toString(response.getEntity());
-				System.out.println(responseString);
-				if (!responseString.contains("New Log Entry ID")) {
-					throw new ELogEntryException("Upload failed, status=" + response.getStatusLine().getStatusCode()
-						+ " response="+responseString
-						+ " targetURL = " + targetURL
-						+ " titleForPost = " + titleForPost
-						+ " logID = " + logID
-						+ " groupID = " + groupID
-						+ " entryType = " + entryType
-						+ " userID = " + userID);
-				}
-			} finally {
-				response.close();
-				httpClient.close();
-			}
-		} catch (ELogEntryException e) {
-			throw e;
-		} catch (Exception e) {
-			throw new ELogEntryException("Error in ELogger.  Database:" + targetURL, e);
-		}
-	}
-
-	/**
-	 * Async version of post @see ElogEntry.post
-	 *
-	 * @param title
-	 * @param content
-	 * @param userID
-	 * @param visit
-	 * @param logID
-	 * @param groupID
-	 * @param fileLocations
-	 */
-	public static void postAsyn(final String title, final String content, final String userID, final String visit,
-			final String logID, final String groupID, final String[] fileLocations) {
-
-		Async.execute(() -> {
-				try {
-					ElogEntry.post(title, content, userID, visit, logID, groupID, fileLocations);
-				} catch (Exception e) {
-					logger.error("Error posting ElogEntry", e);
-				}},
-				"ElogEntry: %s", title);
-	}
-
-	/**
-	 * Create a logger event of the class gda.util.ElogEntry which can be used
-	 * as a filter in logback configuration
-	 * The resultant message is of the form visit + "%%" + title + "%%" + content
-	 * which is understood by the associated ELogAppender class.
-	 * @param title
-	 * @param content
-	 */
-	public static void postViaLogger(String title, String content) {
-		String visit;
-		try {
-			visit = GDAMetadataProvider.getInstance().getMetadataValue("visit");
-		} catch (DeviceException e) {
-			visit = "unknown";
-		}
-		logger.info("{}%%{}%%{}", visit, title, content);
-	}
 
 	public void setTitle(String title) {
 		this.title = title;
