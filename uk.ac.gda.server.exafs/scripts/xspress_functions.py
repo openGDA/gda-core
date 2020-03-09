431e054dfa3431bbb6d2095d989ebf8d56dbb94d
Iain Hall <iain.hall@diamond.ac.uk>
2020-03-09 10:37:39 +0000

    DAQ-2816 : Jython functions for setting up Xspress3/Xspress4 detectors
    Functions for setting up Xspress3/4 detectors :
     - Setup detector with TTL trigger mode and and set the data dimensions if
     they have not been set (by collecting a frame of software triggered data).
     - Collect software triggered frame
     - Set SWMR mode hdf options
    
    Change-Id: I760e30d5c3a01317dde7c89c958598958ca4c5b8

---
+++ uk.ac.gda.server.exafs/scripts/xspress_functions.py
@@ -0,0 +1,69 @@
+from gda.epics import CAClient
+from time import sleep
+
+def caput(basePv, ext, value) :
+    fullPv = basePv + ext
+    print "caput %s = %s"%(fullPv, value)
+    caclient = CAClient()
+    caclient.caput(fullPv, value, 10.0)
+    
+def caget(pvName) :
+    print "caget %s"%(pvName)
+    caclient = CAClient()
+    value = caclient.caget(pvName)
+    print "value = %s"%(value)
+    return value
+
+def set_hdf_input_port(basePvName, arrayPort):
+    print "Setting hdf input"
+    caput(basePvName, ":HDF5:NDArrayPort", arrayPort)
+
+def setup_xspress_detector(basePvName):
+    """ Set up an xspress3, xspress4 detector to get it ready for doing scans where each point in the scan is
+    externally triggered (e.g. by a Tfg) :
+     1) The trigger mode is set to 'TTL Veto Only' 
+     2) If needed, the data dimensions of the Hdf writer (and other plugins) are set by collecting a
+     single software triggered frame of data.
+    """
+    print "Setting up XSpress detector : ", basePvName
+
+    print "  Trigger mode = 'TTL Veto Only'"
+    set_trigger_mode(basePvName, 3) # TTL veto trigger mode
+
+    numDataDimensions = caget(basePvName + ":HDF5:NDimensions_RBV")
+    if int(numDataDimensions) == 0 :
+        print "  Data dimensions are not yet known - collecting one frame of internal triggered data..."
+        collect_software_triggered_frame(basePvName, 1.0)
+
+def set_trigger_mode(basePvName, triggerMode) :
+    caput(basePvName, ":CAM:TriggerMode", triggerMode) # TTL veto trigger mode
+
+def collect_software_triggered_frame(basePvName, collectionTime) :
+    """Collect one frame of software (internal) triggered data on the detector.
+    The original trigger mode is restored after the frame has been collected.
+    """
+    baseCamPvName = basePvName+":CAM"
+    print "Collecting 1 internal triggered frame of data from detector %s"%(basePvName)
+    origTrigger = caget(baseCamPvName+":TriggerMode")
+    caput(baseCamPvName, ":NumImages", 1) # number of framews
+    caput(baseCamPvName, ":ImageMode", 0) # single image mode  
+    set_trigger_mode(basePvName, 0) # internal trigger mode
+    caput(baseCamPvName, ":AcquireTime", collectionTime) # collection time
+    print "Starting detector ..."
+    caput(baseCamPvName, ":Acquire", 1) # start the detector
+
+    #restore the trigger mode
+    print "Finished - restoring trigger mode"
+    set_trigger_mode(basePvName, origTrigger)
+
+def setup_swmr(basePvName, switchOn, numFramesFlush, ndAttributeFlush):
+    """ Set SWMR mode hdf options : switch SWMR mode on, num frame flush, attribute chunk
+    """
+    print "Setting SWMR hdf options for detector %s"%(basePvName)
+
+    print "  HDF5 SWMR mode = %s"%(switchOn)
+    caput(basePvName, ":HDF5:SWMRMode", switchOn) # Set SWMR mode on.
+
+    print "  HDF5 SWMR : Flush on nth frame = %d, NDAttribute flush = %d"%(numFramesFlush, ndAttributeFlush)
+    caput(basePvName, ":HDF5:NumFramesFlush", numFramesFlush)
+    caput(basePvName, ":HDF5:NDAttributeChunk", ndAttributeFlush)
