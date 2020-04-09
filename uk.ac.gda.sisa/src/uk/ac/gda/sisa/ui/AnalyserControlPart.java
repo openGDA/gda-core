2f9012f96f8cf4f2171b0f999e8a767a29027786
Tom Richardson <tom.richardson@diamond.ac.uk>
2020-04-09 16:33:17 +0100

    I09-288 Refactor I09 Alignment Controls View to use Live Controls from config
    Change-Id: Ibeac5a860af12f1eb6c1f3df87b877ca55d36c04

--- uk.ac.gda.sisa/src/uk/ac/gda/sisa/ui/AnalyserControlPart.java
+++ uk.ac.gda.sisa/src/uk/ac/gda/sisa/ui/AnalyserControlPart.java
@@ -18,8 +18,6 @@
 
 package uk.ac.gda.sisa.ui;
 
-import java.util.Arrays;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -33,22 +31,22 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.swtdesigner.SWTResourceManager;
 
-import gda.device.Scannable;
 import gda.factory.Finder;
-import gda.rcp.views.NudgePositionerComposite;
+import uk.ac.gda.api.camera.CameraControl;
+import uk.ac.gda.api.camera.ImageMode;
+import uk.ac.gda.client.livecontrol.ControlSet;
+import uk.ac.gda.client.livecontrol.LiveControl;
 import uk.ac.gda.devices.vgscienta.IVGScientaAnalyserRMI;
 
 public class AnalyserControlPart {
@@ -58,34 +56,35 @@ public class AnalyserControlPart {
 	final Color green = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GREEN);
 
 	private Composite parent;
-
 	private Composite child;
-	private Combo lensModeCombo;
-	private Combo passEnergyCombo;
-	private Combo elementCombo;
-	private Text centreEnergyText;
-
+	
+	Group analyserGroup;
+	
+	private AlignmentControls alignmentControls;
 	private IVGScientaAnalyserRMI analyser;
+	private CameraControl eavCameraControl;
 
 	@Inject
 	public AnalyserControlPart() {
 		logger.trace("Constructor called");
 
-		// Get an analyser
-		List<IVGScientaAnalyserRMI> analysers = Finder.getInstance().listLocalFindablesOfType(IVGScientaAnalyserRMI.class);
-		if (analysers.size() != 1) {
-			String msg = "No Analyser was found! (Or more than 1)";
+		try {
+			alignmentControls = Finder.getInstance().findSingleton(AlignmentControls.class);
+		} catch (IllegalArgumentException exception) {
+			String msg = "No AlignmentControls was found! (Or more than 1)";
 			logger.error(msg);
 			throw new RuntimeException(msg);
 		}
-		analyser = analysers.get(0);
-		logger.info("Connected to analyser {}", analyser);
+		
+		analyser = alignmentControls.getAnalyser();
+		eavCameraControl = alignmentControls.getAnalyserEavControl();
 	}
 
 	@PostConstruct
-	public void postConstruct(Composite parent) {
+	public void postConstruct(Composite parent) {				
 		logger.trace("postConstruct called");
 		this.parent = parent;
+		
 		ScrolledComposite scrollComp = new ScrolledComposite(parent, SWT.V_SCROLL);
 		scrollComp.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
 
@@ -94,237 +93,102 @@ public class AnalyserControlPart {
 
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(child);
 		GridLayoutFactory.swtDefaults().numColumns(1).applyTo(child);
-
-		// Analyser group
-		Group analyserGroup = new Group(child, SWT.NONE);
+		
+		createAnalyserControlsGroup();
+		addLiveControls();
+						
+		// Set the child as the scrolled content of the ScrolledComposite
+		scrollComp.setContent(child);
+		scrollComp.setExpandHorizontal(true);
+		scrollComp.setExpandVertical(true);
+		scrollComp.setMinSize(child.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	}
+	
+	private void createAnalyserControlsGroup() {
+		analyserGroup = new Group(child, SWT.NONE);
 		analyserGroup.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		GridLayoutFactory.swtDefaults().numColumns(4).spacing(10, 20).applyTo(analyserGroup);
-
-		// Lens mode
-		Label lensModeLabel = new Label(analyserGroup, SWT.NONE);
-		setTextToBold(lensModeLabel);
-		lensModeLabel.setText("Lens Mode");
-		GridDataFactory.swtDefaults().align(SWT.BEGINNING, SWT.CENTER).grab(false, true).applyTo(lensModeLabel);
-
-		lensModeCombo = new Combo(analyserGroup, SWT.READ_ONLY);
-		lensModeCombo.setToolTipText("Analyser Lens Mode");
-		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.CENTER).applyTo(lensModeCombo);
-		// Setup lens modes and select currently selected one
-		try {
-			lensModeCombo.setItems(analyser.getLensModes().toArray(new String[0]));
-			String activeLensMode = analyser.getLensMode();
-			lensModeCombo.select(Arrays.asList(lensModeCombo.getItems()).indexOf(activeLensMode));
-		} catch (Exception ex) {
-			logger.error("Failed to get list of lens modes", ex);
-		}
-
-		lensModeCombo.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				logger.info("Changing analyser lens mode to " + lensModeCombo.getText());
-				try {
-					analyser.setLensMode(lensModeCombo.getText());
-				} catch (Exception ex) {
-					logger.error("Failed to change lens mode", ex);
-				}
-				updatePassEnergyCombo();
-			}
-		});
-
-		// Analyser pass energy
-		Label passEnergyLabel = new Label(analyserGroup, SWT.NONE);
-		setTextToBold(passEnergyLabel);
-		passEnergyLabel.setText("Pass Energy");
-		GridDataFactory.swtDefaults().align(SWT.BEGINNING, SWT.CENTER).grab(false, true).applyTo(passEnergyLabel);
-
-		passEnergyCombo = new Combo(analyserGroup, SWT.READ_ONLY);
-		// Call update to setup passEnergyCombo
-		updatePassEnergyCombo();
-		passEnergyCombo.setToolTipText("Select a pass energy");
-
-		// Add listener to update analyser pass energy when changed
-		passEnergyCombo.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				logger.info("Changing analyser pass energy to {}", passEnergyCombo.getText());
-				try {
-					analyser.setPassEnergy(Integer.parseInt(passEnergyCombo.getText()));
-				} catch (Exception ex) {
-					logger.error("Failed to set pass energy", ex);
-				}
-			}
-		});
-
-		// Element control
-		Label elementLabel = new Label(analyserGroup, SWT.NONE);
-		setTextToBold(elementLabel);
-		elementLabel.setText("Element");
-		GridDataFactory.swtDefaults().align(SWT.BEGINNING, SWT.CENTER).grab(false, true).applyTo(elementLabel);
+		GridLayoutFactory.swtDefaults().numColumns(3).spacing(10, 20).applyTo(analyserGroup);
 		
-		elementCombo = new Combo(analyserGroup, SWT.READ_ONLY);
-		updateElementCombo();
-		elementCombo.setToolTipText("Select an element (PSU mode)");
-		elementCombo.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				String selectedPsuMode = elementCombo.getText();
-				logger.info("Changing element set to {}", selectedPsuMode);
-				try {
-					analyser.setPsuMode(selectedPsuMode);
-				} catch (Exception exception) {
-					logger.error("Failed to set PSU mode", exception);
-				}
-			}
-		});
-		
-		// energy width display
-		ScannableDisplayComposite maxCount = new ScannableDisplayComposite(analyserGroup, SWT.NONE);
-		GridDataFactory.swtDefaults().span(2, 1).applyTo(maxCount);
-		maxCount.setScannable((Scannable) Finder.getInstance().find("energy_width"));
-		maxCount.setTextWidth(265);
-		maxCount.setDisplayName("Energy width:");
-		maxCount.setValueSize(14);
-		maxCount.setValueColour(SWT.COLOR_DARK_BLUE);
-		maxCount.setLabelSize(14);
-		
-		NudgePositionerComposite centreEnergyNPC = new NudgePositionerComposite(analyserGroup, SWT.NONE);
-		GridDataFactory.swtDefaults().align(SWT.BEGINNING, SWT.CENTER).applyTo(centreEnergyNPC);
-		centreEnergyNPC.setScannable((Scannable) Finder.getInstance().find("raw_centre_energy"));
-		centreEnergyNPC.setDisplayName("Centre Energy");
-		centreEnergyNPC.hideStopButton();
-		centreEnergyNPC.setUserUnits("eV");
-		centreEnergyNPC.setLabelToBold();
-		GridDataFactory.swtDefaults().span(1, 2).applyTo(centreEnergyNPC);
-
-		// Analyser Start Button
-		Button startButton = new Button(analyserGroup, SWT.DEFAULT);
+		addAnalyserButtons(analyserGroup);
+	}
+	
+	private void addAnalyserButtons(Group analyserGroup) {
+		Group analyserButtonsGroup = new Group(analyserGroup, SWT.NONE);
+		analyserButtonsGroup.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+		GridDataFactory.fillDefaults().grab(true,  true).span(3,  1).applyTo(analyserButtonsGroup);
+		analyserButtonsGroup.setLayout(new FillLayout(SWT.HORIZONTAL));
+			
+		addAnalyserStartButton(analyserButtonsGroup);
+		addAnalyserStopButton(analyserButtonsGroup);
+	}
+	
+	private void addAnalyserStartButton(Group analyserButtonsGroup) {
+		Button startButton = new Button(analyserButtonsGroup, SWT.DEFAULT);
 		startButton.setText("Start");
 		setTextToBold(startButton);
 		startButton.setBackground(green);
 		startButton.setToolTipText("Apply voltages and start acquiring");
-		GridDataFactory.swtDefaults().span(1, 2).align(SWT.BEGINNING, SWT.CENTER).hint(100, SWT.DEFAULT).applyTo(startButton);
 		startButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				logger.debug("Starting fixed mode acquistion");
 				try {
-					// Need to reset lens mode and pass energy as they may have changed from the values
-					// Shown in this GUI so resend the settings
 					analyser.setAcquisitionMode("Fixed");
-					analyser.setLensMode(lensModeCombo.getText());
-					analyser.setPassEnergy(Integer.parseInt(passEnergyCombo.getText()));
 					analyser.start();
+					eavCameraControl.setImageMode(ImageMode.CONTINUOUS);
+					eavCameraControl.startAcquiring();
 				} catch (Exception ex) {
-					logger.error("Failed to start fixed mode acquisition", ex);
+					logger.error("Failed to start fixed mode acquisition or live viewer", ex);
 				}
 			}
 		});
-
-		// Analyser Stop Button
-		Button stopButton = new Button(analyserGroup, SWT.DEFAULT);
+	}
+	
+	private void addAnalyserStopButton(Group analyserButtonsGroup) {
+		Button stopButton = new Button(analyserButtonsGroup, SWT.DEFAULT);
 		stopButton.setText("Stop");
 		setTextToBold(stopButton);
 		stopButton.setBackground(red);
 		stopButton.setToolTipText("Stop acquiring and zero supplies");
-		GridDataFactory.swtDefaults().span(1, 2).align(SWT.BEGINNING, SWT.CENTER).hint(100, SWT.DEFAULT).applyTo(stopButton);
 		stopButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				logger.info("Stopping continuous acquistion");
 				try {
+					eavCameraControl.stopAcquiring();
 					analyser.zeroSupplies();
 				} catch (Exception ex) {
-					logger.error("Failed to stop analyser", ex);
+					logger.error("Failed to stop analyser or live viewer", ex);
 				}
 			}
-		});
-
-		// SMPM group
-		Group smpmGroup = new Group(child, SWT.NONE);
-		analyserGroup.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		GridLayoutFactory.swtDefaults().numColumns(3).spacing(10, 20).applyTo(smpmGroup);
-
-		NudgePositionerComposite smpmXNPC = new NudgePositionerComposite(smpmGroup, SWT.NONE);
-		smpmXNPC.setScannable((Scannable) Finder.getInstance().find("smpmx"));
-		smpmXNPC.hideStopButton();
-		smpmXNPC.setUserUnits("mm");
-		smpmXNPC.setDisplayName("SMPM X Trans");
-		smpmXNPC.setLabelToBold();
-		smpmXNPC.setIncrement(0.2);
-		GridDataFactory.swtDefaults().span(1, 2).applyTo(smpmXNPC);
-
-		NudgePositionerComposite smpmYNPC = new NudgePositionerComposite(smpmGroup, SWT.NONE);
-		smpmYNPC.setScannable((Scannable) Finder.getInstance().find("smpmy"));
-		smpmYNPC.hideStopButton();
-		smpmYNPC.setUserUnits("mm");
-		smpmYNPC.setDisplayName("SMPM Y Trans");
-		smpmYNPC.setLabelToBold();
-		smpmYNPC.setIncrement(0.2);
-		GridDataFactory.swtDefaults().span(1, 2).applyTo(smpmYNPC);
-
-		NudgePositionerComposite smpmZNPC = new NudgePositionerComposite(smpmGroup, SWT.NONE);
-		smpmZNPC.setScannable((Scannable) Finder.getInstance().find("smpmz"));
-		smpmZNPC.hideStopButton();
-		smpmZNPC.setUserUnits("mm");
-		smpmZNPC.setDisplayName("SMPM Z Trans");
-		smpmZNPC.setLabelToBold();
-		smpmZNPC.setIncrement(0.2);
-		GridDataFactory.swtDefaults().span(1, 2).applyTo(smpmZNPC);
-
-		NudgePositionerComposite smpmPolarNPC = new NudgePositionerComposite(smpmGroup, SWT.NONE);
-		smpmPolarNPC.setScannable((Scannable) Finder.getInstance().find("smpmpolar"));
-		smpmPolarNPC.hideStopButton();
-		smpmPolarNPC.setUserUnits("mm");
-		smpmPolarNPC.setDisplayName("SMPM Polar Trans");
-		smpmPolarNPC.setLabelToBold();
-		smpmPolarNPC.setIncrement(0.2);
-		GridDataFactory.swtDefaults().span(1, 2).applyTo(smpmPolarNPC);
-
-		NudgePositionerComposite smpmAzimuthNPC = new NudgePositionerComposite(smpmGroup, SWT.NONE);
-		smpmAzimuthNPC.setScannable((Scannable) Finder.getInstance().find("smpmazimuth"));
-		smpmAzimuthNPC.hideStopButton();
-		smpmAzimuthNPC.setUserUnits("mm");
-		smpmAzimuthNPC.setDisplayName("SMPM Azimuth Trans");
-		smpmAzimuthNPC.setLabelToBold();
-		smpmAzimuthNPC.setIncrement(0.2);
-		GridDataFactory.swtDefaults().span(1, 2).applyTo(smpmAzimuthNPC);
-
-		// Set the child as the scrolled content of the ScrolledComposite
-		scrollComp.setContent(child);
-		scrollComp.setExpandHorizontal(true);
-		scrollComp.setExpandVertical(true);
+		});	
 	}
+		
+	private void addLiveControls() {
+		if (alignmentControls.hasAnalyserControls()) {
+			alignmentControls.getAnalyserControls()
+				.getControls()
+				.stream()
+				.forEachOrdered(c -> c.createControl(analyserGroup));
 
+		}
+		
+		if (alignmentControls.hasSampleControls()) {
+			Group sampleControlGroup = new Group(child, SWT.NONE);
+			sampleControlGroup.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
+			GridLayoutFactory.swtDefaults().numColumns(3).spacing(10, 20).applyTo(sampleControlGroup);
+			
+			alignmentControls.getSampleControls()
+				.getControls()
+				.stream()
+				.forEachOrdered(c -> c.createControl(sampleControlGroup));
+		}
+	}
+	
 	private void setTextToBold(Control control) {
 		FontDescriptor boldDescriptor = FontDescriptor.createFrom(control.getFont()).setStyle(SWT.BOLD);
 		Font boldFont = boldDescriptor.createFont(control.getDisplay());
 		control.setFont(boldFont);
-	}
-
-	private void updatePassEnergyCombo() {
-		try {
-			final String[] passEnergyStrings = analyser.getPassEnergies().toArray(new String[0]);
-
-			logger.debug("Setting items in pass energy combo box");
-			passEnergyCombo.setItems(passEnergyStrings);
-			// Automatically select the current pass energy if available in current PSU mode
-			logger.debug("Selecting currently active pass energy in combo box");
-			int activePassEnergy = analyser.getPassEnergy();
-			passEnergyCombo.select(Arrays.asList(passEnergyStrings).indexOf(Integer.toString(activePassEnergy)));
-		} catch (Exception e) {
-			logger.error("Failed to get PSU mode", e);
-		}
-	}
-	
-	private void updateElementCombo() {
-		try {
-			List<String> psuModes = analyser.getPsuModes();
-			elementCombo.setItems(psuModes.toArray(new String[0]));
-			String psuMode = analyser.getPsuMode();
-			elementCombo.select(psuModes.indexOf(psuMode));
-		} catch (Exception exception) {
-			
-		}
 	}
 
 	@Focus
