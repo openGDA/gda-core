7fea0f88005ad7742ed20af8ac5e3d9912841dcc
Joseph Ware <joseph.ware@diamond.ac.uk>
2020-04-16 14:46:49 +0100

    Deprecating ScanPointIterator
    - Originally left in to cater for JythonGenerators, but now compatible
      with rest of ScanPointGenerator generators. No use outside tests
    
    Change-Id: I7ddbe35625350183fe8b2d4022568c4fbc0d5918

--- org.eclipse.scanning.api/src/org/eclipse/scanning/api/points/ScanPointIterator.java
+++ org.eclipse.scanning.api/src/org/eclipse/scanning/api/points/ScanPointIterator.java
@@ -3,19 +3,22 @@ package org.eclipse.scanning.api.points;
 import java.util.Iterator;
 
 /**
+ * @deprecated GDA 9.17, for deletion 9.18 (just to be extra careful)
+ * Probably not in use at all since GDA 9.16 generator changes.
+ *
  * An iterator over {@link IPosition}s that knows the
  * size, shape and rank of the points it iterates over.
  *
  * <em>Note:</em> This class is an implementation class and should not
- * be used outside the scanning framework. Iterators that wrap jython
- * point generators should implement this.
+ * be used outside the scanning framework.
  * While most {@link AbstractGenerator} get Size/Shape/Rank information from their
  * PPointGenerator, JythonGenerator currently still requires this information from
  * the iterator.
- * For PPointGenerators and generators that use them, the information is taken directly from 
+ * For PPointGenerators and generators that use them, the information is taken directly from
  * the [Python] CompoundGenerator (see jython_spg_interface.py)
  *
  */
+@Deprecated
 public interface ScanPointIterator extends Iterator<IPosition> {
 
 	/**
