b60089a9af613889dca1f9b5d4e9e0097b19c20d
Matthew Dickie <matthew.dickie@diamond.ac.uk>
2020-06-01 13:15:11 +0100

    [DAQ-2932] update to reflect package move for appenders in org.eclipse.dawnsci.nexus
    Change-Id: Ia64c39b0c1f83b4027235ee405f8f9734500c0ed

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/nexus/MalcolmDetectorMetadataScanTest.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/scan/nexus/MalcolmDetectorMetadataScanTest.java
@@ -30,7 +30,7 @@ import java.util.Map;
 import org.eclipse.dawnsci.analysis.api.tree.DataNode;
 import org.eclipse.dawnsci.nexus.NXdetector;
 import org.eclipse.dawnsci.nexus.NXinstrument;
-import org.eclipse.dawnsci.nexus.device.NexusMetadataAppender;
+import org.eclipse.dawnsci.nexus.appender.NexusMetadataAppender;
 import org.eclipse.scanning.api.device.IRunnableDevice;
 import org.eclipse.scanning.api.device.models.IMalcolmDetectorModel;
 import org.eclipse.scanning.api.event.scan.DeviceState;
