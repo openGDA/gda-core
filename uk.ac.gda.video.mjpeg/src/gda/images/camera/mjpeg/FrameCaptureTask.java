a99ac515ec5d3b18f6879b353760dce81d68a6a7
Joe Shannon <joe.shannon@diamond.ac.uk>
2020-06-15 11:05:08 +0100

    DAQ-2993 Add new class to convert Mjpeg stream to Jpeg bytes
    MjpegInputStream takes an InputStream and allows byte arrays containing
    Jpeg data to be read from it.
    
    Mjpeg header lines are read to find Content-Length which allows reading
    the bytes in one pass. If Content-Length is not present reading can
    still occur by looking ahead to find the Jpeg end marker. This resolves
    GDA-3129.
    
    Change-Id: I2f93ea69748d894433c8814411bb3dd921a95d4c

--- uk.ac.gda.video.mjpeg/src/gda/images/camera/mjpeg/FrameCaptureTask.java
+++ uk.ac.gda.video.mjpeg/src/gda/images/camera/mjpeg/FrameCaptureTask.java
@@ -19,10 +19,7 @@
 package gda.images.camera.mjpeg;
 
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -32,11 +29,10 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 
+import javax.imageio.ImageIO;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageDecoder;
 
 import gda.images.camera.FrameStatistics;
 import gda.images.camera.MotionJpegOverHttpReceiverBase;
@@ -44,19 +40,6 @@ import gda.images.camera.MotionJpegOverHttpReceiverBase;
 /**
  * Task that captures frames from the MJPEG stream. A task is created to decode each frame, and the frame is added to a
  * queue of received frames for later dispatch.
- * <p>
- * This class was originally written to receive MJPEG streams from Axis network cameras, specifically an Axis 214 PTZ.
- * As there is no standard specification for Motion JPEG, this may not work for all MJPEG streams. The <a
- * href="http://www.axis.com/techsup/cam_servers/dev/cam_http_api.php">Axis documentation</a> has several useful
- * sections:
- * <ul>
- * <li><a href="http://www.axis.com/techsup/cam_servers/dev/cam_http_api.php#sw_requirements_image_requests_mjpg">5.2.5
- * MJPG video request</a></li>
- * <li><a href="http://www.axis.com/techsup/cam_servers/dev/cam_http_api.php#api_blocks_jpeg_mjpg_mjpg_request">5.2.6
- * MJPG video CGI request</a></li>
- * <li><a href="http://www.axis.com/techsup/cam_servers/dev/cam_http_api.php#api_blocks_jpeg_mjpg_mjpg_response">5.2.7
- * MJPG video response</a></li>
- * </ul>
  */
 public abstract class FrameCaptureTask<E> implements Runnable {
 
@@ -68,16 +51,11 @@ public abstract class FrameCaptureTask<E> implements Runnable {
 
 	private BlockingQueue<Future<E>> receivedImages;
 
-	/** The byte array is to represent the string - Content-Length:&nbsp */
-	private static final byte[] CONTENT_LENGTH_AS_BYTES = "Content-Length: ".getBytes();
-
 	private final int readTimeout;
 
 	private final boolean acceptReadTimeout;
 
-	private BufferedImage bufferedImage;
-
-	private DataInputStream dis;
+	private MjpegInputStream mjpegStream;
 
 	private volatile boolean keepRunning;
 
@@ -128,157 +106,59 @@ public abstract class FrameCaptureTask<E> implements Runnable {
 
 	private void receiveImages() throws Exception {
 		logger.debug("Frame capture starting");
-
+		ImageIO.setUseCache(false); // Provides a slight performance increase
 		frameCaptureStatistics.reset();
 		frameDecodeStatistics.reset();
-
 		URL url = new URL(urlSpec);
 		URLConnection conn = url.openConnection();
 		conn.setReadTimeout(readTimeout);
-		InputStream instream = conn.getInputStream();
-		BufferedInputStream bis = new BufferedInputStream(instream);
-		dis = new DataInputStream(bis);
-
-		// Miss the first image - this is because the first JPEG turns out to be corrupt
-		while (true) {
-			if (readFirstLineAndIsLegal(dis)) {
-				break;
-			}
-		}
-		String cL = getContentLength(dis);
-		dis.skipBytes(Integer.parseInt(cL));
-		readLine(1, dis);
-		//
 		keepRunning = true;
-		while (keepRunning) {
-			readLine(4, dis);
-
-			if (MotionJpegOverHttpReceiverBase.SHOW_STATS) {
-				frameCaptureStatistics.startProcessingFrame();
-			}
-
-			BufferedImage readJPG = readJPG(dis);
-			readLine(1, dis);
-			if( readJPG != null){
-				//only update the member variable if not null
-				bufferedImage = readJPG;
+		try {
+			mjpegStream = new MjpegInputStream(conn.getInputStream());
+			while (keepRunning) {
 				if (MotionJpegOverHttpReceiverBase.SHOW_STATS) {
-					frameCaptureStatistics.finishProcessingFrame();
+					frameCaptureStatistics.startProcessingFrame();
 				}
-
-				Callable<Void> imageDecoderTask = () -> {
+				BufferedImage jpegImage = null;
+				try (ByteArrayInputStream jpegBytes = new ByteArrayInputStream(mjpegStream.getNextFrame())) {
+					jpegImage = ImageIO.read(jpegBytes);
+				}
+				if (jpegImage != null) {
+					final BufferedImage bufferedImage = jpegImage;
 					if (MotionJpegOverHttpReceiverBase.SHOW_STATS) {
-						frameDecodeStatistics.startProcessingFrame();
+						frameCaptureStatistics.finishProcessingFrame();
 					}
 
-					final E decodedImage = convertImage(bufferedImage);
+					Callable<Void> imageDecoderTask = () -> {
+						if (MotionJpegOverHttpReceiverBase.SHOW_STATS) {
+							frameDecodeStatistics.startProcessingFrame();
+						}
+						final E decodedImage = convertImage(bufferedImage);
+						if (MotionJpegOverHttpReceiverBase.SHOW_STATS) {
+							frameDecodeStatistics.finishProcessingFrame();
+						}
 
-					if (MotionJpegOverHttpReceiverBase.SHOW_STATS) {
-						frameDecodeStatistics.finishProcessingFrame();
+						// once the frame has been decoded add it to the receivedImages queue
+						// Do it here rather than adding the result of imageDecodingService.submit(imageDecoderTask);
+						// to allow the decode task to be cancelled without the need to clear the associated future on the
+						// receivedImages queue.
+						receivedImages.offer(CompletableFuture.completedFuture(decodedImage));
+						return null;
+					};
+
+					// We might be in the middle of capturing a frame from the stream when the image decoding service
+					// is shut down
+					if (!imageDecodingService.isShutdown()) {
+						imageDecodingService.submit(imageDecoderTask);
 					}
-
-					// once the frame has been decoded add it to the receivedImages queue
-					// Do it here rather than adding the result of imageDecodingService.submit(imageDecoderTask);
-					// to allow the decode task to be cancelled without the need to clear the associated future on the
-					// receivedImages queue.
-					receivedImages.offer(CompletableFuture.completedFuture(decodedImage));
-					return null;
-				};
-
-				// We might be in the middle of capturing a frame from the stream when the image decoding service
-				// is shut down
-				if (!imageDecodingService.isShutdown()) {
-					imageDecodingService.submit(imageDecoderTask);
-
 				}
 			}
 
-		}
-
-		logger.debug("Frame capture stopping");
-	}
-
-	private boolean readFirstLineAndIsLegal(DataInputStream dis) throws IOException {
-		for (int i = 0; i < CONTENT_LENGTH_AS_BYTES.length; i++) {
-			byte readByte = dis.readByte();
-			if (readByte != CONTENT_LENGTH_AS_BYTES[i]) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	private BufferedImage readJPG(DataInputStream dis) { // read the embedded jpeg image
-		BufferedImage image = null;
-		try {
-			//ImageIO.read fails to decode all images
-			//causing the capture frame rate to be v. low
-			JPEGImageDecoder decoder = JPEGCodec.createJPEGDecoder(dis);
-			image = decoder.decodeAsBufferedImage();
-		} catch (Throwable e) {
-			logger.error("Problem reading JPG->", e);
-			disconnect(dis);
+		} finally {
+			logger.debug("Frame capture stopping");
 			shutdown();
+
 		}
-		return image;
-	}
-
-	private void disconnect(DataInputStream dis) {
-		try {
-			if( dis != null)
-				dis.close();
-
-		} catch (Exception e) {
-			logger.error("Problem disconnecting ->", e);
-		}
-	}
-
-	private void readLine(int n, DataInputStream dis) throws Exception { // used to strip out the
-		// header lines
-		for (int i = 0; i < n; i++) {
-			readLine(dis);
-		}
-	}
-
-	private void readLine(DataInputStream dis) throws Exception {
-		boolean end = false;
-		String lineEnd = "\n"; // assumes that the end of the line is marked
-		// with this
-		byte[] lineEndBytes = lineEnd.getBytes();
-		byte[] byteBuf = new byte[lineEndBytes.length];
-
-		while (!end) {
-			dis.read(byteBuf, 0, lineEndBytes.length);
-			String t = new String(byteBuf);
-			if (t.equals(lineEnd)) {
-				end = true;
-			}
-		}
-	}
-
-	private String getContentLength(DataInputStream dis) {
-		StringBuilder strBuffer = new StringBuilder();
-		try {
-			boolean end = false;
-			String lineEnd = "\n"; // assumes that the end of the line is marked
-			// with this
-			byte[] lineEndBytes = lineEnd.getBytes();
-			byte[] byteBuf = new byte[lineEndBytes.length];
-
-			while (!end) {
-				dis.read(byteBuf, 0, lineEndBytes.length);
-				String t = new String(byteBuf);
-				strBuffer.append(t);
-				if (t.equals(lineEnd)) {
-					end = true;
-				}
-			}
-		} catch (Exception e) {
-			logger.error("Cannot readLine", e);
-		}
-		return strBuffer.substring(0, strBuffer.length() - 2);
-
 	}
 
 	/**
@@ -286,8 +166,17 @@ public abstract class FrameCaptureTask<E> implements Runnable {
 	 */
 	protected abstract E convertImage(BufferedImage imageData);
 
+
 	public void shutdown() {
-		this.keepRunning = false;
-		disconnect(dis);
+		keepRunning = false;
+		if (mjpegStream != null) {
+			try {
+				mjpegStream.close();
+			} catch (Exception e) {
+				logger.error("Error closing mjpeg sream", e);
+			} finally {
+				mjpegStream = null;
+			}
+		}
 	}
 }
