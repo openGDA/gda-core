65343ebac35dc6f7c5a73a025837447cd59586fe
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-06-17 16:19:01 +0100

    DAQ-2094 Make gda.rcp.views.ViewContentProvider private in PeripheralView
    The two classes are designed to go together.
    
    Change-Id: I772e9d71e62031acbaedb449cfac516eaeac13f0

--- uk.ac.gda.client/src/gda/rcp/views/PeripheralView.java
+++ uk.ac.gda.client/src/gda/rcp/views/PeripheralView.java
@@ -18,6 +18,9 @@
 
 package gda.rcp.views;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuListener;
 import org.eclipse.jface.action.IMenuManager;
@@ -28,19 +31,26 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.DrillDownAdapter;
 import org.eclipse.ui.part.ViewPart;
 
+import gda.device.Scannable;
+import gda.factory.Findable;
+import gda.factory.Finder;
 
 /**
  * This sample class demonstrates how to plug-in a new
@@ -187,5 +197,112 @@ public class PeripheralView extends ViewPart {
 	@Override
 	public void setFocus() {
 		viewer.getControl().setFocus();
+	}
+
+	/**
+	 * The content provider class is responsible for providing objects to the view.<br>
+	 * It can wrap existing objects in adapters or simply return objects as-is.<br>
+	 * These objects may be sensitive to the current input of the view, or ignore it and always show the same content
+	 * (like Task List, for example).
+	 */
+	private static class ViewContentProvider implements IStructuredContentProvider, ITreeContentProvider {
+
+		private TreeParent invisibleRoot;
+		private IViewSite viewSite;
+
+		public ViewContentProvider(IViewSite viewSite) {
+			super();
+			this.viewSite = viewSite;
+		}
+
+		@Override
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+
+		@Override
+		public void dispose() {
+		}
+
+		@Override
+		public Object[] getElements(Object parent) {
+			if (parent.equals(viewSite)) {
+				if (invisibleRoot == null)
+					initialize();
+				return getChildren(invisibleRoot);
+			}
+			return getChildren(parent);
+		}
+
+		@Override
+		public Object getParent(Object child) {
+			if (child instanceof TreeObject) {
+				return ((TreeObject) child).getParent();
+			}
+			return null;
+		}
+
+		@Override
+		public Object[] getChildren(Object parent) {
+			if (parent instanceof TreeParent) {
+				return ((TreeParent) parent).getChildren();
+			}
+			return new Object[0];
+		}
+
+		@Override
+		public boolean hasChildren(Object parent) {
+			if (parent instanceof TreeParent)
+				return ((TreeParent) parent).hasChildren();
+			return false;
+		}
+
+		/*
+		 * We will set up a dummy model to initialize tree heararchy. In a real
+		 * code, you will connect to a real model and expose its hierarchy.
+		 */
+		private void initialize() {
+			TreeObject to1 = new TreeObject("Leaf 1");
+			TreeObject to2 = new TreeObject("Leaf 2");
+			TreeObject to3 = new TreeObject("Leaf 3");
+			TreeParent p1 = new TreeParent("Parent 1");
+			p1.addChild(to1);
+			p1.addChild(to2);
+			p1.addChild(to3);
+
+			List<String> interfaces = Finder.getInstance().listAllInterfaces();
+			TreeParent[] parents = new TreeParent[interfaces.size()];
+			int i = 0;
+			for (String intfc : interfaces) {
+				parents[i] = new TreeParent(intfc);
+				List<Findable> objects2 = Finder.getInstance().listAllObjects(intfc);
+				TreeObject[] objects = new TreeObject[objects2.size()];
+				int j = 0;
+				for (Iterator<Findable> iterator = objects2.iterator(); iterator.hasNext();) {
+					Findable findable = iterator.next();
+					if (findable instanceof Scannable) {
+						Scannable scannable = (Scannable) findable;
+						objects[j] = new ScannableTreeObject(scannable);
+					} else {
+						objects[j] = new TreeObject(findable.getName());
+					}
+					parents[i].addChild(objects[j]);
+					j++;
+				}
+				i++;
+			}
+
+			TreeObject to4 = new TreeObject("Leaf 4");
+			TreeParent p2 = new TreeParent("Parent 2");
+			p2.addChild(to4);
+
+			TreeParent root = new TreeParent("All Objects");
+
+			for (int j = 0; j < parents.length; j++) {
+				root.addChild(parents[j]);
+			}
+
+			invisibleRoot = new TreeParent("");
+			invisibleRoot.addChild(root);
+		}
 	}
 }
