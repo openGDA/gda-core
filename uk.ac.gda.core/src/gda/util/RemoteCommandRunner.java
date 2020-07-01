5c21de0d2c711d699153715b23d3320af9e5e8d5
Peter Holloway <peter.holloway@diamond.ac.uk>
2020-07-01 11:59:00 +0100

    Make RemoteCommandRunner stoppable
    Commands started via a RemoteCommandRunner could not be stopped. If the command
    was run using the exec Jython methods, the command would also be unstoppable via
    StopAll command.
    
    NB In the case of SingleCommandRunner, if a command ignores thread
    interruptions, it may be possible for the command to continue running after
    stop has been called. In this case, any further exceptions will not be
    logged and additional commands may be run.
    
    Change-Id: I2017d1b7f6bfbb836f9a4d7e45e22f0e809ff9a3

--- uk.ac.gda.core/src/gda/util/RemoteCommandRunner.java
+++ uk.ac.gda.core/src/gda/util/RemoteCommandRunner.java
@@ -2,9 +2,9 @@ package gda.util;
 
 import java.io.Serializable;
 
-import gda.factory.Findable;
+import gda.device.Stoppable;
 
 /** Command Runner to allow clients to run commands that take arguments */
-public interface RemoteCommandRunner extends Findable {
+public interface RemoteCommandRunner extends Stoppable {
 	void runCommand(Serializable... arguments);
 }
