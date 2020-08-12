2ffeafbde7c8cb19007f4c4b376ad4b47de8560d
Joe Shannon <joe.shannon@diamond.ac.uk>
2020-08-12 17:54:14 +0100

    DAQ-3058 Enable MalcolmEpicsV4ConnectorTest, ExampleMalcolmDeviceTest
    This uses a horrible workaround with reflection but there doesn't
    appear to be another solution until
    https://github.com/epics-base/epicsCoreJava/issues/100 is addressed.
    
    The other option would be to skip these tests but they are fairly
    essential to the Malcolm scanning infrastructure.
    
    Change-Id: I3c96eb2de4b5133659854c74427bb993f9443e8e

--- org.eclipse.scanning.example/src/org/eclipse/scanning/example/malcolm/AbstractEPICSv4Device.java
+++ org.eclipse.scanning.example/src/org/eclipse/scanning/example/malcolm/AbstractEPICSv4Device.java
@@ -1,13 +1,18 @@
 package org.eclipse.scanning.example.malcolm;
 
+import java.io.PrintStream;
+import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 import org.epics.pvaccess.server.ServerContext;
-import org.epics.pvaccess.server.impl.remote.ServerContextImpl;
 import org.epics.pvdata.pv.PVStructure;
 import org.epics.pvdatabase.PVDatabase;
 import org.epics.pvdatabase.PVDatabaseFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class creates an Epics V4 service, that listens for connections and handles RPC, GET, PUT etc. The modelled
@@ -23,6 +28,7 @@ public abstract class AbstractEPICSv4Device implements IEPICSv4Device {
 	protected static int traceLevel = 0;
 	protected final CountDownLatch latch = new CountDownLatch(1);
 	protected DummyMalcolmRecord pvRecord = null;
+	private static final Logger logger = LoggerFactory.getLogger(AbstractEPICSv4Device.class);
 
 	public AbstractEPICSv4Device(String deviceName) {
 		recordName = deviceName;
@@ -49,13 +55,45 @@ public abstract class AbstractEPICSv4Device implements IEPICSv4Device {
 		pvRecord = DummyMalcolmRecord.create(recordName);
 		pvRecord.setTraceLevel(traceLevel);
 		master.addRecord(pvRecord);
-		ServerContext context = ServerContextImpl.startPVAServer(getPvaProviderName(), 0, true,
-				System.out);
+		ServerContext context = startPvaServerWithReflection(getPvaProviderName(), 0, true, System.out);
+		logger.info("PVA Server Started");
 		latch.await();
 		master.removeRecord(pvRecord);
 		context.destroy();
 	}
 
 	protected abstract String getPvaProviderName();
+
+
+	// TODO replace this once there is a solution to https://github.com/epics-base/epicsCoreJava/issues/100
+	/**
+	 * Since Tycho runs in OSGi it cannot see internal class from EpicsV4:
+	 * {@code org.epics.pvaccess.server.impl.remote.ServerContextImpl}
+	 * <p>
+	 * The Epics library currently provides no other exported API mechanism to
+	 * start a PVA server apart from ServerFactory.create() but this is not good
+	 * for tests as there is no way to stop the created server.
+	 * <p>
+	 * This workaround uses reflection to call the method and return the result as a
+	 * ServerContext interface object which is from an exported package.
+	 * <p>
+	 * Keep checking <a href="https://github.com/epics-base/epicsCoreJava/issues/100"> the issue
+	 * for solution so this can be removed.
+	 */
+	private ServerContext startPvaServerWithReflection(String providerNames, int timeToRun,
+			boolean runInSeparateThread, PrintStream printInfoStream) throws Exception {
+		String implClassName = "org.epics.pvaccess.server.impl.remote.ServerContextImpl";
+		Class<?> implClass;
+		Bundle bundle = FrameworkUtil.getBundle(ServerContext.class);
+		if (bundle != null) {
+			implClass = bundle.loadClass(implClassName);
+		} else {
+			implClass = Class.forName(implClassName);
+		}
+		Method startPvaMethod = implClass.getMethod("startPVAServer", String.class, int.class, boolean.class,
+				PrintStream.class);
+		return (ServerContext) startPvaMethod.invoke(null, providerNames, timeToRun, runInSeparateThread,
+				printInfoStream);
+	}
 
 }
