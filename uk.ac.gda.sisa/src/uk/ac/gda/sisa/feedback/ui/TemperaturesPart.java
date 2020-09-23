1c5b04385b92b481a555bb8d324cceca5b1b9062
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2020-09-23 16:36:07 +0100

    I09-300 Feedback perspective
    Change-Id: I042902dc95f17db930c44d13a47491b4ec179c0e

---
+++ uk.ac.gda.sisa/src/uk/ac/gda/sisa/feedback/ui/TemperaturesPart.java
@@ -0,0 +1,28 @@
+package uk.ac.gda.sisa.feedback.ui;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemperaturesPart extends FeedbackPartsBase {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TemperaturesPart.class);
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		logger.trace("postConstruct called");
+		ScrolledComposite scroller = new ScrolledComposite(parent, SWT.V_SCROLL);
+		Composite scrollerContent = new Composite(scroller, SWT.NONE);
+		RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
+		rowLayout.fill = true;
+		rowLayout.center = true;
+		scrollerContent.setLayout(rowLayout);
+		addControlGroup(scrollerContent, feedbackControls.getTemperatureControls(), 1);
+		setScrollerContentAndSize(scrollerContent, scroller);
+	}
+}
