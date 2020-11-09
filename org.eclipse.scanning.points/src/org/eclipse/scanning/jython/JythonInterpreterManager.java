188b5ef020e308c44cfb2c9089ec854374cf765b
Joe Shannon <joe.shannon@diamond.ac.uk>
2020-11-09 10:41:19 +0000

    Reformat JythonInterpreterManager
    Inconsistent whitespace was tricky to read.
    
    Change-Id: I2a01640d9066d3ebee73b27601a59ced4bb71675

--- org.eclipse.scanning.points/src/org/eclipse/scanning/jython/JythonInterpreterManager.java
+++ org.eclipse.scanning.points/src/org/eclipse/scanning/jython/JythonInterpreterManager.java
@@ -18,9 +18,8 @@ import org.slf4j.LoggerFactory;
 
 /**
  *
- * An interpreter manager which is designed to create an interpreter
- * which either is sympathic and working with the GDA configuration or
- * creates an empty and working interpreter.
+ * An interpreter manager which is designed to create an interpreter which either is sympathic and working with the GDA
+ * configuration or creates an empty and working interpreter.
  *
  * @author Matthew Gerring
  *
@@ -29,33 +28,32 @@ public class JythonInterpreterManager {
 
 	private static final Logger logger = LoggerFactory.getLogger(JythonInterpreterManager.class);
 
-
 	private static volatile PySystemState configuredState;
 
 	/**
-	 * Call to ensure that an interpreter is set up and configured and
-	 * able to load the relevant bundles.
+	 * Call to ensure that an interpreter is set up and configured and able to load the relevant bundles.
 	 */
 	public static synchronized void setupSystemState(String... bundleNames) {
 
-		ClassLoader loader=null;
-		if (configuredState==null) { // Relies on setupSystemState() being called early in the server startup.
+		ClassLoader loader = null;
+		if (configuredState == null) { // Relies on setupSystemState() being called early in the server startup.
 			loader = createJythonClassLoader(PySystemState.class.getClassLoader());
 			initializePythonPath(loader);
 		}
 
-	PySystemState state = Py.getSystemState();
-	if (state==configuredState) return;
+		PySystemState state = Py.getSystemState();
+		if (state == configuredState)
+			return;
 
-	if (loader==null) {
-		// Then someone else has changed the PySystemState
-		// They will not have added our
-			loader = createJythonClassLoader(state.getClassLoader());   // Don't clobber their working.
-	}
+		if (loader == null) {
+			// Then someone else has changed the PySystemState
+			// They will not have added our
+			loader = createJythonClassLoader(state.getClassLoader()); // Don't clobber their working.
+		}
 		state.setClassLoader(loader);
 
-	setJythonPaths(state);       // Tries to ensure that enough of jython is on the path
-	setSpgGeneratorPaths(state, bundleNames); // Adds the scripts directory from points
+		setJythonPaths(state); // Tries to ensure that enough of jython is on the path
+		setSpgGeneratorPaths(state, bundleNames); // Adds the scripts directory from points
 		Py.setSystemState(state);
 
 		configuredState = state;
@@ -64,15 +62,19 @@ public class JythonInterpreterManager {
 	public static synchronized void addPath(String directory) throws IOException {
 
 		// Load one of the standard functions if the state has not been created yet.
-		if (configuredState==null) ScanPointGeneratorFactory.JOneAxisLineGeneratorFactory();
+		if (configuredState == null)
+			ScanPointGeneratorFactory.JOneAxisLineGeneratorFactory();
 
 		// Do nothing if the directory is on the path
-		if (configuredState.path.contains(directory)) return;
+		if (configuredState.path.contains(directory))
+			return;
 
 		// Check the directory
 		final File file = new File(directory);
-		if (!file.exists()) throw new IOException("The module directory '"+directory+"' does not exist!");
-		if (!file.isDirectory()) throw new IOException("The module directory path '"+directory+"' is not a folder!");
+		if (!file.exists())
+			throw new IOException("The module directory '" + directory + "' does not exist!");
+		if (!file.isDirectory())
+			throw new IOException("The module directory path '" + directory + "' is not a folder!");
 
 		// Add it to the path.
 		configuredState.path.add(directory);
@@ -82,67 +84,69 @@ public class JythonInterpreterManager {
 
 	private static void setSpgGeneratorPaths(PySystemState state, String... bundleNames) {
 
-        File location = getBundleLocation("org.eclipse.scanning.points");
-        state.path.add(new PyString(location.getAbsolutePath() + SCRIPTS));
+		File location = getBundleLocation("org.eclipse.scanning.points");
+		state.path.add(new PyString(location.getAbsolutePath() + SCRIPTS));
 
-        location = getBundleLocation("org.eclipse.scanning.sequencer");
-        state.path.add(new PyString(location.getAbsolutePath() + SCRIPTS));
+		location = getBundleLocation("org.eclipse.scanning.sequencer");
+		state.path.add(new PyString(location.getAbsolutePath() + SCRIPTS));
 
-        if (bundleNames!=null) {
-		for (String bundle : bundleNames) {
-                location = getBundleLocation(bundle);
-                state.path.add(new PyString(location.getAbsolutePath() + SCRIPTS));
+		if (bundleNames != null) {
+			for (String bundle : bundleNames) {
+				location = getBundleLocation(bundle);
+				state.path.add(new PyString(location.getAbsolutePath() + SCRIPTS));
 			}
-        }
+		}
 	}
-
 
 	private static void initializePythonPath(ClassLoader loader) {
 		try {
-		String jythonBundleName = System.getProperty("org.eclipse.scanning.jython.osgi.bundle.name", "uk.ac.diamond.jython");
-	        File loc = getBundleLocation(jythonBundleName); // TODO Name the jython OSGi bundle without Diamond in it!
-	        if (loc ==null) return;
-	        File jythonDir = find(loc, "jython");
-	        if (jythonDir ==null) {
-	        	logger.warn("JythonDir not found while initializing PythonPath");
-	        	return;
-	        }
+			String jythonBundleName = System.getProperty("org.eclipse.scanning.jython.osgi.bundle.name",
+					"uk.ac.diamond.jython");
+			File loc = getBundleLocation(jythonBundleName); // TODO Name the jython OSGi bundle without Diamond in it!
+			if (loc == null)
+				return;
+			File jythonDir = find(loc, "jython");
+			if (jythonDir == null) {
+				logger.warn("JythonDir not found while initializing PythonPath");
+				return;
+			}
 
-	        Properties props = new Properties();
-		props.setProperty("python.home", jythonDir.getAbsolutePath());
-		props.setProperty("python.console.encoding", "UTF-8"); // Used to prevent: console: Failed to install '': java.nio.charset.UnsupportedCharsetException: cp0.
-		props.setProperty("python.options.showJavaExceptions", "true");
-		props.setProperty("python.verbose", "warning");
+			Properties props = new Properties();
+			props.setProperty("python.home", jythonDir.getAbsolutePath());
+			props.setProperty("python.console.encoding", "UTF-8"); // Used to prevent: console: Failed to install '':
+																	// java.nio.charset.UnsupportedCharsetException:
+																	// cp0.
+			props.setProperty("python.options.showJavaExceptions", "true");
+			props.setProperty("python.verbose", "warning");
 
-		Properties preprops = System.getProperties();
+			Properties preprops = System.getProperties();
 
-		PySystemState.initialize(preprops, props, new String[]{}, loader);
+			PySystemState.initialize(preprops, props, new String[] {}, loader);
 
-	    } catch (Throwable ne) {
-		logger.debug("Problem loading jython bundles!", ne);
-	    }
+		} catch (Throwable ne) {
+			logger.debug("Problem loading jython bundles!", ne);
+		}
 	}
-
 
 	private static ClassLoader createJythonClassLoader(ClassLoader classLoader) {
 
-	ClassLoader jythonClassloader = ScanPointGeneratorFactory.class.getClassLoader();
+		ClassLoader jythonClassloader = ScanPointGeneratorFactory.class.getClassLoader();
 
-	try { // For non-unit tests, attempt to use the OSGi classloader of this bundle.
-		String jythonBundleName = getJythonBundleName();
-		CompositeClassLoader composite = new CompositeClassLoader(classLoader);
-		    // Classloader for org.eclipse.scanning.points
-		composite.addLast(ScanPointGeneratorFactory.class.getClassLoader());
-		addLast(composite, jythonBundleName);
-		jythonClassloader = composite;
+		try { // For non-unit tests, attempt to use the OSGi classloader of this bundle.
+			String jythonBundleName = getJythonBundleName();
+			CompositeClassLoader composite = new CompositeClassLoader(classLoader);
+			// Classloader for org.eclipse.scanning.points
+			composite.addLast(ScanPointGeneratorFactory.class.getClassLoader());
+			addLast(composite, jythonBundleName);
+			jythonClassloader = composite;
 
-	} catch (Throwable ne) {
-		ne.printStackTrace();
-		// Legal, if static classloader does not work in tests, there will be
-		// errors. If bundle classloader does not work in product, there will be errors.
-		// Typically the message is something like: 'cannot find module org.eclipse.scanning.api'
-	}
-	return jythonClassloader;
+		} catch (Throwable ne) {
+			ne.printStackTrace();
+			// Legal, if static classloader does not work in tests, there will be
+			// errors. If bundle classloader does not work in product, there will be errors.
+			// Typically the message is something like: 'cannot find module org.eclipse.scanning.api'
+		}
+		return jythonClassloader;
 	}
 
 	private static String getJythonBundleName() {
@@ -171,7 +175,8 @@ public class JythonInterpreterManager {
 				File jythonDir = find(loc, "jython");
 				lib = find(jythonDir, "Lib");
 				if (lib == null) {
-					logger.warn("Jython Lib not found in jythonDir {}, necessary scripts will not be added to path", jythonDir);
+					logger.warn("Jython Lib not found in jythonDir {}, necessary scripts will not be added to path",
+							jythonDir);
 				} else {
 					logger.info("Adding Jython Lib to PythonPath");
 					state.path.add(new PyString(lib.getAbsolutePath()));
@@ -184,8 +189,10 @@ public class JythonInterpreterManager {
 
 				File[] fa = site.listFiles();
 				for (File dir : fa) {
-					if (!dir.isDirectory()) continue;
-					if (dir.getName().endsWith("-info")) continue;
+					if (!dir.isDirectory())
+						continue;
+					if (dir.getName().endsWith("-info"))
+						continue;
 					state.path.add(new PyString(dir.getAbsolutePath())); // Resolves the collections
 				}
 
@@ -197,12 +204,12 @@ public class JythonInterpreterManager {
 	}
 
 	private static ClassLoader getBundleLoader(String name) {
-	Bundle bundle = Platform.getBundle(name);
-	BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
-	return bundleWiring.getClassLoader();
+		Bundle bundle = Platform.getBundle(name);
+		BundleWiring bundleWiring = bundle.adapt(BundleWiring.class);
+		return bundleWiring.getClassLoader();
 	}
 
-    /**
+	/**
 	 * @param bundleName
 	 * @return File
 	 */
@@ -217,32 +224,37 @@ public class JythonInterpreterManager {
 					logger.error("Error resolving bundle File", e);
 				}
 			}
-		}
-		else {
-			File dir = new File("../"+bundleName);
-			if (dir.exists()) return dir;
+		} else {
+			File dir = new File("../" + bundleName);
+			if (dir.exists())
+				return dir;
 
-			dir = new File("../../diamond-jython.git/"+bundleName);
-			if (dir.exists()) return dir;
+			dir = new File("../../diamond-jython.git/" + bundleName);
+			if (dir.exists())
+				return dir;
 
-			dir = new File("../../scanning.git/"+bundleName);
-			if (dir.exists()) return dir;
+			dir = new File("../../scanning.git/" + bundleName);
+			if (dir.exists())
+				return dir;
 
 			// These paths refer to finding things in the travis build
 			// They will not resolve from the IDE or binary.
-			dir = new File("../../org.eclipse.scanning/"+bundleName);
-			if (dir.exists()) return dir;
+			dir = new File("../../org.eclipse.scanning/" + bundleName);
+			if (dir.exists())
+				return dir;
 
-			dir = new File("../../diamond-jython/"+bundleName);
-			if (dir.exists()) return dir;
+			dir = new File("../../diamond-jython/" + bundleName);
+			if (dir.exists())
+				return dir;
 
-			dir = new File("../../../diamond-jython/"+bundleName);
-			if (dir.exists()) return dir;
+			dir = new File("../../../diamond-jython/" + bundleName);
+			if (dir.exists())
+				return dir;
 		}
 		return null;
 	}
 
-    private static File find(File loc, String name) {
+	private static File find(File loc, String name) {
 		if (loc == null)
 			return null;
 		if (!loc.exists())
