4e4d9713e36c9dbaef58d6010ad6247a56c8a8b7
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-11-16 17:52:04 +0000

    DAQ-3260 LivePlottingComposite: check for null titleUpdateListener
    Change-Id: I6accfbc56a5af1ac78c8294e4fb287d5549a2b55

--- uk.ac.gda.client.live.stream/src/uk/ac/gda/client/live/stream/view/LivePlottingComposite.java
+++ uk.ac.gda.client.live.stream/src/uk/ac/gda/client/live/stream/view/LivePlottingComposite.java
@@ -285,8 +285,10 @@ public class LivePlottingComposite extends Composite {
 			if (dataset != null) {
 				dataset.removeDataListener(dataShapeChangeListener);
 				dataShapeChangeListener = null;
-				dataset.removeDataListener(titleUpdateListener);
-				titleUpdateListener = null;
+				if (titleUpdateListener != null) {
+					dataset.removeDataListener(titleUpdateListener);
+					titleUpdateListener = null;
+				}
 				dataset = null;
 			}
 			liveStreamConnection.removeAxisMoveListener(axisChangeListener);
