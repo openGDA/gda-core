2be1da0f09733f3ac46dd12ef745cdaccb497b0e
Maurizio Nagni <maurizio.nagni@diamond.ac.uk>
2021-01-27 21:19:34 +0000

    K11-1125 Reduce the acquisition configuration file name format
    - Defines a DEFAULT_CONFIGURATION_NAME
    - Refactor ScanningAcquisitionController.formatConfigurationFileName to
    fulfil the ticket request
    - rename ScanningAcquisitionController.getPath to saveTextDocument as
    more meaningful
    - refactor ScanningAcquisitionFileService.saveTextDocument so to
    override is necessary an existing file
    
    
    Change-Id: Id7ee66efac0d7314bb01a6214ce6398dceb2b049

--- uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/services/ScanningAcquisitionFileService.java
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/services/ScanningAcquisitionFileService.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -13,11 +15,14 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.naming.directory.InvalidAttributesException;
+
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.ac.diamond.daq.scanning.FilePathService;
+import uk.ac.gda.api.acquisition.AcquisitionControllerException;
 
 /**
  * In future this class may be merged to a more common one. For now it does the work.
@@ -31,15 +36,35 @@ public class ScanningAcquisitionFileService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ScanningAcquisitionFileService.class);
 
-	public Path saveTextDocument(String text, String fileName, String extension) throws IOException {
-		Path path = createPath(fileName, extension);
+	/**
+	 * Saves a configuration file
+	 * @param text the file content
+	 * @param fileName the file name
+	 * @param extension the file extent
+	 * @param override if {@code true} the file is overwritten, if {@code false} and the file exists throws a {@link FileAlreadyExistsException}
+	 * @return the path where the file has been saved
+	 * @throws IOException if an I/O exception occurs
+	 * @throws InvalidAttributesException if any of the parameters is {@code null}
+	 */
+	public Path saveTextDocument(String text, String fileName, String extension, boolean override) throws IOException, InvalidAttributesException {
+		Path path = buildPathToConfigDir(fileName, extension);
+		if (!override) {
+			fileExists(path);
+		}
 		path.toFile().getParentFile().mkdirs();
 		Files.write(path, text.getBytes(Charset.forName("UTF-8")), StandardOpenOption.CREATE);
 		return path;
 	}
 
-	public byte[] loadFileAsBytes(String fileName, String extension) throws IOException {
-		return Files.readAllBytes(createPath(fileName, extension));
+	/**
+	 * @param fileName
+	 * @param extension
+	 * @return the file content
+	 * @throws IOException if an I/O exception occours
+	 * @throws InvalidAttributesException if any of the parameters is {@code null}
+	 */
+	public byte[] loadFileAsBytes(String fileName, String extension) throws IOException, InvalidAttributesException {
+		return Files.readAllBytes(buildPathToConfigDir(fileName, extension));
 	}
 
 	public byte[] loadFileAsBytes(URL url) throws IOException {
@@ -74,8 +99,14 @@ public class ScanningAcquisitionFileService {
 		return Stream.empty();
 	}
 
-	public void delete(String fileName, String extension) throws IOException {
-		Files.deleteIfExists(createPath(fileName, extension));
+	/**
+	 * @param fileName
+	 * @param extension
+	 * @throws IOException if an I/O exception occours
+	 * @throws InvalidAttributesException if any of the parameters is {@code null}
+	 */
+	public void delete(String fileName, String extension) throws IOException, InvalidAttributesException {
+		Files.deleteIfExists(buildPathToConfigDir(fileName, extension));
 	}
 
 	private FilePathService getFilePathService() {
@@ -89,17 +120,34 @@ public class ScanningAcquisitionFileService {
 		return getFilePathService().getVisitConfigDir();
 	}
 
-	private Path createPath(String fileName, String extension) throws IOException {
+	/**
+	 * @param fileName
+	 * @param extension
+	 * @return the configuration file appended to the full path to the config dir
+	 * @throws InvalidAttributesException if any of the parameters is {@code null}
+	 */
+	private Path buildPathToConfigDir(String fileName, String extension) throws InvalidAttributesException {
 		validateNameAndExtension(fileName, extension);
 		return Paths.get(getBaseDir(), String.format("%s.%s", fileName, extension));
 	}
 
-	private void validateNameAndExtension(String fileName, String extension) throws IOException {
+	/**
+	 * @param fileName
+	 * @param extension
+	 * @throws AcquisitionControllerException if any of the parameters is {@code null}
+	 */
+	private void validateNameAndExtension(String fileName, String extension) throws InvalidAttributesException {
 		if (fileName == null) {
-			throw new IOException("Filename null");
+			throw new InvalidAttributesException("Filename null");
 		}
 		if (extension == null) {
-			throw new IOException("extension null");
+			throw new InvalidAttributesException("Extension null");
+		}
+	}
+
+	private void fileExists(Path path) throws FileAlreadyExistsException {
+		if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
+			throw new FileAlreadyExistsException("File already exists");
 		}
 	}
 }
