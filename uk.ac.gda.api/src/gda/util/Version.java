38681d1c5ac9f57f71fe4921694ce20e22c5874f
Joe Shannon <joe.shannon@diamond.ac.uk>
2021-03-17 15:16:17 +0000

    DAQ-3379 Set version number for next release (9.21)
    Change-Id: I84115119dff415c6523649d90d3325124051716f

--- uk.ac.gda.api/src/gda/util/Version.java
+++ uk.ac.gda.api/src/gda/util/Version.java
@@ -27,7 +27,7 @@ import org.slf4j.LoggerFactory;
  */
 public class Version {
 
-	private static final String RELEASE_VER = "9.20.0";
+	private static final String RELEASE_VER = "9.21.0pre";
 
 	/**
 	 * Returns the release version as a String
