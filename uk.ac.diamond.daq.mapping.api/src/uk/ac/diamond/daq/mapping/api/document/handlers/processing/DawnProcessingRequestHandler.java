f0bc0adda42ba5d6730f7058c4ea3f903b812630
Maurizio Nagni <maurizio.nagni@diamond.ac.uk>
2021-05-10 13:43:54 +0100

    K11-1246-3 Add the DawnProcessingRequestHandler
    Change-Id: I57142430c79023cd079d4b137f720e4d35246e9e

---
+++ uk.ac.diamond.daq.mapping.api/src/uk/ac/diamond/daq/mapping/api/document/handlers/processing/DawnProcessingRequestHandler.java
@@ -0,0 +1,68 @@
+/*-
+ * Copyright © 2020 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.diamond.daq.mapping.api.document.handlers.processing;
+
+import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.eclipse.scanning.api.script.ScriptRequest;
+import org.springframework.stereotype.Component;
+
+import uk.ac.gda.api.acquisition.configuration.processing.DawnProcessingRequest;
+import uk.ac.gda.api.acquisition.configuration.processing.ProcessingRequestPair;
+import uk.ac.gda.api.acquisition.configuration.processing.SavuProcessingRequest;
+import uk.ac.gda.api.exception.GDAException;
+
+/**
+ * Handler for {@link SavuProcessingRequest} devices
+ *
+ * @author Maurizio Nagni
+ */
+@Component
+class DawnProcessingRequestHandler extends ProcessingRequestHandler {
+	@Override
+	Collection<Object> translateToCollection(ProcessingRequestPair<?> processingRequest) throws GDAException {
+		return Optional.ofNullable(processingRequest)
+			.filter(DawnProcessingRequest.class::isInstance)
+			.map(DawnProcessingRequest.class::cast)
+			.map(this::translateValue)
+			.orElse(Collections.emptyList());
+	}
+
+	/**
+	 * 	At the moment savu process does not handle any {@code URL} syntax as {@code file:/path1} consequently
+	 *	the method call {@link URL#getPath()} to strip the URL protocol
+	 * @param request the savu processing request object
+	 * @return the converted object suitable for a ScanRequest
+	 */
+	private Collection<Object> translateValue(DawnProcessingRequest request) {
+		return request.getValue().stream()
+			.map(URL::getPath)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	ScriptRequest createScriptRequest(ProcessingRequestPair<?> processingRequest) throws GDAException {
+		return null;
+	}
+
+}
