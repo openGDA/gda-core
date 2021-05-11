16ca5db49812b66eba44ffa1ca1265749ca73037
Maurizio Nagni <maurizio.nagni@diamond.ac.uk>
2021-05-11 23:36:42 +0100

    K11-1251 Integrate the new AcquisitionConfigurationProperties
    - Refactor DocumentFactory
    - Removed the now useless OneDimensionLine, TwoDimentionLine
    - Deprecate the AcquisitionTypeProperty class
    
    
    Change-Id: I4d4ec840f2f19cb8e54502f95006dfb94be6e0d7

---
+++ uk.ac.gda.client/src/uk/ac/gda/client/properties/acquisition/AcquisitionTemplate.java
@@ -0,0 +1,46 @@
+/*-
+ * Copyright © 2021 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.client.properties.acquisition;
+
+import java.util.List;
+
+import uk.ac.diamond.daq.mapping.api.document.AcquisitionTemplateType;
+
+public class AcquisitionTemplate {
+
+	private AcquisitionTemplateType template;
+
+	private List<ScannableTrackDocumentProperty> tracks;
+
+	public AcquisitionTemplateType getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(AcquisitionTemplateType template) {
+		this.template = template;
+	}
+
+	public List<ScannableTrackDocumentProperty> getTracks() {
+		return tracks;
+	}
+
+	public void setTracks(List<ScannableTrackDocumentProperty> tracks) {
+		this.tracks = tracks;
+	}
+}
