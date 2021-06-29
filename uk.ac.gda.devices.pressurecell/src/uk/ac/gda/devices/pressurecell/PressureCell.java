2b21d60f1be8c92511b11c1c2708d8b05daec921
Peter Holloway <peter.holloway@diamond.ac.uk>
2021-06-29 15:29:46 +0100

    Add data writer controller for pressure cell
    Change-Id: Ic613d03f73200b2f7cb9fe4687a85812c0001435

--- uk.ac.gda.devices.pressurecell/src/uk/ac/gda/devices/pressurecell/PressureCell.java
+++ uk.ac.gda.devices.pressurecell/src/uk/ac/gda/devices/pressurecell/PressureCell.java
@@ -33,6 +33,7 @@ import gda.device.scannable.ScannableBase;
 import gda.device.scannable.ScannableUtils;
 import uk.ac.diamond.daq.concurrent.Async;
 import uk.ac.gda.devices.pressurecell.controller.PressureCellController;
+import uk.ac.gda.devices.pressurecell.data.PressureCellDataController;
 
 /**
  * Scannable interface to the pressure cell allowing it to be used in step scans.
@@ -46,7 +47,25 @@ public class PressureCell extends ScannableBase {
 			"Pressure Cell scannable letting the pressure cell be used in step scans.\n"
 			+ "Access to lower level controls are available via the controller attribute.";
 	private PressureCellController controller;
+	private PressureCellDataController dataController;
 	private Future<?> move = CompletableFuture.completedFuture(null);
+
+	public void setFilePath(String directory, String filename) throws DeviceException {
+		dataController.setFilePath(directory, filename);
+	}
+
+	public void setAcquire(boolean acquiring) throws DeviceException {
+		dataController.setAcquire(acquiring);
+		dataController.setDataWriter(acquiring);
+	}
+
+	public String getLastFileName() throws DeviceException {
+		return dataController.getLastFileName();
+	}
+
+	public void setTriggers(int before, int after) throws DeviceException {
+		dataController.setTriggers(before, after);
+	}
 
 	/** Set pressure at the pump to the same as the intermediate chamber */
 	private void matchIntermediatePressure() throws DeviceException, InterruptedException {
@@ -90,6 +109,30 @@ public class PressureCell extends ScannableBase {
 		});
 	}
 
+	public void setJumpPressures(double from, double to) throws DeviceException {
+		controller.setJumpToPressure(to);
+		controller.setJumpFromPressure(from);
+		controller.setJump();
+	}
+
+	public void armJumpValve() throws DeviceException {
+		double pCell = controller.getCellPressure();
+		double pMid = controller.getIntermediatePressure();
+		// valves should already be closed but just to make sure
+		controller.getV5().close();
+		controller.getV6().close();
+		controller.getV3().close();
+		if (pCell > pMid) {
+			// jumping to a lower pressure
+			controller.getV5().disarm();
+			controller.getV6().arm();
+		} else {
+			// jumping to a higher pressure
+			controller.getV5().arm();
+			controller.getV6().disarm();
+		}
+	}
+
 	public PressureCellController getController() {
 		return controller;
 	}
@@ -129,5 +172,13 @@ public class PressureCell extends ScannableBase {
 		// * This should return immediately if the move is complete.
 		// * If the move timed out, it would have thrown.
 		controller.waitForIdle(millis, MILLISECONDS);
+	}
+
+	public PressureCellDataController getDataController() {
+		return dataController;
+	}
+
+	public void setDataController(PressureCellDataController dataController) {
+		this.dataController = dataController;
 	}
 }
