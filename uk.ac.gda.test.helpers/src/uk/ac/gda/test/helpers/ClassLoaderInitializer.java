3d7afb56fb11e805ac36ddfbbd53e17018a481ae
Joe Shannon <joe.shannon@diamond.ac.uk>
2021-07-12 15:13:52 +0100

    DAQ-3611 Disable Eclipse Buddying for Spring bundle
    Add a Spring context initializer for use in tests to allow a custom
    class loader to be set.
    
    Change-Id: I91804f5a87b149fbcf3b34e7e800c965612bb9fa

---
+++ uk.ac.gda.test.helpers/src/uk/ac/gda/test/helpers/ClassLoaderInitializer.java
@@ -0,0 +1,36 @@
+/*-
+ * Copyright © 2021 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.test.helpers;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+
+import uk.ac.diamond.daq.classloading.GDAClassLoaderService;
+
+/**
+ * Initializer to set the context's class loader allowing Spring tests to run in Equinox.
+ */
+public class ClassLoaderInitializer implements ApplicationContextInitializer<GenericApplicationContext> {
+
+	@Override
+	public void initialize(GenericApplicationContext applicationContext) {
+		var classLoader = GDAClassLoaderService.getClassLoaderService().getClassLoader();
+		applicationContext.getBeanFactory().setBeanClassLoader(classLoader);
+	}
+}
