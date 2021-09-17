2adda9ce3c00b73a6728d5c245abe62e74f9e47b
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2021-09-17 10:57:26 +0100

    DAQ-3709 Open DataVis instead of DataBrowsing on clicking file in queue
    CHange the perspective id used and generate an OSGi file load event to
    open the file.
    
    Change-Id: I67c90fcf1ecb7709fa44c5cab691f6108171d9b7

--- org.eclipse.scanning.event.ui/src/org/eclipse/scanning/event/ui/view/DefaultScanResultsHandler.java
+++ org.eclipse.scanning.event.ui/src/org/eclipse/scanning/event/ui/view/DefaultScanResultsHandler.java
@@ -1,6 +1,7 @@
 package org.eclipse.scanning.event.ui.view;
 
 import java.io.File;
+import java.util.Map;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
@@ -13,10 +14,13 @@ import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.FileStoreEditorInput;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventAdmin;
 
 /**
  * Result handler for opening {@link ScanBean}s by opening their results files, as
@@ -26,7 +30,9 @@ import org.eclipse.ui.ide.FileStoreEditorInput;
  */
 public class DefaultScanResultsHandler implements IResultHandler<ScanBean> {
 
-	private static final String DATA_PERSPECTIVE_ID = "org.edna.workbench.application.perspective.DataPerspective";
+	private static final String DATA_PERSPECTIVE_ID = "org.dawnsci.datavis.DataVisPerspective";
+	private static final String FILE_OPEN_TOPIC = "org/dawnsci/events/file/OPEN";
+	private static final String PATH_KEY = "path";
 
 	@Override
 	public boolean isHandled(StatusBean bean) {
@@ -48,13 +54,12 @@ public class DefaultScanResultsHandler implements IResultHandler<ScanBean> {
 		IWorkbench workbench = PlatformUI.getWorkbench();
 		IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 
-		// Set the perspective to Data Browsing Perspective
-		// TODO FIXME When there is a general data viewing perspective from DAWN, use that.
+		// Set the perspective to Data Visualisation Perspective and load the file
 		workbench.showPerspective(DATA_PERSPECTIVE_ID, window);
-		String editorId = getEditorId(filePath);
-		IEditorInput editorInput = getEditorInput(filePath);
-		IWorkbenchPage page = workbench.getActiveWorkbenchWindow().getActivePage();
-		page.openEditor(editorInput, editorId);
+
+		BundleContext bundleContext = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
+		EventAdmin admin = bundleContext.getService(bundleContext.getServiceReference(EventAdmin.class));
+		admin.sendEvent(new Event(FILE_OPEN_TOPIC, Map.of(PATH_KEY, filePath)));
 		return true;
 	}
 
