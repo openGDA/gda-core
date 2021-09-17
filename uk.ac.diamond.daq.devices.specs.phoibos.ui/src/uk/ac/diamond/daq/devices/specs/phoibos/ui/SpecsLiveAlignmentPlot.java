64c6c136c580d4a037ef8e596602f4b14ba33941
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2021-09-17 14:43:18 +0100

    B07-619 Improve scaling of y axis in alignment plot
    Change-Id: I4acd214a5f2b6b20d305172eb485661eb2ca975d

--- uk.ac.diamond.daq.devices.specs.phoibos.ui/src/uk/ac/diamond/daq/devices/specs/phoibos/ui/SpecsLiveAlignmentPlot.java
+++ uk.ac.diamond.daq.devices.specs.phoibos.ui/src/uk/ac/diamond/daq/devices/specs/phoibos/ui/SpecsLiveAlignmentPlot.java
@@ -36,14 +36,20 @@ public class SpecsLiveAlignmentPlot extends SpecsLivePlot implements IObserver {
 
 	private static final Logger logger = LoggerFactory.getLogger(SpecsLiveAlignmentPlot.class);
 
+	private static final String PLOT_TITLE = "Alignment Spectrum";
+	private static final String XAXIS_TITLE = "Values";
+	private static final String YAXIS_TITLE = "Intensity (counts)";
+
 	@Override
 	public void createPartControl(Composite parent) {
 		super.createPartControl(parent);
 
 		try {
 			plottingSystem.createPlotPart(parent, "Spectrum", actionBars, PlotType.XY, this);
-			plottingSystem.setTitle("Alignment Spectrum");
 			plottingSystem.setShowLegend(false);
+			plottingSystem.getSelectedXAxis().setInverted(false);
+			plottingSystem.getSelectedYAxis().setAutoscale(false);
+			setPlotTitles();
 		} catch (Exception e) {
 			logger.error("Couldn't setup plotting system", e);
 			throw e;
@@ -57,28 +63,28 @@ public class SpecsLiveAlignmentPlot extends SpecsLivePlot implements IObserver {
 		if (update instanceof SpecsPhoibosSpectrumUpdate) {
 			SpecsPhoibosSpectrumUpdate spectrumUpdate = (SpecsPhoibosSpectrumUpdate) update;
 
-			// Generate time axis
-			int[] timeAxis = new int[spectrumUpdate.getDataLength()];
-			for(int i=0; i < timeAxis.length; i++) {
-				timeAxis[i] = i+1;
-			}
-
-			IDataset timeAxisDataset = DatasetFactory.createFromObject(timeAxis);
-
 			// Get data
 			IDataset spectrum = DatasetFactory.createFromObject(spectrumUpdate.getSpectrum());
 			spectrum.setName("Alignment Spectrum");
-			PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
-				// Thread safe so don't need to be in the UI thread
-				plottingSystem.updatePlot1D(timeAxisDataset, Arrays.asList(spectrum), null);
-				plottingSystem.getSelectedYAxis().setTitle("Intensity (counts)");
-				plottingSystem.getSelectedXAxis().setTitle("Values");
-				plottingSystem.getSelectedXAxis().setInverted(false);
-				plottingSystem.repaint();
-			});
+			plottingSystem.updatePlot1D(null, Arrays.asList(spectrum), null);
 
+			// Set plotting system
+			double maxY = spectrum.max(false).doubleValue();
+
+			PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
+				plottingSystem.getSelectedXAxis().setAutoscale(true);
+				plottingSystem.getSelectedYAxis().setRange(0, maxY * 1.05);
+				setPlotTitles();
+			});
 			logger.trace("Updated plotting system");
 
 		}
 	}
+
+	private void setPlotTitles() {
+		plottingSystem.setTitle(PLOT_TITLE);
+		plottingSystem.getSelectedYAxis().setTitle(YAXIS_TITLE);
+		plottingSystem.getSelectedXAxis().setTitle(XAXIS_TITLE);
+	}
+
 }
