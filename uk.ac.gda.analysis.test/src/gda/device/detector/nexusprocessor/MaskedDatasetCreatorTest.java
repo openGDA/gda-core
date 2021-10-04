71b349085cffd346ea509d923cae7da85c56682c
Joe Shannon <joe.shannon@diamond.ac.uk>
2021-10-04 15:21:59 +0100

    I07-414 Add mask dataset creator
    This allows a mask to be applied to the detector dataset before it
    is processed by the NexusProcessors e.g. ROI stats.
    
    Change-Id: Id2b3eee15bc7436b7f38480c1db3e2c97d546540

---
+++ uk.ac.gda.analysis.test/src/gda/device/detector/nexusprocessor/MaskedDatasetCreatorTest.java
@@ -0,0 +1,113 @@
+/*-
+ * Copyright © 2021 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.detector.nexusprocessor;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.eclipse.january.dataset.DatasetFactory;
+import org.eclipse.january.dataset.IntegerDataset;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MaskedDatasetCreatorTest {
+
+	private MaskedDatasetCreator maskProc;
+
+	@Before
+	public void setUp() {
+		maskProc = new MaskedDatasetCreator();
+	}
+
+	@Test
+	public void testExternalMask() throws Exception {
+		maskProc.setExternalMask(DatasetFactory.createFromObject(new int[] { 0, 0, 0, 0, 1, 4, 0, 0, 0 }, 3, 3));
+		var frame = DatasetFactory
+				.createFromObject(new int[] { 5938, 2389, 5998, 3432, 3456, 494944949, 5893, 8913, 1294 }, 3, 3);
+		var processed = maskProc.createDataSet(frame);
+		assertThat(processed.getInt(0, 0), Matchers.is(5938));
+		assertThat(processed.getInt(1, 2), Matchers.is(0));
+
+	}
+
+	@Test
+	public void testThresholdMassk() throws Exception {
+		maskProc.setThreshold(25, 45);
+		var frame = DatasetFactory.createFromObject(new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90 }, 3, 3);
+		var processed = maskProc.createDataSet(frame);
+		assertThat(processed.sum(), Matchers.is(70));
+	}
+
+
+	@Test
+	public void testNoMask() throws Exception {
+		var frame = DatasetFactory.createFromObject(new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90 }, 3, 3);
+		var processed = maskProc.createDataSet(frame);
+		assertThat(processed.sum(), Matchers.is(450));
+	}
+
+	@Test
+	public void testSettingPixels() throws Exception {
+		// [[1,2,3,4],[....11,12]]
+		var frame = DatasetFactory.createRange(IntegerDataset.class, 1, 13, 1).reshape(3,4);
+		maskProc.addMaskedPixel(1, 2); // has value 7
+		var processed = maskProc.createDataSet(frame);
+		assertThat(processed.sum(), Matchers.is(71));
+	}
+
+	@Test
+	public void testAllConditions() throws Exception {
+		// E.g. a detector frame with a gap border and hot pixels
+		var frame = DatasetFactory.createFromObject(new int[] {-1,-1,-1,-1,-1,-1,
+				-1,2659,225,195,763,-1,
+				-1,64,9999,823,2846,-1,
+				-1,484,585,3500,9999,-1,
+				-1,-1,-1,-1,-1,-1,
+		}, 5,6);
+		maskProc.setThreshold(0, 5000); // removes -1s and 9999s
+		maskProc.addMaskedPixel(3, 3); // removes 3500
+		var externalMask= DatasetFactory.zeros(5,6);
+		// These mask the pixels in the two thousands (by setting mask dataset to anything non zero)
+		externalMask.set(1, 1, 1);
+		externalMask.set(1, 2, 4);
+		maskProc.setExternalMask(externalMask);
+		var processed = maskProc.createDataSet(frame);
+		assertThat(processed.sum(), Matchers.is(3139));
+	}
+
+	@Test
+	public void testMaskAdaptsToChange() throws Exception {
+		var frame = DatasetFactory.ones(IntegerDataset.class,5, 5);
+		frame.set(1000, 2, 3);
+		maskProc.setThreshold(0, 10);
+		var processed = maskProc.createDataSet(frame);
+		assertThat(processed.sum(), Matchers.is(24));
+		// Another out of range pixel
+		frame.set(1000, 2,2);
+
+		processed = maskProc.createDataSet(frame);
+		// mask is not regenerated
+		assertThat(processed.sum(), Matchers.is(1023));
+
+		maskProc.regenerateMask();
+		processed = maskProc.createDataSet(frame);
+		assertThat(processed.sum(), Matchers.is(23));
+	}
+
+}
---
+++ uk.ac.gda.analysis/src/gda/device/detector/nexusprocessor/MaskedDatasetCreator.java
@@ -0,0 +1,146 @@
+/*-
+ * Copyright © 2021 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.detector.nexusprocessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.vecmath.Point2i;
+import javax.vecmath.Tuple2i;
+
+import org.eclipse.january.dataset.Comparisons;
+import org.eclipse.january.dataset.Dataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Apply a mask to a dataset. The mask is defined by three additive specifications:
+ * <li>A list of individual pixel cooordinates</li>
+ * <li>A high and low threshold - outliers are masked</li>
+ * <li>A Dataset mask - this could be set manually or provided by an external source</li>
+ * <p>
+ * The mask is cached and only regenerated when one of the parameters change or when {@link #regenerateMask()}
+ * is called (the previous dataset will be used).
+ *
+ */
+public class MaskedDatasetCreator implements DatasetCreator {
+
+	private static final Logger logger = LoggerFactory.getLogger(MaskedDatasetCreator.class);
+
+	/**
+	 * Actual mask to apply, generated with the three inputs
+	 */
+	private Dataset mask;
+
+	private Dataset externalMask;
+
+	private Dataset previousDs;
+
+	private List<Tuple2i> maskedPixels = new ArrayList<>();
+
+	// TODO is this suitable for other datatypes?
+	private Number minThreshold = Integer.MIN_VALUE;
+	private Number maxThreshold = Integer.MAX_VALUE;
+
+	@Override
+	public Dataset createDataSet(Dataset ds) throws Exception {
+		if (previousDs == null) {
+			// first time so create mask
+			createMask(ds);
+		}
+		previousDs = ds;
+		return ds.imultiply(mask);
+	}
+
+	private void createMask(Dataset ds) {
+		mask = Comparisons.logicalAnd(Comparisons.greaterThan(ds, minThreshold), Comparisons.lessThan(ds, maxThreshold));
+		if (externalMask != null) {
+			mask = Comparisons.logicalAnd(mask, externalMask);
+		}
+		maskedPixels.forEach(this::maskAPixel);
+	}
+
+	private void maskAPixel(Tuple2i point) {
+		mask.set(false, point.x, point.y);
+	}
+
+	public void regenerateMask() {
+		if (previousDs != null) {
+			createMask(this.previousDs);
+			return;
+		}
+		logger.info("No cached dataset - will not update mask");
+	}
+
+	public void addMaskedPixel(int x, int y) {
+		var tuple = new Point2i(x, y);
+		if (!maskedPixels.contains(tuple)) {
+			maskedPixels.add(tuple);
+		}
+		regenerateMask();
+	}
+
+	public List<Tuple2i> getMaksedPixels() {
+		return List.copyOf(maskedPixels);
+	}
+
+	public void removeMakedPixel(int x, int y) {
+		maskedPixels.remove(new Point2i(x,y));
+		regenerateMask();
+	}
+
+	public void clearMakedPixels() {
+		maskedPixels.clear();
+		regenerateMask();
+	}
+
+	/**
+	 * If mask is set directly it is assumed to be using conventions of <a href=
+	 * "https://manual.nexusformat.org/classes/base_classes/NXdetector.html#nxdetector-pixel-mask-field">NXdetector</a>
+	 * @param  externalMask
+	 */
+	public void setExternalMask(Dataset externalMask) {
+		this.externalMask = Comparisons.logicalNot(externalMask);
+		regenerateMask();
+	}
+
+	public Number getMinThreshold() {
+		return minThreshold;
+	}
+
+	public void setThreshold(Number min, Number max) {
+		setMinThreshold(min);
+		setMaxThreshold(max);
+	}
+
+	public void setMinThreshold(Number minThreshold) {
+		this.minThreshold = minThreshold;
+		regenerateMask();
+	}
+
+	public Number getMaxThreshold() {
+		return maxThreshold;
+	}
+
+	public void setMaxThreshold(Number maxThreshold) {
+		this.maxThreshold = maxThreshold;
+		regenerateMask();
+	}
+
+}
