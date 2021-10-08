996c4ce633c24db05f3c377cc8f1e9d0ec638ee6
Joe Shannon <joe.shannon@diamond.ac.uk>
2021-10-08 17:45:19 +0100

    Fix AbstractJythonTest
    If the state is already set and Jython has already been initialised by
    JythonInterpreterManager then the passed in bundle does not get added
    to the path, i.e. is essentially a silent failure.
    
    Add the path to Jython manually. This should be fixed in a later commit.
    
    Change-Id: I9158e966ba77dddfc0725796006a4cbb911fa984

--- org.eclipse.scanning.test/src/org/eclipse/scanning/test/command/AbstractJythonTest.java
+++ org.eclipse.scanning.test/src/org/eclipse/scanning/test/command/AbstractJythonTest.java
@@ -11,6 +11,10 @@
  *******************************************************************************/
 package org.eclipse.scanning.test.command;
 
+import static org.eclipse.scanning.jython.JythonInterpreterManager.getBundleLocation;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.Arrays;
 
 import org.eclipse.dawnsci.analysis.api.persistence.IMarshallerService;
@@ -47,7 +51,9 @@ public abstract class AbstractJythonTest extends BrokerTest{
 	}
 
 	static {
-		JythonInterpreterManager.setupSystemState("org.eclipse.scanning.command");
+		JythonInterpreterManager.setupSystemState();
+		try {
+			JythonInterpreterManager.addPath(getBundleLocation("org.eclipse.scanning.command") + "/scripts");
 		pi = new PythonInterpreter();
 		pi.exec("from mapping_scan_commands import *");
 		pi.exec("from mapping_scan_commands import _instantiate");
@@ -57,5 +63,8 @@ public abstract class AbstractJythonTest extends BrokerTest{
 		pi.exec("mandelbrot = lambda t: ('mandelbrot',"
 			+	"_instantiate(MandelbrotModel,"
 			+	"{'exposureTime': t, 'name': 'mandelbrot'}))");
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
 	}
 }
