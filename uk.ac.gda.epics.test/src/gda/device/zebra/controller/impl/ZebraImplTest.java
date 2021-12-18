2588ce9a2797824921c3486d8385a60621ec6e77
Dominic Oram <dominic.oram@diamond.ac.uk>
2021-12-18 12:35:01 +0000

    MXGDA-3723 Added unit tests for the Zebra to confirm correct behaviour against ophyd.
    Change-Id: I0a7af129a6fea65e48f3e4a8fe4a83e9cfe0fd8f

--- uk.ac.gda.epics.test/src/gda/device/zebra/controller/impl/ZebraImplTest.java
+++ uk.ac.gda.epics.test/src/gda/device/zebra/controller/impl/ZebraImplTest.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright © 2016 Diamond Light Source Ltd.
+ * Copyright © 2021 Diamond Light Source Ltd.
  *
  * This file is part of GDA.
  *
@@ -24,14 +24,23 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import gda.device.zebra.controller.Zebra;
-import gda.epics.CachedLazyPVFactory;
-import gda.epics.ReadOnlyPV;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import gda.device.zebra.LogicGateConfiguration;
+import gda.device.zebra.controller.Zebra;
+import gda.epics.CachedLazyPVFactory;
+import gda.epics.PV;
+import gda.epics.ReadOnlyPV;
 
 /**
  * Tests for ZebraImpl
@@ -42,14 +51,17 @@ public class ZebraImplTest {
 
 	private ZebraImpl zebraImpl;
 	private CachedLazyPVFactory pvFactory;
-	private ReadOnlyPV<Double[]> mockPV;
+	private ReadOnlyPV<Double[]> mockReadOnlyDoubleArrayPV;
+	private PV<Integer> mockIntegerPV;
 
 	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() throws Exception {
-		mockPV = mock(ReadOnlyPV.class);
+		mockReadOnlyDoubleArrayPV = mock(ReadOnlyPV.class);
+		mockIntegerPV = mock(PV.class);
 		pvFactory = mock(CachedLazyPVFactory.class);
-		when(pvFactory.getReadOnlyPVDoubleArray(anyString())).thenReturn(mockPV);
+		when(pvFactory.getReadOnlyPVDoubleArray(anyString())).thenReturn(mockReadOnlyDoubleArrayPV);
+		when(pvFactory.getPVInteger(anyString())).thenReturn(mockIntegerPV);
 
 		zebraImpl = new ZebraImpl();
 		zebraImpl.setPvFactory(pvFactory);
@@ -106,7 +118,7 @@ public class ZebraImplTest {
 	public void testGetEnc1AvalPVEnc1() {
 		ReadOnlyPV<Double[]> result = zebraImpl.getEnc1AvalPV();
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC1");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
 	}
 
 	@Test
@@ -114,14 +126,14 @@ public class ZebraImplTest {
 		zebraImpl.setUseAvalField(true);
 		ReadOnlyPV<Double[]> result = zebraImpl.getEnc1AvalPV();
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC1.AVAL");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
 	}
 
 	@Test
 	public void testGetEnc1AvalPVEnc2() {
 		ReadOnlyPV<Double[]> result = zebraImpl.getEnc1AvalPV();
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC1");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
 	}
 
 	@Test
@@ -129,7 +141,7 @@ public class ZebraImplTest {
 		zebraImpl.setUseAvalField(true);
 		ReadOnlyPV<Double[]> result = zebraImpl.getEnc1AvalPV();
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC1.AVAL");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
 	}
 
 	// getEncPV() returns the encoder corresponding to the parameter passed to it
@@ -138,20 +150,80 @@ public class ZebraImplTest {
 	public void testGetEncPVEnc1() {
 		ReadOnlyPV<Double[]> result = zebraImpl.getEncPV(Zebra.PC_ENC_ENC1);
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC1");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
 	}
 
 	@Test
 	public void testGetEncPVEnc2() {
 		ReadOnlyPV<Double[]> result = zebraImpl.getEncPV(Zebra.PC_ENC_ENC2);
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC2");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
 	}
 
 	@Test
 	public void testGetEncPVEnc3() {
 		ReadOnlyPV<Double[]> result = zebraImpl.getEncPV(Zebra.PC_ENC_ENC3);
 		verify(pvFactory).getReadOnlyPVDoubleArray("PC_ENC3");
-		assertEquals(mockPV, result);
+		assertEquals(mockReadOnlyDoubleArrayPV, result);
+	}
+
+	private void applyAndGateCheckPVsAndCheckValues(LogicGateConfiguration config, List<Integer> expectedPVValues) throws IOException {
+		zebraImpl.applyAndGateConfig(0, config);
+
+		ArgumentCaptor<String> pvNames = ArgumentCaptor.forClass(String.class);
+		ArgumentCaptor<Integer> putValues = ArgumentCaptor.forClass(Integer.class);
+		verify(pvFactory, times(6)).getPVInteger(pvNames.capture());
+		verify(mockIntegerPV, times(6)).putWait(putValues.capture());
+
+		assertEquals("AND0_ENA", pvNames.getAllValues().get(0));
+		assertEquals("AND0_INP1", pvNames.getAllValues().get(1));
+		assertEquals("AND0_INP2", pvNames.getAllValues().get(2));
+		assertEquals("AND0_INP3", pvNames.getAllValues().get(3));
+		assertEquals("AND0_INP4", pvNames.getAllValues().get(4));
+		assertEquals("AND0_INV", pvNames.getAllValues().get(5));
+
+		for(int i = 0; i < expectedPVValues.size(); i++) {
+			assertEquals(expectedPVValues.get(i), putValues.getAllValues().get(i));
+		}
+	}
+
+	@Test
+	public void testGivenSimpleLogicGateConfigWhenApplyAndGateConfigCalledThenPVsWrittenAsExpected() throws IOException {
+		LogicGateConfiguration config = new LogicGateConfiguration.Builder().input(1).source(3).build();
+
+		applyAndGateCheckPVsAndCheckValues(config, Arrays.asList(1, 3, 0, 0, 0, 0));
+	}
+
+	@Test
+	public void testGivenComplexLogicGateConfigWhenApplyAndGateConfigCalledThenPVsWrittenAsExpected() throws IOException {
+		LogicGateConfiguration config = new LogicGateConfiguration.Builder()
+				.input(1).source(17).invert()
+				.input(4).source(53)
+				.build();
+
+		applyAndGateCheckPVsAndCheckValues(config, Arrays.asList(9, 17,0, 0, 53, 1));
+	}
+
+	@Test
+	public void testGivenAnotherComplexLogicGateConfigWhenApplyAndGateConfigCalledThenPVsWrittenAsExpected() throws IOException {
+		LogicGateConfiguration config = new LogicGateConfiguration.Builder()
+				.input(3).source(23).invert()
+				.input(1).source(61)
+				.input(2).source(8).invert()
+				.build();
+
+		applyAndGateCheckPVsAndCheckValues(config, Arrays.asList(7, 61, 8, 23, 0, 6));
+	}
+
+	@Test
+	public void testGivenAllLogicGatesAppliedWhenApplyAndGateConfigCalledThenPVsWrittenAsExpected() throws IOException {
+		LogicGateConfiguration config = new LogicGateConfiguration.Builder()
+				.input(1).source(4).invert()
+				.input(2).source(15).invert()
+				.input(3).source(27).invert()
+				.input(4).source(38).invert()
+				.build();
+
+		applyAndGateCheckPVsAndCheckValues(config, Arrays.asList(15, 4, 15, 27, 38, 15));
 	}
 }
