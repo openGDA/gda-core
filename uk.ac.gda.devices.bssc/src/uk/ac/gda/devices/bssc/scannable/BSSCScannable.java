5476946ccd658db4843f8e6a3c4f6900da1cae91
Joseph Ware <joseph.ware@diamond.ac.uk>
2022-02-01 15:13:06 +0000

    DAQ-3941 Remove infinite loop in BSSCScannable
    - Encapsulate BSSCScannable
    - Move scheduled update of Scannable into Async scheduler
    - Observable updates now every 20s + when called, rather than every 20s
      since last called manually
    
    Change-Id: Ic2fed3ff63f4b47b885753b4553ad7a3f0f85a53

--- uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/scannable/BSSCScannable.java
+++ uk.ac.gda.devices.bssc/src/uk/ac/gda/devices/bssc/scannable/BSSCScannable.java
@@ -18,6 +18,9 @@
 
 package uk.ac.gda.devices.bssc.scannable;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 import org.embl.BaseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,56 +29,17 @@ import gda.device.DeviceException;
 import gda.device.Scannable;
 import gda.device.scannable.ScannableBase;
 import gda.factory.FactoryException;
+import uk.ac.diamond.daq.concurrent.Async;
 import uk.ac.gda.api.remoting.ServiceInterface;
 import uk.ac.gda.devices.bssc.BioSAXSSampleChanger;
 
 @ServiceInterface(Scannable.class)
 public class BSSCScannable extends ScannableBase {
+
 	private static final Logger logger = LoggerFactory.getLogger(BSSCScannable.class);
 
-	BioSAXSSampleChanger bssc;
-	Thread poller;
-	final BSSCScannable us = this;
-	public int waittime = 20000;
-	Object cachedPosition = null;
-
-	class Poller implements Runnable {
-
-		Object lastUpdateSent = null;
-
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					synchronized (us) {
-						us.wait(waittime);
-						Thread.sleep(100);
-					}
-				} catch (InterruptedException e) {
-					// Reset interrupt status and terminate loop
-					Thread.currentThread().interrupt();
-					return;
-				}
-				if (cachedPosition != null && cachedPosition != lastUpdateSent) {
-					notifyIObservers(us, cachedPosition);
-					lastUpdateSent = cachedPosition;
-				} else {
-					try {
-						notifyIObservers(us, us.getPosition());
-						lastUpdateSent = cachedPosition;
-					} catch (DeviceException e) {
-						logger.error("error reading postion for updates", e);
-					}
-				}
-			}
-		}
-	}
-
-	public void whackPoller() {
-		synchronized (us) {
-			us.notify();
-		}
-	}
+	private BioSAXSSampleChanger bssc;
+	private double[] cachedPosition = null;
 
 	@Override
 	public void configure() throws FactoryException {
@@ -83,13 +47,16 @@ public class BSSCScannable extends ScannableBase {
 			return;
 		}
 		super.configure();
-		setInputNames(new String[] {});
 		setExtraNames(new String[] { "seutemp", "storagetemp", "detergentlevel", "waterlevel", "wastelevel" });
 		setOutputFormat(new String[] { "%3.1f", "%3.1f", "%2.0f", "%2.0f", "%2.0f" });
-		if (poller == null || !poller.isAlive()) {
-			poller = new Thread(new Poller(), getName() + " polling thread");
-			poller.start();
-		}
+		// Try to update any IObservers at least every 20s
+		Async.scheduleAtFixedRate(() -> {
+			try {
+				getPosition();
+			} catch (DeviceException e) {
+				logger.error("error reading postion for updates", e);
+			}
+		}, 0, 20, TimeUnit.SECONDS);
 		setConfigured(true);
 	}
 
@@ -103,12 +70,15 @@ public class BSSCScannable extends ScannableBase {
 	}
 
 	@Override
-	public Object getPosition() throws DeviceException {
+	public double[] getPosition() throws DeviceException {
 		try {
-			cachedPosition = new double[] { bssc.getTemperatureSEU(), bssc.getTemperatureSampleStorage(),
+			double[] currentPosition = new double[] { bssc.getTemperatureSEU(), bssc.getTemperatureSampleStorage(),
 					bssc.getDetergentLevel(), bssc.getWaterLevel(), bssc.getWasteLevel() };
-			whackPoller();
-			return cachedPosition;
+			if (!Arrays.equals(currentPosition, cachedPosition)) {
+				notifyIObservers(this, currentPosition);
+				cachedPosition = currentPosition;
+			}
+			return currentPosition;
 		} catch (BaseException e) {
 			throw new DeviceException("Error getting sample changer values", e);
 		}
