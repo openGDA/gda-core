f3df3355e057048f43d64908fd411ee27dcf7f28
Peter Holloway <peter.holloway@diamond.ac.uk>
2022-05-30 15:29:21 +0100

    Add Bitfield column editor type
    Allow integer values to be edited as an array of boolean values with a
    checkbox for each.
    
    Change-Id: Iaf9c864f00b3dd8f840ea4fdc0516f5a808ff4bb

--- uk.ac.gda.devices.hatsaxs.ui/src/uk/ac/gda/devices/hatsaxs/ui/Column.java
+++ uk.ac.gda.devices.hatsaxs.ui/src/uk/ac/gda/devices/hatsaxs/ui/Column.java
@@ -18,8 +18,14 @@
 
 package uk.ac.gda.devices.hatsaxs.ui;
 
+import static org.eclipse.swt.SWT.CHECK;
+import static org.eclipse.swt.SWT.CR;
+import static org.eclipse.swt.SWT.ESC;
+import static org.eclipse.swt.SWT.NONE;
 import static uk.ac.gda.devices.hatsaxs.ui.HatsaxsMenu.UPDATE_METHOD;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
@@ -40,6 +46,8 @@ import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.richbeans.api.event.ValueEvent;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -75,6 +83,12 @@ public abstract class Column<T, V> {
 			@Override
 			public <E> CellEditor getCellEditor(Composite parent, @SuppressWarnings("unchecked") E... options) {
 				return new CheckboxCellEditor(parent);
+			}
+		},
+		BITFIELD {
+			@Override
+			public <E> CellEditor getCellEditor(Composite parent, E... options) {
+				return new BitfieldCellEditor(parent, options);
 			}
 		},
 		CHOICE {
@@ -205,7 +219,71 @@ public abstract class Column<T, V> {
 			}
 		}
 	}
-	
+
+	private static final class BitfieldCellEditor extends CellEditor {
+		private Map<Object, Button> buttons;
+		private Composite optionComp;
+
+		public BitfieldCellEditor(Composite parent, Object... labels) {
+			buttons = new LinkedHashMap<>();
+			for (var label : labels) {
+				buttons.put(label, null);
+			}
+			create(parent);
+		}
+
+		@Override
+		protected Control createControl(Composite parent) {
+			optionComp = new Composite(parent, NONE);
+			optionComp.setLayout(new GridLayout(buttons.size(), false));
+			for (var button : buttons.entrySet()) {
+				Button b = new Button(optionComp, CHECK);
+				b.setToolTipText(button.getKey().toString());
+				b.addKeyListener(new KeyAdapter() {
+					@Override
+					public void keyPressed(KeyEvent e) {
+						if (e.character == ESC) {
+							BitfieldCellEditor.this.fireCancelEditor();
+						} else if (e.character == CR) {
+							BitfieldCellEditor.this.fireApplyEditorValue();
+						}
+					}
+				});
+				b.setLayoutData(new GridData());
+				button.setValue(b);
+			}
+			return optionComp;
+		}
+
+		@Override
+		protected Object doGetValue() {
+			int state = 0;
+			int i = 0;
+			for (var entry : buttons.entrySet()) {
+				if (entry.getValue().getSelection()) {
+					state |= (1 << i);
+				}
+				i++;
+			}
+			return state;
+		}
+
+		@Override
+		protected void doSetFocus() {
+			optionComp.setFocus();
+		}
+
+		@Override
+		protected void doSetValue(Object value) {
+			var state = (int) value;
+			var i = 0;
+			for (var entry : buttons.entrySet()) {
+				entry.getValue().setSelection((state >> i & 1) == 1);
+				i++;
+			}
+		}
+	}
+
 	private int width;
 	private EditingSupport support;
 	private CellEditor cellEditor;
@@ -221,8 +299,8 @@ public abstract class Column<T, V> {
 	public Column(int width, TableViewer table, RichBeanEditorPart rbEditor, @SuppressWarnings("unchecked") V... choices) {
 		this(width, table,rbEditor,ColumnType.CHOICE.getCellEditor(table.getTable(), choices), false);
 	}
-	public Column(int width, TableViewer table, RichBeanEditorPart rbEditor, ColumnType type) {
-		this(width, table,rbEditor,type.getCellEditor(table.getTable()), false);
+	public Column(int width, TableViewer table, RichBeanEditorPart rbEditor, ColumnType type, @SuppressWarnings("unchecked") V... choices) {
+		this(width, table,rbEditor,type.getCellEditor(table.getTable(), choices), false);
 	}
 	public Column(int width, TableViewer table, RichBeanEditorPart rbEditor, ColumnType type, boolean fixed) {
 		this(width, table,rbEditor,type.getCellEditor(table.getTable()), fixed);
