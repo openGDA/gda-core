5daf53ce322e68f44f43815bdca707f057fbb2a4
Douglas Winter <douglas.winter@diamond.ac.uk>
2022-09-14 16:30:12 +0100

    Simplify driver profile wizard (now a dialog)
    Wizard was definitely the wrong tool for this configuration since there
    was no clear destination the wizard could take the user. The useful
    controls have been condensed into a single dialog.
    
    Still to do is making the model editors generic - presently assumes only
    one implementation (SingleAxisLinearSeries).
    
    Change-Id: I79b5a36a8a87a5c59346f6502aa7899f40cfede6

--- uk.ac.diamond.daq.experiment.ui/src/uk/ac/diamond/daq/experiment/ui/driver/DriverProfileSectionEditor.java
+++ uk.ac.diamond.daq.experiment.ui/src/uk/ac/diamond/daq/experiment/ui/driver/DriverProfileSectionEditor.java
@@ -1,3 +1,21 @@
+/*-
+ * Copyright © 2019 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.diamond.daq.experiment.ui.driver;
 
 import static uk.ac.diamond.daq.experiment.ui.ExperimentUiUtils.STRETCH;
@@ -9,11 +27,11 @@ import java.util.List;
 import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.UpdateValueStrategy;
-import org.eclipse.core.databinding.beans.BeanProperties;
+import org.eclipse.core.databinding.beans.typed.BeanProperties;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.core.databinding.validation.IValidator;
 import org.eclipse.core.databinding.validation.ValidationStatus;
-import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.jface.databinding.swt.typed.WidgetProperties;
 import org.eclipse.jface.fieldassist.ControlDecoration;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
@@ -29,21 +47,21 @@ import uk.ac.diamond.daq.experiment.api.ui.EditableWithListWidget;
 import uk.ac.diamond.daq.experiment.ui.widget.ElementEditor;
 
 public class DriverProfileSectionEditor implements ElementEditor {
-	
+
 	private Text start;
 	private Text stop;
 	private Text duration;
 	private Label startUnits;
 	private Label stopUnits;
-	
+
 	private final Color invalidEntryColour;
-	
+
 	private ControlDecoration durationDecoration;
-	
+
 	private DriverProfileSection section;
 	private final DataBindingContext dbc;
 	private List<Binding> bindings;
-	
+
 	public DriverProfileSectionEditor() {
 		dbc = new DataBindingContext();
 		bindings = new ArrayList<>();
@@ -55,25 +73,25 @@ public class DriverProfileSectionEditor implements ElementEditor {
 		Composite composite = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.swtDefaults().margins(10, 10).numColumns(4).equalWidth(true).applyTo(composite);
 		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(composite);
-		
+
 		new Label(composite, SWT.NONE).setText("Start");
 		start = new Text(composite, SWT.BORDER);
 		STRETCH.copy().span(2, 1).applyTo(start);
 		startUnits = new Label(composite, SWT.NONE);
 		STRETCH.applyTo(startUnits);
-		
+
 		new Label(composite, SWT.NONE).setText("Stop");
 		stop = new Text(composite, SWT.BORDER);
 		STRETCH.copy().span(2, 1).applyTo(stop);
 		stopUnits = new Label(composite, SWT.NONE);
 		STRETCH.applyTo(stopUnits);
-		
+
 		new Label(composite, SWT.NONE).setText("Duration");
 		duration = new Text(composite, SWT.BORDER);
 		durationDecoration = new ControlDecoration(duration, SWT.RIGHT | SWT.TOP);
 		STRETCH.copy().span(2, 1).applyTo(duration);
 		new Label(composite, SWT.NONE).setText("min");
-		
+
 		enableControls(false);
 	}
 
@@ -84,22 +102,21 @@ public class DriverProfileSectionEditor implements ElementEditor {
 		recreateBindings();
 		enableControls(true);
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	private void recreateBindings() {
-		IObservableValue<Double> startWidget = WidgetProperties.text(SWT.Modify).observe(start);
-		IObservableValue<Double> stopWidget = WidgetProperties.text(SWT.Modify).observe(stop);
-		IObservableValue<Double> durationWidget = WidgetProperties.text(SWT.Modify).observe(duration);
-		
-		IObservableValue<Double> startModel = BeanProperties.value("start").observe(section);
-		IObservableValue<Double> stopModel = BeanProperties.value("stop").observe(section);
-		IObservableValue<Double> durationModel = BeanProperties.value("duration").observe(section);
-		
+		IObservableValue<String> startWidget = WidgetProperties.text(SWT.Modify).observe(start);
+		IObservableValue<String> stopWidget = WidgetProperties.text(SWT.Modify).observe(stop);
+		IObservableValue<String> durationWidget = WidgetProperties.text(SWT.Modify).observe(duration);
+
+		IObservableValue<Double> startModel = BeanProperties.value("start", double.class).observe(section);
+		IObservableValue<Double> stopModel = BeanProperties.value("stop", double.class).observe(section);
+		IObservableValue<Double> durationModel = BeanProperties.value("duration", double.class).observe(section);
+
 		bindings.add(dbc.bindValue(startWidget, startModel));
 		bindings.add(dbc.bindValue(stopWidget, stopModel));
-		
-		IValidator positiveDuration = value -> {
-			if ((double) value >= 0) {
+
+		IValidator<Double> positiveDuration = value -> {
+			if (value >= 0) {
 				durationDecoration.hide();
 				duration.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
 				return ValidationStatus.ok();
@@ -107,15 +124,15 @@ public class DriverProfileSectionEditor implements ElementEditor {
 				duration.setBackground(invalidEntryColour);
 				durationDecoration.show();
 				durationDecoration.showHoverText("Duration cannot be negative!");
-				
+
 				return ValidationStatus.error("Duration cannot be negative!");
 			}
 		};
-		
-		UpdateValueStrategy strategy = new UpdateValueStrategy();
+
+		UpdateValueStrategy<String, Double> strategy = new UpdateValueStrategy<>();
 		strategy.setBeforeSetValidator(positiveDuration);
-		bindings.add(dbc.bindValue(durationWidget, durationModel, strategy, new UpdateValueStrategy()));
-		
+		bindings.add(dbc.bindValue(durationWidget, durationModel, strategy, new UpdateValueStrategy<>()));
+
 	}
 
 	private void removeOldBindings() {
@@ -132,11 +149,11 @@ public class DriverProfileSectionEditor implements ElementEditor {
 		section = null;
 		enableControls(false);
 	}
-	
+
 	private void enableControls(boolean enable) {
 		Arrays.asList(start, stop, duration).forEach(widget -> widget.setEnabled(enable));
 	}
-	
+
 	public void setUnits(String units) {
 		startUnits.setText(units);
 		stopUnits.setText(units);
--- uk.ac.diamond.daq.experiment.ui/src/uk/ac/diamond/daq/experiment/ui/driver/ProfileEditor.java
+++ uk.ac.diamond.daq.experiment.ui/src/uk/ac/diamond/daq/experiment/ui/driver/ProfileEditor.java
@@ -1,11 +1,29 @@
+/*-
+ * Copyright © 2022 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package uk.ac.diamond.daq.experiment.ui.driver;
 
 import static uk.ac.diamond.daq.experiment.ui.ExperimentUiUtils.STRETCH;
 
 import java.beans.PropertyChangeListener;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
@@ -15,102 +33,58 @@ import org.eclipse.january.dataset.Dataset;
 import org.eclipse.january.dataset.DatasetFactory;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
-import org.eclipse.jface.wizard.IWizardPage;
-import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.ac.diamond.daq.experiment.api.driver.DriverModel;
+import uk.ac.diamond.daq.experiment.api.Services;
 import uk.ac.diamond.daq.experiment.api.driver.DriverProfileSection;
 import uk.ac.diamond.daq.experiment.api.driver.SingleAxisLinearSeries;
 import uk.ac.diamond.daq.experiment.api.ui.EditableWithListWidget;
 import uk.ac.diamond.daq.experiment.ui.widget.ListWithCustomEditor;
 
-public class ProfileEditorPage extends WizardPage {
+public class ProfileEditor {
 
-	private static final Logger logger = LoggerFactory.getLogger(ProfileEditorPage.class);
-	private SingleAxisLinearSeries model;
-	
-	private Text profileNameText;
-	private Button saveButton;
-	private final Predicate<DriverModel> profileSaver;
-	private boolean profileChanged;
-	
-	protected ProfileEditorPage(Predicate<DriverModel> profileSaver) {
-		super(ProfileEditorPage.class.getCanonicalName());
-		setTitle("Editing driver profile");
-		setDescription("Don't forget to save your changes!");
-		this.profileSaver = profileSaver;
-	}
+	private static final Logger logger = LoggerFactory.getLogger(ProfileEditor.class);
 
 	private ListWithCustomEditor listEditor = new ListWithCustomEditor();
 	private DriverProfileSectionEditor elementEditor = new DriverProfileSectionEditor();
-	
+
 	private PropertyChangeListener modelChanged = event -> updatePlot();
-	
+
 	private IPlottingSystem<Composite> plottingSystem;
 	private String quantityName;
 	private String quantityUnits;
-	
-	@Override
+
+	private SingleAxisLinearSeries model;
+
 	public void createControl(Composite parent) {
-		
 		Composite base = new Composite(parent, SWT.BORDER);
+		base.setBackground(parent.getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		base.setBackgroundMode(SWT.INHERIT_FORCE);
 		GridLayoutFactory.fillDefaults().numColumns(2).applyTo(base);
-		STRETCH.applyTo(base);
-		
-		Composite topBit = new Composite(base, SWT.NONE);
-		GridLayoutFactory.swtDefaults().margins(10, 10).numColumns(10).equalWidth(true).applyTo(topBit);
-		STRETCH.copy().span(2, 1).applyTo(topBit);
-		
-		Composite nameComposite = new Composite(topBit, SWT.NONE);
-		GridLayoutFactory.fillDefaults().applyTo(nameComposite);
-		STRETCH.copy().span(8, 1).applyTo(nameComposite);
-		
-		new Label(nameComposite, SWT.NONE).setText("Profile name");
-		profileNameText = new Text(nameComposite, SWT.BORDER);
-		profileNameText.addListener(SWT.Modify, event -> updateName());
-		STRETCH.applyTo(profileNameText);
-		
-		saveButton = new Button(topBit, SWT.BORDER);
-		saveButton.setText("Save profile");
-		GridDataFactory.swtDefaults().span(2, 2).grab(true, true).align(SWT.FILL, SWT.FILL).applyTo(saveButton);
-		saveButton.addListener(SWT.Selection, event -> {
-			boolean saved = profileSaver.test(model);
-			saveButton.setEnabled(!saved);
-			setPageComplete(saved);
-			profileChanged |= saved;
-		});
-		saveButton.setEnabled(false);
-		
-		// a separator
-		GridDataFactory.fillDefaults().span(2, 1).grab(true, false).applyTo(
-				new Label(base, SWT.SEPARATOR | SWT.HORIZONTAL));
-		
+		STRETCH.copy().align(SWT.FILL, SWT.FILL).grab(true, true).applyTo(base);
+
 		Composite listEditorComposite = new Composite(base, SWT.NONE);
 		GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).grab(false, true).applyTo(listEditorComposite);
 		GridLayoutFactory.swtDefaults().applyTo(listEditorComposite);
-		
+
 		listEditorComposite.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
 		listEditorComposite.setBackgroundMode(SWT.INHERIT_FORCE);
-		
+
 		new Label(listEditorComposite, SWT.NONE).setText("Sections");
-		
-		listEditor.setMinimumElements(1);
+
 		listEditor.setListHeight(150);
 		listEditor.setTemplate(new DriverProfileSection());
 
 		listEditor.setElementEditor(elementEditor);
 		listEditor.addListListener(event -> listChanged());
 		listEditor.create(listEditorComposite);
-		
+
 		try {
 			plottingSystem = PlottingFactory.createPlottingSystem();
 		} catch (Exception e) {
@@ -119,85 +93,77 @@ public class ProfileEditorPage extends WizardPage {
 		}
 		plottingSystem.createPlotPart(base, "profile", null, PlotType.XY, null);
 		plottingSystem.getPlotComposite().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
+
 		listEditor.addListListener(modelChanged);
 		updatePlot();
-		
-		setControl(base);
 	}
-	
-	private void listChanged() {
-		if (model != null) {
-			model.setProfile(listEditor.getList().stream().map(DriverProfileSection.class::cast).collect(Collectors.toList()));
-			saveButton.setEnabled(true);
-			setPageComplete(false);
-			updatePlot();
-		}
+
+	public void setModel(SingleAxisLinearSeries model) {
+		this.model = model;
+		listEditor.getList().forEach(section -> section.removePropertyChangeListener(modelChanged));
+		listEditor.setList(model.getProfile().stream()
+				.map(EditableWithListWidget.class::cast).collect(Collectors.toList()));
+
+		model.getProfile().forEach(section -> section.addPropertyChangeListener(modelChanged));
+		updatePlot();
 	}
-	
-	void updatePlot() {
+
+	public void clear() {
+		this.model = null;
+		listEditor.setList(Collections.emptyList());
 		plottingSystem.clear();
-		
+	}
+
+	private void updatePlot() {
+		plottingSystem.clear();
+
 		// create dataset from model
-		List<DriverProfileSection> segments = listEditor.getList().stream().map(DriverProfileSection.class::cast).collect(Collectors.toList());
+		List<DriverProfileSection> segments = getProfile();
 		if (segments.isEmpty()) return;
-		
+
 		double[] x = new double[segments.size()+1];
 		double[] y = new double[segments.size()+1];
-		
+
 		x[0] = 0;
 		y[0] = segments.get(0).getStart();
-		
+
 		for (int i = 0; i < segments.size(); i++) {
 			x[i+1] = segments.get(i).getDuration() + x[i];
 			y[i+1] = segments.get(i).getStop();
 		}
-		
+
 		final Dataset xDataset = DatasetFactory.createFromObject(x);
 		final Dataset yDataset = DatasetFactory.createFromObject(y);
-		
+
 		xDataset.setName("Time (min)");
 		yDataset.setName(getQuantityName() + " (" + getQuantityUnits() + ")");
-		
+
 		plottingSystem.createPlot1D(xDataset, Arrays.asList(yDataset), null);
 		plottingSystem.clearAnnotations();
 		plottingSystem.setTitle("");
 		plottingSystem.setShowLegend(false);
 	}
-	
-	private void updateName() {
-		if (model != null) {
-			model.setName(profileNameText.getText());
-			saveButton.setEnabled(true);
-		}
-	}
-	
+
 	private String getQuantityName() {
 		return quantityName != null ? quantityName : "Unknown";
 	}
 	private String getQuantityUnits() {
 		return quantityUnits != null ? quantityUnits : "a.u.";
 	}
-	
-	@Override
-	public IWizardPage getPreviousPage() {
-		if (profileChanged && getWizard() instanceof ExperimentDriverWizard) {
-			((ExperimentDriverWizard) getWizard()).shouldReloadData();
-		}
-		return super.getPreviousPage();
+
+	private void listChanged() {
+		listEditor.getList().forEach(item -> {
+			item.removePropertyChangeListener(modelChanged);
+			item.addPropertyChangeListener(modelChanged);
+		});
 	}
-	
-	void setProfile(SingleAxisLinearSeries profile, String quantityName, String quantityUnits) {
-		this.model = profile;
-		profileNameText.setText(profile.getName());
-		listEditor.setList(profile.getProfile().stream().map(EditableWithListWidget.class::cast).collect(Collectors.toList()));
-		listEditor.refresh();
-		saveButton.setEnabled(false);
-		profileChanged = false;
-		this.quantityName = quantityName;
-		this.quantityUnits = quantityUnits;
-		elementEditor.setUnits(quantityUnits);
-		updatePlot();
+
+	public void saveProfile(String driver, String experimentId) {
+		model.setProfile(getProfile());
+		Services.getExperimentService().saveDriverProfile(model, driver, experimentId);
 	}
-	
+
+	private List<DriverProfileSection> getProfile() {
+		return listEditor.getList().stream().map(DriverProfileSection.class::cast).collect(Collectors.toList());
+	}
 }
