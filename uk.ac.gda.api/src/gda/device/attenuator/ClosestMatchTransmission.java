fc892ead9b51bb239e23a7ece875f0d5a152da36
Paul Coe <paul.coe@diamond.ac.uk>
2022-09-14 14:20:07 +0100

    I04-675 updates use of ClosestMatchTransmission
    * Adds getter and setter methods and encapsulates
       serializable fields
    
    * Improves toString report after suggestions
        from beamline scientists who reported
       sources of potential confusion
    
    * Adds comments to methods in Attenuator class
    
    Change-Id: I9ec3bc8eb3aa5f52294cca0983a6d8feaf67e949

--- uk.ac.gda.api/src/gda/device/attenuator/ClosestMatchTransmission.java
+++ uk.ac.gda.api/src/gda/device/attenuator/ClosestMatchTransmission.java
@@ -21,22 +21,55 @@ package gda.device.attenuator;
 import java.io.Serializable;
 
 /**
- * Holds the closest transmission that was achievable, together with the energy that was used to calculate it.
+ * Instances wrap the nearest achievable transmission together with the energy that was used to calculate it:
+ * As some beamline attenuation systems provides discrete states, where only specific transmission levels are available.
  */
-public class ClosestMatchTransmission implements Serializable {
-	
-	/** Closest transmission value that could be achieved */
-	public double closestAchievableTransmission;
-	
-	/** Energy which was used in the calculation (may be different to the supplied energy) */
-	public double energy;
-	
+public final class ClosestMatchTransmission implements Serializable {
+
+	private static final String DISPLAY_FORMAT = "%s (transmission %.6f ( %.4f percent ), energy %.2f keV)";
+
+	/** Closest transmission value that could be achieved (in 0.0 to 1.0 unit range )*/
+	private double closestAchievableTransmission;
+
+	/** Energy / keV which was used in the calculation (may be different to the supplied energy) */
+	private double energy;
+
 	@Override
 	public String toString() {
-		return String.format("%s(transmission=%.6f, energy=%.2f)",
+		return String.format(DISPLAY_FORMAT,
 			getClass().getSimpleName(),
-			closestAchievableTransmission,
-			energy);
+			getClosestAchievableTransmission(),
+			getClosestAchievableTransmissionAsPercentage(),
+			getEnergy());
 	}
 
+	/**
+	 * Provides energy at which transmission matching was calculated.
+	 * @return corresponding energy in keV
+	 */
+	public double getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(double energyInKev) {
+		energy = energyInKev;
+	}
+
+	/**
+	 * @return transmission in 0.0 to 1.0 scale
+	 */
+	public double getClosestAchievableTransmission() {
+		return closestAchievableTransmission;
+	}
+
+	/**
+	 * Sets transmission in 0.0 to 1.0 scale
+	 */
+	public void setClosestAchievableTransmission(double transmission) {
+		closestAchievableTransmission = transmission;
+	}
+
+	public double getClosestAchievableTransmissionAsPercentage() {
+		return closestAchievableTransmission * 100.0;
+	}
 }
