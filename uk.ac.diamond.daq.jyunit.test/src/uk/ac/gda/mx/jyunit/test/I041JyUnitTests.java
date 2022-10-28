2132307eed4c60f3c171312a43a5ca6e21c44268
Paul Coe <paul.coe@diamond.ac.uk>
2022-10-28 14:18:25 +0100

    I04_1-1023 Adds test runner for I04-1 Jython tests
    * See also, accompanying change in gda-mx
       gerrit change 36769
    
    Change-Id: I23213c1367ca820eb6d02fde53c115aa75600094

---
+++ uk.ac.diamond.daq.jyunit.test/src/uk/ac/gda/mx/jyunit/test/I041JyUnitTests.java
@@ -0,0 +1,31 @@
+/*-
+ * Copyright © 2022 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.mx.jyunit.test;
+
+import uk.ac.diamond.daq.jyunit.test.BaseMxJyUnitTestRunner;
+
+public final class I041JyUnitTests extends BaseMxJyUnitTestRunner {
+
+	private static final String CONFIG_SPECIFIER = "i04-1";
+
+	@Override
+	public String getSpecifiedConfiguration() {
+		return CONFIG_SPECIFIER;
+	}
+}
