eb7e042ff5612678226340c22f8d1ba189d921d2
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2023-08-22 10:04:26 +0100

    DAQ-4871 ensure OSGi Service is called dynamically only once
    - add initialization checking in method calls
    - fix "CommonBeamlineDeviceConfiguration object has no attribute
    'setEnforceMandatoryDevices'"
    
    Change-Id: I4566ea2666457c093b3998f9da9e3d48aabc3a68

--- uk.ac.gda.core/scripts/gdascripts/metadata/nexus_metadata_class.py
+++ uk.ac.gda.core/scripts/gdascripts/metadata/nexus_metadata_class.py
@@ -6,10 +6,12 @@ from uk.ac.gda.epics.nexus.device import EpicsNexusMetadataUtility
 from org.eclipse.scanning.device.utils import NexusMetadataUtility
 from gdascripts import installation
 from gdascripts.synchrotron_operation_mode import get_machine_state, USER, SPECIAL
-from org.eclipse.scanning.device import Services
 
 class Metadata:
     '''provide methods for dynamic metadata operation during runtime'''
+    
+    def __init__(self):
+        self.initialized = False
 
     def addScalar(self, device_name, field_name, field_value, unit=None):
         '''
@@ -19,7 +21,7 @@ class Metadata:
         @param field_value: the SCALAR value to be added.
         @param unit : the physical unit for this field_value if available, default is None
         '''
-        NexusMetadataUtility.INSTANCE.addScalar(device_name, field_name, field_value, unit)
+        self.get_metadata_instance().addScalar(device_name, field_name, field_value, unit)
   
     def addScannable(self, device_name, scannable):
         '''
@@ -27,7 +29,7 @@ class Metadata:
         @param device_name: the name of metadata device
         @param scannable: the Scannable or scannable name to be added.
         '''
-        NexusMetadataUtility.INSTANCE.addScannable(device_name,  scannable)
+        self.get_metadata_instance().addScannable(device_name,  scannable)
     
     def addPV(self, device_name, field_name, pv_name, unit=None):
         '''
@@ -37,7 +39,7 @@ class Metadata:
         @param pv_name: the PV name to be added.
         @param unit : the physical unit for this pv_name if available, default is None
         '''
-        EpicsNexusMetadataUtility.INSTANCE.addPV(device_name, field_name, pv_name, unit)    
+        self.get_epics_metadata_instance().addPV(device_name, field_name, pv_name, unit)    
     
     def addLink(self, device_name, field_name, link_path, file_name=None):
         '''
@@ -47,7 +49,7 @@ class Metadata:
         @param link_path: the LINK path to be added, starting from Tree ROOT '/'.
         @param file_name: the external file name in which the link path to be added, if None the link will be internal
         '''
-        NexusMetadataUtility.INSTANCE.addLink(device_name, field_name, file_name, link_path)
+        self.get_metadata_instance().addLink(device_name, field_name, file_name, link_path)
     
     def rm(self, device_name, field_name):
         """
@@ -55,7 +57,7 @@ class Metadata:
         @param device_name: the name of metadata device from which a given field to be removed
         @param field_name: the name of the field to be removed from the give metadata device
         """
-        NexusMetadataUtility.INSTANCE.remove(device_name, field_name)
+        self.get_metadata_instance().remove(device_name, field_name)
     
     def disable(self, *device_names):
         """
@@ -63,7 +65,7 @@ class Metadata:
         @param args: list of metadata device names to be disabled
         """
         for device_name in device_names:
-            NexusMetadataUtility.INSTANCE.disable(device_name) 
+            self.get_metadata_instance().disable(device_name) 
     
     def enable(self, *device_names):
         """
@@ -71,7 +73,7 @@ class Metadata:
         @param args: list of metadata device names to be enabled
         """
         for device_name in device_names:
-            NexusMetadataUtility.INSTANCE.enable(device_name) 
+            self.get_metadata_instance().enable(device_name) 
     
     def ll(self, *args):
         """
@@ -79,11 +81,11 @@ class Metadata:
         @param args: list of device names to be displayed in Jython terminal, if empty or not supplied it will display all metadata
         """
         if len(args) == 0:
-            NexusMetadataUtility.INSTANCE.list(True)
+            self.get_metadata_instance().list(True)
         else:
             for each in args:
                 if isinstance(each, str):
-                    NexusMetadataUtility.INSTANCE.display(each, True)
+                    self.get_metadata_instance().display(each, True)
                 else:
                     raise ValueError("Input must be name or names of metadata devices in String!")
     
@@ -93,11 +95,11 @@ class Metadata:
         @param args: list of device names to be displayed in Jython terminal, if empty or not supplied it will display all metadata
         """
         if len(args) == 0:
-            NexusMetadataUtility.INSTANCE.list(False)
+            self.get_metadata_instance().list(False)
         else:
             for each in args:
                 if isinstance(each, str):
-                    NexusMetadataUtility.INSTANCE.display(each, False)
+                    self.get_metadata_instance().display(each, False)
                 else:
                     raise ValueError("Input must be name or names of metadata devices in String!")
     
@@ -105,10 +107,21 @@ class Metadata:
         '''
         clear all user added metadata.
         '''
-        NexusMetadataUtility.INSTANCE.clear()
-    
+        self.get_metadata_instance().clear()
+        
+    def initialize_common_device_configuration(self):
+        if not self.initialized:
+            from org.eclipse.scanning.device import Services
+            Services.getCommonBeamlineDevicesConfiguration().setEnforceMandatoryDeviceNames(
+                    installation.isLive() and get_machine_state() in [USER, SPECIAL])
+            self.initialized = True
 
-Services.getCommonBeamlineDevicesConfiguration().setEnforceMandatoryDevices(
-        installation.isLive() and get_machine_state() in [USER, SPECIAL])
+    def get_metadata_instance(self):
+        self.initialize_common_device_configuration()
+        return NexusMetadataUtility.INSTANCE
+    
+    def get_epics_metadata_instance(self):
+        self.initialize_common_device_configuration()
+        return EpicsNexusMetadataUtility.INSTANCE
 
 meta = Metadata()
