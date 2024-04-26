e8c356f56cc494155ab12c4d614af50fb1ef525e
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2024-04-26 11:25:29 +0100

    I09-538 - add new property setting to further customise NXdata group
    Added "gda.nexus.nexusScanDataWriter.primaryAddAllExtraNames" (default
    false) to CounterTimerNexusDevice which adds ability to add all extra
    names to the NXdata group rather than a single one.
    
    Change-Id: Ie39b05609527d905b97c188510d97236e9aca73a

--- uk.ac.gda.core/src/gda/data/scan/nexus/device/CounterTimerNexusDevice.java
+++ uk.ac.gda.core/src/gda/data/scan/nexus/device/CounterTimerNexusDevice.java
@@ -25,10 +25,12 @@ import java.util.LinkedHashMap;
 import org.eclipse.dawnsci.analysis.api.tree.DataNode;
 import org.eclipse.dawnsci.nexus.INexusDevice;
 import org.eclipse.dawnsci.nexus.IWritableNexusDevice;
+import org.eclipse.dawnsci.nexus.NXdata;
 import org.eclipse.dawnsci.nexus.NXdetector;
 import org.eclipse.dawnsci.nexus.NexusException;
 import org.eclipse.dawnsci.nexus.NexusScanInfo;
 import org.eclipse.dawnsci.nexus.NexusUtils;
+import org.eclipse.dawnsci.nexus.builder.NexusObjectWrapper;
 import org.eclipse.january.DatasetException;
 import org.eclipse.january.dataset.ILazyWriteableDataset;
 import org.eclipse.january.dataset.SliceND;
@@ -46,11 +48,41 @@ public class CounterTimerNexusDevice extends AbstractDetectorNexusDeviceAdapter 
 
 	public static final String PRIMARY_EXTRANAME_DATA_FIELD_INDEX = "gda.nexus.nexusScanDataWriter.primaryExtraNameDataFieldIndex";
 	public static final int PRIMARY_EXTRANAME_DATA_FIELD_LAST = -1;
+	public static final String ADD_ALL_EXTRA_NAMES_AS_AXES_FIELDS = "gda.nexus.nexusScanDataWriter.addAllExtraNamesAsAxesFields";
 
 	private LinkedHashMap<String, DataNode> dataNodes = null;
 
 	public CounterTimerNexusDevice(Detector detector) {
 		super(detector);
+	}
+
+	/**
+	 * Configure the nexus wrapper, an object that contains the nexus object and contains additional information
+	 * describing how the nexus object should be added to the nexus tree - in particular to {@link NXdata} groups.
+	 * <BR><BR>
+	 * The default behaviour is it will use the last extra name to add to the primary field in the NXdata group. Please
+	 * see the function:
+	 * <code>
+	 * getPrimaryDataFieldName()
+	 * </code>
+	 * An additional setting to add all extra names to the NXdata group is by using the following property:
+	 * <code>
+	 * gda.nexus.nexusScanDataWriter.addAllExtraNamesAsAxesFields
+	 * </code>
+	 * when set to true. The default is false.
+	 *
+	 * @param nexusWrapper
+	 * @param info
+	 * @throws NexusException
+	 */
+	@Override
+	protected void configureNexusWrapper(NexusObjectWrapper<NXdetector> nexusWrapper, NexusScanInfo info)
+			throws NexusException {
+
+		super.configureNexusWrapper(nexusWrapper, info);
+		if (LocalProperties.check(ADD_ALL_EXTRA_NAMES_AS_AXES_FIELDS, false)) {
+			nexusWrapper.addAxisDataFieldNames(getFieldNames());
+		}
 	}
 
 	/**
