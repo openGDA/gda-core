9123ebd4717b348f410b6e839d95cc79e1fa9541
Douglas Winter <douglas.winter@diamond.ac.uk>
2024-09-18 10:00:57 +0100

    K11-2206 Ensure periodic triggers with offset are within bounds
    Change-Id: I9ef9a843d1c422bc1252e26aa4327d3ecef65a56

--- uk.ac.diamond.daq.experiment.test/src/uk/ac/diamond/daq/experiment/ui/plan/preview/RepeatingTimeBasedTriggerLocatorTest.java
+++ uk.ac.diamond.daq.experiment.test/src/uk/ac/diamond/daq/experiment/ui/plan/preview/RepeatingTimeBasedTriggerLocatorTest.java
@@ -2,8 +2,10 @@ package uk.ac.diamond.daq.experiment.ui.plan.preview;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -83,11 +85,72 @@ public class RepeatingTimeBasedTriggerLocatorTest {
 		assertThat(locator.getY().isPresent(), is(false));
 	}
 
+	@Test
+	public void triggersWithOffsetAllBoundWithinSegment() {
+
+		var limit = 1.0; // in min
+
+		var interval = 5; // in s
+		var offset = 1; // in s
+
+		Dataset xData = data(0.0, limit);
+		Dataset yData = data(0.0, 0.0);
+
+		TriggerLocator<List<Double>> locator = getLocator(interval, offset, xData, yData);
+		locator.search(0.0, limit);
+
+		var xResults = locator.getX().get();
+
+		var allBoundWithinSegment = xResults.stream().noneMatch(x -> x > limit);
+		assertTrue("At least one trigger found outside the segment bounds!", allBoundWithinSegment);
+
+		var expectedTriggers = (int) Math.floor((limit * 60 - offset) / interval);
+		assertThat(xResults.size(), is(equalTo(expectedTriggers)));
+	}
+
+	@Test
+	public void negativeOffsetCorrectlyHandled() {
+		// in min:
+		var limit = 1; // 60 s
+
+		// in seconds:
+		var period = 10;
+		var offset = -2;
+
+		// without offset we would expect 5 triggers
+		// (6th would be exactly on the limit so would not occur)
+
+		Dataset xData = data(0.0, limit);
+		Dataset yData = data(0.0, 0.0);
+
+		TriggerLocator<List<Double>> locator = getLocator(period, offset, xData, yData);
+		locator.search(0.0, limit);
+
+		var xResults = locator.getX().get();
+
+		var allBoundWithinSegment = xResults.stream().noneMatch(x -> x > limit);
+		assertTrue("At least one trigger found outside the segment bounds!", allBoundWithinSegment);
+
+		// with the above offset we would expect 6 triggers
+		assertThat(xResults.size(), is(equalTo(6)));
+
+		// first at 8 seconds...
+		assertEquals(8/60.0, xResults.getFirst(), ERROR);
+
+		// last at 58 seconds...
+		assertEquals(58/60.0, xResults.getLast(), ERROR);
+	}
+
 	private TriggerLocator<List<Double>> getLocator(double interval, Dataset xData, Dataset yData) {
+		return getLocator(interval, 0, xData, yData);
+	}
+
+	private TriggerLocator<List<Double>> getLocator(double interval, double offset, Dataset xData, Dataset yData) {
 		TriggerDescriptor trigger = new TriggerDescriptor();
 		trigger.setSignalSource(SignalSource.TIME);
 		trigger.setExecutionPolicy(ExecutionPolicy.REPEATING);
 		trigger.setInterval(interval);
+		trigger.setOffset(offset);
 
 		return new RepeatingTimeBasedTriggerLocator(trigger, xData, yData);
 	}
