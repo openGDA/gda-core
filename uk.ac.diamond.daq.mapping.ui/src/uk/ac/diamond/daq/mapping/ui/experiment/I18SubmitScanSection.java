832207a8d84c062cdebcbbf702dff068e515c67f
Iain Hall <iain.hall@diamond.ac.uk>
2024-10-09 16:52:09 +0100

    I18-642 : Add tomography scan to mapping GUI
    This change is to add tomography scan to the mapping GUI. When I18SubmitScanSection
    is shown in the GUI (e.g. via SubmitScanSelector) it automatically sets
    the fast and slow axes to t1x and t1theta respectively. The previous fast
    and slow axes settings are restored when the view is hidden.
    
    Change-Id: I94a0ce3ebd23a012b0def9edca80345a9c68b370

--- uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/I18SubmitScanSection.java
+++ uk.ac.diamond.daq.mapping.ui/src/uk/ac/diamond/daq/mapping/ui/experiment/I18SubmitScanSection.java
@@ -18,36 +18,63 @@
 
 package uk.ac.diamond.daq.mapping.ui.experiment;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
-import org.eclipse.scanning.api.IScannable;
-import org.eclipse.scanning.api.scan.ScanningException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import uk.ac.diamond.daq.mapping.api.IMappingExperimentBean;
+import uk.ac.diamond.daq.mapping.impl.MappingStageInfo;
 
 public class I18SubmitScanSection  extends SubmitScanSection {
-	private static final Logger log = LoggerFactory.getLogger(I18SubmitScanSection.class);
+	private static final Logger logger = LoggerFactory.getLogger(I18SubmitScanSection.class);
+
+	private String fastAxisName = "t1x";
+	private String slowAxisName = "t1theta";
+	private String[] axes = {};
 
 	@Override
-	protected void submitScan() {
-		IMappingExperimentBean mappingBean = super.getBean();
-		try {
-			Map<String, Object> config = (mappingBean.getBeamlineConfiguration()==null)?new HashMap<String, Object>() : mappingBean.getBeamlineConfiguration();
-			if(!config.keySet().contains("t1z")) {
-				IScannable<?> scannable = getView().getScannableDeviceService().getScannable("t1z");
-				// Set the t1z position to the current position in the mapping bean
-				// if it has not already been defined by the user
-				config.put("t1z", scannable.getPosition());
-				mappingBean.setBeamlineConfiguration(config);
-			}
+	protected void onShow() {
+		var mappingStage = getMappingStageInfo();
 
-		} catch (ScanningException e) {
-			log.warn("Unable to set the t1z position to the current position - the t1z position at scan start will be used instead");
-		}
-		super.submitScan();
+		// save the names of the axes currently being used
+		axes = new String[] {mappingStage.getPlotXAxisName(), mappingStage.getPlotYAxisName()};
+		logger.debug("Saving current axes names : {}", Arrays.asList(axes));
+
+		// set the ones to be used for the scan
+		mappingStage.setPlotXAxisName(fastAxisName);
+		mappingStage.setPlotYAxisName(slowAxisName);
+
+		// update the 'region and path' section to show the selected axes
+		updateRegionAndPathSection();
 	}
 
+	@Override
+	protected void onHide() {
+		if (axes.length == 2) {
+			// restore the original axes names in the 'region and path' section
+			logger.debug("Restoring axes names : {}", Arrays.asList(axes));
+			var mappingStage = getMappingStageInfo();
+			mappingStage.setPlotXAxisName(axes[0]);
+			mappingStage.setPlotYAxisName(axes[1]);
+			updateRegionAndPathSection();
+		}
+	}
+
+	private MappingStageInfo getMappingStageInfo() {
+		return getService(MappingStageInfo.class);
+	}
+
+	private void updateRegionAndPathSection() {
+		final MappingExperimentView mappingView = getView();
+		final RegionAndPathSection regionAndPathSection = mappingView.getSection(RegionAndPathSection.class);
+		regionAndPathSection.updateControls();
+	}
+
+	public void setSlowAxisName(String slowAxisName) {
+		this.slowAxisName = slowAxisName;
+	}
+
+	public void setFastAxisName(String fastAxisName) {
+		this.fastAxisName = fastAxisName;
+	}
 }
