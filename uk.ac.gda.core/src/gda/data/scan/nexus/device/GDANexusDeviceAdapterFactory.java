33ea49d290cb5df6a195709808d8fc44b8791e4b
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2024-11-29 11:02:00 +0000

    DAQ-5237 - Update INexusDeviceService factory methods to allow detectors to work per scan monitor if supported
    If a detector is added as monitor per scan, it crashes with
    NexusScanDataWriter because the nexus device wrapper are not designed to
    work per scan. However, with NexusDataWriter it works fine because it
    treats it as a scannable and does getPosition() on it.
    
    This fix mirrors NexusDataWriter as it treats all per scan monitor as a
    scannable and therefore wraps it with the DefaultScannableNexusDevice
    which uses getPosition() to save data, the same as NexusDataWriter.
    
    Change-Id: I9557de7d804064fe2d222f8d9a2bc28bdb17aafa

--- uk.ac.gda.core/src/gda/data/scan/nexus/device/GDANexusDeviceAdapterFactory.java
+++ uk.ac.gda.core/src/gda/data/scan/nexus/device/GDANexusDeviceAdapterFactory.java
@@ -25,6 +25,7 @@ import org.eclipse.dawnsci.nexus.INexusDevice;
 import org.eclipse.dawnsci.nexus.NXdetector;
 import org.eclipse.dawnsci.nexus.NXobject;
 import org.eclipse.dawnsci.nexus.NexusException;
+import org.eclipse.dawnsci.nexus.NexusScanInfo.ScanRole;
 import org.eclipse.dawnsci.nexus.device.INexusDeviceAdapterFactory;
 
 import gda.configuration.properties.LocalProperties;
@@ -36,30 +37,36 @@ import uk.ac.diamond.osgi.services.ServiceProvider;
 
 public class GDANexusDeviceAdapterFactory implements INexusDeviceAdapterFactory<Scannable> {
 
-	private static class NexusDetectorAdapterFactory implements INexusDeviceAdapterFactory<Detector> {
+	private static void checkIfDeviceCanAdopt(INexusDeviceAdapterFactory<?> adaopter, Scannable scannable, ScanRole scanRole) {
+		if (!adaopter.canAdapt(scannable, scanRole)) {
+			throw new IllegalArgumentException("Cannot createNexusDevice for " + adaopter.getClass().getSimpleName() + " using scannable: " + scannable.getName() + " with scan role " + scanRole);
+		}
+	}
+
+	private static class NexusDetectorAdapter implements INexusDeviceAdapterFactory<Detector> {
 
 		@Override
-		public boolean canAdapt(Object device) {
-			return device instanceof NexusDetector;
+		public boolean canAdapt(Object device, ScanRole scanRole) {
+			return device instanceof NexusDetector && scanRole != ScanRole.MONITOR_PER_SCAN;
 		}
 
 		@SuppressWarnings("unchecked")
 		@Override
-		public INexusDevice<NXdetector> createNexusDevice(Detector detector) throws NexusException {
+		public INexusDevice<NXdetector> createNexusDevice(Detector detector, ScanRole scanRole) throws NexusException {
+			checkIfDeviceCanAdopt(this, detector, scanRole);
 			if (LocalProperties.check(PROPERTY_NAME_USE_LEGACY_NEXUS_DETECTOR_ADAPTER)) {
 				return new LegacyNexusDetectorNexusDevice((NexusDetector) detector);
 			}
 			return new NexusDetectorNexusDevice((NexusDetector) detector);
 		}
-
 	}
 
-	private static class DetectorCreatesOwnFilesAdapterFactory implements INexusDeviceAdapterFactory<Detector> {
+	private static class DetectorCreatesOwnFilesAdapter implements INexusDeviceAdapterFactory<Detector> {
 
 		@Override
-		public boolean canAdapt(Object device) {
+		public boolean canAdapt(Object device, ScanRole scanRole) {
 			try {
-				return device instanceof Detector && ((Detector) device).createsOwnFiles();
+				return device instanceof Detector detector && detector.createsOwnFiles() && scanRole != ScanRole.MONITOR_PER_SCAN;
 			} catch (DeviceException e) {
 				throw new RuntimeException("Error calling createsOwnFiles on detector: " + ((Scannable) device).getName());
 			}
@@ -67,105 +74,105 @@ public class GDANexusDeviceAdapterFactory implements INexusDeviceAdapterFactory<
 
 		@SuppressWarnings("unchecked")
 		@Override
-		public INexusDevice<NXdetector> createNexusDevice(Detector detector) throws NexusException {
+		public INexusDevice<NXdetector> createNexusDevice(Detector detector, ScanRole scanRole) throws NexusException {
+			checkIfDeviceCanAdopt(this, detector, scanRole);
 			return new FileCreatorDetectorNexusDevice(detector);
 		}
-
 	}
 
-	private static class CounterTimerAdapterFactory implements INexusDeviceAdapterFactory<Detector> {
+	private static class CounterTimerAdapter implements INexusDeviceAdapterFactory<Detector> {
 
 		@Override
-		public boolean canAdapt(Object device) {
-			return device instanceof Detector &&
-					((Detector) device).getExtraNames() != null &&
-					((Detector) device).getExtraNames().length > 0;
+		public boolean canAdapt(Object device, ScanRole scanRole) {
+			return device instanceof Detector detector &&
+				detector.getExtraNames() != null &&
+				detector.getExtraNames().length > 0 &&
+				scanRole != ScanRole.MONITOR_PER_SCAN;
 		}
 
 		@SuppressWarnings("unchecked")
 		@Override
-		public INexusDevice<NXdetector> createNexusDevice(Detector detector) throws NexusException {
+		public INexusDevice<NXdetector> createNexusDevice(Detector detector, ScanRole scanRole) throws NexusException {
+			checkIfDeviceCanAdopt(this, detector, scanRole);
 			return new CounterTimerNexusDevice(detector);
 		}
-
 	}
 
-	private static class GenericDetectorAdapterFactory implements INexusDeviceAdapterFactory<Detector> {
+	private static class GenericDetectorAdapter implements INexusDeviceAdapterFactory<Detector> {
 
 		@Override
-		public boolean canAdapt(Object device) {
-			return device instanceof Detector;
+		public boolean canAdapt(Object device, ScanRole scanRole) {
+			return device instanceof Detector && scanRole != ScanRole.MONITOR_PER_SCAN;
 		}
 
 		@SuppressWarnings("unchecked")
 		@Override
-		public INexusDevice<NXdetector> createNexusDevice(Detector detector) throws NexusException {
+		public INexusDevice<NXdetector> createNexusDevice(Detector detector, ScanRole scanRole) throws NexusException {
+			checkIfDeviceCanAdopt(this, detector, scanRole);
 			return new GenericDetectorNexusDevice(detector);
 		}
-
 	}
 
-	private static class ScannableAdapterFactory implements INexusDeviceAdapterFactory<Scannable> {
+	private static class ScannableAdapter implements INexusDeviceAdapterFactory<Scannable> {
 
 		@Override
-		public boolean canAdapt(Object device) {
-			return device instanceof Scannable && !(device instanceof Detector);
+		public boolean canAdapt(Object device, ScanRole scanRole) {
+			return device instanceof Scannable;
 		}
 
 		@Override
-		public <N extends NXobject> INexusDevice<N> createNexusDevice(Scannable device) throws NexusException {
-			final ScannableNexusDeviceConfiguration config = getScannableNexusDeviceConfiguration(device.getName());
-			return config != null ? new ConfiguredScannableNexusDevice<>(device, config) :
-				new DefaultScannableNexusDevice<>(device);
+		public <N extends NXobject> INexusDevice<N> createNexusDevice(Scannable scannable, ScanRole scanRole) throws NexusException {
+			checkIfDeviceCanAdopt(this, scannable, scanRole);
+			final ScannableNexusDeviceConfiguration config = getScannableNexusDeviceConfiguration(scannable.getName());
+			return config != null ? new ConfiguredScannableNexusDevice<>(scannable, config) :
+				new DefaultScannableNexusDevice<>(scannable);
 		}
 
 		private ScannableNexusDeviceConfiguration getScannableNexusDeviceConfiguration(String deviceName) {
 			final ScannableNexusDeviceConfigurationRegistry registry = ServiceProvider.getService(ScannableNexusDeviceConfigurationRegistry.class);
 			return registry.getScannableNexusDeviceConfiguration(deviceName);
 		}
-
 	}
 
 	public static final String PROPERTY_NAME_USE_LEGACY_NEXUS_DETECTOR_ADAPTER = "gda.nexus.nexusScanDataWriter.useLegacyNexusDetectorAdapter";
 
-	private final List<INexusDeviceAdapterFactory<? extends Scannable>> adapterFactories;
+	private final List<INexusDeviceAdapterFactory<? extends Scannable>> adapters;
 
 	public GDANexusDeviceAdapterFactory() {
 		// private constructor to prevent external instantiation
 
-		// create the list of adapter factories.
+		// create the list of adapters.
 		// The way this is used is very similar to the Chain of Responsibility
 		// design pattern, except that we are using a list rather than each element referring to the next.
 		// Note: the order of the elements is very important, as each are tried in turn. This emulates the
-		// logic in NexusDataWrite.makeDetectorEntry
-		adapterFactories = new ArrayList<>();
-		adapterFactories.add(new NexusDetectorAdapterFactory());
-		adapterFactories.add(new DetectorCreatesOwnFilesAdapterFactory());
-		adapterFactories.add(new CounterTimerAdapterFactory());
-		adapterFactories.add(new GenericDetectorAdapterFactory());
-		adapterFactories.add(new ScannableAdapterFactory());
+		// logic in NexusDataWriter.makeDetectorEntry
+		adapters = new ArrayList<>();
+		adapters.add(new NexusDetectorAdapter());
+		adapters.add(new DetectorCreatesOwnFilesAdapter());
+		adapters.add(new CounterTimerAdapter());
+		adapters.add(new GenericDetectorAdapter());
+		adapters.add(new ScannableAdapter());
 	}
 
 	@Override
-	public boolean canAdapt(Object device) {
-		return device instanceof Scannable;
+	public boolean canAdapt(Object device, ScanRole scanRole) {
+		return device instanceof Scannable || (device instanceof Scannable && device instanceof INexusDevice<?>);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public <N extends NXobject> INexusDevice<N> createNexusDevice(Scannable device) throws NexusException {
+	public <N extends NXobject> INexusDevice<N> createNexusDevice(Scannable device, ScanRole scanRole) throws NexusException {
 		if (device instanceof INexusDevice<?>) {
 			return (INexusDevice<N>) device;
 		}
+		if (scanRole == null) throw new IllegalArgumentException("scanRole cannot be null!");
 
-		for (INexusDeviceAdapterFactory<? extends Scannable> adapterFactory : adapterFactories) {
-			if (adapterFactory.canAdapt(device)) {
-				return ((INexusDeviceAdapterFactory<Scannable>)adapterFactory).createNexusDevice(device);
+		for (INexusDeviceAdapterFactory<? extends Scannable> adapter : adapters) {
+			if (adapter.canAdapt(device, scanRole)) {
+				return ((INexusDeviceAdapterFactory<Scannable>)adapter).createNexusDevice(device, scanRole);
 			}
 		}
-
 		// this line cannot be reached, the configured adapters support all Scannables
 		throw new IllegalArgumentException("Cannot create an INexusDevice for scannable: " + device.getName());
 	}
-
 }
