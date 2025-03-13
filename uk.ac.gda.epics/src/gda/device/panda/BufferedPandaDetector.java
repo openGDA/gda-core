ca71d17b2589ee5f78f502462066ffb7fb6ab6c0
Iain Hall <iain.hall@diamond.ac.uk>
2025-03-13 14:16:24 +0000

    K14-51 : Improvements for Panda Detector
    - Added DataSocketDetector - to allow data to be read from TCP socket
    connection to the Panda, as an alternative to reading data from Hdf
    file. This can be added as additional detector to step and continuous
    scans.
    - Added BufferedDetector implementation of PandaDetector.
    
    Change-Id: I65e130312bcdc9796d0f25460f9b41856d82c966

---
+++ uk.ac.gda.epics/src/gda/device/panda/BufferedPandaDetector.java
@@ -0,0 +1,190 @@
+/*-
+ * Copyright © 2025 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.panda;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.eclipse.dawnsci.nexus.NexusException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gda.device.ContinuousParameters;
+import gda.device.DeviceException;
+import gda.device.detector.BufferedDetector;
+import gda.device.detector.DetectorBase;
+
+public class BufferedPandaDetector extends DetectorBase implements BufferedDetector {
+	private final Logger logger = LoggerFactory.getLogger(BufferedPandaDetector.class);
+	private PandaDetector pandaDetector;
+	private PandaController controller;
+	private ContinuousParameters parameters;
+	private int maximumReadFrames = 100;
+
+
+	public BufferedPandaDetector() {
+		setInputNames(new String[] {});
+	}
+
+	public PandaDetector getPandaDetector() {
+		return pandaDetector;
+	}
+
+	public void setPandaScalers(PandaDetector pandaDetector) {
+		this.pandaDetector = pandaDetector;
+		controller = pandaDetector.getController();
+	}
+
+
+	@Override
+	public void collectData() throws DeviceException {
+		controller.setSeqBitA("ONE");
+	}
+
+	@Override
+	public int getStatus() throws DeviceException {
+		return pandaDetector.getStatus();
+	}
+
+	@Override
+	public Object readout() throws DeviceException {
+		return pandaDetector.readout();
+	}
+
+	@Override
+	public boolean createsOwnFiles() throws DeviceException {
+		return false;
+	}
+
+	@Override
+	public void clearMemory() throws DeviceException {
+		// nothing to do here
+	}
+
+	@Override
+	public void setContinuousMode(boolean on) throws DeviceException {
+		logger.info("SetContinuousMode : {}", on);
+
+		if (on) {
+			// set collection time on pandaScalers, so at atScanStart uses correct time per point.
+			double timePerPoint = parameters.getTotalTime()/parameters.getNumberDataPoints();
+			pandaDetector.setCollectionTime(timePerPoint);
+
+			// prepare for the scan
+			pandaDetector.atScanStart();
+		}
+
+		String state = on ? "ONE":"ZERO";
+		controller.setSeqBitA(state);
+	}
+
+	@Override
+	public void atScanStart() throws DeviceException {
+		// pandaScalers.atScanStart(); is called in
+
+	}
+
+	@Override
+	public void atScanEnd() throws DeviceException {
+		pandaDetector.atScanEnd();
+	}
+
+	@Override
+	public void stop() throws DeviceException {
+		pandaDetector.stop();
+	}
+
+	@Override
+	public void atCommandFailure() throws DeviceException {
+		pandaDetector.atCommandFailure();
+	}
+
+	@Override
+	public String[] getExtraNames() {
+		return pandaDetector.getExtraNames();
+	}
+
+	@Override
+	public String[] getOutputFormat() {
+		return pandaDetector.getOutputFormat();
+	}
+
+	@Override
+	public boolean isContinuousMode() throws DeviceException {
+		return false;
+	}
+
+	@Override
+	public void setContinuousParameters(ContinuousParameters parameters) throws DeviceException {
+		this.parameters = parameters;
+	}
+
+	@Override
+	public ContinuousParameters getContinuousParameters() throws DeviceException {
+		return parameters;
+	}
+
+	@Override
+	public int getNumberFrames() throws DeviceException {
+		return controller.getHdfNumCaptured();
+	}
+
+	@Override
+	public Object[] readFrames(int startFrame, int finalFrame) throws DeviceException {
+		if (!pandaDetector.isReadSwmrFile()) {
+			return IntStream.range(startFrame, finalFrame+1).mapToObj(i -> i).toArray();
+		}
+
+		if (startFrame==0) {
+			pandaDetector.readout();
+		}
+		try {
+			var reader = pandaDetector.getPandaHdfReader();
+			reader.waitForFrames(finalFrame);
+			List<double[]> data = reader.readData(startFrame, finalFrame);
+			int nFrames = data.get(0).length;
+			Object[][] frameData = new Object[nFrames][data.size()];
+
+			for(int i=0; i<data.size(); i++) {
+				var itemData = data.get(i);
+				for(int frame=0; frame<itemData.length; frame++) {
+					frameData[frame][i] = itemData[frame];
+				}
+			}
+			return frameData;
+		}catch(NexusException ne) {
+			throw new DeviceException("Problem reading out frames "+startFrame+" to "+finalFrame);
+		}
+	}
+
+	@Override
+	public Object[] readAllFrames() throws DeviceException {
+		int totalFrames = controller.getHdfNumCaptured();
+		return readFrames(0, totalFrames);
+	}
+
+	@Override
+	public int maximumReadFrames() throws DeviceException {
+		return maximumReadFrames;
+	}
+
+	public void setMaximumReadFrames(int maximumReadFrames) {
+		this.maximumReadFrames = maximumReadFrames;
+	}
+}
---
+++ uk.ac.gda.epics/src/gda/device/panda/DataSocketDetector.java
@@ -0,0 +1,203 @@
+/*-
+ * Copyright © 2025 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.panda;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gda.device.ContinuousParameters;
+import gda.device.DeviceException;
+import gda.device.detector.BufferedDetector;
+import gda.device.detector.DetectorBase;
+
+public class DataSocketDetector extends DetectorBase implements BufferedDetector {
+	private static final Logger logger = LoggerFactory.getLogger(DataSocket.class);
+
+	private DataSocket dataSocket;
+	private String socketIpAddress = "bl20j-ts-panda-02";
+	private int socketPort = 8889;
+	private int frameCount;
+	private int maxNumRetries = 19;
+	private List<String> dataNames = Collections.emptyList();
+	private String dataFormatString = "%.6g";
+	private int maximumReadFrames = 50;
+
+	public DataSocketDetector() {
+		setInputNames(new String[] {});
+	}
+
+	@Override
+	public String[] getExtraNames() {
+		return dataNames.toArray(new String[] {});
+	}
+
+	@Override
+	public String[] getOutputFormat() {
+		return Collections.nCopies(dataNames.size(), dataFormatString).toArray(new String[] {});
+	}
+
+	private void connect() throws DeviceException {
+		if (dataSocket == null) {
+			try {
+				dataSocket = new DataSocket(socketIpAddress, socketPort);
+			} catch (IOException e) {
+				throw new DeviceException("Problem creating connection to data socket on "+socketIpAddress);
+			}
+		}
+	}
+
+	@Override
+	public void atScanStart() throws DeviceException {
+		logger.debug("Clearing socket data at scan start");
+		connect();
+		dataSocket.clearData();
+		frameCount = -1;
+	}
+
+	@Override
+	public void collectData() throws DeviceException {
+		frameCount++; // the index of the frame being collected (0 for first).
+	}
+
+	@Override
+	public int getStatus() throws DeviceException {
+		return 0;
+	}
+
+	@Override
+	public Object readout() throws DeviceException {
+		try {
+			logger.debug("Reading frame {}", frameCount);
+			dataSocket.updateValueData();
+			waitForNumFrames(frameCount);
+			return dataSocket.getFrame(frameCount, dataNames);
+		} catch (IOException | InterruptedException e) {
+			throw new DeviceException(e);
+		}
+	}
+
+	@Override
+	public boolean createsOwnFiles() throws DeviceException {
+		return false;
+	}
+
+	public String getDataFormatString() {
+		return dataFormatString;
+	}
+
+	public void setDataFormatString(String dataFormatString) {
+		this.dataFormatString = dataFormatString;
+	}
+
+	public List<String> getDataNames() {
+		return dataNames;
+	}
+
+	public void setDataNames(List<String> dataNames) {
+		this.dataNames = dataNames;
+	}
+
+	@Override
+	public void clearMemory() throws DeviceException {
+		// Nothing needed here  - atScanStart clears any cached values
+	}
+
+	@Override
+	public void setContinuousMode(boolean on) throws DeviceException {
+		// passive detector, nothing to prepare
+	}
+
+	@Override
+	public boolean isContinuousMode() throws DeviceException {
+		return false;
+	}
+
+	@Override
+	public void setContinuousParameters(ContinuousParameters parameters) throws DeviceException {
+		// passive detector, nothing to prepare
+	}
+
+	@Override
+	public ContinuousParameters getContinuousParameters() throws DeviceException {
+		return null;
+	}
+
+	@Override
+	public int getNumberFrames() throws DeviceException {
+		try {
+			dataSocket.updateValueData();
+		} catch (IOException e) {
+			throw new DeviceException("Problem getting number of frames from data socket", e);
+		}
+		return dataSocket.getNumFrames();
+	}
+
+	@Override
+	public Object[] readFrames(int startFrame, int finalFrame) throws DeviceException {
+		try {
+			logger.debug("Reading frames : {} ...  {}", startFrame, finalFrame);
+			dataSocket.updateValueData();
+			waitForNumFrames(finalFrame);
+			return dataSocket.getFrames(startFrame, finalFrame, dataNames).toArray();
+		} catch (IOException | InterruptedException e) {
+			throw new DeviceException(e);
+		}
+	}
+
+	@Override
+	public Object[] readAllFrames() throws DeviceException {
+		return readFrames(0, getNumberFrames());
+	}
+
+	/**
+	 * Wait until specified number of data frames are available to read from socket.
+	 * The number of available frames is checked every 0.5 sec; up to {@link #maxNumRetries}
+	 * attempts will be made to wait for the requested number of frames
+	 *
+	 * @param numFrames
+	 * @throws IOException
+	 * @throws InterruptedException if requested number of frames is not available within 5 seconds.
+	 */
+	private void waitForNumFrames(int numFrames) throws IOException, InterruptedException {
+		int numRetries = 0;
+		while (numFrames >= dataSocket.getNumFrames() && numRetries < maxNumRetries) {
+			Thread.sleep(500);
+			dataSocket.updateValueData();
+			numRetries++;
+		}
+		if (numRetries == maxNumRetries) {
+			throw new IOException("Timed out waiting for frame "+numFrames+" on data socket. Highest frame reached = "+dataSocket.getNumFrames());
+		}
+	}
+
+	@Override
+	public int maximumReadFrames() throws DeviceException {
+		return maximumReadFrames;
+	}
+
+	public void setMaximumReadFrames(int maximumReadFrames) {
+		this.maximumReadFrames = maximumReadFrames;
+	}
+
+
+}
